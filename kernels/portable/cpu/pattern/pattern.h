aba0d6fa0a2d7682edbf5b997ea1f6e9a7e3e72e
Manuel Candales <mcandales@meta.com>
2023-06-29 19:42:21 -0700

    Introduce op patterns
    Reviewed By: digantdesai
    
    Differential Revision: D47118016
    
    fbshipit-source-id: 01cd05e1cee03db2dc0c46e0c7fb5d8571db79f1

---
+++ kernels/portable/cpu/pattern/pattern.h
@@ -0,0 +1,60 @@
+/* Naming Convention
+--------------------
+Each pattern name should be all lowercase words separated by underscores.
+
+It should start with unary/binary/ternary depending on the number of input
+tensors.
+
+It should then specify the type of function that it is. E.g: ufunc, reduce, etc
+Ufunc is a concept used in NumPy and PyTorch (short for universal function)
+See NumPy's definition here: https://numpy.org/doc/stable/reference/ufuncs.html
+
+It should then specify the sets of dtypes accepted as inputs and output.
+We currently support the following sets of dtypes:
+ - bool   {Bool}
+ - int    {Byte, Char, Short, Int, Long}
+ - intb   {Byte, Char, Short, Int, Long, Bool}
+ - float  {Float, Double}
+ - floatb {Float, Double, Bool}
+ - real   {Byte, Char, Short, Int, Long, Float, Double}
+ - realb  {Byte, Char, Short, Int, Long, Float, Double, Bool}
+
+Input types are separated from output types by the "to" word.
+Input types are separated by underscores. Output types as well, in the cases
+when there are multiple outputs.
+In the case of ops that only allow the same dtype (for example abs/neg where
+the input and output dtype must be the same, only that dtype is specified.
+E.g, there is a difference between:
+ - unary_ufunc_real_to_real
+     (output and input types must be real, but might be different)
+ - unary_ufunc_real
+     (output must be same type than input, which must be real)
+
+A pattern named using this convention will be quite generic. If the pattern in
+question is a bit more specific, then add a descriptive sufix. */
+
+#pragma once
+
+#include <executorch/core/FunctionRef.h>
+
+namespace torch {
+namespace executor {
+namespace native {
+namespace internal {
+
+/**
+ * Implements an op pattern for ops that take a single input tensor of any
+ * realb dtye (real and boolean), no additional arguments, and outputs a
+ * boolean tensor of the same size. The function fn specifies the math
+ * operation which is applied to the input tensor element-wise.
+ */
+Tensor& unary_ufunc_realb_to_bool(
+    FunctionRef<bool(double)> fn,
+    RuntimeContext& ctx,
+    const Tensor& in,
+    Tensor& out);
+
+} // namespace internal
+} // namespace native
+} // namespace executor
+} // namespace torch
---
+++ kernels/portable/cpu/pattern/unary_ufunc_realb_to_bool.cpp
@@ -0,0 +1,45 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#include <executorch/core/FunctionRef.h>
+#include <executorch/kernels/kernel_includes.h>
+#include <executorch/kernels/portable/cpu/pattern/pattern.h>
+#include <executorch/kernels/portable/cpu/util/functional_util.h>
+
+namespace torch {
+namespace executor {
+namespace native {
+namespace internal {
+
+Tensor& unary_ufunc_realb_to_bool(
+    FunctionRef<bool(double)> fn,
+    RuntimeContext& ctx,
+    const Tensor& in,
+    Tensor& out) {
+  (void)ctx;
+
+  // Resize for dynamic shape
+  auto error = resize_tensor(out, in.sizes());
+  ET_CHECK_MSG(error == Error::Ok, "Failed to resize output tensor.");
+
+  ET_CHECK_MSG(
+      out.scalar_type() == exec_aten::ScalarType::Bool,
+      "Expected out tensor to have dtype Bool, but got %hhd instead.",
+      out.scalar_type());
+
+  const auto in_type = in.scalar_type();
+
+  ET_SWITCH_REAL_TYPES_AND(Bool, in_type, ctx, __func__, CTYPE_IN, [&] {
+    apply_unary_map_fn(
+        [fn](const CTYPE_IN val_in) { return fn(val_in); },
+        in.const_data_ptr<CTYPE_IN>(),
+        out.mutable_data_ptr<bool>(),
+        in.numel());
+  });
+
+  return out;
+}
+
+} // namespace internal
+} // namespace native
+} // namespace executor
+} // namespace torch
