9c24c6c943aa34de6ff411e689d63665426fac54
Huy Do <huydhn@gmail.com>
2023-07-14 13:04:02 -0700

    Keep only the minimum while getting calculate docker image ready on test-infra

--- .ci/docker/build.sh
+++ .ci/docker/build.sh
@@ -2,9 +2,6 @@
 
 set -ex
 
-IMAGE_NAME="$1"
-shift
-
 OS="ubuntu"
 OS_VERSION=22.04
 CLANG_VERSION=12
@@ -12,7 +9,6 @@ CLANG_VERSION=12
 docker build \
   --no-cache \
   --progress=plain \
-  --build-arg "IMAGE_NAME=${IMAGE_NAME}" \
   --build-arg "OS_VERSION=${OS_VERSION}" \
   --build-arg "CLANG_VERSION=${CLANG_VERSION}" \
   -f "${OS}"/Dockerfile \
