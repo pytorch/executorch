f9206537bce9096288db4da752977d80f4ea7932
Huy Do <huydhn@gmail.com>
2023-07-21 16:07:24 -0700

    Setup conda and pip

---
+++ .ci/docker/common/install_conda.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -ex
+
+source "$(dirname "${BASH_SOURCE[0]}")/utils.sh"
+
+install_miniconda() {
+  BASE_URL="https://repo.anaconda.com/miniconda"
+  CONDA_FILE="Miniconda3-py${PYTHON_VERSION//./}_${MINICONDA_VERSION}-Linux-x86_64.sh"
+
+  mkdir -p /opt/conda
+  chown ci-user:ci-user /opt/conda
+
+  pushd /tmp
+  wget -q "${BASE_URL}/${CONDA_FILE}"
+  # Install miniconda
+  as_ci_user bash "${CONDA_FILE}" -b -f -p "/opt/conda"
+  popd
+
+  sed -e 's|PATH="\(.*\)"|PATH="/opt/conda/bin:\1"|g' -i /etc/environment
+  export PATH="/opt/conda/bin:$PATH"
+}
+
+install_python() {
+  pushd /opt/conda
+  # Install the correct Python version
+  as_ci_user conda create -n py_$PYTHON_VERSION -y python="$PYTHON_VERSION"
+  popd
+}
+
+install_pip_dependencies() {
+  pushd /opt/conda
+  # Install all Python dependencies, including PyTorch
+  pip_install -r /opt/conda/requirements-ci.txt
+  pip_install --pre torch --index-url https://download.pytorch.org/whl/nightly/cpu
+  popd
+}
+
+install_miniconda
+install_python
+install_pip_dependencies
