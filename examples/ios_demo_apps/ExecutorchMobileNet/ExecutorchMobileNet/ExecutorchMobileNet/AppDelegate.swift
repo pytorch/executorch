215e48a9c47b28a77fe6e5e2e42a25985c815e5f
Chen Lai <chenlai@meta.com>
2023-08-11 10:21:29 -0700

    Add ios example app
    Summary: As title, add an example ios app so it's easier to test executorch library + ios app
    
    Reviewed By: kimishpatel
    
    Differential Revision: D48161442
    
    fbshipit-source-id: dcc47acfc112b70fcba4d12bc610732dd36b3db0

---
+++ examples/ios_demo_apps/ExecutorchMobileNet/ExecutorchMobileNet/ExecutorchMobileNet/AppDelegate.swift
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+//
+//  AppDelegate.swift
+//  ExecutorchMobileNet
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+
+  // MARK: UISceneSession Lifecycle
+
+  func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    // Called when a new scene session is being created.
+    // Use this method to select a configuration to create the new scene with.
+    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+  }
+
+  func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    // Called when the user discards a scene session.
+    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+  }
+}
