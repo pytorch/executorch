ba47322bf186afc5d07149e1836aa3e7c3d69ad1
Mengwei Liu <larryliu@meta.com>
2023-08-15 11:53:06 -0700

    Load dylib for custom ops if platform is MacOS
    Summary: As titled, a minor fix on custom ops example.
    
    Reviewed By: kimishpatel
    
    Differential Revision: D48359155
    
    fbshipit-source-id: ffcf49371e1c5c052bfc9fc317f4025b6ffd35b1

--- examples/custom_ops/custom_ops_2.py
+++ examples/custom_ops/custom_ops_2.py
@@ -4,12 +4,14 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-"""Example of showcasing registering custom operator through torch library API."""
+"""Example of showcasing registering custom operator through loading a shared
+library that calls PyTorch C++ op registration API.
+"""
+
 import torch
 
 from examples.export.export_example import export_to_pte
 
-torch.ops.load_library("cmake-out/examples/custom_ops/libcustom_ops_aot_lib.so")
 
 # example model
 class Model(torch.nn.Module):
@@ -20,6 +22,18 @@ class Model(torch.nn.Module):
 def main():
     m = Model()
     input = torch.randn(2, 3)
+    # load shared library
+    from sys import platform
+
+    if platform == "linux" or platform == "linux2":
+        extension = ".so"
+    elif platform == "darwin":
+        extension = ".dylib"
+    else:
+        raise RuntimeError(f"Unsupported platform {platform}")
+    torch.ops.load_library(
+        f"cmake-out/examples/custom_ops/libcustom_ops_aot_lib{extension}"
+    )
     # capture and lower
     export_to_pte("custom_ops_2", m, (input,))
 
