4b201fbc337ecbbd3d0c99f4863273f2fee604e4
Huy Do <huydhn@gmail.com>
2023-08-16 13:20:50 -0700

    Debug

--- .ci/scripts/setup-macos.sh
+++ .ci/scripts/setup-macos.sh
@@ -40,12 +40,6 @@ install_conda() {
   popd
 }
 
-use_conda_python() {
-  PYTHON_PATH=$(dirname $(conda run --no-capture-output which python))
-  # Use conda python
-  export PATH="${PYTHON_PATH}:${PATH}"
-}
-
 install_pip_dependencies() {
   pushd .ci/docker
   # Install all Python dependencies, including PyTorch
@@ -58,5 +52,4 @@ install_pip_dependencies() {
 
 install_buck
 install_conda
-use_conda_python
 install_pip_dependencies
