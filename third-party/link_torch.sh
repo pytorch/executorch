ad782912138869047f6d3515ca52ef2dcca812ee
Mengwei Liu <larryliu@meta.com>
2023-08-16 19:25:57 -0700

    Add custom ops test to CI job (#72)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/72
    
    Add it to pull.yml for both linux and macos
    
    Reviewed By: huydhn, JacobSzwejbka
    
    Differential Revision: D48408763
    
    fbshipit-source-id: 5558fa16c23c4c490a9f362d9c7d94bbcc8ebc9b

--- third-party/link_torch.sh
+++ third-party/link_torch.sh
@@ -11,14 +11,39 @@
 
 set -e
 
+# Named argument:
+# -o: output directory/file
+# -f: a list of files/directories that we want to link to the output directory, separated by comma ",".
+# These paths need to be in relative path format to the python library path.
+while getopts ":o:f:" opt; do
+  case $opt in
+    o) OUT="$OPTARG"
+    ;;
+    f) FILES="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    exit 1
+    ;;
+  esac
+
+  case $OPTARG in
+    -*) echo "Option $opt needs to be one of -o, -f."
+    exit 1
+    ;;
+  esac
+done
+
 LIB=$(python3 -c 'from distutils.sysconfig import get_python_lib; print(get_python_lib())')
 
-SUBPATH=$1
-OUT=$2
-if [[ -f "$LIB/$SUBPATH" ]] || [[ -d "$LIB/$SUBPATH" ]];
-then
-    ln -s "$LIB/$SUBPATH" "$OUT"
-else
-    echo "Error: $LIB/$SUBPATH doesn't exist"
-    exit 1
-fi
+# delimiter ,
+export IFS=","
+
+for SUBPATH in $FILES; do
+  if [[ -f "$LIB/$SUBPATH" ]] || [[ -d "$LIB/$SUBPATH" ]];
+  then
+      ln -s "$LIB/$SUBPATH" "$OUT"
+  else
+      echo "Error: $LIB/$SUBPATH doesn't exist"
+      exit 1
+  fi
+done
