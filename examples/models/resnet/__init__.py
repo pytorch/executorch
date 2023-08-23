1f903c464a2136ede6c2b767526d5a089bcb9594
Hansong Zhang <hsz@meta.com>
2023-08-23 14:02:30 -0700

    Enable ResNet-18 (#107)
    Summary: Pull Request resolved: https://github.com/pytorch/executorch/pull/107
    
    Reviewed By: guangy10
    
    Differential Revision: D48591055
    
    fbshipit-source-id: f815b8b31d19f2b26714d4e0484c0ade7eb61dac

--- examples/models/resnet/__init__.py
+++ examples/models/resnet/__init__.py
@@ -4,8 +4,9 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .export import ResNet50Model
+from .export import ResNet18Model, ResNet50Model
 
 __all__ = [
+    ResNet18Model,
     ResNet50Model,
 ]
