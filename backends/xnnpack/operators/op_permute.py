ee5203ba96694524d31ba80db191259102e9cfad
Max Ren <maxren@meta.com>
2023-08-23 16:21:57 -0700

    fix permute copy name (#80)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/80
    
    We have a node_visitor for permute already, it was mistakenly named static_transpose after its xnnpack counterpart. permute_copy should not be skipped. It should only be skipped when applied to linear weights.
    
    Instead our linear conversion pass will convert away any permute_copies which are not actually to be executed.
    
    Reviewed By: kirklandsign
    
    Differential Revision: D48488930
    
    fbshipit-source-id: 19abfd849b5cc1d38b956947af7137463185aaf1

--- backends/xnnpack/operators/op_permute.py
+++ backends/xnnpack/operators/op_permute.py
@@ -20,7 +20,7 @@ from executorch.backends.xnnpack.utils.utils import get_input_node
 
 
 @register_node_visitor
-class StaticTransposeVisitor(NodeVisitor):
+class PermuteVisitor(NodeVisitor):
     target = "aten.permute_copy.default"
 
     def __init__(self, *args) -> None:
