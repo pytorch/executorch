4072a5ea6eef968f2f290ec840466a3ba3cf8c49
Hansong Zhang <hsz@fb.com>
2023-09-01 10:11:26 -0700

    Add MobileBERT (#181)
    Summary:
    
    Using a pretrained [MobileBert Model](https://huggingface.co/docs/transformers/model_doc/mobilebert#transformers.MobileBertModel) outputting raw hidden-states without any specific head on top
    
    Validating the export and runtime flow only.
    
    Op coverage is good.  All in core IR.
    
    Differential Revision: D48155032

---
+++ examples/models/mobilebert/model.py
@@ -0,0 +1,34 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import logging
+
+import torch
+
+from transformers import AutoTokenizer, MobileBertModel  # @manual
+
+from ..model_base import EagerModelBase
+
+FORMAT = "[%(filename)s:%(lineno)s] %(message)s"
+logging.basicConfig(format=FORMAT)
+
+
+class MobileBertModelExample(EagerModelBase):
+    def __init__(self):
+        pass
+
+    def get_eager_model(self) -> torch.nn.Module:
+        logging.info("loading mobilebert model")
+        # pyre-ignore
+        model = MobileBertModel.from_pretrained(
+            "google/mobilebert-uncased", return_dict=False
+        )
+        logging.info("loaded mobilebert model")
+        return model
+
+    def get_example_inputs(self):
+        tokenizer = AutoTokenizer.from_pretrained("google/mobilebert-uncased")
+        return (tokenizer("Hello, my dog is cute", return_tensors="pt")["input_ids"],)
