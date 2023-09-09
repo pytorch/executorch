1c595fa3a80d487a8ebb0024c7e2de73bc769d5d
Mengwei Liu <larryliu@fb.com>
2023-09-08 20:56:03 -0700

    Add util to find functional and/or out variant of an operator
    Summary:
    This is a useful API to find functional or out variant from a given operator.
    
    It looks for all the overload of an operator name and see if the target variant exists in all the overloads.
    
    Differential Revision: https://internalfb.com/D49070398
    
    fbshipit-source-id: 084fafdfd347b5e06ddf85d21ed6c0ca491cf208

--- exir/dialects/edge/op/api.py
+++ exir/dialects/edge/op/api.py
@@ -7,10 +7,15 @@
 """
 APIs to help lowering edge dialect ops to other dialects.
 """
-from typing import Optional
+import dataclasses
+import logging
+from typing import List, Optional
 
 import torch
-from torch._ops import OpOverloadPacket
+
+from executorch.exir.operator.convert import _pybind_schema_to_native_schema
+from torch._ops import OpOverload, OpOverloadPacket
+from torchgen.model import FunctionSchema, SchemaKind
 
 
 def get_torch_op_overload(
@@ -26,3 +31,56 @@ def get_torch_op_overload(
 def get_callable(name):
     main, suffix = name.split(".")
     return get_torch_op_overload("aten", main, suffix)
+
+
+def to_variant(op: OpOverload, variant: SchemaKind) -> OpOverload:
+    """Given an operator overload, return its corresponding variant. Currently
+    only supports functional variant and out variant.
+    Argument:
+        op (OpOverload): operator overload instance.
+        variant (SchemaKind): the variant we are looking for.
+    Returns:
+        OpOverload: The matched variant operator.
+    Example:
+        torch.ops.aten.add.Tensor, SchemaKind.out -> torch.ops.aten.add.out
+        torch.ops.aten.add.out, SchemaKind.functional -> torch.ops.aten.add.Tensor
+    """
+    assert (
+        variant == SchemaKind.functional or variant == SchemaKind.out
+    ), f"Only support out variant and functional variant, got {variant}"
+    # first check if the current operator is the target variant
+    native_schema: Optional[FunctionSchema] = _pybind_schema_to_native_schema(
+        op._schema
+    )
+    assert (
+        native_schema is not None
+    ), f"Schema: {op._schema} cannot be converted to torch.FunctionSchema"
+
+    # get all overloads
+    torch_packet = getattr(
+        getattr(torch.ops, op.namespace), op._schema.name.split("::")[1]
+    )
+    schemas: List[torch._C.FunctionSchema] = [
+        getattr(torch_packet, o)._schema
+        for o in torch._C._jit_get_operation(op._schema.name)[1]
+    ]
+    # compare the signature of out variant overload with the signature of the original overload
+    signature = dataclasses.replace(native_schema.signature(), returns=())
+    for schema in schemas:
+        native_s: Optional[FunctionSchema] = _pybind_schema_to_native_schema(schema)
+        if native_s is None:
+            logging.warning(
+                f"Schema: {schema} cannot be converted to torch.FunctionSchema"
+            )
+            continue
+        if (
+            native_s.kind() == variant
+            and dataclasses.replace(native_s.signature(), returns=()) == signature
+        ):
+            op_variant = get_torch_op_overload(
+                op.namespace, schema.name.split("::")[1], schema.overload_name
+            )
+            return op_variant
+    raise RuntimeError(
+        f"{variant} variant of operator {op.name()} can't be found. We've found the schemas of all the overloads: {[str(s) for s in schemas]}"
+    )
