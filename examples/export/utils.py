71470a750870f8effff2f796f20026728a2f7151
Kimish Patel <kimishpatel@meta.com>
2023-09-11 20:57:31 -0700

    Move examples export to two stage export (#238)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/238
    
    Following the alignment, this diff moves examples export to two stage export
    api.
    
    Will follow up with changes to quantization examples.
    
    However, before landing, must land the torch nightly update to executorch
    
    Reviewed By: guangy10
    
    Differential Revision: D49025972
    
    fbshipit-source-id: 0b2c71dd7d562a714ff723b87b660d564a96cc37

--- examples/export/utils.py
+++ examples/export/utils.py
@@ -6,7 +6,14 @@
 
 import logging
 
+from typing import Tuple
+
 import executorch.exir as exir
+
+import torch
+import torch._export as export
+from executorch.exir.program import ExirExportedProgram
+from executorch.exir.tracer import Value
 
 
 _CAPTURE_CONFIG = exir.CaptureConfig(enable_aot=True)
@@ -17,16 +24,38 @@ _EDGE_COMPILE_CONFIG = exir.EdgeCompileConfig(
 )
 
 
-def export_to_edge(
-    model,
-    example_inputs,
+def _to_core_aten(
+    model: torch.fx.GraphModule,
+    example_inputs: Tuple[Value, ...],
     capture_config=_CAPTURE_CONFIG,
+) -> ExirExportedProgram:
+    # post autograd export. eventually this will become .to_core_aten
+    if not isinstance(model, torch.fx.GraphModule):
+        raise ValueError(
+            f"Expected passed in model to be an instance of fx.GraphModule, got {type(model)}"
+        )
+    core_aten_exir_ep = exir.capture(model, example_inputs, capture_config)
+    logging.info(f"Core ATen graph:\n{core_aten_exir_ep.exported_program.graph}")
+    return core_aten_exir_ep
+
+
+def _core_aten_to_edge(
+    core_aten_exir_ep: ExirExportedProgram,
     edge_compile_config=_EDGE_COMPILE_CONFIG,
-):
-    m = model.eval()
-    edge = exir.capture(m, example_inputs, capture_config).to_edge(edge_compile_config)
+) -> ExirExportedProgram:
+    edge = core_aten_exir_ep.to_edge(edge_compile_config)
     logging.info(f"Exported graph:\n{edge.exported_program.graph}")
     return edge
+
+
+def export_to_edge(
+    model: torch.fx.GraphModule,
+    example_inputs: Tuple[Value, ...],
+    capture_config=_CAPTURE_CONFIG,
+    edge_compile_config=_EDGE_COMPILE_CONFIG,
+) -> ExirExportedProgram:
+    core_aten_exir_ep = _to_core_aten(model, example_inputs, capture_config)
+    return _core_aten_to_edge(core_aten_exir_ep, edge_compile_config)
 
 
 def export_to_exec_prog(
@@ -36,7 +65,14 @@ def export_to_exec_prog(
     edge_compile_config=_EDGE_COMPILE_CONFIG,
     backend_config=None,
 ):
-    edge_m = export_to_edge(model, example_inputs, capture_config, edge_compile_config)
+    m = model.eval()
+    # pre-autograd export. eventually this will become torch.export
+    m = export.capture_pre_autograd_graph(m, example_inputs)
+
+    core_aten_exir_ep = _to_core_aten(m, example_inputs)
+
+    edge_m = _core_aten_to_edge(core_aten_exir_ep, edge_compile_config)
+
     exec_prog = edge_m.to_executorch(backend_config)
     return exec_prog
 
