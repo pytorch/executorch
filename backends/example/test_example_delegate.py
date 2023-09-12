86d6743050c51141a514f1d49849fdd4c020f779
Chen Lai <chenlai@meta.com>
2023-09-12 15:52:27 -0700

    fix the test name
    Summary: remove the keyword
    
    Reviewed By: tarun292
    
    Differential Revision: D49210422
    
    fbshipit-source-id: 5a48dafc3c988cc787f4e0cfe1432046969dd882

--- backends/example/test_example_delegate.py
+++ backends/example/test_example_delegate.py
@@ -25,7 +25,7 @@ from torch.ao.quantization.quantize_pt2e import convert_pt2e, prepare_pt2e
 from torchvision.models.quantization import mobilenet_v2
 
 
-class TestQualCommDelegate(unittest.TestCase):
+class TestExampleDelegate(unittest.TestCase):
     def test_delegate_linear(self):
         class Conv2dModule(torch.nn.Module):
             def __init__(self):
