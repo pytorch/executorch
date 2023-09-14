1bf2ff4efe2af773cc4ea2cfd7061e9fe48314a0
Tarun Karuturi <tkaruturi@meta.com>
2023-09-13 23:42:12 -0700

    Add macros and helper classes in event_tracer needed for profiling (#294)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/294
    
    All the internal invocations (inside ExecuTorch codebase) of event_tracer will be controlled through the macros added in this diff. The benefit of this is that we can leave these hooks in the hot path and when the flag is disabled they essentially compile to a no-op in prod releases.
    
    It's also ensured that in all these macros if we pass in a `nullptr` as event_tracer (default case if user doesn't pass in one when method is created) we just return immediately.
    
    The specific macros added here are only meant to be used inside the ExecuTorch codebase as the header explains.
    
    Reviewed By: dbort
    
    Differential Revision: D49204729
    
    fbshipit-source-id: 9bd2945ad94a5064aab6076210346b14ed9bcf11

---
+++ runtime/core/test/event_tracer_test.cpp
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <gtest/gtest.h>
+
+#include <executorch/runtime/core/event_tracer.h>
+// Enable flag for test
+#define ET_EVENT_TRACER_ENABLED
+#include <executorch/runtime/core/event_tracer_hooks.h>
+
+namespace torch {
+namespace executor {
+
+using namespace internal;
+
+class DummyEventTracer : public EventTracer {
+ public:
+  DummyEventTracer() {}
+
+  ~DummyEventTracer() override {}
+
+  void create_event_block(const char* name) override {
+    (void)name;
+    return;
+  }
+
+  EventTracerEntry start_profiling(
+      const char* name,
+      ChainID chain_id = kUnsetChainId,
+      DebugHandle debug_handle = kUnsetDebugHandle) override {
+    (void)name;
+    (void)chain_id;
+    (void)debug_handle;
+    return EventTracerEntry();
+  }
+
+  void end_profiling(EventTracerEntry prof_entry) override {
+    (void)prof_entry;
+    return;
+  }
+
+  void track_allocation(AllocatorID id, size_t size) override {
+    (void)id;
+    (void)size;
+    return;
+  }
+
+  AllocatorID track_allocator(const char* name) override {
+    (void)name;
+    return 0;
+  }
+};
+
+void RunSimpleTracerTest(EventTracer* event_tracer) {
+  event_tracer_create_event_block(event_tracer, "ExampleEvent");
+  event_tracer_create_event_block(event_tracer, "ExampleEvent");
+  EventTracerEntry event_entry =
+      event_tracer_begin_profiling_event(event_tracer, "ExampleEvent");
+  event_tracer_end_profiling_event(event_tracer, event_entry);
+  {
+    EventTracerProfileScope event_tracer_profile_scope(
+        event_tracer, "ExampleScope");
+  }
+  {
+    EventTracerProfileInstructionScope event_tracer_profile_instruction_scope(
+        event_tracer, 0, 1);
+  }
+  AllocatorID allocator_id =
+      event_tracer_track_allocator(event_tracer, "AllocatorName");
+  event_tracer_track_allocation(event_tracer, allocator_id, 64);
+}
+
+TEST(TestEventTracer, SimpleEventTracerTest) {
+  // Call all the EventTracer macro's with a valid pointer to an event tracer
+  // and also with a null pointer (to test that the null case works).
+  DummyEventTracer dummy;
+  std::vector<DummyEventTracer*> dummy_event_tracer_arr = {&dummy, nullptr};
+  for (size_t i = 0; i < dummy_event_tracer_arr.size(); i++) {
+    RunSimpleTracerTest(&dummy);
+    RunSimpleTracerTest(nullptr);
+  }
+}
+
+} // namespace executor
+} // namespace torch
+// TODO : (T163645377) Add more test coverage to log and verify events passed
+// into DummyTracer.
