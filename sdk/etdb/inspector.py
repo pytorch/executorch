8209cb3e3cc145ff302fdd2869a3c78a2492b768
Olivia Liu <olivialpx@meta.com>
2023-09-14 19:36:13 -0700

    Update Inspector interface based on new design and define underlying data classes (Event, EventBlock, PerfData) (#343)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/343
    
    Design doc: https://docs.google.com/document/d/1-0z9yq2g2B8JFTpVUQ_7L8JqOe8n5esk8gfaFd1bg4E/edit?usp=sharing.
    
    The .txts are regenerated fixtures because of the et_schema change
    
    Reviewed By: Jack-Khuu
    
    Differential Revision: D49099840
    
    fbshipit-source-id: 9c61af685be468c268baa2b023fbd992af113e41

--- sdk/etdb/inspector.py
+++ sdk/etdb/inspector.py
@@ -4,102 +4,190 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-import os
-from typing import Mapping, Optional
+import dataclasses
+from dataclasses import dataclass
+from typing import Dict, List, Mapping, Optional, Union
 
-from executorch.sdk.edir.et_schema import InferenceRun, OperatorGraphWithStats
-from pandas import DataFrame
+import numpy as np
+import pandas as pd
+import torch
+from executorch.exir import ExportedProgram
+
+from executorch.sdk.edir.et_schema import OperatorGraphWithStats
+from executorch.sdk.etdb._inspector_utils import gen_graphs_from_etrecord
+from executorch.sdk.etrecord import parse_etrecord
+from tabulate import tabulate
+
+
+@dataclass
+class PerfData:
+    def __init__(self, raw: List[float]):
+        self.raw: List[float] = raw
+
+    @property
+    def p50(self) -> float:
+        return np.percentile(self.raw, 50)
+
+    @property
+    def p90(self) -> float:
+        return np.percentile(self.raw, 90)
+
+    @property
+    def avg(self) -> float:
+        return np.mean(self.raw)
+
+    @property
+    def min(self) -> float:
+        return min(self.raw)
+
+    @property
+    def max(self) -> float:
+        return max(self.raw)
+
+    @property
+    def median(self) -> float:
+        return np.median(self.raw)
+
+
+# TODO: detailed documentation
+@dataclass
+class Event:
+    """
+    Corresponds to an op instance
+    """
+
+    name: str
+    perf_data: PerfData
+    op_type: List[str] = dataclasses.field(default_factory=list)
+
+    # Instruction Id of the original profiling event
+    instruction_id: Optional[int] = None
+
+    # Supplemental Identifier used in combination with instruction_identifier
+    delegate_debug_identifier: Optional[Union[int, str]] = None
+
+    # Debug Handles in the model graph to which this event is correlated
+    debug_handles: Optional[Union[int, List[int]]] = None
+
+    stack_trace: Dict[str, str] = dataclasses.field(default_factory=dict)
+    module_hierarchy: Dict[str, Dict] = dataclasses.field(default_factory=dict)
+    is_delegated_op: Optional[bool] = None
+    delegate_backend_name: Optional[str] = None
+    debug_data: List[torch.Tensor] = dataclasses.field(default_factory=list)
+
+
+@dataclass
+class EventBlock:
+    """
+    EventBlock contains a collection of events associated with a particular profiling/debugging block retrieved from the runtime.
+    Attributes:
+        name (str): Name of the profiling/debugging block
+        events (List[Event]): List of events associated with the profiling/debugging block
+    """
+
+    name: str
+    events: List[Event] = dataclasses.field(default_factory=list)
+
+    def to_dataframe(self) -> pd.DataFrame:
+        """
+        Converts the EventBlock into a DataFrame with each row being an event instance
+        """
+        # TODO: push row generation down to Event
+        data = {
+            "event_block_name": [self.name] * len(self.events),
+            "event_name": [event.name for event in self.events],
+            "raw": [event.perf_data.raw for event in self.events],
+            "p50": [event.perf_data.p50 for event in self.events],
+            "p90": [event.perf_data.p90 for event in self.events],
+            "avg": [event.perf_data.avg for event in self.events],
+            "min": [event.perf_data.min for event in self.events],
+            "max": [event.perf_data.max for event in self.events],
+            "median": [event.perf_data.median for event in self.events],
+            "op_type": [event.op_type for event in self.events],
+            "delegate_debug_identifier": [
+                event.delegate_debug_identifier for event in self.events
+            ],
+            "stack_traces": [event.stack_trace for event in self.events],
+            "module_hierarchy": [event.module_hierarchy for event in self.events],
+            "is_delegated_op": [event.is_delegated_op for event in self.events],
+            "delegate_backend_name": [
+                event.delegate_backend_name for event in self.events
+            ],
+            "debug_data": [event.debug_data for event in self.events],
+        }
+        df = pd.DataFrame(data)
+        return df
 
 
 class Inspector:
-
     """
     APIs for examining model architecture and performance stats
     """
 
     def __init__(
-        self,
-        op_graph_dict: Mapping[str, OperatorGraphWithStats],
-        show_stack_trace: Optional[bool] = False,
-        verbose: Optional[bool] = False,
+        self, etdump_path: Optional[str] = None, etrecord_path: Optional[str] = None
     ) -> None:
         """
-        Constructor that returns a Debugger instance from a dict of OperatorGraph instances and some optional parameters
+        Create an inspector instance from the provided ETDump/ETRecord
         """
-        # Save the parameters into class members
-        self.op_graph_dict = op_graph_dict
-        self.show_stack_trace = show_stack_trace
-        self.verbose = verbose
 
-        # Construct the initial tables and save in class members
-        self.architecture_table = self._gen_architecture_table()
-        self.high_level_profile_info_table = self._gen_high_level_profile_info_table()
+        # Gen op graphs from etrecord
+        if etrecord_path is not None:
+            self._etrecord = parse_etrecord(etrecord_path=etrecord_path)
+            self._op_graph_dict: Mapping[
+                str, OperatorGraphWithStats
+            ] = gen_graphs_from_etrecord(etrecord=self._etrecord)
 
-    def attach_etdump(self, etdump_path: str) -> None:
+        self.event_blocks: List[EventBlock] = []
+        # TODO: create event blocks from etdump, and associate events with op graph nodes
+
+    def print_data_tabular(self) -> None:
         """
-        API that attaches ETDump to this inspector instance
+        Prints the underlying EventBlocks (essentially all the performance data)
         """
-        op_graph = self.op_graph_dict["et_dialect_graph_module/forward"]
 
-        if os.path.exists(etdump_path):
-            op_graph.attach_metadata(
-                inference_run=InferenceRun.extract_runs_from_path(
-                    file_path=etdump_path
-                )[0]
-            )
+        def style_text_size(val, size=12):
+            return f"font-size: {size}px"
+
+        df_list = [event_block.to_dataframe() for event_block in self.event_blocks]
+        combined_df = pd.concat(df_list, ignore_index=True)
+        # TODO: filter out raw, delegate_debug_identifier, stack_traces and module_hierarchy
+        try:
+            from IPython.display import display
+
+            styled_df = combined_df.style.applymap(style_text_size)
+            display(styled_df)
+        except:
+            print(tabulate(combined_df, headers="keys", tablefmt="fancy_grid"))
+
+    def get_event_blocks(self) -> List[EventBlock]:
+        """
+        Returns EventBlocks containing contents from ETDump (correlated to ETRecord if provided)
+        """
+        return self.event_blocks
+
+    def get_op_list(
+        self, event_block: str, show_delegated_ops: Optional[bool] = True
+    ) -> Dict[str, List[Event]]:
+        """
+        Return a map of op_types to Events of that op_type
+        """
+        # TODO: implement
+        return {}
+
+    def write_tensorboard_artifact(self, path: str) -> None:
+        """
+        Write to the provided path, the artifacts required for visualization in TensorBoard
+        """
+        # TODO: implement
+        pass
+
+    # TODO: add a unittest for this function
+    def get_exported_program(self, graph: Optional[str]) -> ExportedProgram:
+        """
+        Access helper for ETRecord, defaults to returning Edge Dialect Program
+        """
+        if not graph:
+            return self._etrecord["edge_dialect_output/forward"]
         else:
-            raise Exception("Invalid ET Dump path")
-
-    def get_high_level_profile_info_table(self) -> DataFrame:
-        """
-        API that returns the high level profile information table from class member
-        """
-        return self.high_level_profile_info_table
-
-    def get_architecture_table(self) -> DataFrame:
-        """
-        API that returns the architecture table from class member, filtered by user options (e.g. self.show_stack_trace)
-        """
-        # TODO: filter based on user options (self.show_stack_trace) before return
-        return self.architecture_table
-
-    def cli_flow(self):
-        """
-        API that enters the CLI debugging flow
-        """
-        print("Entering the CLI debugging flow...")
-
-        print("High level profile information table:")
-        print(self.get_high_level_profile_info_table())
-        print("Architecture table:")
-        print(self.get_architecture_table())
-
-        # TODO: Take user commands, process by calling other APIs in the Inspector class
-
-    def select_by_instance_id(self, instance_id: str) -> DataFrame:
-        """
-        API that returns a DataFrame containing information for a specific instance id
-        """
-        # TODO: filter the architecture table by the instance id before return
-        return self.architecture_table
-
-    def select_by_instance_type(self, instance_type: str) -> DataFrame:
-        """
-        API that returns a DataFrame containing instances with the given instance type
-        """
-        # TODO: filter the architecture table by the instance type before return
-        return self.architecture_table
-
-    def _gen_high_level_profile_info_table(self) -> DataFrame:
-        """
-        Private helper function that generates the high level profile information table
-        """
-        # TODO: implement
-        pass
-
-    def _gen_architecture_table(self) -> DataFrame:
-        """
-        Private helper function that generates the architecture table
-        """
-        # TODO: implement
-        pass
+            return self._etrecord.get(graph)
