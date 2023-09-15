ce21efee37adea3e7c31cb3e48a8a938d84b9359
Chen Lai <chenlai@meta.com>
2023-09-14 21:01:32 -0700

    Change partition function to take exported program (#340)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/340
    
    right now to_backend takes `ExportedProgram` and for partitioner sometimes it'll need to access parameters/buffers to decide how to partition.
    
    this change is needed for partition to take graph with lifted params
    
    Reviewed By: mergennachin
    
    Differential Revision: D49171496
    
    fbshipit-source-id: 25758a47989d8a6507396cf6ec6d5bda58fe7de3

--- exir/backend/test/test_backends_lifted.py
+++ exir/backend/test/test_backends_lifted.py
@@ -67,6 +67,7 @@ from torch.ao.quantization.quantize_fx import (
     _convert_to_reference_decomposed_fx,
     prepare_fx,
 )
+from torch.export import ExportedProgram
 from torch.testing import FileCheck
 
 
@@ -952,17 +953,18 @@ class TestBackends(unittest.TestCase):
         class BadPartitioner(Partitioner):
             partition_tags = {"tag1": DelegationSpec("BackendWithCompilerDemo", [])}
 
-            def partition(self, edge_graph_module):
+            def partition(self, exported_program: ExportedProgram) -> PartitionResult:
                 # Partitioner should not modify the given graph module
                 partition_tags: Dict[str, DelegationSpec] = {}
-                for node in edge_graph_module.graph.nodes:
+                for node in exported_program.graph.nodes:
                     if (
                         node.op == "call_function"
                         and node.target == exir_ops.edge.aten.add.Tensor
                     ):
                         node.target = exir_ops.edge.aten.mul.Tensor
                 return PartitionResult(
-                    tagged_graph=edge_graph_module, partition_tags=partition_tags
+                    tagged_exported_program=exported_program,
+                    partition_tags=partition_tags,
                 )
 
         ep = exir.capture(Model(), inputs, get_testing_capture_config()).to_edge()
