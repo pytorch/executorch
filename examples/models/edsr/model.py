81c97190f05506255e7e2794853391396f76489a
Hansong Zhang <hsz@fb.com>
2023-09-21 14:05:30 -0700

    Add EDSR model using third-party subproject
    Internal BUCK rules are not ready - missing rules from third-party EDSR
    lib.
    
    Test: python -m examples.export.export_example -m edsr

---
+++ examples/models/edsr/model.py
@@ -0,0 +1,28 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import logging
+
+import torch
+
+from ..model_base import EagerModelBase
+
+from ...third_party.edsr.torchsr.models.edsr import edsr_r16f64
+
+
+class EdsrModel(EagerModelBase):
+    def __init__(self):
+        pass
+
+    def get_eager_model(self) -> torch.nn.Module:
+        logging.info("Loading edsr model")
+        mv2 = edsr_r16f64(2, True)
+        logging.info("Loaded edsr model")
+        return mv2
+
+    def get_example_inputs(self):
+        tensor_size = (1, 3, 224, 224)
+        return (torch.randn(tensor_size),)
