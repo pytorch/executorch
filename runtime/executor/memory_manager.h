7ab2d73d4c474e2592f8d2a90b05237b8997f4b6
Dave Bort <dbort@meta.com>
2023-09-29 09:39:28 -0700

    Work around an old GCC bug in memory_manager.h (#529)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/529
    
    We were hitting https://gcc.gnu.org/bugzilla/show_bug.cgi?id=81429 "maybe_unused attribute triggers syntax error when used on first argument to a constructor" when building with older versions of GCC, like v8.5.0 on our devvms.
    
    Rather than changing things everywhere by modifying compiler.h to avoid using `[[maybe_unused]]`, patch this one spot. It's deprecated anyway, and should go away soon.
    ghstack-source-id: 202346140
    exported-using-ghexport
    
    Reviewed By: digantdesai
    
    Differential Revision: D49759354
    
    fbshipit-source-id: 9e912d96f4b162f456ec88daca79a6e5da33af16

--- runtime/executor/memory_manager.h
+++ runtime/executor/memory_manager.h
@@ -63,7 +63,11 @@ class MemoryManager final {
    * TODO(T162089316): Remove this once all users migrate to the new ctor.
    */
   __ET_DEPRECATED MemoryManager(
-      __ET_UNUSED MemoryAllocator* constant_allocator,
+      // We would normally use __ET_UNUSED here, but GCC older than 9.3 has a
+      // bug that triggers a syntax error when using [[maybe_unused]] on the
+      // first parameter of a constructor:
+      // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=81429
+      __attribute__((unused)) MemoryAllocator* constant_allocator,
       HierarchicalAllocator* non_constant_allocator,
       MemoryAllocator* runtime_allocator,
       MemoryAllocator* kernel_temporary_allocator)
