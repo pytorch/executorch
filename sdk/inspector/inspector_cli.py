76079166c4ceca016180af7a74bae50eba98dba8
Olivia Liu <olivialpx@meta.com>
2023-10-02 16:47:18 -0700

    "CLI" aka a script to run the Inspector (#546)
    Summary:
    
    Not a CLI strictly speaking, but should be good enough for MVP. See Test Plan for how to use it.
    
    Regarding added dependencies:
    * pandas: for dataframe usage [here](https://fburl.com/code/5i7nghbq)
    * tabulate: for pretty printing [here](https://fburl.com/code/krmppoh8)
    
    ***Note***: the changes in "executorch/oss/pyproject.toml" and "executorch/oss/setup.py" are not just for the CLI, but also *necessary* to enable the Python Inspector APIs in OSS.
    
    Reviewed By: tarun292
    
    Differential Revision: D49758493

---
+++ sdk/inspector/inspector_cli.py
@@ -0,0 +1,29 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import argparse
+
+from executorch.sdk.inspector.inspector import Inspector
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--etdump_path",
+        required=True,
+        help="Provide an ETDump file path.",
+    )
+    parser.add_argument(
+        "--etrecord_path",
+        required=False,
+        help="Provide an optional ETRecord file path.",
+    )
+
+    args = parser.parse_args()
+
+    inspector = Inspector(
+        etdump_path=args.etdump_path, etrecord_path=args.etrecord_path
+    )
+    inspector.print_data_tabular()
