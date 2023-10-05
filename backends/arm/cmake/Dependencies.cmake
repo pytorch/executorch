683d4284383831059b24bb29c6931f1a2d213df1
Rob Elliott <robert.elliott@arm.com>
2023-10-05 19:15:31 +0000

    Removed ethos u driver build and cmsis dependency
    Signed-off-by: Rob Elliott <robert.elliott@arm.com>

--- backends/arm/cmake/Dependencies.cmake
+++ backends/arm/cmake/Dependencies.cmake
@@ -6,7 +6,5 @@
 set(THIRD_PARTY_ROOT "${CMAKE_CURRENT_SOURCE_DIR}/third-party")
 
 # Ethos-U driver
-set(DRIVER_ETHOSU_SOURCE_DIR "${THIRD_PARTY_ROOT}/ethos-u-core-driver")
 set(DRIVER_ETHOSU_INCLUDE_DIR "${THIRD_PARTY_ROOT}/ethos-u-core-driver/include")
-add_subdirectory( ${DRIVER_ETHOSU_SOURCE_DIR} )
 include_directories( ${DRIVER_ETHOSU_INCLUDE_DIR} )
