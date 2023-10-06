3afa24f678b6c379519641041f9ca5be53601795
Hansong Zhang <hsz@meta.com>
2023-10-05 17:48:57 -0700

    Fix some license header (#647)
    Summary: Pull Request resolved: https://github.com/pytorch/executorch/pull/647
    
    Reviewed By: mcr229
    
    Differential Revision: D49978258
    
    Pulled By: kirklandsign
    
    fbshipit-source-id: 2282af64c5aa4c8451314917054580db511fb6e1

--- examples/demo-apps/android/ExecuTorchDemo/app/src/main/java/com/example/executorchdemo/ClassificationActivity.java
+++ examples/demo-apps/android/ExecuTorchDemo/app/src/main/java/com/example/executorchdemo/ClassificationActivity.java
@@ -1,8 +1,10 @@
-// Copyright (c) 2020 Facebook, Inc. and its affiliates.
-// All rights reserved.
-//
-// This source code is licensed under the BSD-style license found in the
-// LICENSE file in the root directory of this source tree.
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
 package com.example.executorchdemo;
 
