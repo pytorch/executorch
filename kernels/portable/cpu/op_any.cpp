b66106598e92e0bf408465fb163f0cd738141806
Manuel Candales <manuelcandales@gmail.com>
2023-10-08 20:59:09 -0700

    [ET][Portable] Fix & cleanup op any
    Resize out tensor
    
    Differential Revision: [D49735858](https://our.internmc.facebook.com/intern/diff/D49735858/)
    
    [ghstack-poisoned]

--- kernels/portable/cpu/op_any.cpp
+++ kernels/portable/cpu/op_any.cpp
@@ -7,7 +7,6 @@
  */
 
 #include <executorch/runtime/kernel/kernel_includes.h>
-#include <executorch/runtime/platform/assert.h>
 
 namespace torch {
 namespace executor {
@@ -19,21 +18,24 @@ using ScalarType = exec_aten::ScalarType;
 Tensor& any_all_out(RuntimeContext& ctx, const Tensor& in, Tensor& out) {
   (void)ctx;
 
-  ET_CHECK_MSG(out.dim() == 0, "dimension of the output Tensor shall be 0.");
+  ET_KERNEL_CHECK(
+      ctx, resize_tensor(out, {}) == Error::Ok, InvalidArgument, out);
 
-  ET_SWITCH_REAL_TYPES_AND(Bool, in.scalar_type(), ctx, "any", CTYPE_IN, [&] {
-    ET_SWITCH_TWO_TYPES(
-        Bool, Byte, out.scalar_type(), ctx, "any", CTYPE_OUT, [&] {
-          const auto data_in = in.const_data_ptr<CTYPE_IN>();
-          auto data_out = out.mutable_data_ptr<CTYPE_OUT>();
-          data_out[0] = static_cast<CTYPE_OUT>(false);
-          for (auto i = 0; i < in.numel(); ++i) {
-            if (static_cast<CTYPE_OUT>(data_in[i])) {
-              data_out[0] = static_cast<CTYPE_OUT>(true);
-              break;
-            }
-          }
-        });
+  ScalarType in_type = in.scalar_type();
+  ScalarType out_type = out.scalar_type();
+
+  ET_SWITCH_REAL_TYPES_AND(Bool, in_type, ctx, __func__, CTYPE_IN, [&] {
+    ET_SWITCH_TWO_TYPES(Bool, Byte, out_type, ctx, __func__, CTYPE_OUT, [&] {
+      const auto data_in = in.const_data_ptr<CTYPE_IN>();
+      auto data_out = out.mutable_data_ptr<CTYPE_OUT>();
+      data_out[0] = static_cast<CTYPE_OUT>(false);
+      for (auto i = 0; i < in.numel(); ++i) {
+        if (static_cast<CTYPE_OUT>(data_in[i])) {
+          data_out[0] = static_cast<CTYPE_OUT>(true);
+          break;
+        }
+      }
+    });
   });
 
   return out;
