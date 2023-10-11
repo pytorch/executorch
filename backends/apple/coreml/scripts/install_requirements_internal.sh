4cf13bbd3330aaa69d39b609e1e6c705f57869c3
Gyan Sinha <gyanendra_sinha@apple.com>
2023-10-10 23:41:26 -0700

    Enable CI

--- backends/apple/coreml/scripts/install_requirements_internal.sh
+++ backends/apple/coreml/scripts/install_requirements_internal.sh
@@ -21,17 +21,10 @@ if [ -d "/tmp/coremltools" ]; then
     rm -rf "/tmp/coremltools"
 fi
 
-if [ -z "${SECRET_TOKEN_COREML_PRIVATE_REPO}" ]; then
+if [ -n "${SECRET_TOKEN_COREML_PRIVATE_REPO}" ]; then
     git clone "https://${SECRET_TOKEN_COREML_PRIVATE_REPO}@github.com/DawerG/coremltools.git" /tmp/coremltools
 else
     git clone "https://github.com/DawerG/coremltools.git" /tmp/coremltools
-fi
-
-echo "${green}ExecuTorch: Cloning coremltools."
-if [ -z "$ANDROID_NDK" ]
-then
-  echo "ANDROID_NDK not set; please set it to the Android NDK directory"
-  exit 1
 fi
 
 cd /tmp/coremltools
