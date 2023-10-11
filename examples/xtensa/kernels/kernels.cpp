2c6b9dddd1dbfc7e2ccd22494ca52798d673baab
Mergen Nachin <mnachin@meta.com>
2023-10-10 22:11:16 -0700

    Remove the temp for xtensa lint (#829)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/829
    
    Now D50135341 is merged, we can enable back internal linter
    
    Reviewed By: mcremon-meta
    
    Differential Revision: D50157118
    
    fbshipit-source-id: ebc2faf0976379dfa00680e289b77d135d1a6504

--- examples/xtensa/kernels/kernels.cpp
+++ examples/xtensa/kernels/kernels.cpp
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 #include "kernels.h"
 #include "NatureDSP_Signal.h"
 #include "NatureDSP_Signal_vector.h"
