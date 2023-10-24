c6c2ea9d12702e0409057276785cbb19ccb3705e
Martin Yuan <myuan@meta.com>
2023-10-24 14:23:46 -0700

    Add Llama2 license (#1085)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/1085
    
    Add the proper Llama2 license to this example.
    
    Reviewed By: orionr, cccclai
    
    Differential Revision: D50613275
    
    fbshipit-source-id: 2a8da15b2d4980586f10a8ccfee32c755c847956

--- examples/models/llama2/model.py
+++ examples/models/llama2/model.py
@@ -1,8 +1,8 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # All rights reserved.
 #
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree.
+# Llama 2 is licensed under the LLAMA 2 Community License,
+# Copyright (c) Meta Platforms, Inc. All Rights Reserved.
 
 # Please refer to README.md in the same folder for more information.
 
