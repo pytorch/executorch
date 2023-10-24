eb11f1901f61f4f4a187a83c5807340d71d82714
Hansong Zhang <hsz@meta.com>
2023-10-23 22:29:31 -0700

    fix check

--- .ci/scripts/test.sh
+++ .ci/scripts/test.sh
@@ -168,7 +168,7 @@ else
     echo "::endgroup::"
   fi
   set -e
-  if [[ -z "${GOT_ERROR:}" ]]; then
+  if [[ -z "${Q_ERROR:-}" ]] || [[ -z "${D_ERROR:-}" ]] || [[ -z "${Q_D_ERROR:-}" ]]; then
     echo "${Q_ERROR}" "${D_ERROR}" "${Q_D_ERROR}"
     exit 1
   fi
