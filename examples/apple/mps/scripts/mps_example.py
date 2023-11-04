7dca70eed57453062bcecdc4d5bed7c785f59c80
Songhao Jia <gasoonjia@meta.com>
2023-11-03 23:50:44 -0700

    put bundled program under executorch/sdk (#1014)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/1014
    
    This stack aims to restructure bundledprogram codebase. The target structure should be like D49605947.
    
    This diff moves bundled program codebase from executorch/ to executorch/sdk/
    
    Reviewed By: tarun292
    
    Differential Revision: D50422728
    
    fbshipit-source-id: 6f51b726bf0f883a2798a400e1dd16ab8f3b84cc

--- examples/apple/mps/scripts/mps_example.py
+++ examples/apple/mps/scripts/mps_example.py
@@ -13,13 +13,13 @@ import torch
 import torch._export as export
 from executorch import exir
 from executorch.backends.apple.mps.mps_preprocess import MPSBackend
-from executorch.bundled_program.config import MethodTestCase, MethodTestSuite
-from executorch.bundled_program.core import create_bundled_program
-from executorch.bundled_program.serialize import (
-    serialize_from_bundled_program_to_flatbuffer,
-)
 
 from executorch.exir.backend.backend_api import to_backend
+from executorch.sdk.bundled_program.config import MethodTestCase, MethodTestSuite
+from executorch.sdk.bundled_program.core import create_bundled_program
+from executorch.sdk.bundled_program.serialize import (
+    serialize_from_bundled_program_to_flatbuffer,
+)
 
 from ....models import MODEL_NAME_TO_MODEL
 from ....models.model_factory import EagerModelFactory
