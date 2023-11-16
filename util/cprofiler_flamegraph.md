63a194cca611d5861872ee21d893e2d72e865b43
Kimish Patel <kimishpatel@fb.com>
2023-11-16 13:49:35 -0800

    Update on "Add a way to use cprofile to generate stats and render as flamegraph"
    Summary:
    While trying to debug slow export I found using cprofile quite useful.
    However, it is much more intuitive to use icicle kind of flamegraphs.
    This PR adds this util by building on top of snakeviz
    
    Test Plan:
    Tested locally
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    Differential Revision: [D51380270](https://our.internmc.facebook.com/intern/diff/D51380270)
    
    [ghstack-poisoned]

--- util/cprofiler_flamegraph.md
+++ util/cprofiler_flamegraph.md
@@ -1,12 +1,10 @@
 ## Requirements
 
-CProfilerFlameGraph depends on snakeviz to render flamegraph as html file. Main snakeviz repo requires renders flamegraph by launching webserver and rendering html through that. On some enviornments you want to just get a statically renderable html that can be shared. Snakeviz's author provides utility for this at https://gist.github.com/jiffyclub/6b5e0f0f05ab487ff607. In my, kimishpatel's, private fork of snakeviz, I just combined the patch from gist and added a util that does not need to launch server. I intend to upstream that if author approves. In the meantime, install snakeviz from my fork.
+CProfilerFlameGraph depends on snakeviz to render flamegraph as html file. Main snakeviz repo requires renders flamegraph by launching webserver and rendering html through that. On some enviornments you want to just get a statically renderable html that can be shared. Snakeviz's author provides utility for this at https://gist.github.com/jiffyclub/6b5e0f0f05ab487ff607. This util just combined the patch from gist and added a util that does not need to launch server.
 
 ## Installation
 
-git clone https://github.com/kimishpatel/snakeviz.git utils/third-party-deps/
-cd utils/third-party-deps/
-pip install -e .
+pip install snakeviz
 
 ## Usage
 
--- util/python_profiler.py
+++ util/python_profiler.py
@@ -1,14 +1,60 @@
 import cProfile, io, pstats
 import logging
-from pstats import SortKey
+import os
+from pstats import Stats
+import re
+
+from snakeviz.stats import json_stats, table_rows
+from tornado import template
 
 module_found = True
 try:
     import snakeviz
-    from snakeviz.export_static_html import from_pstat_to_static_html
 except ImportError:
     module_found = False
 
+snakeviz_dir = os.path.dirname(os.path.abspath(snakeviz.__file__))
+snakeviz_templates_dir = os.path.join(snakeviz_dir, "templates")
+
+def _from_pstat_to_static_html(stats: Stats, html_filename: str):
+    """
+    Parses pstats data and populates viz.html template stored under templates dir.
+    This utility allows to export html file without kicking off webserver.
+
+    Note that it relies js scripts stored at rawgit cdn. This is not super
+    reliable, however it does allow one to not have to rely on webserver and
+    local rendering. On the other hand, for local rendering please follow
+    the main snakeviz tutorial
+
+    Inspiration for this util is from https://gist.github.com/jiffyclub/6b5e0f0f05ab487ff607.
+
+    Args:
+        stats: Stats generated from cProfile data
+        html_filename: Output filename in which populated template is rendered
+    """
+    RESTR = r'(?<!] \+ ")/static/'
+    REPLACE_WITH = "https://cdn.rawgit.com/jiffyclub/snakeviz/v0.4.2/snakeviz/static/"
+
+    if not isinstance(html_filename, str):
+        raise ValueError("A valid file name must be provided.")
+
+    viz_html_loader = template.Loader(snakeviz_templates_dir)
+    html_bytes_renderer = viz_html_loader.load("viz.html")
+    file_split = html_filename.split(".")
+    if len(file_split) < 2:
+        raise ValueError(
+            f"\033[0;32;40m Provided filename \033[0;31;47m {html_filename} \033[0;32;40m does not contain . separator."
+        )
+    profile_name = file_split[0]
+    html_bytes = html_bytes_renderer.generate(
+        profile_name=profile_name,
+        table_rows=table_rows(stats),
+        callees=json_stats(stats),
+    )
+    html_string = html_bytes.decode("utf-8")
+    html_string = re.sub(RESTR, REPLACE_WITH, html_string)
+    with open(html_filename, "w") as f:
+        f.write(html_string)
 
 class CProfilerFlameGraph:
     def __init__(self, filename: str):
@@ -29,4 +75,4 @@ class CProfilerFlameGraph:
         self.pr.disable()
         s = io.StringIO()
         ps = pstats.Stats(self.pr, stream=s)
-        from_pstat_to_static_html(ps, self.filename)
+        _from_pstat_to_static_html(ps, self.filename)
