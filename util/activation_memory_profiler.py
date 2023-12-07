f4bd6e2d09762b7a26e43d4db0d31a9a38d0e933
Kimish Patel <kimishpatel@meta.com>
2023-12-07 11:07:25 -0800

    Memory profiling (#1301)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/1301
    
    This diff adds memory profiling capability by analyzing tensors
    allocated by memory planning pass. It tracks at each point in program
    execution, what are the live tensor objects.
    
    It would be good to extend this to delegate where we can get a sense
    of estimated runtime memory needed by delegate instance.
    
    Reviewed By: digantdesai
    
    Differential Revision: D51625497
    
    fbshipit-source-id: 798418d16de03e9497b0a5b05e321537513f5249

---
+++ util/activation_memory_profiler.py
@@ -0,0 +1,140 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import json
+from dataclasses import dataclass, field
+from typing import List
+
+import executorch.exir.memory as memory
+import torch
+from executorch.exir import ExecutorchProgramManager
+from executorch.exir.memory_planning import get_node_tensor_specs
+from executorch.exir.tensor import num_bytes_from_shape_and_dtype
+from torch._export import ExportedProgram
+
+
+@dataclass
+class Allocation:
+    name: str
+    op_name: str
+    memory_id: int
+    size_bytes: int
+    fqn: str
+    file_and_line_num: str
+
+
+@dataclass
+class MemoryTimeline:
+    allocations: List[Allocation] = field(default_factory=list)
+
+
+def _get_module_hierarchy(node: torch.fx.Node) -> str:
+    """
+    Get the module hierarchy of the given node.
+    """
+    module_stack = node.meta.get("nn_module_stack")
+    if module_stack is not None:
+        module_values_list = list(module_stack.values())
+        return module_values_list[-1][0]
+    return ""
+
+
+def create_tensor_allocation_info(graph: torch.fx.Graph) -> List[MemoryTimeline]:
+    """
+    Creates a memory timlines, where each step in the timeline is a list of active
+    allocations at that timestep.
+    """
+    nodes = graph.nodes
+    memory_timeline = [None] * len(nodes)
+    for i, node in enumerate(nodes):
+        if node.target == memory.alloc:
+            continue
+        tensor_specs = get_node_tensor_specs(node)
+        if tensor_specs is None:
+            continue
+        for tensor_spec in tensor_specs:
+            # TODO: Make use of mem_id in the allocation info
+            if tensor_spec is None or tensor_spec.mem_id is None or tensor_spec.const:
+                continue
+            start, end = tensor_spec.lifetime
+            size = num_bytes_from_shape_and_dtype(tensor_spec.shape, tensor_spec.dtype)
+            stack_trace = node.meta.get("stack_trace")
+            fqn = _get_module_hierarchy(node)
+            for j in range(start, end + 1):
+                if memory_timeline[j] is None:
+                    memory_timeline[j] = MemoryTimeline()
+                memory_timeline[j].allocations.append(
+                    Allocation(node.name, node.target, i, size, fqn, stack_trace)
+                )
+    return memory_timeline
+
+
+def _validate_memory_planning_is_done(exported_program: ExportedProgram):
+    """
+    Validate whether the memory planning has been done on the given program.
+    """
+    for node in exported_program.graph.nodes:
+        # If there is at least one memory allocation node, then we know the memory planning has been done.
+        if node.target == memory.alloc:
+            return True
+    return False
+
+
+def generate_memory_trace(
+    executorch_program_manager: ExecutorchProgramManager, chrome_trace_filename: str
+):
+    """
+    Generate the memory timeline from the given ExecuTorch program.
+    Args:
+        executorch_program The ExecuTorch program to be analyzed.
+    Returns:
+        Chrome trace in JSON format:
+        Format:
+        Each thread represents a unit of time. Thus to navigate timeline scroll up and down.
+        For each thread, the x axis represents live tensor objects that are normalized according the allocation size.
+    """
+    if not isinstance(executorch_program_manager, ExecutorchProgramManager):
+        raise ValueError(
+            f"generate_memory_trace expects ExecutorchProgramManager instance but got {type(executorch_program_manager)}"
+        )
+
+    exported_program = executorch_program_manager.exported_program()
+    if not _validate_memory_planning_is_done(exported_program):
+        raise ValueError("Executorch program does not have memory planning.")
+
+    memory_timeline = create_tensor_allocation_info(exported_program.graph)
+    root = {}
+    trace_events = []
+    root["traceEvents"] = trace_events
+
+    tid = 0
+    for memory_timeline_event in memory_timeline:
+        start_time = 0
+        if memory_timeline_event is None:
+            continue
+        for allocation in memory_timeline_event.allocations:
+            e = {}
+            e["name"] = allocation.name
+            e["cat"] = "memory_allocation"
+            e["ph"] = "X"
+            e["ts"] = int(start_time)
+            allocation_size_kb = allocation.size_bytes
+            e["dur"] = int(allocation_size_kb)
+            e["pid"] = 0
+            e["tid"] = tid
+            e["args"] = {}
+            e["args"]["op_name"] = f"{allocation.op_name}"
+            e["args"]["fqn"] = f"{allocation.fqn}"
+            e["args"]["source"] = f"{allocation.file_and_line_num}"
+            e["args"]["bytes"] = allocation.size_bytes
+            start_time += allocation_size_kb
+            trace_events.append(e)
+        tid += 1
+
+    json_content: str = json.dumps(root, indent=2)
+
+    with open(chrome_trace_filename, "wb") as json_file:
+        json_file.write(json_content.encode("ascii"))
