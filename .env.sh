e6c90ddf45fc128609d637f5a71c43711cd5cb70
megakuo <megan20010704@gmail.com>
2024-01-03 14:08:03 +0800

    clean repo

--- .env.sh
+++
@@ -1,8 +0,0 @@
-conda activate executorch
-export QNN_SDK_ROOT=/opt/qcom/aistack/qnn/2.14.2.230905
-export EXECUTORCH_ROOT=/home/megankuo/repos/executorch
-export ANDROID_NDK=/home/megankuo/android-ndk
-export LD_LIBRARY_PATH=$QNN_SDK_ROOT/lib/x86_64-linux-clang/:$LD_LIBRARY_PATH
-export PYTHONPATH=$EXECUTORCH_ROOT/..
-export PATH="$EXECUTORCH_ROOT/build_x86_64/third-party/flatbuffers:$PATH"
-
