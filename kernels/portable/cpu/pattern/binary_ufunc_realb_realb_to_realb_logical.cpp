d1eac35a698903d198738feda40162fe12623ab3
Gregory Comer <gjcomer@meta.com>
2024-01-10 18:44:50 -0800

    Refactor resize_to_broadcast_target_size for non-fatal (#1558)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/1558
    
    Update resize_to_broadcast_target_size to return bool and update call sites.
    
    Note that there is follow-up work for some of these ops. I will address in a follow-up diff.
    
    Reviewed By: SS-JIA, manuelcandales
    
    Differential Revision: D52562125
    
    fbshipit-source-id: 4558192eece3a6f1a91196e839a114b8a433fa03

--- kernels/portable/cpu/pattern/binary_ufunc_realb_realb_to_realb_logical.cpp
+++ kernels/portable/cpu/pattern/binary_ufunc_realb_realb_to_realb_logical.cpp
@@ -22,10 +22,11 @@ Tensor& binary_ufunc_realb_realb_to_realb_logical(
     const Tensor& a,
     const Tensor& b,
     Tensor& out) {
-  (void)ctx;
-
-  // Determine output size and resize for dynamic shapes
-  resize_to_broadcast_target_size(a, b, out);
+  ET_KERNEL_CHECK(
+      ctx,
+      resize_to_broadcast_target_size(a, b, out) == Error::Ok,
+      InvalidArgument,
+      out);
 
   ScalarType a_type = a.scalar_type();
   ScalarType b_type = b.scalar_type();
