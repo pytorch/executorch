99bdfaf8aba6db50042f0e476b31b12b2a130d6a
Mengwei Liu <larryliu@meta.com>
2024-01-21 19:38:25 -0800

    Add a sampler (#1643)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/1643
    
    Add a simple sampler for Llama inference.
    
    Reviewed By: mikekgfb
    
    Differential Revision: D52899011
    
    fbshipit-source-id: c1f5cf53b6f3c3327c207b411e47b17225180130

---
+++ examples/models/llama2/sampler/sampler.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <math.h>
+#include <cctype>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <memory>
+
+namespace torch {
+namespace executor {
+// A simple llama2 sampler.
+
+struct ProbIndex {
+  float prob;
+  int32_t index;
+}; // struct used when sorting probabilities during top-p sampling
+
+class Sampler {
+ public:
+  Sampler(
+      int32_t vocab_size,
+      float temperature,
+      float topp,
+      unsigned long long rng_seed);
+
+  int32_t sample(float* logits);
+
+ private:
+  int32_t sample_topp(float* probabilities, float coin);
+  int32_t sample_mult(float* probabilities, float coin);
+  int32_t sample_argmax(float* probabilities);
+
+ private:
+  int32_t vocab_size_;
+  std::unique_ptr<ProbIndex[]> probindex_; // buffer used in top-p sampling
+  float temperature_;
+  float topp_;
+  unsigned long long rng_state_;
+};
+
+} // namespace executor
+} // namespace torch
