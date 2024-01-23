cb04be3c599611b79f8579550f1b1115f788e09c
Max Ren <maxren@meta.com>
2024-01-23 10:37:27 -0800

    create target for serializer and schema (#1628)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/1628
    
    Creating new buck targets for serialization and schema. Want to split out Targets which explicitly use the schema (things like passes and node visitors use the dataclasses instantiated in the schema) and targets which are actually serializing, like xnnpack_preprocess
    
    Reviewed By: digantdesai
    
    Differential Revision: D52809539
    
    fbshipit-source-id: 9e496b249d92e76211965417a125d499dfe28c45

--- backends/xnnpack/runtime/XNNCompiler.cpp
+++ backends/xnnpack/runtime/XNNCompiler.cpp
@@ -8,7 +8,7 @@
 
 #include <executorch/backends/xnnpack/runtime/XNNCompiler.h>
 #include <executorch/backends/xnnpack/runtime/XNNHeader.h>
-#include <executorch/backends/xnnpack/schema_generated.h>
+#include <executorch/backends/xnnpack/serialization/schema_generated.h>
 #include <executorch/backends/xnnpack/threadpool/threadpool.h>
 #include <executorch/runtime/core/exec_aten/util/scalar_type_util.h>
 #include <unordered_map>
