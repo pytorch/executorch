0d4cd755e27a08f224217edff5403d81c668f47e
Mengwei Liu <larryliu@meta.com>
2024-01-23 17:17:44 -0800

    Split llama_runner into main and class (#1690)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/1690
    
    As titled
    
    Reviewed By: mikekgfb
    
    Differential Revision: D53024517
    
    fbshipit-source-id: 513f2f8c90e2bb54b8bc9acd49280b59b2b11d35

---
+++ examples/models/llama2/main.cpp
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <gflags/gflags.h>
+
+#include <executorch/examples/models/llama2/llama_runner.h>
+
+DEFINE_string(
+    model_path,
+    "llama2.pte",
+    "Model serialized in flatbuffer format.");
+
+DEFINE_string(tokenizer_path, "tokenizer.bin", "Tokenizer stuff.");
+
+DEFINE_string(prompt, "The answer to the ultimate question is", "Prompt.");
+
+using namespace torch::executor;
+
+int32_t main(int32_t argc, char** argv) {
+  runtime_init();
+
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+  // Create a loader to get the data of the program file. There are other
+  // DataLoaders that use mmap() or point32_t to data that's already in memory,
+  // and users can create their own DataLoaders to load from arbitrary sources.
+  const char* model_path = FLAGS_model_path.c_str();
+
+  const char* tokenizer_path = FLAGS_tokenizer_path.c_str();
+
+  const char* prompt = FLAGS_prompt.c_str();
+
+  // create llama runner
+  LlamaRunner llama_runner(model_path, tokenizer_path);
+
+  // generate
+  llama_runner.generate(prompt);
+  return 0;
+}
