6a9fecb2d713db696ef476006375dc5ee77c10e7
Mengwei Liu <larryliu@meta.com>
2024-01-24 13:36:26 -0800

    Allow sampler to take fp16 logits type (#1695)
    Summary:
    
    For fp16 models we will be expecting logits to be fp16. Here I'm allowing the sampler to take logits of fp16.
    
    Reviewed By: shoumikhin
    
    Differential Revision: D53033252

--- examples/models/llama2/sampler/test/test_sampler.cpp
+++ examples/models/llama2/sampler/test/test_sampler.cpp
@@ -31,5 +31,18 @@ TEST_F(SamplerTest, TestArgMax) {
   EXPECT_EQ(sampler.sample(input.data_ptr<float>()), 396);
 }
 
+TEST_F(SamplerTest, TestArgMaxWithFP16) {
+  torch::executor::Sampler sampler{
+      /*vocab_size*/ 32000,
+      /*temperature*/ 0.0f,
+      /*topp*/ 0.9f,
+      /*rng_seed*/ 0};
+  // tensor([[[-12.9832,  -7.4133,  -0.4327,  ...,  -6.8297,  -8.0880,
+  // -7.5863]]])
+  torch::Tensor input = torch::rand({1, 1, 32000}, at::kHalf);
+  input[0][0][396] = 1.0f;
+  EXPECT_EQ(sampler.sample(input.data_ptr<c10::Half>()), 396);
+}
+
 } // namespace executor
 } // namespace torch
