349fff030859774dd1944fe9a2291efeb8b83ebd
Manuel Candales <manuelcandales@gmail.com>
2024-01-25 12:25:10 -0800

    [ET][Portable] Half support: mul
    Differential Revision: [D53066174](https://our.internmc.facebook.com/intern/diff/D53066174/)
    
    [ghstack-poisoned]

--- kernels/optimized/cpu/op_mul.cpp
+++ kernels/optimized/cpu/op_mul.cpp
@@ -31,12 +31,13 @@ Tensor& opt_mul_out(
   ScalarType b_type = b.scalar_type();
   ScalarType out_type = out.scalar_type();
 
-  if (a_type == b_type && a_type == out_type && a.sizes().equals(b.sizes())) {
+  if (a_type == b_type && a_type == out_type && a.sizes().equals(b.sizes()) &&
+      a_type != ScalarType::Half) {
     // Resize for dynamic shape
     auto error = resize_tensor(out, a.sizes());
     ET_CHECK_MSG(error == Error::Ok, "Failed to resize output tensor.");
 
-    ET_SWITCH_REAL_TYPES_AND(Bool, out_type, ctx, "mul.out", CTYPE, [&]() {
+    ET_SWITCH_REALB_TYPES(out_type, ctx, "mul.out", CTYPE, [&]() {
       using Vec = executorch::vec::Vectorized<CTYPE>;
       executorch::vec::map2<CTYPE>(
           [](Vec x, Vec y) { return x * y; },
@@ -46,7 +47,8 @@ Tensor& opt_mul_out(
           out.numel());
     });
   } else {
-    ScalarType common_type = promoteTypes(a_type, b_type);
+    ScalarType common_type =
+        promoteTypes(a_type, b_type, /*half_to_float*/ true);
     ET_CHECK(canCast(common_type, out_type));
 
     ET_KERNEL_CHECK(
@@ -55,25 +57,23 @@ Tensor& opt_mul_out(
         InvalidArgument,
         out);
 
-    ET_SWITCH_REAL_TYPES_AND(Bool, a_type, ctx, "mul.out", CTYPE_A, [&]() {
-      ET_SWITCH_REAL_TYPES_AND(Bool, b_type, ctx, "mul.out", CTYPE_B, [&]() {
-        ET_SWITCH_REAL_TYPES_AND(
-            Bool, common_type, ctx, "mul.out", CTYPE_IN, [&]() {
-              ET_SWITCH_REAL_TYPES_AND(
-                  Bool, out_type, ctx, "mul.out", CTYPE_OUT, [&]() {
-                    apply_binary_elementwise_fn<CTYPE_A, CTYPE_B, CTYPE_OUT>(
-                        [](const CTYPE_A val_a, const CTYPE_B val_b) {
-                          CTYPE_IN a_casted = static_cast<CTYPE_IN>(val_a);
-                          CTYPE_IN b_casted = static_cast<CTYPE_IN>(val_b);
-                          CTYPE_IN value = a_casted * b_casted;
+    ET_SWITCH_REALHB_TYPES(a_type, ctx, "mul.out", CTYPE_A, [&]() {
+      ET_SWITCH_REALHB_TYPES(b_type, ctx, "mul.out", CTYPE_B, [&]() {
+        ET_SWITCH_REALB_TYPES(common_type, ctx, "mul.out", CTYPE_IN, [&]() {
+          ET_SWITCH_REALHB_TYPES(out_type, ctx, "mul.out", CTYPE_OUT, [&]() {
+            apply_binary_elementwise_fn<CTYPE_A, CTYPE_B, CTYPE_OUT>(
+                [](const CTYPE_A val_a, const CTYPE_B val_b) {
+                  CTYPE_IN a_casted = static_cast<CTYPE_IN>(val_a);
+                  CTYPE_IN b_casted = static_cast<CTYPE_IN>(val_b);
+                  CTYPE_IN value = a_casted * b_casted;
 
-                          return static_cast<CTYPE_OUT>(value);
-                        },
-                        a,
-                        b,
-                        out);
-                  });
-            });
+                  return static_cast<CTYPE_OUT>(value);
+                },
+                a,
+                b,
+                out);
+          });
+        });
       });
     });
   }
@@ -90,7 +90,8 @@ Tensor& opt_mul_scalar_out(
 
   ScalarType a_type = a.scalar_type();
   ScalarType b_type = utils::get_scalar_dtype(b);
-  ScalarType common_type = utils::promote_type_with_scalar(a_type, b);
+  ScalarType common_type =
+      utils::promote_type_with_scalar(a_type, b, /*half_to_float*/ true);
   ScalarType out_type = out.scalar_type();
 
   ET_CHECK(common_type == out_type);
@@ -99,52 +100,44 @@ Tensor& opt_mul_scalar_out(
   auto error = resize_tensor(out, a.sizes());
   ET_CHECK_MSG(error == Error::Ok, "Failed to resize output tensor.");
 
-  if (a_type == common_type && a_type == out_type) {
-    ET_SWITCH_REAL_TYPES_AND(Bool, a_type, ctx, "mul.Scalar_out", CTYPE, [&]() {
-      ET_SWITCH_REAL_TYPES_AND(
-          Bool, b_type, ctx, "mul.Scalar_out", CTYPE_B, [&]() {
-            CTYPE_B b_val;
-            ET_EXTRACT_SCALAR(b, b_val);
-            CTYPE b_casted = static_cast<CTYPE>(b_val);
+  if (a_type == common_type && a_type == out_type &&
+      a_type != ScalarType::Half) {
+    ET_SWITCH_REALB_TYPES(a_type, ctx, "mul.Scalar_out", CTYPE, [&]() {
+      ET_SWITCH_SCALAR_OBJ_TYPES(b_type, ctx, "mul.Scalar_out", CTYPE_B, [&]() {
+        CTYPE_B b_val;
+        ET_EXTRACT_SCALAR(b, b_val);
+        CTYPE b_casted = static_cast<CTYPE>(b_val);
 
-            using Vec = executorch::vec::Vectorized<CTYPE>;
-            executorch::vec::map<CTYPE>(
-                [b_casted](Vec x) { return x * Vec(b_casted); },
-                out.mutable_data_ptr<CTYPE>(),
-                a.const_data_ptr<CTYPE>(),
-                out.numel());
-          });
+        using Vec = executorch::vec::Vectorized<CTYPE>;
+        executorch::vec::map<CTYPE>(
+            [b_casted](Vec x) { return x * Vec(b_casted); },
+            out.mutable_data_ptr<CTYPE>(),
+            a.const_data_ptr<CTYPE>(),
+            out.numel());
+      });
     });
   } else {
-    ET_SWITCH_REAL_TYPES_AND(
-        Bool, a_type, ctx, "mul.Scalar_out", CTYPE_A, [&]() {
-          ET_SWITCH_REAL_TYPES_AND(
-              Bool, b_type, ctx, "mul.Scalar_out", CTYPE_B, [&]() {
-                ET_SWITCH_REAL_TYPES_AND(
-                    Bool, common_type, ctx, "mul.Scalar_out", CTYPE_IN, [&]() {
-                      ET_SWITCH_REAL_TYPES_AND(
-                          Bool,
-                          out_type,
-                          ctx,
-                          "mul.Scalar_out",
-                          CTYPE_OUT,
-                          [&]() {
-                            CTYPE_B b_val;
-                            ET_EXTRACT_SCALAR(b, b_val);
-                            CTYPE_IN b_casted = static_cast<CTYPE_IN>(b_val);
+    ET_SWITCH_REALHB_TYPES(a_type, ctx, "mul.Scalar_out", CTYPE_A, [&]() {
+      ET_SWITCH_SCALAR_OBJ_TYPES(b_type, ctx, "mul.Scalar_out", CTYPE_B, [&]() {
+        ET_SWITCH_REALB_TYPES(
+            common_type, ctx, "mul.Scalar_out", CTYPE_IN, [&]() {
+              ET_SWITCH_REALHB_TYPES(
+                  out_type, ctx, "mul.Scalar_out", CTYPE_OUT, [&]() {
+                    CTYPE_B b_val;
+                    ET_EXTRACT_SCALAR(b, b_val);
+                    CTYPE_IN b_casted = static_cast<CTYPE_IN>(b_val);
 
-                            const size_t n = a.numel();
-                            const CTYPE_A* a_data = a.const_data_ptr<CTYPE_A>();
-                            CTYPE_OUT* out_data =
-                                out.mutable_data_ptr<CTYPE_OUT>();
-                            for (auto i = 0; i < n; ++i) {
-                              out_data[i] = static_cast<CTYPE_OUT>(
-                                  static_cast<CTYPE_IN>(a_data[i]) * b_casted);
-                            }
-                          });
-                    });
-              });
-        });
+                    const size_t n = a.numel();
+                    const CTYPE_A* a_data = a.const_data_ptr<CTYPE_A>();
+                    CTYPE_OUT* out_data = out.mutable_data_ptr<CTYPE_OUT>();
+                    for (auto i = 0; i < n; ++i) {
+                      out_data[i] = static_cast<CTYPE_OUT>(
+                          static_cast<CTYPE_IN>(a_data[i]) * b_casted);
+                    }
+                  });
+            });
+      });
+    });
   }
 
   return out;
