49af5d6fcdf7dbe36977eb24f7ad0477f722e8e9
Jacob Szwejbka <jakeszwe@meta.com>
2024-01-25 12:17:21 -0800

    remove exir.capture from exported_module.py (#1674)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/1674
    
    title
    
    Reviewed By: lucylq
    
    Differential Revision: D52970715
    
    fbshipit-source-id: a68e00ab86c7994df8dde764ab1150804b6524f3

--- test/end2end/test_end2end.py
+++ test/end2end/test_end2end.py
@@ -529,7 +529,7 @@ def maketest(
                     result = module.graph_module(*inputs)
                 self.assertTrue(allclose(expected, result[0], rtol, atol))
 
-        program = module.executorch_program.program
+        program = module.executorch_program.executorch_program
         pretty_print(program)
         print_program(program, show_meminfo=True, mark_dynamic_shape_tensor=True)
         print(f"mem buffer sizes: {program.execution_plan[0].non_const_buffer_sizes}")
@@ -672,18 +672,6 @@ class DynamicModelE2ETest(unittest.TestCase):
     End2end tests for dynamic models. For dynamic models we mean models with
     control flow or dynamic shape.
     """
-
-    def test_input_dynamic_shape(self):
-        maketest(
-            ModuleInputDynamicShape,
-            # can not run the graph module directly since out tensor are allocated
-            # with upperbound shape and may not match the actual shape.
-            run_graph_module=False,
-            capture_config=exir.CaptureConfig(
-                enable_dynamic_shape=True,
-                # enable_functionalization=False,  # TODO enable functionalization
-            ),
-        )(self)
 
     @skip("Revisit when unbacked symint is ready")
     def test_intermediate_dynamic_shape(self):
