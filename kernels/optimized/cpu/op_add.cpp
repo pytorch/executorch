98048c3a494cfdfa1e99b01bc7aae7df421fae21
Manuel Candales <mcandales@meta.com>
2024-01-24 19:46:28 -0800

    Add support for ScalarType::Half (#1693)
    Summary:
    
    Imported code from https://www.internalfb.com/code/fbsource/fbcode/caffe2/c10/util/Half-inl.h and https://www.internalfb.com/code/fbsource/fbcode/caffe2/c10/util/Half.h into Half.h to enable conversions and arithmetic operations with halfs
    
    Introduced ET_INTERNAL_SWITCH_CASE_REAL_TYPES_AND2 macro, in order to switch over REAL TYPES + Bool + Half
    
    Updated add op to support Half
    Updated add tests to test Half support
    
    Reviewed By: dbort
    
    Differential Revision: D53027795

--- kernels/optimized/cpu/op_add.cpp
+++ kernels/optimized/cpu/op_add.cpp
@@ -32,7 +32,8 @@ Tensor& opt_add_out(
   ScalarType b_type = b.scalar_type();
   ScalarType out_type = out.scalar_type();
 
-  if (a_type == b_type && a_type == out_type && a.sizes().equals(b.sizes())) {
+  if (a_type == b_type && a_type == out_type && a.sizes().equals(b.sizes()) &&
+      out_type != ScalarType::Half) {
     // Resize for dynamic shape
     auto error = resize_tensor(out, a.sizes());
     ET_CHECK_MSG(error == Error::Ok, "Failed to resize output tensor.");
@@ -50,7 +51,8 @@ Tensor& opt_add_out(
           out.numel());
     });
   } else {
-    ScalarType common_type = promoteTypes(a_type, b_type);
+    ScalarType common_type =
+        promoteTypes(a_type, b_type, /*half_to_float*/ true);
     ET_CHECK(canCast(common_type, out_type));
 
     ET_KERNEL_CHECK(
@@ -59,30 +61,45 @@ Tensor& opt_add_out(
         InvalidArgument,
         out);
 
-    ET_SWITCH_REAL_TYPES_AND(Bool, a_type, ctx, "add.out", CTYPE_A, [&]() {
-      ET_SWITCH_REAL_TYPES_AND(Bool, b_type, ctx, "add.out", CTYPE_B, [&]() {
-        ET_SWITCH_REAL_TYPES_AND(
-            Bool, common_type, ctx, "add.out", CTYPE_IN, [&]() {
-              ET_SWITCH_REAL_TYPES_AND(
-                  Bool, out_type, ctx, "add.out", CTYPE_OUT, [&]() {
-                    CTYPE_IN alpha_val;
-                    ET_EXTRACT_SCALAR(alpha, alpha_val);
+    ET_SWITCH_REAL_TYPES_AND2(
+        Bool, Half, a_type, ctx, "add.out", CTYPE_A, [&]() {
+          ET_SWITCH_REAL_TYPES_AND2(
+              Bool, Half, b_type, ctx, "add.out", CTYPE_B, [&]() {
+                ET_SWITCH_REAL_TYPES_AND(
+                    Bool, common_type, ctx, "add.out", CTYPE_IN, [&]() {
+                      ET_SWITCH_REAL_TYPES_AND2(
+                          Bool,
+                          Half,
+                          out_type,
+                          ctx,
+                          "add.out",
+                          CTYPE_OUT,
+                          [&]() {
+                            CTYPE_IN alpha_val;
+                            ET_EXTRACT_SCALAR(alpha, alpha_val);
 
-                    apply_binary_elementwise_fn<CTYPE_A, CTYPE_B, CTYPE_OUT>(
-                        [alpha_val](const CTYPE_A val_a, const CTYPE_B val_b) {
-                          CTYPE_IN a_casted = static_cast<CTYPE_IN>(val_a);
-                          CTYPE_IN b_casted = static_cast<CTYPE_IN>(val_b);
-                          CTYPE_IN value = a_casted + alpha_val * b_casted;
+                            apply_binary_elementwise_fn<
+                                CTYPE_A,
+                                CTYPE_B,
+                                CTYPE_OUT>(
+                                [alpha_val](
+                                    const CTYPE_A val_a, const CTYPE_B val_b) {
+                                  CTYPE_IN a_casted =
+                                      static_cast<CTYPE_IN>(val_a);
+                                  CTYPE_IN b_casted =
+                                      static_cast<CTYPE_IN>(val_b);
+                                  CTYPE_IN value =
+                                      a_casted + alpha_val * b_casted;
 
-                          return static_cast<CTYPE_OUT>(value);
-                        },
-                        a,
-                        b,
-                        out);
-                  });
-            });
-      });
-    });
+                                  return static_cast<CTYPE_OUT>(value);
+                                },
+                                a,
+                                b,
+                                out);
+                          });
+                    });
+              });
+        });
   }
 
   return out;
--- kernels/portable/cpu/op_add.cpp
+++ kernels/portable/cpu/op_add.cpp
@@ -32,36 +32,38 @@ Tensor& add_out(
   ScalarType a_type = a.scalar_type();
   ScalarType b_type = b.scalar_type();
   ScalarType alpha_type = utils::get_scalar_dtype(alpha);
-  ScalarType common_type = promoteTypes(a_type, b_type);
+  ScalarType common_type = promoteTypes(a_type, b_type, /*half_to_float*/ true);
   ScalarType out_type = out.scalar_type();
 
   ET_KERNEL_CHECK(ctx, canCast(common_type, out_type), InvalidArgument, out);
   ET_KERNEL_CHECK(
       ctx, check_alpha_type(alpha_type, common_type), InvalidArgument, out);
 
-  ET_SWITCH_REAL_TYPES_AND(Bool, a_type, ctx, "add.out", CTYPE_A, [&]() {
-    ET_SWITCH_REAL_TYPES_AND(Bool, b_type, ctx, "add.out", CTYPE_B, [&]() {
-      ET_SWITCH_REAL_TYPES_AND(
-          Bool, common_type, ctx, "add.out", CTYPE_IN, [&]() {
-            ET_SWITCH_REAL_TYPES_AND(
-                Bool, out_type, ctx, "add.out", CTYPE_OUT, [&]() {
-                  CTYPE_IN alpha_val;
-                  utils::extract_scalar(alpha, &alpha_val);
+  ET_SWITCH_REAL_TYPES_AND2(Bool, Half, a_type, ctx, "add.out", CTYPE_A, [&]() {
+    ET_SWITCH_REAL_TYPES_AND2(
+        Bool, Half, b_type, ctx, "add.out", CTYPE_B, [&]() {
+          ET_SWITCH_REAL_TYPES_AND(
+              Bool, common_type, ctx, "add.out", CTYPE_IN, [&]() {
+                ET_SWITCH_REAL_TYPES_AND2(
+                    Bool, Half, out_type, ctx, "add.out", CTYPE_OUT, [&]() {
+                      CTYPE_IN alpha_val;
+                      utils::extract_scalar(alpha, &alpha_val);
 
-                  apply_binary_elementwise_fn<CTYPE_A, CTYPE_B, CTYPE_OUT>(
-                      [alpha_val](const CTYPE_A val_a, const CTYPE_B val_b) {
-                        CTYPE_IN a_casted = static_cast<CTYPE_IN>(val_a);
-                        CTYPE_IN b_casted = static_cast<CTYPE_IN>(val_b);
-                        CTYPE_IN value = a_casted + alpha_val * b_casted;
+                      apply_binary_elementwise_fn<CTYPE_A, CTYPE_B, CTYPE_OUT>(
+                          [alpha_val](
+                              const CTYPE_A val_a, const CTYPE_B val_b) {
+                            CTYPE_IN a_casted = static_cast<CTYPE_IN>(val_a);
+                            CTYPE_IN b_casted = static_cast<CTYPE_IN>(val_b);
+                            CTYPE_IN value = a_casted + alpha_val * b_casted;
 
-                        return static_cast<CTYPE_OUT>(value);
-                      },
-                      a,
-                      b,
-                      out);
-                });
-          });
-    });
+                            return static_cast<CTYPE_OUT>(value);
+                          },
+                          a,
+                          b,
+                          out);
+                    });
+              });
+        });
   });
 
   return out;
