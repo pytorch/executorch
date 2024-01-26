3bcc7cf15a04187459218a37a050a70539a51cc6
Michael Gschwind <mikekg@meta.com>
2024-01-26 02:12:29 -0800

    Matmul kernels: op_mm, op_bmm (#1721)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/1721
    
    Matmul kernels: op_mm, op_bmm
    
    Reviewed By: larryliu0820
    
    Differential Revision: D53090325
    
    fbshipit-source-id: 65189e27a746347c24d01d819f6031280488256f

--- kernels/optimized/cpu/op_bmm.cpp
+++ kernels/optimized/cpu/op_bmm.cpp
@@ -146,7 +146,7 @@ Tensor& opt_bmm_out(
 
   auto scalar_type = self.scalar_type();
   switch (scalar_type) {
-    ET_FORALL_REAL_TYPES(BMM_TENSOR)
+    ET_FORALL_REAL_TYPES_AND(Half, BMM_TENSOR)
     default:
       ET_CHECK_MSG(
           false, "Unhandled dtype %" PRId8, static_cast<int8_t>(scalar_type));
