6ca6479acef13801a979f485b41b2f85d91cc7c3
Mergen Nachin <mnachin@meta.com>
2024-01-26 09:22:18 -0800

    Fix lint errors (#1725)
    Summary: Pull Request resolved: https://github.com/pytorch/executorch/pull/1725
    
    Reviewed By: larryliu0820
    
    Differential Revision: D53131409
    
    fbshipit-source-id: 35b11bac60453e1e0ee822904ee5b78e047878cb

--- examples/models/llama2/llama_test.py
+++ examples/models/llama2/llama_test.py
@@ -6,16 +6,12 @@
 
 import unittest
 
-import torch
-
-from executorch.extension.pybindings import aten_lib
-
 from .export_llama import build_model
 
 
 class LlamaTest(unittest.TestCase):
     def test_quantized_llama(self):
-        output_path = build_model(
+        _ = build_model(
             modelname="model",
             extra_opts="--fairseq2 -Q",
             par_local_output=True,
@@ -23,7 +19,7 @@ class LlamaTest(unittest.TestCase):
         )
 
     def test_half_llama(self):
-        output_path = build_model(
+        _ = build_model(
             modelname="model",
             extra_opts="--fairseq2 -H",
             par_local_output=True,
