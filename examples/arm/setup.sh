af0db565ac804ddc1b9349ee8497c52603b917fd
Rob Elliott <robert.elliott@arm.com>
2024-02-01 09:34:36 +0000

    Minor copyright update
    Signed-off-by: Rob Elliott <robert.elliott@arm.com>

--- examples/arm/setup.sh
+++ examples/arm/setup.sh
@@ -2,6 +2,8 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # All rights reserved.
 #
+# Copyright 2023-2024 Arm Limited and/or its affiliates.
+#
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
