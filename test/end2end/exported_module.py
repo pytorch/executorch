7fd95996f1541026c755d04b02c155a923a362ec
Boyuan Feng <boyuan@meta.com>
2024-02-01 16:31:52 -0800

    Replace `constraints` with `dynamic_shapes` in pye and executorch (#1714)
    Summary:
    
    X-link: https://github.com/pytorch/pytorch/pull/118345
    
    `constraints` argument for `torch.export` has been deprecated in favor of the `dynamic_shapes` argument. This PR updates the use of the deprecated API in `pye` and `executorch`.
    
    Reviewed By: avikchaudhuri
    
    Differential Revision: D53109299

--- test/end2end/exported_module.py
+++ test/end2end/exported_module.py
@@ -11,7 +11,6 @@ import inspect
 from typing import Callable, Sequence, Type
 
 import executorch.exir as exir
-import torch
 from executorch.exir import ExecutorchBackendConfig, ExecutorchProgramManager, to_edge
 from executorch.exir.dynamic_shape import DynamicMemoryPlanningMode
 from executorch.exir.pass_manager import PassManager
@@ -134,26 +133,23 @@ class ExportedModule:
         for method in methods:
             method_name_to_args[method] = trace_inputs
 
-        method_name_to_constraints = None
-        if hasattr(eager_module, "get_constraints"):
+        method_name_to_dynamic_shapes = None
+        if hasattr(eager_module, "get_dynamic_shapes"):
             assert capture_config is not None
             assert capture_config.enable_aot is True
-            trace_constraints = eager_module.get_constraints()
-            method_name_to_constraints = {}
+            trace_dynamic_shapes = eager_module.get_dynamic_shapes()
+            method_name_to_dynamic_shapes = {}
             for method in methods:
-                method_name_to_constraints[method] = trace_constraints
+                method_name_to_dynamic_shapes[method] = trace_dynamic_shapes
 
         memory_planning_pass = MemoryPlanningPass("greedy")
         if hasattr(eager_module, "get_memory_planning_pass"):
             memory_planning_pass = eager_module.get_memory_planning_pass()
 
-        class WrapperModule(torch.nn.Module):
-            def __init__(self, fn):
+        class WrapperModule(nn.Module):
+            def __init__(self, method):
                 super().__init__()
-                self.fn = fn
-
-            def forward(self, *args, **kwargs):
-                return self.fn(*args, **kwargs)
+                self.forward = method
 
         exported_methods = {}
         # These cleanup passes are required to convert the `add` op to its out
@@ -163,8 +159,8 @@ class ExportedModule:
             exported_methods[method_name] = export(
                 module,
                 method_input,
-                constraints=method_name_to_constraints[method_name]
-                if method_name_to_constraints
+                dynamic_shapes=method_name_to_dynamic_shapes[method_name]
+                if method_name_to_dynamic_shapes
                 else None,
             )
 
