dd0e3b45b812add211bbd7f748fe928aeb9bae31
shewu-quic <quic_shewu@quicinc.com>
2024-02-06 08:17:52 -0800

    Qualcomm AI Engine Direct - Enable quantized VIT (#1839)
    Summary:
    On behalf of shewu-quic with original [PR](https://github.com/pytorch/executorch/pull/1442).
    
    Pull Request resolved: https://github.com/pytorch/executorch/pull/1839
    
    Reviewed By: cccclai
    
    Differential Revision: D53426576
    
    Pulled By: kimishpatel
    
    fbshipit-source-id: e03e34878617991dd05ca49d6b61c1ae7a1a3e6c

--- backends/qualcomm/builders/op_bmm.py
+++ backends/qualcomm/builders/op_bmm.py
@@ -10,12 +10,12 @@ import executorch.backends.qualcomm.python.PyQnnWrapperAdaptor as PyQnnWrapper
 import torch
 
 from .node_visitor import NodeVisitor, register_node_visitor
-from .qnn_constants import OpReshape, QNN_OP_PACKAGE_NAME_QTI_AISW
+from .qnn_constants import OpMatMul, QNN_OP_PACKAGE_NAME_QTI_AISW
 
 
 @register_node_visitor
-class Unsqueeze(NodeVisitor):
-    target = "aten.unsqueeze_copy.default"
+class BMM(NodeVisitor):
+    target = "aten.bmm.default"
 
     def __init__(self, *args) -> None:
         super().__init__(*args)
@@ -25,15 +25,18 @@ class Unsqueeze(NodeVisitor):
         node: torch.fx.Node,
         nodes_to_wrappers: Dict[torch.fx.Node, PyQnnWrapper.TensorWrapper],
     ) -> PyQnnWrapper.PyQnnOpWrapper:
-        input_node = node.args[0]
-        input_tensor = self.get_tensor(input_node, node)
+        bmm_input_tensors = []
+        for index in range(2):
+            input_node = node.args[index]
+            input_tensor = self.get_tensor(input_node, node)
 
-        input_tensor_wrapper = self.define_tensor(
-            input_node,
-            input_tensor,
-            PyQnnWrapper.Qnn_TensorType_t.QNN_TENSOR_TYPE_NATIVE,
-            nodes_to_wrappers,
-        )
+            input_tensor_wrapper = self.define_tensor(
+                input_node,
+                input_tensor,
+                PyQnnWrapper.Qnn_TensorType_t.QNN_TENSOR_TYPE_NATIVE,
+                nodes_to_wrappers,
+            )
+            bmm_input_tensors.append(input_tensor_wrapper)
 
         output_tensor = self.get_tensor(node, node)
         output_tensor_wrapper = self.define_tensor(
@@ -42,13 +45,12 @@ class Unsqueeze(NodeVisitor):
             PyQnnWrapper.Qnn_TensorType_t.QNN_TENSOR_TYPE_NATIVE,
             nodes_to_wrappers,
         )
+        bmm_output_tensors = [output_tensor_wrapper]
 
-        unsqueeze_op = PyQnnWrapper.PyQnnOpWrapper(
-            node.name,
-            QNN_OP_PACKAGE_NAME_QTI_AISW,
-            OpReshape.op_name,
+        bmm_op = PyQnnWrapper.PyQnnOpWrapper(
+            node.name, QNN_OP_PACKAGE_NAME_QTI_AISW, OpMatMul.op_name
         )
-        unsqueeze_op.AddInputTensors([input_tensor_wrapper])
-        unsqueeze_op.AddOutputTensors([output_tensor_wrapper])
+        bmm_op.AddInputTensors(bmm_input_tensors)
+        bmm_op.AddOutputTensors(bmm_output_tensors)
 
-        return unsqueeze_op
+        return bmm_op
