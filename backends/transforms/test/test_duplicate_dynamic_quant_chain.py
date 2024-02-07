c5342e6ea0e896c5bba199ed17fe212727787161
Kimish Patel <kimishpatel@fb.com>
2024-02-07 12:25:39 -0800

    Update base for Update on "[Executorch][llama2] Use xnnpack and quantized ops deps"
    Differential Revision: [D53363746](https://our.internmc.facebook.com/intern/diff/D53363746/)
    
    [ghstack-poisoned]

--- backends/transforms/test/test_duplicate_dynamic_quant_chain.py
+++ backends/transforms/test/test_duplicate_dynamic_quant_chain.py
@@ -1,4 +1,9 @@
-# Owner(s): ["oncall: quantization"]
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 import copy
 import unittest
 
