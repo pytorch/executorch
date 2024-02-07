361734bfa6524d7a2a226008304b7c561040804f
Dave Bort <dbort@meta.com>
2024-02-07 15:12:06 -0800

    Handle tensor memory offsets > 32 bits (#1792)
    Summary:
    
    The `AllocationDetails.memory_offset` field is declared as a `uint`, so it can only handle offsets of 32 bits (4GB).
    
    Changing the type would break backward compatibility, so instead add a new field for the high 32 bits.
    
    Based on some experimentation, this doesn't seem to increase the size of existing .pte files; there's a decent chance that it's sneaking its way into an alignment hole.
    
    While I'm in here, add checks for negative values and values too big to fit in 64 bits, so that users see an error in python instead of during the flatc step.
    
    Reviewed By: JacobSzwejbka
    
    Differential Revision: D53293800

--- exir/tensor.py
+++ exir/tensor.py
@@ -286,8 +286,17 @@ def make_allocation_info(mem_id: int, mem_offset: int) -> schema.AllocationDetai
     """
     Creates the allocation_details object for creating tensors
     """
+    if mem_offset < 0:
+        raise ValueError(f"mem_offset {mem_offset} must not be negative")
+    memory_offset_low = mem_offset & ((1 << 32) - 1)
+    memory_offset_high = mem_offset >> 32
+    if memory_offset_high >= 1 << 32:
+        raise ValueError(f"mem_offset {mem_offset} does not fit in 64 bits")
+
     allocation_info = schema.AllocationDetails(
-        memory_id=mem_id, memory_offset=mem_offset
+        memory_id=mem_id,
+        memory_offset_low=memory_offset_low,
+        memory_offset_high=memory_offset_high,
     )
     return allocation_info
 
