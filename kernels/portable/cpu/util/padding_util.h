ccfabb807973a0678fbbbbdc72a0d3f8cb44595c
Manuel Candales <manuelcandales@gmail.com>
2024-02-09 16:19:20 -0800

    [ET][Portable] Add op: replication_pad3d
    Pull Request resolved: https://github.com/pytorch/executorch/pull/1923
    
    
    ghstack-source-id: 214759729
    @exported-using-ghexport
    
    Differential Revision: [D53536221](https://our.internmc.facebook.com/intern/diff/D53536221/)

--- kernels/portable/cpu/util/padding_util.h
+++ kernels/portable/cpu/util/padding_util.h
@@ -88,5 +88,46 @@ void pad2d(
   }
 }
 
+template <typename CTYPE, typename PaddingIx>
+void pad3d(
+    const PaddingIx& padding_ix,
+    const Tensor& in,
+    Tensor& out,
+    exec_aten::ArrayRef<int64_t> padding) {
+  const CTYPE* const in_data = in.const_data_ptr<CTYPE>();
+  CTYPE* const out_data = out.mutable_data_ptr<CTYPE>();
+
+  const auto dim = in.dim() - 3;
+  const auto outer = getLeadingDims(out, dim);
+  const auto in_depth = in.size(dim);
+  const auto in_height = in.size(dim + 1);
+  const auto in_width = in.size(dim + 2);
+  const auto out_depth = out.size(dim);
+  const auto out_height = out.size(dim + 1);
+  const auto out_width = out.size(dim + 2);
+  const auto pad_left = padding[0];
+  const auto pad_top = padding[2];
+  const auto pad_front = padding[4];
+
+  for (size_t i = 0; i < outer; i++) {
+    size_t out_i_base = i * out_depth * out_height * out_width;
+    size_t in_i_base = i * in_depth * in_height * in_width;
+    for (size_t d = 0; d < out_depth; d++) {
+      size_t out_d_base = out_i_base + d * out_height * out_width;
+      size_t in_d_base =
+          in_i_base + padding_ix(d, in_depth, pad_front) * in_height * in_width;
+      for (size_t h = 0; h < out_height; h++) {
+        size_t out_h_base = out_d_base + h * out_width;
+        size_t in_h_base =
+            in_d_base + padding_ix(h, in_height, pad_top) * in_width;
+        for (size_t w = 0; w < out_width; w++) {
+          out_data[out_h_base + w] =
+              in_data[in_h_base + padding_ix(w, in_width, pad_left)];
+        }
+      }
+    }
+  }
+}
+
 } // namespace executor
 } // namespace torch
