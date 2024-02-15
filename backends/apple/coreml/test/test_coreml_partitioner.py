0deef84134193cf4124f7b60a3338b34addbc121
yifan_shen3 <yifan_shen3@apple.com>
2024-02-14 19:49:54 -0800

    add vit as e2e example

--- backends/apple/coreml/test/test_coreml_partitioner.py
+++ backends/apple/coreml/test/test_coreml_partitioner.py
@@ -46,7 +46,29 @@ class TestCoreMLPartitioner(unittest.TestCase):
             "getitem",
         ]
 
+    def test_vit_skip_conv(self):
+        model = torchvision.models.vit_b_16(weights="IMAGENET1K_V1")
+        model.eval()
+
+        example_inputs = (torch.randn(1, 3, 224, 224),)
+        exir_program_aten = torch.export.export(model, example_inputs)
+        edge_program_manager = executorch.exir.to_edge(exir_program_aten)
+        delegated_program_manager = edge_program_manager.to_backend(
+            CoreMLPartitioner(skip_ops_for_coreml_delegation=["aten.convolution.default"])
+        )
+
+        assert [
+            node.target.__name__
+            for node in delegated_program_manager.exported_program().graph.nodes
+            if node.op == "call_function"
+        ] == [
+            "aten.convolution.default",
+            "executorch_call_delegate",
+            "getitem",
+        ]
+
 
 if __name__ == "__main__":
     test_runner = TestCoreMLPartitioner()
     test_runner.test_add_sub_skip_mm()
+    test_runner.test_vit_skip_conv()
