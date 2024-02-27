7ab5bd203d8345afb19f7b4dccca2f7aa6879ffa
Rob Elliott <robert.elliott@arm.com>
2024-02-27 16:47:41 +0000

    fix for venv check exit code crashing ci
    Signed-off-by: Rob Elliott <robert.elliott@arm.com>

--- examples/arm/setup.sh
+++ examples/arm/setup.sh
@@ -221,8 +221,8 @@ function setup_vela() {
     cd "${root_dir}/ethos-u-vela"
 
     # different command for conda vs venv
-    python3 -c "import sys; sys.exit(0) if (sys.prefix != sys.base_prefix) else sys.exit(1)"
-    if [ $? -eq 0 ]; then
+    VNV=$(python3 -c "import sys; print('venv') if (sys.prefix != sys.base_prefix) else print('not_venv')")
+    if [ ${VNV} == "venv" ]; then
        pip3 install .
     else
        pip3 install . --user
