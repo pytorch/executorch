e61ad429fb849726532161febe9f8f4293720947
Stephen Jia <ssjia@meta.com>
2024-03-04 14:54:34 -0800

    Update on "[ET-VK] Set up Vulkan executor_runner"
    ## Context
    
    This changeset introduces the `vulkan_executor_runner`, which can be used to run the Vulkan Backend in open source. It is the Vulkan equivalent of `executor_runner`s found in other backends, such as XNNPACK and MPS.
    
    Differential Revision: [D54512106](https://our.internmc.facebook.com/intern/diff/D54512106)
    
    [ghstack-poisoned]

--- backends/vulkan/__init__.py
+++ backends/vulkan/__init__.py
@@ -4,10 +4,8 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-# Exposed Partitioners in XNNPACK Package
 from .partitioner.vulkan_partitioner import VulkanPartitioner
 
-# Vulkan Backend
 from .vulkan_preprocess import VulkanBackend
 
 __all__ = [
