bc969fc442815360f79738882a785b8de51b4d45
Jorge Pineda <jorgep31415@meta.com>
2024-03-04 15:03:34 -0800

    [ET-VK] Remove Functions.h/cpp
    In D53982443, OperatorRegistry.h/cpp and Functions.h/cpp were both introduced, as they were split across the PT and ET repos, but now both are in ET.
    
    ## OperatorRegistry.cpp
    Here, we see all our operators. OPERATOR_ENTRY maps from Vulkan Dialect op name to the OpFunction, which have an op-specific name. Note that all OpFunction carry the same function signature.
    
    ## Functions.h/cpp -> Arithmetic.h/cpp
    We don't need another place to see all our operators. They will each reference one ops/impl file, so we group them accordingly in their ops/impl file.
    
    ## Nit
    Also, sort `add_arithmetic_node()` declarations according to their execution order.
    
    Differential Revision: [D54370467](https://our.internmc.facebook.com/intern/diff/D54370467/)
    
    [ghstack-poisoned]

--- backends/vulkan/runtime/graph/ops/Utils.h
+++ backends/vulkan/runtime/graph/ops/Utils.h
@@ -10,21 +10,16 @@
 
 #ifdef USE_VULKAN_API
 
+#include <ATen/native/vulkan/impl/Arithmetic.h>
+
 #include <executorch/backends/vulkan/runtime/graph/ComputeGraph.h>
 
 namespace at {
 namespace native {
 namespace vulkan {
 
-#define DEFINE_OP_FN(name) \
-  ValueRef name(ComputeGraph& graph, const std::vector<ValueRef>& args);
-
-DEFINE_OP_FN(add);
-DEFINE_OP_FN(sub);
-DEFINE_OP_FN(mul);
-DEFINE_OP_FN(div);
-DEFINE_OP_FN(floor_div);
-DEFINE_OP_FN(pow);
+#define DECLARE_OP_FN(function) \
+  ValueRef function(ComputeGraph& graph, const std::vector<ValueRef>& args);
 
 } // namespace vulkan
 } // namespace native
