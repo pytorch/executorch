e10a70bfee57fe83d49c90f9b603b661d57da37d
Manuel Candales <mcandales@meta.com>
2024-03-04 11:26:21 -0800

    Add embedding_byte.dtype to enable output dtype be different than scales/zp dtype (#2236)
    Summary: Pull Request resolved: https://github.com/pytorch/executorch/pull/2236
    
    Reviewed By: mikekgfb
    
    Differential Revision: D54454644

--- exir/passes/_quant_patterns_and_replacements.py
+++ exir/passes/_quant_patterns_and_replacements.py
@@ -31,6 +31,11 @@ quantized_decomposed_lib.define(
 )
 
 quantized_decomposed_lib.define(
+    "embedding_byte.dtype(Tensor weight, Tensor weight_scales, Tensor? weight_zero_points, "
+    "int weight_quant_min, int weight_quant_max, Tensor indices, *, ScalarType? dtype=None) -> Tensor",
+)
+
+quantized_decomposed_lib.define(
     "mixed_mm(Tensor input, Tensor weight, Tensor weight_scales, Tensor? weight_zero_points) -> Tensor",
 )
 
@@ -523,6 +528,48 @@ def _get_embedding_ops_patterns_and_replacements() -> (
             )
             return out
 
+        @bind_pattern_to_op(quantized_decomposed_lib, "embedding_byte.dtype")
+        def pattern_with_dtype(
+            weight,
+            weight_scales,
+            weight_zero_points,
+            weight_quant_min,
+            weight_quant_max,
+            indicies,
+            dtype,
+        ):
+            weight = torch.ops.quantized_decomposed.dequantize_per_channel.default(
+                weight,
+                weight_scales,
+                weight_zero_points,
+                0,
+                weight_quant_min,
+                weight_quant_max,
+                torch.uint8,
+            )
+            out = torch.ops.aten.embedding.default(weight, indicies).to(dtype)
+            return out
+
+        def replacement_with_dtype(
+            weight,
+            weight_scales,
+            weight_zero_points,
+            weight_quant_min,
+            weight_quant_max,
+            indicies,
+            dtype,
+        ):
+            out = torch.ops.quantized_decomposed.embedding_byte.dtype(
+                weight,
+                weight_scales,
+                weight_zero_points,
+                weight_quant_min,
+                weight_quant_max,
+                indicies,
+                dtype=dtype,
+            )
+            return out
+
         return [
             (
                 _trace_and_lower_to_edge_ops(pattern),
@@ -532,6 +579,11 @@ def _get_embedding_ops_patterns_and_replacements() -> (
             (
                 _trace_and_lower_to_edge_ops(pattern_with_padding_idx),
                 _trace_and_lower_to_edge_ops(replacement_with_padding_idx),
+                [],
+            ),
+            (
+                _trace_and_lower_to_edge_ops(pattern_with_dtype),
+                _trace_and_lower_to_edge_ops(replacement_with_dtype),
                 [],
             ),
         ]
