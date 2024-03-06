3b0dd33ac5daea6ab13a966765bbba5cb51e44d5
Zhengxu Chen <zhxchen17@fb.com>
2024-03-06 12:22:11 -0800

    Revert D54553809: Serialize storage_offset in sym int.
    Differential Revision:
    D54553809
    
    Original commit changeset: cf6025f6995e
    
    Original Phabricator Diff: D54553809
    
    fbshipit-source-id: 295b2d9476aae5943b80b3af7e2f089a8406b92a

--- exir/serde/export_serialize.py
+++ exir/serde/export_serialize.py
@@ -220,7 +220,7 @@ def serialize_tensor_meta(t: torch.Tensor) -> TensorMeta:
         requires_grad=t.requires_grad,
         device=Device(type=t.device.type, index=t.device.index),
         strides=[serialize_sym_int(s) for s in t.stride()],
-        storage_offset=serialize_sym_int(0),
+        storage_offset=0,
         layout=_TORCH_TO_SERIALIZE_LAYOUT[t.layout],
     )
 
