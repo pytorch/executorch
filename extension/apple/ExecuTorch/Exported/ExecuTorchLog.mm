7bbb6d578352f4c7e96ba458ca16c11642d888db
Anthony Shoumikhin <shoumikhin@meta.com>
2024-03-08 22:44:33 -0800

    Dump logs to syslog.
    Summary: .
    
    Differential Revision: D54711001

--- extension/apple/ExecuTorch/Exported/ExecuTorchLog.mm
+++ extension/apple/ExecuTorch/Exported/ExecuTorchLog.mm
@@ -8,6 +8,8 @@
 
 #import "ExecuTorchLog.h"
 
+#import <os/log.h>
+
 #import <executorch/runtime/platform/platform.h>
 
 @interface ExecuTorchLog ()
@@ -29,7 +31,7 @@
   static ExecuTorchLog *sharedLog;
   static dispatch_once_t onceToken;
   dispatch_once(&onceToken, ^{
-    sharedLog = [[self alloc] init];
+    sharedLog = [self new];
     sharedLog->_sinks = [NSHashTable weakObjectsHashTable];
     sharedLog->_queue = dispatch_queue_create("org.pytorch.executorch.log",
                                               DISPATCH_QUEUE_SERIAL);
@@ -78,9 +80,45 @@ void et_pal_emit_log_message(et_timestamp_t timestamp,
                              size_t line,
                              const char *__nonnull message,
                              __ET_UNUSED size_t length) {
+  NSTimeInterval timeInterval = timestamp / 1000000000.0;
+  NSUInteger totalSeconds = (NSUInteger)timeInterval;
+  NSUInteger hours = (totalSeconds / 3600) % 24;
+  NSUInteger minutes = (totalSeconds / 60) % 60;
+  NSUInteger seconds = totalSeconds % 60;
+  NSUInteger microseconds = (timestamp - totalSeconds) * 1000000;
+  NSString *formattedMessage = [NSString
+      stringWithFormat:@"%c %02lu:%02lu:%02lu.%06lu executorch:%s:%zu] %s",
+                       (char)level,
+                       hours,
+                       minutes,
+                       seconds,
+                       microseconds,
+                       filename,
+                       line,
+                       message];
+  os_log_type_t logType = OS_LOG_TYPE_DEFAULT;
+  switch (level) {
+  case kDebug:
+    logType = OS_LOG_TYPE_DEBUG;
+    break;
+  case kInfo:
+    logType = OS_LOG_TYPE_INFO;
+    break;
+  case kError:
+    logType = OS_LOG_TYPE_ERROR;
+    break;
+  case kFatal:
+    logType = OS_LOG_TYPE_FAULT;
+    break;
+  default:
+    logType = OS_LOG_TYPE_DEFAULT;
+    break;
+  }
+  os_log_with_type(OS_LOG_DEFAULT, logType, "%{public}@", formattedMessage);
+
   [ExecuTorchLog.sharedLog
       logWithLevel:(ExecuTorchLogLevel)level
-         timestamp:timestamp / 1000000000.0
+         timestamp:timeInterval
           filename:[NSString stringWithUTF8String:filename]
               line:(NSUInteger)line
            message:[NSString stringWithUTF8String:message]];
