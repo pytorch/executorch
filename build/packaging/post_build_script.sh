0d8214d6c1093a18ea276bc7a415e8bbe8c9606a
Huy Do <huydhn@gmail.com>
2024-03-11 20:15:55 -0700

    Add nightly wheel build workflows for Linux and macOS

---
+++ build/packaging/post_build_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+set -eux
+
+echo "This script is run after building ExecuTorch binaries"
