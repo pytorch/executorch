2d292813f99e35745c80643b0a817f6303c8ebd5
Jack Khuu <jackkhuu@meta.com>
2024-03-13 14:35:06 -0700

    Adding lm-eval 0.3.0 as an install req in llama2 (#2394)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/2394
    
    Adding install reqs for enabling model evaluation in llama2.
    
    The versions are intentional due to conflicts with ET install reqs
    
    pip install datasets==2.14.0
    pip install lm-eval==0.3.0
    
    bypass-github-export-checks
    bypass-github-pytorch-ci-checks
    bypass-github-executorch-ci-checks
    
    Reviewed By: jerryzh168
    
    Differential Revision: D54839623
    
    fbshipit-source-id: 0ccf588172a707de6c16c693179946b64c428465

--- examples/models/llama2/install_requirements.sh
+++ examples/models/llama2/install_requirements.sh
@@ -9,3 +9,14 @@
 # Install sentencepiece for llama tokenizer
 pip install snakeviz sentencepiece
 pip install torchao-nightly
+
+# Install datasets for HuggingFace dataloader
+# v2.14.0 is intentional to force lm-eval v0.3.0 compatibility
+pip install datasets==2.14.0
+
+# Install lm-eval for Model Evaluation with lm-evalution-harness
+# v0.3.0 is intentional
+pip install lm-eval==0.3.
+
+# Call the install helper for further setup
+python examples/models/llama2/install_requirement_helper.py
