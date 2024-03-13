59c9287c8dad7d3b28f5420e0c0b97f942fa64a5
gjcomer <gjcomer@meta.com>
2024-03-12 19:35:56 -0700

    Update kernels for non-fatal (part 14)
    Summary:
    Update kernels and tests starting with 'p'.
    
    Note to reviewers: Because the changeset for non-fatal touches so many files, I've split into a number of diffs. The tests will not all pass until the final diff, given that non-fatal tests are switched on in the first diff but tests are not updated until the respective later diff. See revision 6.6 of D53251417 to observe the full change-set passing CI.
    
    Differential Revision: D54182362

--- kernels/test/op_pixel_shuffle_test.cpp
+++ kernels/test/op_pixel_shuffle_test.cpp
@@ -22,42 +22,47 @@ using exec_aten::Tensor;
 using torch::executor::testing::SupportedFeatures;
 using torch::executor::testing::TensorFactory;
 
-Tensor&
-op_pixel_shuffle_out(const Tensor& self, int64_t upscale_factor, Tensor& out) {
-  exec_aten::RuntimeContext context{};
-  return torch::executor::aten::pixel_shuffle_outf(
-      context, self, upscale_factor, out);
-}
+class OpPixelShuffleOutTest : public OperatorTest {
+ protected:
+  Tensor& op_pixel_shuffle_out(
+      const Tensor& self,
+      int64_t upscale_factor,
+      Tensor& out) {
+    return torch::executor::aten::pixel_shuffle_outf(
+        context_, self, upscale_factor, out);
+  }
+
+  template <ScalarType DTYPE_IN>
+  void test_pixel_shuffle() {
+    TensorFactory<DTYPE_IN> tf_in;
+
+    const std::vector<int32_t> sizes = {1, 4, 2, 2};
+    const std::vector<int32_t> out_sizes = {1, 1, 4, 4};
+
+    // Destination for the pixel_shuffle.
+    Tensor out = tf_in.zeros(out_sizes);
+
+    op_pixel_shuffle_out(
+        tf_in.make(
+            sizes, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}),
+        2,
+        out);
+    EXPECT_TENSOR_EQ(
+        out,
+        // Pixel shuffle distributes channels amongst the spatial dimensions.
+        tf_in.make(
+            out_sizes, {0, 4, 1, 5, 8, 12, 9, 13, 2, 6, 3, 7, 10, 14, 11, 15}));
+  }
+};
 
 //
 // Correctness Tests
 //
 
-template <ScalarType DTYPE_IN>
-void test_pixel_shuffle() {
-  TensorFactory<DTYPE_IN> tf_in;
-
-  const std::vector<int32_t> sizes = {1, 4, 2, 2};
-  const std::vector<int32_t> out_sizes = {1, 1, 4, 4};
-
-  // Destination for the pixel_shuffle.
-  Tensor out = tf_in.zeros(out_sizes);
-
-  op_pixel_shuffle_out(
-      tf_in.make(sizes, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}),
-      2,
-      out);
-  EXPECT_TENSOR_EQ(
-      out,
-      // Pixel shuffle distributes channels amongst the spatial dimensions.
-      tf_in.make(
-          out_sizes, {0, 4, 1, 5, 8, 12, 9, 13, 2, 6, 3, 7, 10, 14, 11, 15}));
-}
-
 /**
  * Uses the function templates above to test all input dtypes.
  */
-TEST(OpPixelShuffleOutKernelTest, AllRealDtypesSupported) {
+TEST_F(OpPixelShuffleOutTest, AllRealDtypesSupported) {
 #define ENUMERATE_TEST_ENTRY(ctype, dtype) \
   test_pixel_shuffle<ScalarType::dtype>();
 
@@ -66,7 +71,7 @@ TEST(OpPixelShuffleOutKernelTest, AllRealDtypesSupported) {
 #undef ENUMERATE_TEST_ENTRY
 }
 
-TEST(OpPixelShuffleOutKernelTest, LargerInputRank) {
+TEST_F(OpPixelShuffleOutTest, LargerInputRank) {
   TensorFactory<ScalarType::Int> tf;
 
   // Pixel shuffle allows a 4D (or higher) input tensor, make sure the extra
@@ -81,7 +86,7 @@ TEST(OpPixelShuffleOutKernelTest, LargerInputRank) {
 }
 
 // Mismatched shape tests.
-TEST(OpPixelShuffleOutKernelTest, InvalidInputChannelsDies) {
+TEST_F(OpPixelShuffleOutTest, InvalidInputChannelsDies) {
   TensorFactory<ScalarType::Int> tf;
 
   // Input tensors with invalid shapes. 7 is not divisible by upsample_factor
@@ -91,10 +96,10 @@ TEST(OpPixelShuffleOutKernelTest, InvalidInputChannelsDies) {
   Tensor out = tf.zeros(/*sizes=*/{1, 1, 8, 8});
 
   // Using the wrong input shape should exit with an error code.
-  ET_EXPECT_KERNEL_FAILURE(op_pixel_shuffle_out(a, 2, out));
+  ET_EXPECT_KERNEL_FAILURE(context_, op_pixel_shuffle_out(a, 2, out));
 }
 
-TEST(OpPixelShuffleOutKernelTest, WrongInputRankDies) {
+TEST_F(OpPixelShuffleOutTest, WrongInputRankDies) {
   TensorFactory<ScalarType::Int> tf;
 
   // Pixel shuffle requires a 4D input tensor.
@@ -105,10 +110,10 @@ TEST(OpPixelShuffleOutKernelTest, WrongInputRankDies) {
   Tensor out = tf.zeros(/*sizes=*/{1, 2});
 
   // Using the wrong input shape should exit with an error code.
-  ET_EXPECT_KERNEL_FAILURE(op_pixel_shuffle_out(a, 2, out));
+  ET_EXPECT_KERNEL_FAILURE(context_, op_pixel_shuffle_out(a, 2, out));
 }
 
-TEST(OpPixelShuffleOutKernelTest, DifferentDtypeDies) {
+TEST_F(OpPixelShuffleOutTest, DifferentDtypeDies) {
   TensorFactory<ScalarType::Int> tf;
   TensorFactory<ScalarType::Float> tf_float;
 
@@ -118,13 +123,13 @@ TEST(OpPixelShuffleOutKernelTest, DifferentDtypeDies) {
   Tensor out = tf_float.zeros(/*sizes=*/{1, 2, 12, 12});
 
   // Using the wrong output shape should exit with an error code.
-  ET_EXPECT_KERNEL_FAILURE(op_pixel_shuffle_out(a, 3, out));
+  ET_EXPECT_KERNEL_FAILURE(context_, op_pixel_shuffle_out(a, 3, out));
 }
 
-TEST(OpPixelShuffleOutKernelTest, NegativeUpscaleFactorDies) {
+TEST_F(OpPixelShuffleOutTest, NegativeUpscaleFactorDies) {
   TensorFactory<ScalarType::Int> tf;
   Tensor a = tf.ones(/*sizes=*/{1, 18, 4, 4});
   Tensor out = tf.zeros(/*sizes=*/{1, 2, 12, 12});
   // Using a negative upscale factor should exit with an error code.
-  ET_EXPECT_KERNEL_FAILURE(op_pixel_shuffle_out(a, -3, out));
+  ET_EXPECT_KERNEL_FAILURE(context_, op_pixel_shuffle_out(a, -3, out));
 }
