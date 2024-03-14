64e4ac0f549818426a78fc0ac62dd9ebac5348b2
Anthony Shoumikhin <shoumikhin@meta.com>
2024-03-13 18:29:32 -0700

    Fix linter for Core ML code. (#2422)
    Summary:
    
    .
    
    Differential Revision: D54883427

--- backends/apple/coreml/quantizer/coreml_quantizer.py
+++ backends/apple/coreml/quantizer/coreml_quantizer.py
@@ -2,4 +2,6 @@
 #
 # Please refer to the license found in the LICENSE file in the root directory of the source tree.
 
-from coremltools.optimize.torch.quantization._coreml_quantizer import CoreMLQuantizer
+from coremltools.optimize.torch.quantization._coreml_quantizer import (  # noqa: F401
+    CoreMLQuantizer,
+)
