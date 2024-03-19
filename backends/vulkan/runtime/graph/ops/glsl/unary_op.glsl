eb793597c0d2d8b5f5c8f5b73a7202016b271db2
Jorge Pineda <jorgep31415@meta.com>
2024-03-19 15:25:02 -0700

    [ET-VK][EZ] Return if unary_op is out of bounds
    Pull Request resolved: https://github.com/pytorch/executorch/pull/2520
    
    Eliminate additional computation when we go out of bounds, which can occur when
    1. global work group size is not a perfect multiple of local work group size, and/or
    2. dynamic shapes are used to reshape the logical tensor data.
    
    ## Aside on sizes and extents
    
    This is the extension of me struggling on the `max_pool2d` implementation and hence looking closer at how `gpu_sizes_ubo()` and `extents_ubo()` differ. I'm writing this summary to explain this to future me when I inevitably forget. This knowledge can be obtained by studying [`Tensor.*`](https://github.com/pytorch/pytorch/blob/cceabe873f11c6611f627a3bb0055994952ec6b8/aten/src/ATen/native/vulkan/api/Tensor.cpp).
    
    If our tensor has
    - `cpu_sizes`: (N, C, H, W)
    
    then
    - `gpu_sizes`: (W, H, C, N) but each size is aligned up to a multiple of 4.
    - `extents`: Size of the actual image texture, i.e., gpu_sizes but merging C,N and packing the corresponding dimension:
    
    So we obtain:
    1. WIDTH_PACKED => `extents`: (W / 4, H, C*N)
    2. HEIGHT_PACKED => `extents`: (W, H / 4, C*N)
    3. CHANNELS_PACKED => `extents`: (W, H, C*N / 4)
    
    Hence,
    - for texture positions, use `extents`,
    - for logical coordinates, use `gpu_sizes`.
    
    Differential Revision: [D55097275](https://our.internmc.facebook.com/intern/diff/D55097275/)
    ghstack-source-id: 219292658

--- backends/vulkan/runtime/graph/ops/glsl/unary_op.glsl
+++ backends/vulkan/runtime/graph/ops/glsl/unary_op.glsl
@@ -17,10 +17,10 @@ layout(std430) buffer;
 layout(set = 0, binding = 0, ${IMAGE_FORMAT[DTYPE]}) uniform PRECISION restrict writeonly ${IMAGE_T[NDIM][DTYPE]} image_out;
 layout(set = 0, binding = 1) uniform PRECISION sampler3D image_in;
 
-layout(set = 0, binding = 2) uniform PRECISION restrict OutSizes {
-  ivec4 data;
+layout(set = 0, binding = 2) uniform PRECISION restrict OutExtents {
+  uvec4 data;
 }
-out_sizes;
+out_extents;
 
 layout(set = 0, binding = 3) uniform PRECISION restrict Min {
   float data;
@@ -36,6 +36,10 @@ layout(local_size_x_id = 0, local_size_y_id = 1, local_size_z_id = 2) in;
 
 void main() {
   const ivec3 pos = ivec3(gl_GlobalInvocationID);
+
+  if (any(greaterThanEqual(pos, out_extents.data.xyz))) {
+    return;
+  }
 
   vec4 in_texel = texelFetch(image_in, pos, 0);
   imageStore(image_out, pos, OP(in_texel, minimum.data, maximum.data));
