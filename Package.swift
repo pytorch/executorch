6ad7e27f843f588ebd247e90fc12ac63880a66c6
Anthony Shoumikhin <shoumikhin@meta.com>
2024-03-19 17:52:43 -0700

    Update Swift PM haches. (#2525)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/2525
    
    bypass-github-export-checks
    
    Reviewed By: huydhn
    
    Differential Revision: D55104813
    
    fbshipit-source-id: 2f1cf810afd2336ec2b277141b14d2405a5ca049

--- Package.swift
+++ Package.swift
@@ -11,11 +11,11 @@ import PackageDescription
 
 let url = "https://ossci-ios.s3.amazonaws.com/executorch"
 let version = "0.1.0"
-let coreml_sha256 = ""
-let executorch_sha256 = ""
-let mps_sha256 = ""
-let portable_sha256 = ""
-let xnnpack_sha256 = ""
+let coreml_sha256 = "1d2b8d2a5805a699eb39f347977894d3af5dfa763b298b926016550e9ffefda5"
+let executorch_sha256 = "39f19740a7c656d972e6082bae49583a6d4cc6396dea6ace2e4193688cef6225"
+let mps_sha256 = "866739b76baec70e603d331ff34ff9f028202fef69161f63a35d2e8a0cf502e9"
+let portable_sha256 = "6f761c0ae5651002e321bc6320604476ba0210f9383e535a2905cc1a74be55a3"
+let xnnpack_sha256 = "ef2cb2145a466a0a9e32489497c7f4880e4b582cea3883158b7ae24427d8ae7a"
 
 struct Framework {
   let name: String
