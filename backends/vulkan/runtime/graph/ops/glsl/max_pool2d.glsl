ca8c03b98f0a0c3b042570b1f003017aad123c7e
Jorge Pineda <jorgep31415@meta.com>
2024-03-20 22:57:32 -0700

    aten.max_pool2d_with_indices (#2547)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/2547
    
    ## The Operator
    An `nn.Module` invocation of `torch.nn.MaxPool2d()` is represented as `aten.max_pool2d_with_indices.default` in the Edge Dialect, indpendent of `use_indices = True/False`.
    ```
    # Return: (Tensor output, Tensor indices)
    - func: max_pool2d_with_indices(Tensor self, int[2] kernel_size, int[2] stride=[], int[2] padding=0, int[2] dilation=1, bool ceil_mode=False) -> (Tensor, Tensor)
    ```
    
    This is different from PT-VK where `torch.nn.MaxPool2d()` was represented as `aten.max_pool2d.default`.
    ```
    - func: max_pool2d(Tensor self, int[2] kernel_size, int[2] stride=[], int[2] padding=0, int[2] dilation=1, bool ceil_mode=False) -> Tensor
    ```
    
    The difference is we now return an additional tensor for the max indices. Still, much of the core logic is taken from [`max_pool2d.glsl`](https://github.com/pytorch/pytorch/blob/cceabe873f11c6611f627a3bb0055994952ec6b8/aten/src/ATen/native/vulkan/glsl/max_pool2d.glsl) and [`Pool.cpp`](https://github.com/pytorch/pytorch/blob/cceabe873f11c6611f627a3bb0055994952ec6b8/aten/src/ATen/native/vulkan/ops/Pool.cpp).
    
    We provide only a `CHANNELS_PACKED` implementation.
    
    ## The Smoke Test
    Given any input and kernel sizes, we fill the input tensor with increasing values, e.g.,
    ```
    tensor([[[10., 11., 12., 13., 14., 15.],
             [16., 17., 18., 19., 20., 21.],
             [22., 23., 24., 25., 26., 27.],
             [28., 29., 30., 31., 32., 33.]]])
    ```
    With this setup, the max number for each pool is always in the lower-right.
    
    We use the kernel size to compute the size of the lower-right block and verify that
    1. the output tensor values match the lower-right block values, and
    2. the index tensor values match the lower-right block indices.
    ```
    tensor([[[ 18., 19., 20., 21.],
             [ 24., 25., 26., 27.],
             [ 30., 31., 32., 33.]]])
    tensor([[[ 8,  9, 10, 11],
             [14, 15, 16, 17],
             [20, 21, 22, 23]]])
    ```
    ghstack-source-id: 219506264
    exported-using-ghexport
    bypass-github-export-checks
    
    Reviewed By: SS-JIA
    
    Differential Revision: D54961929
    
    fbshipit-source-id: 277a629e973dd72d49fc059ace2d7a9a9388ab36

---
+++ backends/vulkan/runtime/graph/ops/glsl/max_pool2d.glsl
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#version 450 core
+
+#define PRECISION ${PRECISION}
+#define FLT_MIN -3.402823466e+38
+
+#include "indexing_utils.h"
+
+layout(std430) buffer;
+
+layout(set = 0, binding = 0, ${IMAGE_FORMAT[DTYPE]}) uniform PRECISION restrict writeonly ${IMAGE_T[NDIM][DTYPE]} image_out;
+layout(set = 0, binding = 1, ${IMAGE_FORMAT["int"]}) uniform PRECISION restrict writeonly ${IMAGE_T[NDIM]["int"]} image_idx;
+layout(set = 0, binding = 2) uniform PRECISION sampler3D image_in;
+
+layout(set = 0, binding = 3) uniform PRECISION restrict OutExtents {
+  uvec4 data;
+}
+out_extents;
+
+layout(set = 0, binding = 4) uniform PRECISION restrict InExtents {
+  uvec4 data;
+}
+in_extents;
+
+layout(set = 0, binding = 5) uniform PRECISION restrict Params {
+  ivec2 kernel;
+  ivec2 stride;
+  ivec2 padding;
+  ivec2 dilation;
+}
+params;
+
+layout(local_size_x_id = 0, local_size_y_id = 1, local_size_z_id = 2) in;
+
+void main() {
+  const ivec3 pos = ivec3(gl_GlobalInvocationID);
+
+  if (any(greaterThanEqual(pos, out_extents.data.xyz))) {
+    return;
+  }
+
+  const ivec2 ipos = pos.xy * params.stride - params.padding;
+
+  const ivec2 start = ipos;
+  const ivec2 end = ipos + params.kernel * params.dilation;
+
+  vec4 out_texel = vec4(FLT_MIN);
+  ivec4 idx_texel = ivec4(0);
+
+  for (int y = start.y; y < end.y; y += params.dilation.y) {
+    for (int x = start.x; x < end.x; x += params.dilation.x) {
+      if ((x >= 0 && x < in_extents.data.x) && (y >= 0 && y < in_extents.data.y)) {
+        const vec4 cur_texel = texelFetch(image_in, ivec3(x, y, pos.z), 0);
+
+        // Set idx if value is greatest in the pool; else, keep the existing idx.
+        ivec4 cur_idx = ivec4(x + int(in_extents.data.x) * y);
+        ivec4 mask = ivec4(greaterThan(cur_texel, out_texel));
+        idx_texel = ivec4(mix(idx_texel, cur_idx, mask));
+
+        out_texel = max(cur_texel, out_texel);
+      }
+      else {
+        out_texel = max(vec4(FLT_MIN), out_texel);
+      }
+    }
+  }
+
+  imageStore(image_out, pos, out_texel);
+  imageStore(image_idx, pos, idx_texel);
+}
