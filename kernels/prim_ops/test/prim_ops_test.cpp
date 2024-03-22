307d914f13a2291cdbb68d35a2e2894f6d434c50
Scott Roy <scroy@meta.com>
2024-03-22 08:57:38 -0700

    create et_view primop (#2553)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/2553
    
    Implements a new view prim op kernel.
    
    Reviewed By: larryliu0820
    
    Differential Revision: D55099757

--- kernels/prim_ops/test/prim_ops_test.cpp
+++ kernels/prim_ops/test/prim_ops_test.cpp
@@ -17,6 +17,8 @@
 #include <executorch/runtime/kernel/operator_registry.h>
 #include <executorch/runtime/platform/runtime.h>
 #include <executorch/test/utils/DeathTest.h>
+#include <cstdint>
+#include <cstdio>
 
 using exec_aten::SizesType;
 using torch::executor::Error;
@@ -270,6 +272,181 @@ TEST_F(RegisterPrimOpsTest, LocalScalarDenseReturnsCorrectValue) {
 
   int64_t expected = 1;
   EXPECT_EQ(stack[1]->toInt(), expected);
+}
+
+TEST_F(RegisterPrimOpsTest, TestETView) {
+  EXPECT_TRUE(hasOpsFn("executorch_prim::et_view.default"));
+
+  testing::TensorFactory<ScalarType::Int> tf;
+
+  // ***************************************************************************
+  // Make self for tests
+  // ***************************************************************************
+  auto self = tf.make({3, 2}, {1, 2, 3, 4, 5, 6});
+  auto self_evalue = EValue(self);
+
+  // ***************************************************************************
+  // Make size for tests
+  // ***************************************************************************
+  int64_t size[3] = {1, 3, -1};
+  EValue size_as_evals[3] = {EValue(size[0]), EValue(size[1]), EValue(size[2])};
+  EValue* size_wrapped_vals[3] = {
+      &size_as_evals[0], &size_as_evals[1], &size_as_evals[2]};
+  int64_t size_unwrapped_vals[3] = {0, 0, 0};
+  EValue size_int_list_evalue = EValue(
+      BoxedEvalueList<int64_t>(size_wrapped_vals, size_unwrapped_vals, 3));
+
+  int64_t bad_size1[3] = {-1, 3, -1}; // two inferred dimensions
+  EValue bad_size_as_evals1[3] = {
+      EValue(bad_size1[0]), EValue(bad_size1[1]), EValue(bad_size1[2])};
+  EValue* bad_size_wrapped_vals1[3] = {
+      &bad_size_as_evals1[0], &bad_size_as_evals1[1], &bad_size_as_evals1[2]};
+  int64_t bad_size_unwrapped_vals1[3] = {0, 0, 0};
+  EValue bad_size_int_list_evalue1 = EValue(BoxedEvalueList<int64_t>(
+      bad_size_wrapped_vals1, bad_size_unwrapped_vals1, 3));
+
+  int64_t bad_size2[3] = {-2, -3, 1}; // negative size not supported
+  EValue bad_size_as_evals2[3] = {
+      EValue(bad_size2[0]), EValue(bad_size2[1]), EValue(bad_size2[2])};
+  EValue* bad_size_wrapped_vals2[3] = {
+      &bad_size_as_evals2[0], &bad_size_as_evals2[1], &bad_size_as_evals2[2]};
+  int64_t bad_size_unwrapped_vals2[3] = {0, 0, 0};
+  EValue bad_size_int_list_evalue2 = EValue(BoxedEvalueList<int64_t>(
+      bad_size_wrapped_vals2, bad_size_unwrapped_vals2, 3));
+
+  // ***************************************************************************
+  // Make outs for tests
+  // ***************************************************************************
+  constexpr int N_GOOD_OUTS = 2;
+  Tensor good_outs[N_GOOD_OUTS] = {
+      tf.ones({1, 3, 2}), // correct size with nullptr
+      tf.ones({1, 3, 2}), // correct size with self data_ptr
+  };
+  internal::reset_data_ptr(good_outs[0]);
+  ET_CHECK(
+      internal::set_tensor_data(
+          good_outs[1], self.mutable_data_ptr(), good_outs[1].nbytes()) ==
+      Error::Ok);
+  EValue good_out_evalues[N_GOOD_OUTS] = {
+      EValue(good_outs[0]), EValue(good_outs[1])};
+
+  // bad outs expect death
+  constexpr int N_BAD_OUTS = 3;
+  Tensor bad_outs[N_BAD_OUTS] = {
+      tf.ones({1, 3, 2, 1}), // wrong rank
+      tf.ones({1, 3, 3}), // wrong size
+      tf.ones({1, 3, 2}) // occupied data_ptr
+  };
+  EValue bad_out_evalues[N_BAD_OUTS] = {
+      EValue(bad_outs[0]), EValue(bad_outs[1]), EValue(bad_outs[2])};
+
+  // ***************************************************************************
+  // Run tests
+  // ***************************************************************************
+
+  constexpr int N_BAD_STACKS = N_BAD_OUTS + 2;
+  EValue* bad_stacks[N_BAD_STACKS][3] = {
+      // Bad out stacks
+      {&self_evalue, &size_int_list_evalue, &bad_out_evalues[0]},
+      {&self_evalue, &size_int_list_evalue, &bad_out_evalues[1]},
+      {&self_evalue, &size_int_list_evalue, &bad_out_evalues[2]},
+      // Bad size stacks
+      {&self_evalue, &bad_size_int_list_evalue1, &good_out_evalues[0]},
+      {&self_evalue, &bad_size_int_list_evalue2, &good_out_evalues[0]}};
+
+  // Bad stacks expect death
+  for (int i = 0; i < N_BAD_STACKS; i++) {
+    ET_EXPECT_DEATH(
+        getOpsFn("executorch_prim::et_view.default")(context, bad_stacks[i]),
+        "");
+  }
+
+  constexpr int N_GOOD_STACKS = N_GOOD_OUTS;
+  EValue* good_out_stacks[N_GOOD_STACKS][3] = {
+      {&self_evalue, &size_int_list_evalue, &good_out_evalues[0]},
+      {&self_evalue, &size_int_list_evalue, &good_out_evalues[1]}};
+
+  // Good outs expect no death and correct output
+  for (int i = 0; i < N_GOOD_STACKS; i++) {
+    getOpsFn("executorch_prim::et_view.default")(context, good_out_stacks[i]);
+    EXPECT_TENSOR_EQ(good_outs[i], tf.make({1, 3, 2}, {1, 2, 3, 4, 5, 6}));
+    EXPECT_EQ(good_outs[i].const_data_ptr(), self.const_data_ptr());
+  }
+}
+
+TEST_F(RegisterPrimOpsTest, TestETViewDynamic) {
+  testing::TensorFactory<ScalarType::Int> tf;
+
+  auto self = tf.make({3, 1}, {1, 2, 3});
+  auto self_evalue = EValue(self);
+
+  int64_t size[3] = {1, 3, -1}; // inferred size should be {1, 3, 1}
+  // Construct the size as an EValue int_list
+  EValue size_as_evals[3] = {EValue(size[0]), EValue(size[1]), EValue(size[2])};
+  EValue* size_wrapped_vals[3] = {
+      &size_as_evals[0], &size_as_evals[1], &size_as_evals[2]};
+  int64_t size_unwrapped_vals[3] = {0, 0, 0};
+  EValue size_int_list_evalue = EValue(
+      BoxedEvalueList<int64_t>(size_wrapped_vals, size_unwrapped_vals, 3));
+
+#ifdef USE_ATEN_LIB
+  // ATen mode tensors don't need dynamism specification.
+  auto out = tf.make({3, 2, 1}, {0, 0, 0, 0, 0, 0});
+#else
+  auto out = tf.make(
+      {3, 2, 1}, {0, 0, 0, 0, 0, 0}, {}, TensorShapeDynamism::DYNAMIC_BOUND);
+#endif
+
+  internal::reset_data_ptr(out);
+  EValue out_evalue = EValue(out);
+
+  EValue* stack[3] = {&self_evalue, &size_int_list_evalue, &out_evalue};
+
+  getOpsFn("executorch_prim::et_view.default")(context, stack);
+
+  EXPECT_TENSOR_EQ(out, tf.make({1, 3, 1}, {1, 2, 3}));
+  EXPECT_EQ(out.const_data_ptr(), self.const_data_ptr());
+}
+
+TEST_F(RegisterPrimOpsTest, TestETViewEmpty) {
+  testing::TensorFactory<ScalarType::Int> tf;
+
+  auto self = tf.make({3, 1, 0}, {});
+  auto self_evalue = EValue(self);
+  EXPECT_EQ(self.const_data_ptr(), nullptr); // empty tensor has null data
+
+  // Construct the sizes
+  int64_t size[3] = {3, 1, -1};
+  EValue size_as_evals[3] = {EValue(size[0]), EValue(size[1]), EValue(size[2])};
+  EValue* size_wrapped_vals[3] = {
+      &size_as_evals[0], &size_as_evals[1], &size_as_evals[2]};
+  int64_t size_unwrapped_vals[3] = {0, 0, 0};
+  EValue size_int_list_evalue = EValue(
+      BoxedEvalueList<int64_t>(size_wrapped_vals, size_unwrapped_vals, 3));
+
+  int64_t bad_size[3] = {0, 1, -1}; // bad size: cannot infer with 0
+  EValue bad_size_as_evals[3] = {
+      EValue(bad_size[0]), EValue(bad_size[1]), EValue(bad_size[2])};
+  EValue* bad_size_wrapped_vals[3] = {
+      &bad_size_as_evals[0], &bad_size_as_evals[1], &bad_size_as_evals[2]};
+  int64_t bad_size_unwrapped_vals[3] = {0, 0, 0};
+  EValue bad_size_int_list_evalue = EValue(BoxedEvalueList<int64_t>(
+      bad_size_wrapped_vals, bad_size_unwrapped_vals, 3));
+
+  auto out = tf.make({3, 1, 0}, {}, {});
+  EValue out_evalue = EValue(out);
+  EXPECT_EQ(out.const_data_ptr(), nullptr);
+
+  // good size test
+  EValue* stack[3] = {&self_evalue, &size_int_list_evalue, &out_evalue};
+  getOpsFn("executorch_prim::et_view.default")(context, stack);
+  EXPECT_TENSOR_EQ(out, tf.make({3, 1, 0}, {}));
+  EXPECT_EQ(out.const_data_ptr(), self.const_data_ptr());
+
+  // bad size test
+  EValue* bad_stack[3] = {&self_evalue, &bad_size_int_list_evalue, &out_evalue};
+  ET_EXPECT_DEATH(
+      getOpsFn("executorch_prim::et_view.default")(context, bad_stack), "");
 }
 
 } // namespace executor
