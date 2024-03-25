cca63c44f9a8c8f7c5d21cba1309ed7f897302f5
shewu-quic <quic_shewu@quicinc.com>
2024-03-25 01:39:44 -0700

    Qualcomm AI Engine Direct - Enable zero copy feature (#2531)
    Summary:
    - Add argument "shared_buffer" into compiler_spec, qnn_executor_runner and test scripts
      -  Actually, shared_buffer should be a runtime option since user are responsible to allocate memory for tensors on device. But it seems to have no way to set the runtime option to QnnBackend. Therefore, we put it to compile_spec for now.
    - Implement SharedBuffer to allocate and free RPC memory
    - Add QnnMemManger to register shared buffer for tensor
      - During exection time, we will register memory of tensor data for QNN. And we will deregister them during destruction time of QnnBackend
    - Add two API `void* QnnExecuTorchAllocCustomMem(size_t bytes, size_t alignment)` and `void QnnExecuTorchFreeCustomMem(void* buffer_ptr)` to allocate RPC memory with SharedBuffer
      -  Users are responsible to allocate "enough" tensor bytes, and set alignment as MemoryAllocator::kDefaultAlignment. See runtime/core/memory_allocator.h.
    
    Pull Request resolved: https://github.com/pytorch/executorch/pull/2531
    
    Reviewed By: kirklandsign
    
    Differential Revision: D55142607
    
    Pulled By: cccclai
    
    fbshipit-source-id: 6a394ef0023e70362c3cf963a0519c3efaa95bc2

--- backends/qualcomm/aot/wrappers/TensorWrapper.h
+++ backends/qualcomm/aot/wrappers/TensorWrapper.h
@@ -59,15 +59,37 @@ class TensorWrapper {
     return QNN_VER_PTR(tensor_)->type == QNN_TENSOR_TYPE_STATIC;
   };
 
-  const void* GetStaticTensorData() const {
-    return QNN_VER_PTR(tensor_)->clientBuf.data;
+  std::uint32_t* GetDims() const {
+    return QNN_VER_PTR(tensor_)->dimensions;
+  };
+
+  Qnn_DataType_t GetDataType() const {
+    return QNN_VER_PTR(tensor_)->dataType;
+  };
+
+  Qnn_MemHandle_t const GetMemHandle() {
+    return QNN_VER_PTR(tensor_)->memHandle;
+  };
+
+  Qnn_TensorMemType_t GetMemType() const {
+    return QNN_VER_PTR(tensor_)->memType;
   };
 
   std::string GetName() const {
     return qnn_tensor_name_;
   };
 
+  std::uint32_t GetRank() const {
+    return QNN_VER_PTR(tensor_)->rank;
+  };
+
+  const void* GetStaticTensorData() const {
+    return QNN_VER_PTR(tensor_)->clientBuf.data;
+  };
+
   Error SetName(const std::string& name);
+
+  Error SetMemHandle(Qnn_MemHandle_t mem_handle);
 
  private:
   // need this to handle QNN_TENSOR_ERROR_NAME_HASH_COLLISION
