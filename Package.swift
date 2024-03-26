de1b33366395ffd318b854c53142d5ef36c844b2
Anthony Shoumikhin <shoumikhin@meta.com>
2024-03-26 06:31:18 -0700

    Update Swift PM manifest to include optimized ops. (#2668)
    Summary:
    
    bypass-github-export-checks
    
    Reviewed By: GregoryComer
    
    Differential Revision: D55342114

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let version = "0.1.0"
 let coreml_sha256 = "1f0432ee5782eab259ce9e3007415006be152c27ce530c1ef8a79bca940c6a32"
 let executorch_sha256 = "bf1ea573338bd30e90433bfb93db5d713aa7ecc351579ee7432f57fec329441b"
 let mps_sha256 = "df851be40444509f622ed155b787073aaba7643669eda36b6b3eb902e29b43a6"
+let optimized_sha256 = ""
 let portable_sha256 = "f183c5934491047b04130304466357959ae65e99b0b482a8a3372676ef817777"
 let quantized_sha256 = "bf79625d32161ac1892e172ec69e83a94b60ea400c8fa32bf4c4e85c7d338e8a"
 let xnnpack_sha256 = "85ab60d6a29fcebaf7aac1eb775ef96c1d87d1fa104458d0efdc681b7ea6b558"
@@ -68,6 +69,10 @@ let frameworks = [
       "MetalPerformanceShaders",
       "MetalPerformanceShadersGraph",
     ]
+  ),
+  Framework(
+    name: "optimized_backend",
+    checksum: portable_sha256
   ),
   Framework(
     name: "portable_backend",
