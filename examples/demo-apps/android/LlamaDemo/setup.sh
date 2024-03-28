eb8eca807ba99d25d7a1c1da09fe433413b9b06e
Hansong Zhang <hsz@meta.com>
2024-03-28 10:18:06 -0700

    Android build improvements (#2725)
    Summary: Pull Request resolved: https://github.com/pytorch/executorch/pull/2725
    
    Reviewed By: shoumikhin
    
    Differential Revision: D55464604
    
    Pulled By: kirklandsign
    
    fbshipit-source-id: 460fe36372bf7ef23f4a7c4ebec849ecf7ee2f16

--- examples/demo-apps/android/LlamaDemo/setup.sh
+++ examples/demo-apps/android/LlamaDemo/setup.sh
@@ -7,31 +7,46 @@
 
 set -eu
 
-pushd cmake-out
-# Note: Set up ANDROID_NDK, ANDROID_ABI, BUCK2, and FLATC_EXECUTABLE
-cmake .. -DBUCK2="$BUCK" \
-         -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake \
-         -DANDROID_ABI="$ANDROID_ABI" \
-         -DCMAKE_INSTALL_PREFIX=cmake-out \
-         -DEXECUTORCH_BUILD_EXTENSION_MODULE=ON \
-         -DEXECUTORCH_BUILD_FLATC=OFF \
-         -DFLATC_EXECUTABLE="${FLATC}" \
-         -DEXECUTORCH_BUILD_EXTENSION_DATA_LOADER=ON \
-         -DPYTHON_EXECUTABLE="$PYTHON_EXECUTABLE" \
-         -DEXECUTORCH_BUILD_ANDROID_JNI=ON \
-         -DEXECUTORCH_BUILD_XNNPACK=ON
+CMAKE_OUT=cmake-out
+# Note: Set up ANDROID_NDK, ANDROID_ABI, BUCK2, and FLATC
+cmake . -DCMAKE_INSTALL_PREFIX="${CMAKE_OUT}" \
+  -DCMAKE_TOOLCHAIN_FILE="${ANDROID_NDK}/build/cmake/android.toolchain.cmake" \
+  -DANDROID_ABI="${ANDROID_ABI}" \
+  -DBUCK2="${BUCK2}" \
+  -DEXECUTORCH_BUILD_XNNPACK=ON \
+  -DEXECUTORCH_BUILD_FLATC=OFF \
+  -DEXECUTORCH_BUILD_EXTENSION_DATA_LOADER=ON \
+  -DFLATC_EXECUTABLE="${FLATC}" \
+  -DEXECUTORCH_BUILD_EXTENSION_MODULE=ON \
+  -B"${CMAKE_OUT}"
 
 if [ "$(uname)" == "Darwin" ]; then
   CMAKE_JOBS=$(( $(sysctl -n hw.ncpu) - 1 ))
 else
   CMAKE_JOBS=$(( $(nproc) - 1 ))
 fi
-cmake --build . -j "${CMAKE_JOBS}"
-popd
+cmake --build "${CMAKE_OUT}" -j "${CMAKE_JOBS}" --target install
+
+cmake examples/models/llama2 -DBUCK2="$BUCK" \
+         -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake \
+         -DANDROID_ABI="$ANDROID_ABI" \
+         -DCMAKE_INSTALL_PREFIX="${CMAKE_OUT}" \
+         -B"${CMAKE_OUT}"/examples/models/llama2
+
+cmake --build "${CMAKE_OUT}"/examples/models/llama2 -j "${CMAKE_JOBS}"
+
+cmake extension/android -DBUCK2="${BUCK2}" \
+  -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK}/build/cmake/android.toolchain.cmake \
+  -DANDROID_ABI="${ANDROID_ABI}" \
+  -DCMAKE_INSTALL_PREFIX="${CMAKE_OUT}" \
+  -DEXECUTORCH_BUILD_LLAMA_JNI=ON \
+  -B"${CMAKE_OUT}"/extension/android
+
+cmake --build "${CMAKE_OUT}"/extension/android -j "${CMAKE_JOBS}"
 
 JNI_LIBS_PATH="examples/demo-apps/android/LlamaDemo/app/src/main/jniLibs"
 mkdir -p "${JNI_LIBS_PATH}/${ANDROID_ABI}"
-cp cmake-out/extension/android/libexecutorch_llama_jni.so "${JNI_LIBS_PATH}/${ANDROID_ABI}/"
+cp "${CMAKE_OUT}"/extension/android/libexecutorch_llama_jni.so "${JNI_LIBS_PATH}/${ANDROID_ABI}/"
 
 pushd extension/android
 ./gradlew build
