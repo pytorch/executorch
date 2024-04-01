7ebf1e19c35a11b9beab90820df2f941fdb1b9fd
Tarun Karuturi <tkaruturi@meta.com>
2024-03-31 19:48:03 -0700

    Refactor select_copy into util (#2784)
    Summary:
    
    Refactoring the core logic of select_copy into a helper utility function so that it can be utilized by other operators that internally need to do a select operation.
    
    Differential Revision: D55565485

--- kernels/portable/cpu/util/select_copy_util.h
+++ kernels/portable/cpu/util/select_copy_util.h
@@ -6,30 +6,18 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-#include <cstring>
+#pragma once
 
-#include <executorch/kernels/portable/cpu/util/select_copy_util.h>
 #include <executorch/runtime/kernel/kernel_includes.h>
 
 namespace torch {
 namespace executor {
-namespace native {
 
-using Tensor = exec_aten::Tensor;
-
-Tensor& select_copy_int_out(
-    RuntimeContext& ctx,
+Error select_copy_util(
     const Tensor& in,
     int64_t dim,
     int64_t index,
-    Tensor& out) {
-  Error err = torch::executor::select_copy_util(in, dim, index, out);
-  if (err != Error::Ok) {
-    ctx.fail(err);
-  }
-  return out;
-}
+    Tensor& out);
 
-} // namespace native
 } // namespace executor
 } // namespace torch
