72854c820f6250f7c771712dd9f99f25e90e896d
Dave Bort <dbort@meta.com>
2024-04-11 17:26:01 -0700

    Install build requirements in pre_build_script.sh
    Manually install build requirements because `python setup.py
    bdist_wheel` does not install them.

--- build/packaging/pre_build_script.sh
+++ build/packaging/pre_build_script.sh
@@ -5,6 +5,21 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-set -eux
+set -euxo pipefail
 
-echo "This script is run before building ExecuTorch binaries"
+# This script is run before building ExecuTorch binaries
+
+# Manually install build requirements because `python setup.py bdist_wheel` does
+# not install them. TODO(dbort): Switch to using `python -m build --wheel`,
+# which does install them. Though we'd need to disable build isolation to be
+# able to see the installed torch package.
+readonly BUILD_DEPS=(
+  # This list must match the build-system.requires list from pyproject.toml.
+  "cmake"
+  "pyyaml"
+  "setuptools"
+  "tomli"
+  "wheel"
+  "zstd"
+)
+pip install --progress-bar off "${BUILD_DEPS[@]}"
