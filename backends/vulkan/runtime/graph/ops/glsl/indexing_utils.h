2fbd99481d00bddfb05914e7902a2670af1a4703
Justin Yip <yipjustin@fb.com>
2024-04-18 15:07:18 -0700

    [ET-VK][6/n] aten.view_copy
    Pull Request resolved: https://github.com/pytorch/executorch/pull/3129
    
    aten.view_copy, supporting all packing.
    
    Using @ssjia's idea to do a direct lookup.
    
    Differential Revision: [D56281400](https://our.internmc.facebook.com/intern/diff/D56281400/)
    ghstack-source-id: 223111187

--- backends/vulkan/runtime/graph/ops/glsl/indexing_utils.h
+++ backends/vulkan/runtime/graph/ops/glsl/indexing_utils.h
@@ -8,9 +8,21 @@
 
 #define divup4(x) ((x + 3) / 4)
 
-#define to_buffer_i(idx, sizes)                      \
-  idx.x + idx.y* sizes.x + idx.z* sizes.y* sizes.x + \
-      idx.w* sizes.z* sizes.y* sizes.x;
+// Input: idx is a ivec4 user-level coordinate, sizes is the tensor shape
+// Output: buffer_idx in the continuous nchw-buffer.
+#define to_buffer_i(idx, sizes)                          \
+  (idx.x + idx.y * sizes.x + idx.z * sizes.y * sizes.x + \
+   idx.w * sizes.z * sizes.y * sizes.x)
+
+// Inverse of to_buffer_i
+// Input: buffer_idx in the continuous nchw-buffer, sizes is the tensor shape
+// Output: ivec4 user-level coorindate
+#define from_buffer_i(buf_i, sizes)            \
+  ivec4(                                       \
+      buf_i % sizes.x,                         \
+      (buf_i / (sizes.x)) % sizes.y,           \
+      (buf_i / (sizes.x * sizes.y)) % sizes.z, \
+      (buf_i / (sizes.x * sizes.y * sizes.z)))
 
 #define get_packed_dim_C_packed(vec) vec.z
 #define get_packed_dim_W_packed(vec) vec.x
@@ -20,6 +32,8 @@
 #define get_packed_stride_W_packed(vec) (1)
 #define get_packed_stride_H_packed(vec) (vec.x)
 
+// Input: pos is a texture position, sizes is a pack-aligned size.
+// Output: a user-level (w, h, c, n) coordinate
 #define to_tensor_idx_C_packed(pos, sizes) \
   ivec4(pos.x, pos.y, (pos.z * 4) % sizes.z, (pos.z * 4) / sizes.z)
 
@@ -29,6 +43,9 @@
 #define to_tensor_idx_H_packed(pos, sizes) \
   ivec4(pos.x, (pos.y * 4), pos.z % sizes.z, pos.z / sizes.z)
 
+// Input: idx is a user-level (w, h, c, n) coordinate. size is a pack-aligned
+// size.
+// Output: texture location
 #define to_texture_pos_C_packed(idx, sizes) \
   ivec3(idx.x, idx.y, (idx.z + idx.w * sizes.z) / 4)
 
@@ -37,6 +54,19 @@
 
 #define to_texture_pos_H_packed(idx, sizes) \
   ivec3(idx.x, idx.y / 4, (idx.z + idx.w * sizes.z))
+
+// Input: idx is a user-level (w, h, c, n) coordinate. size is a pack-aligned
+// size with the index in the texel.
+// Output: ivec4, xyz is the texture position, w is the element index in the
+// texel.
+#define to_texture_pos_elem_C_packed(idx, sizes) \
+  ivec4(idx.x, idx.y, (idx.z + idx.w * sizes.z) / 4, idx.z % 4)
+
+#define to_texture_pos_elem_W_packed(idx, sizes) \
+  ivec4(idx.x / 4, idx.y, (idx.z + idx.w * sizes.z), idx.x % 4)
+
+#define to_texture_pos_elem_H_packed(idx, sizes) \
+  ivec4(idx.x, idx.y / 4, (idx.z + idx.w * sizes.z), idx.y % 4)
 
 // Given a buffer(1-D) index cur, compute a new index where the corresponding
 // tensor(N-D)'s adjacent dimensions are swapped. The parameters x,y and plane
