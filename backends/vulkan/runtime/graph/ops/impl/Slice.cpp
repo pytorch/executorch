f6194e61cec8d6bdeb390009e1091f9a05186827
Justin Yip <yipjustin@fb.com>
2024-04-19 13:19:04 -0700

    Update on "[ET-VK][7/n] Slice, with lots of codegen improvements"
    1. Add slice operation. Instead of using copy in LI, we implement a simple shader with offsets.
    
    2. Improvement in codegen.
    - add support of optional variables
    - improve indent of the code, for better readability
    - allow user to specify tensor value generation, possible to generate sequential values for easier debugging for index operations
    - sample code improve test-case specification, particularly with long and optional values.
    
    Differential Revision: [D56295985](https://our.internmc.facebook.com/intern/diff/D56295985/)
    
    [ghstack-poisoned]

--- backends/vulkan/runtime/graph/ops/impl/Slice.cpp
+++ backends/vulkan/runtime/graph/ops/impl/Slice.cpp
@@ -8,15 +8,12 @@
 
 #include <executorch/backends/vulkan/runtime/graph/ops/OperatorRegistry.h>
 
-#include <executorch/backends/vulkan/runtime/api/api.h>
 #include <executorch/backends/vulkan/runtime/graph/Logging.h>
 
 #include <executorch/backends/vulkan/runtime/graph/ops/impl/utils/DimUtils.h>
 #include <executorch/backends/vulkan/runtime/graph/ops/impl/utils/KernelUtils.h>
 #include <executorch/backends/vulkan/runtime/graph/ops/impl/utils/TensorUtils.h>
 #include <executorch/backends/vulkan/runtime/graph/ops/utils/ShaderNameUtils.h>
-
-#include <iostream>
 
 namespace vkcompute {
 
