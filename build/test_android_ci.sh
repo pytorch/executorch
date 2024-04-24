7656d725583921fbed70a6f55073d42462e2fb8d
Nikita Shulga <nshulga@fb.com>
2024-04-24 10:27:39 -0700

    Revert D56480274: Add more prebuilt artifacts
    Differential Revision:
    D56480274
    
    Original commit changeset: 451116a0f907
    
    Original Phabricator Diff: D56480274
    
    fbshipit-source-id: e9603e5076113560b1224a56432abf321f82e284

--- build/test_android_ci.sh
+++ build/test_android_ci.sh
@@ -8,7 +8,7 @@
 set -ex
 
 # https://github.com/pytorch/executorch/tree/main/examples/demo-apps/android/ExecuTorchDemo
-export_model() {
+build_executorch() {
   MODEL_NAME=dl3
   # Delegating DeepLab v3 to XNNPACK backend
   python -m examples.xnnpack.aot_compiler --model_name="${MODEL_NAME}" --delegate
@@ -16,12 +16,10 @@ export_model() {
   ASSETS_DIR=examples/demo-apps/android/ExecuTorchDemo/app/src/main/assets/
   mkdir -p "${ASSETS_DIR}"
   cp "${MODEL_NAME}_xnnpack_fp32.pte" "${ASSETS_DIR}"
-}
 
-build_android_native_library() {
-  pushd examples/demo-apps/android/LlamaDemo
-  CMAKE_OUT="cmake-out-android-$1" ANDROID_NDK=/opt/ndk ANDROID_ABI="$1" ./gradlew setup
-  popd
+  rm -rf cmake-out && mkdir cmake-out
+  ANDROID_NDK=/opt/ndk BUCK2=$(which buck2) FLATC=$(which flatc) ANDROID_ABI=arm64-v8a \
+    bash examples/demo-apps/android/ExecuTorchDemo/setup.sh
 }
 
 build_android_demo_app() {
@@ -32,13 +30,12 @@ build_android_demo_app() {
 
 build_android_llama_demo_app() {
   pushd examples/demo-apps/android/LlamaDemo
+  ANDROID_NDK=/opt/ndk ANDROID_ABI=arm64-v8a ./gradlew setup
   ANDROID_HOME=/opt/android/sdk ./gradlew build
   ANDROID_HOME=/opt/android/sdk ./gradlew assembleAndroidTest
   popd
 }
 
-build_android_native_library arm64-v8a
-build_android_native_library x86_64
-export_model
+build_executorch
 build_android_demo_app
 build_android_llama_demo_app
