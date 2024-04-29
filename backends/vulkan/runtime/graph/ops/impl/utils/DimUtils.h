02630bcaa5bcdadd63f8d6210f7a8cc1e9473d6d
Justin Yip <yipjustin@fb.com>
2024-04-29 16:32:51 -0700

    [ET-VK][15/n] reconcile Dim4D and NchwDim
    TSIA.
    
    Differential Revision: [D56731155](https://our.internmc.facebook.com/intern/diff/D56731155/)
    
    [ghstack-poisoned]

--- backends/vulkan/runtime/graph/ops/impl/utils/DimUtils.h
+++ backends/vulkan/runtime/graph/ops/impl/utils/DimUtils.h
@@ -12,27 +12,6 @@
 
 namespace vkcompute {
 
-// A canonical way to represent dimensions as enum. Motivation behind a
-// canonical enum is that in the user tensor, it is using a "big-endian"-ish
-// mechanism to reference a dimension in a nchw-tensor, leading to tensor of
-// different dimension have different mapping from dim to the underlying texture
-// dimension. For instasnce, for a 2d (height x width) tensors, dim 0 refers to
-// height and dim 1 refers to width; for a 4d (batch x channel x height x width)
-// tensor, dim 0 refers to batch and dim 1 refers to channel. Using this
-// canonical enum allows us to bring clarity in code.
-
-enum NchwDim : uint32_t {
-  DimWidth = 1u,
-  DimHeight = 2u,
-  DimChannel = 3u,
-  DimBatch = 4u,
-};
-
-// Convert a dim provided by user into canonical enum.
-inline NchwDim normalize_to_nchw_dim(const vTensor& v_in, int32_t dim) {
-  return static_cast<NchwDim>(v_in.dim() - dim);
-}
-
 /*
  * Maps a semantic dimension name to an integer that
  * corresponds to its innermost ordering in a 4D tensor in
@@ -40,12 +19,16 @@ inline NchwDim normalize_to_nchw_dim(const vTensor& v_in, int32_t dim) {
  * corresponds to 1, height is the next innermost, so it
  * corresponds to 2, and so on.
  */
-struct Dim4D {
-  static constexpr uint32_t Width = DimWidth;
-  static constexpr uint32_t Height = DimHeight;
-  static constexpr uint32_t Channel = DimChannel;
-  static constexpr uint32_t Batch = DimBatch;
+enum Dim4DType : uint32_t {
+  DIM4D_WIDTH = 1u,
+  DIM4D_HEIGHT = 2u,
+  DIM4D_CHANNEL = 3u,
+  DIM4D_BATCH = 4u,
 };
+
+inline Dim4DType normalize_to_dim4d(const vTensor& v_in, int32_t dim) {
+  return static_cast<Dim4DType>(v_in.dim() - dim);
+}
 
 /*
  * Semantic dimension names for a 1D tensor
@@ -86,11 +69,11 @@ uint32_t dim_at(const std::vector<int64_t>& sizes) {
   return dims < N ? 1 : api::utils::safe_downcast<uint32_t>(sizes[dims - N]);
 }
 
-inline uint32_t dim_at(const std::vector<int64_t>& sizes, NchwDim nchw_dim) {
+inline uint32_t dim_at(const std::vector<int64_t>& sizes, Dim4DType dim4d) {
   const uint32_t dims = sizes.size();
-  return dims < nchw_dim
+  return dims < dim4d
       ? 1
-      : api::utils::safe_downcast<uint32_t>(sizes[dims - nchw_dim]);
+      : api::utils::safe_downcast<uint32_t>(sizes[dims - dim4d]);
 }
 
 template <uint32_t N>
@@ -98,23 +81,23 @@ uint32_t dim_at(const vTensor& v_in) {
   return dim_at<N>(v_in.sizes());
 }
 
-inline uint32_t dim_at(const vTensor& v_in, NchwDim nchw_dim) {
-  return dim_at(v_in.sizes(), nchw_dim);
+inline uint32_t dim_at(const vTensor& v_in, Dim4DType dim4d) {
+  return dim_at(v_in.sizes(), dim4d);
 }
 
-inline std::ostream& operator<<(std::ostream& os, NchwDim nchw_dim) {
-  switch (nchw_dim) {
-    case DimWidth:
-      os << "DimWidth";
+inline std::ostream& operator<<(std::ostream& os, Dim4DType dim4d) {
+  switch (dim4d) {
+    case DIM4D_WIDTH:
+      os << "DIM4D_WIDTH";
       break;
-    case DimHeight:
-      os << "DimHeight";
+    case DIM4D_HEIGHT:
+      os << "DIM4D_HEIGHT";
       break;
-    case DimChannel:
-      os << "DimChannel";
+    case DIM4D_CHANNEL:
+      os << "DIM4d_CHANNEL";
       break;
-    case DimBatch:
-      os << "DimBatch";
+    case DIM4D_BATCH:
+      os << "DIM4D_BATCH";
       break;
     default:
       os << "DimUnknown";
@@ -122,5 +105,4 @@ inline std::ostream& operator<<(std::ostream& os, NchwDim nchw_dim) {
   }
   return os;
 }
-
 } // namespace vkcompute
