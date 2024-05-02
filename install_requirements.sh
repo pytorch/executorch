864d2900c97c02152c23c76e267661928f72d3da
Dave Bort <dbort@meta.com>
2024-05-01 17:37:07 -0700

    Look on pytorch servers when installing pip deps (#3477)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/3477
    
    When installing the executorch pip package for CI jobs, look on the
    pytorch servers when resolving dependencies. This lets the executorch
    package depend on pytorch pre-release and nightly versions.
    
    Also run the llava setup with `-x` to make it easier to debug failures.
    
    imported-using-ghimport
    
    Test Plan: Imported from OSS
    
    Reviewed By: mikekgfb
    
    Differential Revision: D56857486
    
    Pulled By: dbort
    
    fbshipit-source-id: dc2bc1c7e8ee59ad9461f2b5eb8e7da785485ad7

--- install_requirements.sh
+++ install_requirements.sh
@@ -105,9 +105,12 @@ $PIP_EXECUTABLE install --extra-index-url "${TORCH_NIGHTLY_URL}" \
 
 #
 # Install executorch pip package. This also makes `flatc` available on the path.
+# The --extra-index-url may be necessary if pyproject.toml has a dependency on a
+# pre-release or nightly version of a torch package.
 #
 
 EXECUTORCH_BUILD_PYBIND="${EXECUTORCH_BUILD_PYBIND}" \
     CMAKE_ARGS="${CMAKE_ARGS}" \
     CMAKE_BUILD_ARGS="${CMAKE_BUILD_ARGS}" \
-    $PIP_EXECUTABLE install . --no-build-isolation -v
+    $PIP_EXECUTABLE install . --no-build-isolation -v \
+        --extra-index-url "${TORCH_URL}"
