25b63528e7f8d1396f498b730268b3244004bb33
Max Ren <maxren@meta.com>
2024-05-02 17:10:43 -0700

    make quantized model tests better (#3495)
    Summary:
    
    XNNPACK should be testing with random inputs and quantized models should be calibrated before lowering and testing. Right now there are some numerical issues being debugged and tracked by: T187799178. this has been a bit difficult, but for now, we can first make these tests better by giving random inptus and enabling calibration.  
    
    We don't fix the numerical issues in this diff.
    
    Differential Revision: D56906440

--- backends/xnnpack/test/models/mobilenet_v3.py
+++ backends/xnnpack/test/models/mobilenet_v3.py
@@ -15,7 +15,7 @@ from torchvision import models
 class TestMobileNetV3(unittest.TestCase):
     mv3 = models.mobilenetv3.mobilenet_v3_small(pretrained=True)
     mv3 = mv3.eval()
-    model_inputs = (torch.ones(1, 3, 224, 224),)
+    model_inputs = (torch.randn(1, 3, 224, 224),)
     dynamic_shapes = (
         {
             2: torch.export.Dim("height", min=224, max=455),
@@ -51,7 +51,29 @@ class TestMobileNetV3(unittest.TestCase):
             .run_method_and_compare_outputs(num_runs=5)
         )
 
+    @unittest.skip("T187799178: Debugging Numerical Issues with Calibration")
     def test_qs8_mv3(self):
+        ops_after_quantization = self.all_operators - {
+            "executorch_exir_dialects_edge__ops_aten__native_batch_norm_legit_no_training_default",
+        }
+        ops_after_lowering = self.all_operators
+
+        (
+            Tester(self.mv3, self.model_inputs, dynamic_shapes=self.dynamic_shapes)
+            .quantize()
+            .export()
+            .to_edge()
+            .check(list(ops_after_quantization))
+            .partition()
+            .check(["torch.ops.higher_order.executorch_call_delegate"])
+            .check_not(list(ops_after_lowering))
+            .to_executorch()
+            .serialize()
+            .run_method_and_compare_outputs(num_runs=5)
+        )
+
+    # TODO: Delete and only used calibrated test after T187799178
+    def test_qs8_mv3_no_calibration(self):
         ops_after_quantization = self.all_operators - {
             "executorch_exir_dialects_edge__ops_aten__native_batch_norm_legit_no_training_default",
         }
