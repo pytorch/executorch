3b4453bab5ef7f0bb6b85720e75f9dec1968db46
Wei Lu <luwei@meta.com>
2024-05-07 16:01:19 -0700

    `addmm` and `baddbmm` (#3401)
    Summary:
    
    We modify the `matmul` shader to support `addmm`.  `baddbmm` is therefore supported since it is decomposed as
    ```
    aten.add.Tensor
    aten.mul.Tensor
    aten.mul.Tensor
    aten.bmm.default
    ```
    
    Reviewed By: SS-JIA
    
    Differential Revision: D56678410

--- backends/vulkan/runtime/graph/ops/glsl/matmul_optimized.glsl
+++ backends/vulkan/runtime/graph/ops/glsl/matmul_optimized.glsl
@@ -8,33 +8,61 @@
 
 #version 450 core
 
-// To convince the SPIR-V compiler to unroll the loops optimally, need this
-// macro
-#define FOUR 4
-
 #define PRECISION ${PRECISION}
 
 #include "indexing_utils.h"
+#include "matmul.h"
 
-layout(set = 0, binding = 0, ${IMAGE_FORMAT[DTYPE]}) uniform PRECISION restrict writeonly image3D im_out;
-layout(set = 0, binding = 1) uniform PRECISION ${SAMPLER_T[NDIM][DTYPE]} im_mat1;
-layout(set = 0, binding = 2) uniform PRECISION ${SAMPLER_T[NDIM][DTYPE]} im_mat2;
+$if IS_ADDMM:
+  // addmm will have additional arguments compared to regular mm
+  layout(set = 0, binding = 0, ${IMAGE_FORMAT[DTYPE]}) uniform PRECISION restrict writeonly image3D im_out;
+  layout(set = 0, binding = 1) uniform PRECISION ${SAMPLER_T[NDIM][DTYPE]} im_mat1;
+  layout(set = 0, binding = 2) uniform PRECISION ${SAMPLER_T[NDIM][DTYPE]} im_mat2;
+  layout(set = 0, binding = 3) uniform PRECISION ${SAMPLER_T[NDIM][DTYPE]} im_self;
 
-layout(set = 0, binding = 3) uniform PRECISION restrict OutLimits {
-  ivec3 out_limits;
-};
+  layout(set = 0, binding = 4) uniform PRECISION restrict OutLimits {
+    ivec3 out_limits;
+  };
 
-layout(set = 0, binding = 4) uniform PRECISION restrict StepSize {
-  int step_size;
-};
+  layout(set = 0, binding = 5) uniform PRECISION restrict StepSize {
+    int step_size;
+  };
 
-layout(set = 0, binding = 5) uniform PRECISION restrict Reminder {
-  int reminder;
-};
+  layout(set = 0, binding = 6) uniform PRECISION restrict Reminder {
+    int reminder;
+  };
 
-layout(set = 0, binding = 6) uniform PRECISION restrict BatchSize {
-  int batch_size;
-};
+  layout(set = 0, binding = 7) uniform PRECISION restrict BatchSize {
+    int batch_size;
+  };
+
+  layout(set = 0, binding = 8) uniform PRECISION restrict AddmmParams {
+    int broadcast_at_width;
+    int broadcast_at_height;
+    float alpha;
+    float beta;
+  };
+$else:
+  // define original matmul_optimized arguments
+  layout(set = 0, binding = 0, ${IMAGE_FORMAT[DTYPE]}) uniform PRECISION restrict writeonly image3D im_out;
+  layout(set = 0, binding = 1) uniform PRECISION ${SAMPLER_T[NDIM][DTYPE]} im_mat1;
+  layout(set = 0, binding = 2) uniform PRECISION ${SAMPLER_T[NDIM][DTYPE]} im_mat2;
+
+  layout(set = 0, binding = 3) uniform PRECISION restrict OutLimits {
+    ivec3 out_limits;
+  };
+
+  layout(set = 0, binding = 4) uniform PRECISION restrict StepSize {
+    int step_size;
+  };
+
+  layout(set = 0, binding = 5) uniform PRECISION restrict Reminder {
+    int reminder;
+  };
+
+  layout(set = 0, binding = 6) uniform PRECISION restrict BatchSize {
+    int batch_size;
+  };
 
 layout(local_size_x_id = 0, local_size_y_id = 1, local_size_z_id = 2) in;
 
@@ -45,72 +73,25 @@ void main() {
     return;
   }
 
-  // we avoid mat4 and vec4 usage here as they compile to much less efficient
-  // SPIR-V
-  float results[FOUR][FOUR][FOUR];
-  for (int i = 0; i < FOUR; i++) {
-    for (int j = 0; j < FOUR; j++) {
-      for (int k = 0; k < FOUR; k++) {
-        results[i][j][k] = 0.0f;
-      }
-    }
-  }
+  FloatMatrix results = matmul_partial_4x4(im_mat1, im_mat2, pos, batch_size, step_size, reminder);
 
-  // read and cache 4x4 tile of im_mat1 (4 adjacent rows)
-  vec4 im_mat1_partial_rows[FOUR];
-  vec4 im_mat2_partial_cols[FOUR];
-
-  for (int c = 0; c < FOUR; c++) {
-    if (FOUR * pos.z + c >= batch_size) {
-      break;
-    }
-    for (int j = 0; j < step_size; j++) {
-      for (int k = 0; k < FOUR; k++) {
-        const int pos_y_offset = (FOUR * pos.y) + k;
-        const ivec3 pos_rd = ivec3(j, pos_y_offset, FOUR * pos.z + c);
-        im_mat1_partial_rows[k] = texelFetch(im_mat1, pos_rd, 0);
-        // set the value out of the boundary to be 0
-        if (j == step_size - 1 && reminder > 0) {
-          for (int kk = 0; kk < 4 - reminder; kk++) {
-            im_mat1_partial_rows[k][3 - kk] = 0;
-          }
-        }
-      }
-      // read and cache 4x4 tile of im_mat2 (4 adjacent columns)
-      for (int k = 0; k < FOUR; k++) {
-        const int pos_x_offset = (FOUR * pos.x) + k;
-        const ivec3 pos_rd = ivec3(pos_x_offset, j, FOUR * pos.z + c);
-        im_mat2_partial_cols[k] = texelFetch(im_mat2, pos_rd, 0);
-        // set the value out of the boundary to be 0
-        if (j == step_size - 1 && reminder > 0) {
-          for (int kk = 0; kk < 4 - reminder; kk++) {
-            im_mat2_partial_cols[k][3 - kk] = 0;
-          }
-        }
-      }
-      // perform partial dot products and add partial result to results
-      for (int idx_r = 0; idx_r < FOUR; idx_r++) {
-        for (int idx_c = 0; idx_c < FOUR; idx_c++) {
-          results[idx_r][idx_c][c] +=
-              dot(im_mat1_partial_rows[idx_r], im_mat2_partial_cols[idx_c]);
-        }
-      }
-    }
-  }
-
-  // results is in transposed order w.r.t. the desired output
   for (int idx_c = 0; idx_c < FOUR; idx_c++) {
     for (int idx_r = 0; idx_r < FOUR; idx_r++) {
       const ivec3 out_pos =
           ivec3(idx_r + FOUR * pos.x, idx_c + FOUR * pos.y, pos.z);
+      $if IS_ADDMM:
+        vec4 self_texel = get_texel_C_packed(im_self, out_pos, broadcast_at_width, broadcast_at_height);
+        results.data[idx_c][idx_r][0] = beta * self_texel.x + alpha * results.data[idx_c][idx_r][0];
+
+      // results is in transposed order w.r.t. the desired output
       imageStore(
           im_out,
           out_pos,
           vec4(
-              results[idx_c][idx_r][0],
-              results[idx_c][idx_r][1],
-              results[idx_c][idx_r][2],
-              results[idx_c][idx_r][3]));
+              results.data[idx_c][idx_r][0],
+              results.data[idx_c][idx_r][1],
+              results.data[idx_c][idx_r][2],
+              results.data[idx_c][idx_r][3]));
     }
   }
 }
--- backends/vulkan/test/op_tests/cases.py
+++ backends/vulkan/test/op_tests/cases.py
@@ -74,6 +74,26 @@ def get_bmm_inputs():
     return test_suite
 
 
+def get_addmm_inputs():
+    test_suite = VkTestSuite(
+        [
+            ((1, S), (S1, S), (S, S), 1.0, 1.5),
+            ((S, 1), (S, S1), (S1, S1), 1.0, 1.0),
+            ((M1, M2), (M1, M2), (M2, M2)),
+            ((M1, M2), (M1, M2), (M2, M2), 4.2, 2.3),
+            ((M1, 1), (M1, L), (L, L), 2.0, 3.0),
+            ((M2), (M1, M2), (M2, M2)),
+        ]
+    )
+    # ATen matmul doesn't support half
+    test_suite.dtypes = ["at::kFloat"]
+    test_suite.layouts = [
+        "api::kWidthPacked",
+        "api::kChannelsPacked",
+    ]
+    return test_suite
+
+
 def get_pool2d_inputs():
     test_suite = VkTestSuite(
         [
@@ -589,8 +609,9 @@ test_suites = {
     "aten.sub.Tensor": get_binary_elementwise_inputs(),
     "aten.div.Tensor": get_binary_elementwise_inputs(),
     "aten.mul.Tensor": get_binary_elementwise_inputs(),
-    "aten.mm.default": get_mm_inputs(),
+    "aten.addmm.default": get_addmm_inputs(),
     "aten.bmm.default": get_bmm_inputs(),
+    "aten.mm.default": get_mm_inputs(),
     "aten.max_pool2d_with_indices.default": get_pool2d_inputs(),
     "aten.convolution.default": get_conv_inputs(),
     "aten.native_layer_norm.default": get_native_layer_norm_inputs(),
