870aa8b2fe526e9094b723d40d09780540c0a974
Zingo Andersen <zingo@zingo.org>
2024-05-09 08:44:54 -0700

    Fix backend/arm tests failing when there is no tosa installed (#3560)
    Summary:
    This fixes problem with clone and view unit test if TOSA is not installed
    
    Pull Request resolved: https://github.com/pytorch/executorch/pull/3560
    
    Reviewed By: mergennachin
    
    Differential Revision: D57161708
    
    Pulled By: digantdesai
    
    fbshipit-source-id: e4b6733ef4da89bb894c5197a90912eaa7fe4b5c

--- backends/arm/test/ops/test_clone.py
+++ backends/arm/test/ops/test_clone.py
@@ -73,8 +73,8 @@ class TestSimpleClone(unittest.TestCase):
         if common.TOSA_REF_MODEL_INSTALLED:
             tester.run_method_and_compare_outputs(qtol=1)
         else:
-            logger.warning(
-                "TOSA ref model tool not installed, skip numerical correctness tests"
+            raise RuntimeError(
+                "TOSA ref model tool not installed and the test is an expected fail"
             )
 
     def _test_clone_tosa_u55_pipeline(
