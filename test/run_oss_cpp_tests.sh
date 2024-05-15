eada5279be8a8c268977112ab9e7f3b8c46e8011
Hansong Zhang <hsz@fb.com>
2024-05-14 21:47:10 -0700

    Add a way to run c++ unittest on OSS (#3606)
    Summary:
    Add CMake rule for tests, and a script to invoke it.
    
    Pull Request resolved: https://github.com/pytorch/executorch/pull/3606
    
    Reviewed By: larryliu0820
    
    Differential Revision: D57343746
    
    Pulled By: kirklandsign
    
    fbshipit-source-id: 289a37fb97c7f80cab44aa2ba30b859d1d527e59

---
+++ test/run_oss_cpp_tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+set -ex
+
+build_executorch() {
+  cmake . -DCMAKE_INSTALL_PREFIX=cmake-out -DEXECUTORCH_BUILD_GTESTS=ON -Bcmake-out
+  cmake --build cmake-out -j9 --target install
+}
+
+build_and_run_test() {
+  local test_dir=$1
+  cmake "${test_dir}" -Bcmake-out/"${test_dir}" -DCMAKE_INSTALL_PREFIX=cmake-out
+  cmake --build cmake-out/"${test_dir}"
+  for t in $(cmake-out/"${test_dir}"/*test); do ./"$t"; done
+}
+
+build_executorch
+build_and_run_test runtime/core/portable_type/test/
