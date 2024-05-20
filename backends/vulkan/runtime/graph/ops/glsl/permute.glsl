a397bff431793c12ce511713a7df8e8ccd6f3c31
Nathanael See <nathanaelsee@meta.com>
2024-05-20 15:46:41 -0700

    Add int to possible input dtypes for permute (#3685)
    Summary: Pull Request resolved: https://github.com/pytorch/executorch/pull/3685
    
    Reviewed By: copyrightly
    
    Differential Revision: D57453408
    
    fbshipit-source-id: 9aeee256b704b27edf2c64ce6148f2d5f2f8cbd0

--- backends/vulkan/runtime/graph/ops/glsl/permute.glsl
+++ backends/vulkan/runtime/graph/ops/glsl/permute.glsl
@@ -17,7 +17,7 @@ layout(std430) buffer;
 #include "indexing_utils.h"
 
 layout(set = 0, binding = 0, ${IMAGE_FORMAT[DTYPE]}) uniform PRECISION restrict writeonly ${IMAGE_T[NDIM][DTYPE]} image_out;
-layout(set = 0, binding = 1) uniform PRECISION sampler3D image_in;
+layout(set = 0, binding = 1) uniform PRECISION ${SAMPLER_T[NDIM][DTYPE]} image_in;
 
 layout(set = 0, binding = 2) uniform PRECISION restrict OutLimits {
   ivec3 out_limits;
