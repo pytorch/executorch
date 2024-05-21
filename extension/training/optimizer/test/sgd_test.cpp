ddb0f014b314d75523c962fe67e49663e96ede04
David Lin <lind@meta.com>
2024-05-21 10:49:09 -0700

    Add SGD state and options (#3595)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/3595
    
    title. added constructors/inline functions, and documentation for the parameters
    
    Reviewed By: JacobSzwejbka
    
    Differential Revision: D57216864
    
    fbshipit-source-id: 1a82f5fef8b601b897da1dcfc2a68a1b59357f23

--- extension/training/optimizer/test/sgd_test.cpp
+++ extension/training/optimizer/test/sgd_test.cpp
@@ -7,22 +7,54 @@
  */
 
 #include <executorch/extension/training/optimizer/sgd.h>
+#include <executorch/runtime/core/exec_aten/exec_aten.h>
+#include <executorch/runtime/core/exec_aten/testing_util/tensor_factory.h>
+#include <executorch/runtime/platform/runtime.h>
 
 #include <gtest/gtest.h>
 
 using namespace ::testing;
 using namespace torch::executor::optimizer;
+using exec_aten::ScalarType;
+using exec_aten::Tensor;
+using torch::executor::testing::TensorFactory;
 
-class SGDOptimizerTest : public ::testing::Test {};
+class SGDOptimizerTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    torch::executor::runtime_init();
+  }
+};
 
-TEST_F(SGDOptimizerTest, InstantiateTypes) {
-  SGDParamState state;
-  SGDOptions options;
-  SGDParamGroup param_group;
-  SGD sgd;
+TEST_F(SGDOptimizerTest, SGDParamStateTest) {
+  TensorFactory<ScalarType::Int> tf;
+  Tensor momentum_buffer = tf.make({2, 2}, {1, 2, 3, 4});
+  SGDParamState state(momentum_buffer);
 
-  EXPECT_TRUE(dynamic_cast<SGDParamState*>(&state) != nullptr);
-  EXPECT_TRUE(dynamic_cast<SGDOptions*>(&options) != nullptr);
-  EXPECT_TRUE(dynamic_cast<SGDParamGroup*>(&param_group) != nullptr);
-  EXPECT_TRUE(dynamic_cast<SGD*>(&sgd) != nullptr);
+  auto data_p = state.momentum_buffer().const_data_ptr<int32_t>();
+
+  ASSERT_EQ(data_p[0], 1);
+  ASSERT_EQ(data_p[1], 2);
+  ASSERT_EQ(data_p[2], 3);
+  ASSERT_EQ(data_p[3], 4);
+}
+
+TEST_F(SGDOptimizerTest, SGDOptionsNonDefaultValuesTest) {
+  SGDOptions options(0.1, 1.0, 2.0, 3.0, true);
+
+  EXPECT_EQ(options.lr(), 0.1);
+  EXPECT_EQ(options.momentum(), 1.0);
+  EXPECT_EQ(options.dampening(), 2.0);
+  EXPECT_EQ(options.weight_decay(), 3.0);
+  EXPECT_TRUE(options.nesterov());
+}
+
+TEST_F(SGDOptimizerTest, SGDOptionsDefaultValuesTest) {
+  SGDOptions options(0.1);
+
+  EXPECT_EQ(options.lr(), 0.1);
+  EXPECT_EQ(options.momentum(), 0);
+  EXPECT_EQ(options.dampening(), 0);
+  EXPECT_EQ(options.weight_decay(), 0);
+  EXPECT_TRUE(!options.nesterov());
 }
