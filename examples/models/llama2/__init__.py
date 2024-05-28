a2c7d6f100d8b53f5b4f3d0408f0b6a33f940166
Chen Lai <chenlai@fb.com>
2024-05-28 14:49:13 -0700

    Update on "Factor out eager val from eval_llama_lib"
    Would like to re-use EagerEvalWrapper and eval function for quantization calibration.
    
    Differential Revision: [D57881028](https://our.internmc.facebook.com/intern/diff/D57881028/)
    
    [ghstack-poisoned]

--- examples/models/llama2/__init__.py
+++ examples/models/llama2/__init__.py
@@ -4,6 +4,8 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .eager_eval import EagerEvalWrapper, evaluate_model
+from .model import Llama2Model
 
-__all__ = ["EagerEvalWrapper", "evaluate_model"]
+__all__ = [
+    Llama2Model,
+]
--- examples/models/llama2/evaluate/__init__.py
+++ examples/models/llama2/evaluate/__init__.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .eval import EagerEvalWrapper, evaluate_model
+from .eager_eval import EagerEvalWrapper, evaluate_model
 
 __all__ = [
     "evaluate_model",
