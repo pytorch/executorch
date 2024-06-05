d84bc6d47c31d91f1b97d90c52f0ac3d656efb3b
Martin Yuan <myuan@meta.com>
2024-06-05 07:03:58 -0700

    Add image preprocessor into the model

--- examples/models/llava_encoder/model.py
+++ examples/models/llava_encoder/model.py
@@ -13,17 +13,51 @@ from llava.mm_utils import get_model_name_from_path
 from llava.model.builder import load_pretrained_model
 from torch import nn
 
+import torchvision
+from torchvision.transforms import v2
+
+from dataclasses import dataclass
+from torch.export import Dim
+
+
+
+@dataclass
+class PreprocessConfig:
+    crop_size: dict
+    image_mean: list[float]
+    image_std: list[float]
+    rescale_factor: float
+
+
+class MyPreprocess(nn.Module):
+    def __init__(self, config: PreprocessConfig):
+        super().__init__()
+        self.config = config
+
+    def forward(self, img):
+        w = max(img.shape[1], img.shape[2])
+        padded = v2.functional.center_crop(img, output_size=[w, w])
+        resized = torchvision.transforms._functional_tensor.resize(padded, size=[self.config.crop_size['height'], self.config.crop_size['width']])
+        torch._check(resized.size(1) == self.config.crop_size['height'])
+        torch._check(resized.size(2) == self.config.crop_size['width'])
+        scaled = resized * self.config.rescale_factor
+        normed = v2.functional.normalize(scaled, self.config.image_mean, self.config.image_std)
+        return normed
 
 class EncoderModel(nn.Module):
-    def __init__(self, llava_model):
+    def __init__(self, image_processor, llava_model):
         super().__init__()
+        pre_config = PreprocessConfig(image_processor.crop_size, image_processor.image_mean, image_processor.image_std,
+                                      image_processor.rescale_factor)
+        self.pre_processer_ = MyPreprocess(pre_config)
         self.model_ = llava_model
 
     def forward(self, images_tensor):
-        features = self.model_.get_model().get_vision_tower()(images_tensor)
+        processed = self.pre_processer_(images_tensor)
+        processed = torch.unsqueeze(processed, dim=0)
+        features = self.model_.get_model().get_vision_tower()(processed)
         features = self.model_.get_model().mm_projector(features)
         return features
-
 
 class LlavaModel(EagerModelBase):
     def __init__(self):
@@ -40,13 +74,17 @@ class LlavaModel(EagerModelBase):
         self.model_.to(device=self.device, dtype=self.dtype)
 
     def get_eager_model(self):
-        model = EncoderModel(self.model_)
+        model = EncoderModel(self.image_processor_, self.model_)
         return model
 
     def get_example_inputs(self):
-        image_file = "https://llava-vl.github.io/static/images/view.jpg"
-        images = load_images([image_file])
-        images_tensor = process_images(
-            images, self.image_processor_, self.model_.config
-        ).to(self.model_.device)
-        return (images_tensor,)
+        tensor_size = (3, 1024, 2048)
+        return (torch.randn(tensor_size),)
+
+    def get_dynamic_shapes(self):
+        length = Dim('length', min=8, max=4090)
+        width = Dim('width', min=10, max=4092)  # 1346 <= 2*_width <= 2048
+        # width = 2*_width
+        dynamic_shapes = [{1: length, 2: width}]
+        return dynamic_shapes
+
