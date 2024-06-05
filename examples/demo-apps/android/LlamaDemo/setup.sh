32041161a679f65aa60d2519c2b518b750771ff7
Hansong Zhang <hsz@fb.com>
2024-06-05 10:00:21 -0700

    Android improve prebuilt workflow and provide tutorial (#3840)
    Summary:
    - Don't depend on demo app. Instead, build JAR/JNI/AAR directly
    - Update tutorial, example scripts for LLAMA demo app
    
    Pull Request resolved: https://github.com/pytorch/executorch/pull/3840
    
    Reviewed By: mcr229
    
    Differential Revision: D58172672
    
    Pulled By: kirklandsign
    
    fbshipit-source-id: ea6e2ba499bd1269380724d46e9860e3c5d95fa9

--- examples/demo-apps/android/LlamaDemo/setup.sh
+++ examples/demo-apps/android/LlamaDemo/setup.sh
@@ -56,6 +56,16 @@ cmake extension/android \
 
 cmake --build "${CMAKE_OUT}"/extension/android -j "${CMAKE_JOBS}" --config Release
 
-JNI_LIBS_PATH="examples/demo-apps/android/LlamaDemo/app/src/main/jniLibs"
-mkdir -p "${JNI_LIBS_PATH}/${ANDROID_ABI}"
-cp "${CMAKE_OUT}"/extension/android/libexecutorch_llama_jni.so "${JNI_LIBS_PATH}/${ANDROID_ABI}/"
+BUILD_AAR_DIR="$(mktemp -d)"
+mkdir -p "${BUILD_AAR_DIR}/jni/${ANDROID_ABI}" "${BUILD_AAR_DIR}/libs"
+cp "${CMAKE_OUT}"/extension/android/libexecutorch_llama_jni.so "${BUILD_AAR_DIR}/jni/${ANDROID_ABI}"
+cp extension/android/build/libs/executorch.jar "${BUILD_AAR_DIR}/libs"
+echo \<manifest xmlns:android=\"http://schemas.android.com/apk/res/android\" \
+  package=\"org.pytorch.executorch\"\> \
+  \<uses-sdk android:minSdkVersion=\"19\" /\> \
+  \</manifest\> > "${BUILD_AAR_DIR}/AndroidManifest.xml"
+pushd "${BUILD_AAR_DIR}"
+zip -r executorch-llama.aar libs jni/${ANDROID_ABI} AndroidManifest.xml
+popd
+mkdir -p examples/demo-apps/android/LlamaDemo/app/libs
+mv "${BUILD_AAR_DIR}/executorch-llama.aar" examples/demo-apps/android/LlamaDemo/app/libs
