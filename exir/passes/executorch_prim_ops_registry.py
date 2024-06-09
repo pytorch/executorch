3c4339d5ff4c5b6652e554abced0e98ea721b33d
Edward Yang <ezyang@meta.com>
2024-06-08 18:20:16 -0700

    Complete revamp of float/promotion sympy handling (#3911)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/3911
    
    Original PR https://github.com/pytorch/pytorch/pull/126905
    
    Below line forces Sandcastle to run only specified contbuilds.
    build_only[github-export-checks,executorch,pytorch_benchmark,pytorch_quantization,pytorch_distributed,pytorch_distributed_gpu,pytorch_dynamo_inductor,pytorch_functorch,pytorch_fx2trt,pytorch_diff_train_tests_ads,glow_fb_pytorch_tests,training_platform,training_platform_compatibility,training_toolkit_applications,training_toolkit_examples,training_toolkit_model_optimization,dper3_pytorch,xplat_caffe2,pytorch_dev,android-pytorch-instrumentation-tests,smart__pytorch__github_first_try_merge,frl-target-determinator,f6-buck,training_platform_for_github,sigmoid_cpu,sigmoid_gpu,aiplatform_modelprocessing_for_github,accelerators_workloads_models_slimdsnn,ae_aotinductor_benchmark_test,aps_,apf,aps_deterministic_ne_tests,dper_lib_silvertorch,torchrec,torchrec_fb,deeplearning_aot_inductor,aiplatform_modelstore]
    #skipfbcodelongtail
    
    Differential Revision: D58294450

--- exir/passes/executorch_prim_ops_registry.py
+++ exir/passes/executorch_prim_ops_registry.py
@@ -49,6 +49,11 @@ def truediv(a: _SymScalar, b: _SymScalar) -> _SymScalar:
     return a / b  # pyre-ignore
 
 
+@bind_pattern_to_op(executorch_prims_lib, "sym_float.Scalar(Scalar a) -> Scalar")
+def sym_float(a: _SymScalar) -> _SymScalar:
+    return float(a)  # pyre-ignore
+
+
 # TODO: ideally we should return SymBool in the schema, but it seems
 # the schema parser does not recognize SymBool yet: P629748075
 @bind_pattern_to_op(executorch_prims_lib, "gt.Scalar(Scalar a, Scalar b) -> bool")
@@ -87,6 +92,7 @@ _PYTHON_SYM_OPS_TO_EXECUTORCH_SYM_OPS: Dict[OpOverload, OpOverload] = {
     operator.lt: ops.backend.executorch_prim.lt.Scalar,
     operator.ge: ops.backend.executorch_prim.ge.Scalar,
     operator.le: ops.backend.executorch_prim.le.Scalar,
+    torch.sym_float: ops.backend.executorch_prim.sym_float.Scalar,
 }
 
 
