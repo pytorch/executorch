aaafa24c43e98dbb6398e946697e59d8afa8533c
Songhao Jia <gasoonjia@meta.com>
2024-06-10 15:06:11 -0700

    enable bp test in oss (#3917)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/3917
    
    as title.
    
    Reviewed By: kirklandsign
    
    Differential Revision: D58378535
    
    fbshipit-source-id: 32d7ea47724244a023cdb4326a58206acae88b4b

--- sdk/bundled_program/schema/test/test_schema.py
+++ sdk/bundled_program/schema/test/test_schema.py
@@ -7,15 +7,21 @@
 # pyre-strict
 
 import filecmp
+import os
 import unittest
 
 
 class TestSchema(unittest.TestCase):
     def test_schema_sync(self) -> None:
+        # make the test work in both internal and oss.
+        prefix = (
+            "executorch/" if os.path.exists("executorch/schema/scalar_type.fbs") else ""
+        )
+
         self.assertTrue(
             filecmp.cmp(
-                "executorch/sdk/bundled_program/schema/scalar_type.fbs",
-                "executorch/schema/scalar_type.fbs",
+                prefix + "sdk/bundled_program/schema/scalar_type.fbs",
+                prefix + "schema/scalar_type.fbs",
             ),
             'Please run "hg cp fbcode//executorch/schema/scalar_type.fbs fbcode//executorch/sdk/bundled_program/schema/scalar_type.fbs" to sync schema changes.',
         )
