4cd8290f7d7d74760b525c79a7b47212876a8832
Di Xu <dixu@meta.com>
2024-06-12 11:41:38 -0700

    Save batch prefill onnx and exit early

--- examples/models/llama2/export_llama_lib.py
+++ examples/models/llama2/export_llama_lib.py
@@ -395,9 +395,22 @@ def _export_llama(modelname, args) -> LlamaEdgeManager:  # noqa: C901
     pt2e_quant_params, quantizers, quant_dtype = get_quantizer_and_quant_params(args)
 
     llmManager = _prepare_for_llama_export(modelname, args)
-    
-    import pdb; pdb.set_trace()
-    # llmManager.example_inputs
+    # # llmManager.example_inputs
+    # batch_prefix
+    input_names = ['input_ids']
+    import torch
+    # torch.onnx.export(llmManager.model, inputs, "~/models/odllm0b5_prefill.onnx", input_names=input_names)
+    output_path = "/home/dixu/models/onnx/odllm0b5_prefill.onnx"
+    torch.onnx.export(llmManager.model, llmManager.example_inputs, output_path, input_names=input_names)
+    print(f"Saved onnx file to {output_path}")
+    assert False, "exit early"
+    # import pdb; pdb.set_trace()
+
+    # inputs = {'input_ids':llmManager.example_inputs, 'attention_mask': padded_attention_mask, 'position_ids': position_ids,}
+    # input_names = ['input_ids', 'attention_mask', 'position_ids']
+
+    # attention_mask: 
+    # position_ids (input_pos ? ): Scalar tensor indicating size of window of the caches
 
     # export_to_edge
     builder_exported_to_edge = (
