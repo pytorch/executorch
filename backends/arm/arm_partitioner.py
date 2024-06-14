af12e78635976908b5a02127f6e468fee1f31958
Oscar Andersson <oscar.andersson@arm.com>
2024-06-14 15:06:37 +0200

    Add mul-op for Arm backend
    Implements mul node visitor and tests.
    Quantization of mul is already implemented.
    
    Signed-off-by: Oscar Andersson <oscar.andersson@arm.com>
    Change-Id: Id8c618cc0b9725d9eb913e4f4cb5ba0dedf86bdc

--- backends/arm/arm_partitioner.py
+++ backends/arm/arm_partitioner.py
@@ -44,6 +44,7 @@ class TOSASupportedOperators(OperatorSupportBase):
             exir_ops.edge.aten.convolution.default,
             exir_ops.edge.aten.div.Tensor,
             exir_ops.edge.aten.full.default,
+            exir_ops.edge.aten.mul.Tensor,
             exir_ops.edge.aten._native_batch_norm_legit_no_training.default,
             exir_ops.edge.aten.avg_pool2d.default,
             exir_ops.edge.aten.sigmoid.default,
