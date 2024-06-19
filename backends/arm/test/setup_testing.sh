c1f7970af831449dcbc8364c415fee7d096d182e
Erik Lundell <erik.lundell@arm.com>
2024-06-19 15:53:25 +0200

    Add copyright header to setup_testing.sh
    Signed-off-by: Erik Lundell <erik.lundell@arm.com>
    Change-Id: I7859514463feae3d22cf283afa9517e60ca1365d

--- backends/arm/test/setup_testing.sh
+++ backends/arm/test/setup_testing.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+# Copyright 2024 Arm Limited and/or its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 set -eu
 
 script_dir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
