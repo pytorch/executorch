a88f2f13ed4a5b81e7623a3ff13c82e024c12731
huiyujie0105 <huiyujie0105@fb.com>
2024-06-24 11:26:24 -0700

    decomposition
    Differential Revision: D58891533

--- backends/vulkan/partitioner/supported_ops.py
+++ backends/vulkan/partitioner/supported_ops.py
@@ -125,6 +125,7 @@ CREATION_OPS = [
     exir_ops.edge.aten.ones_like.default,
     exir_ops.edge.aten.zeros.default,
     exir_ops.edge.aten.zeros_like.default,
+    exir_ops.edge.aten.upsample_nearest2d.vec,
 ]
 
 
