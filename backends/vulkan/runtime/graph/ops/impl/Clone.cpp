1ba8946b82b6adc7e9d7b47313929573c2f012d2
Jorge Pineda <jorgep31415@meta.com>
2024-06-25 11:55:51 -0700

    Migrate workgroup API for trivial cases (#4061)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/4061
    
    This will allow us to override local workgroup sizes with https://github.com/pytorch/executorch/pull/4046.
    
    ## Before
    ```
    vTensorPtr t_out = graph.get_tensor(out);
    api::utils::uvec3 global_size = t_out->image_extents();
    api::utils::uvec3 local_size = adaptive_work_group_size(global_size);
    
    graph.execute_nodes().emplace_back(new ExecuteNode(
          ...,
          global_size,
          local_size,
          ...,
          );
    ```
    
    ## After
    ```
    graph.execute_nodes().emplace_back(new ExecuteNode(
          ...,
          graph.create_global_wg_size(out),
          graph.create_local_wg_size(out),
          ...,
          );
    ```
    
    Note we do not migrate cases where the global size is nontrivial (MatMul, Linear, Conv1D, Repeat) or the image isn't a ValueRef (MaxPool2D, NativeLayerNorm). We should first align on an API design for those cases.
    
    bypass-github-export-checks
    bypass-github-pytorch-ci-checks
    bypass-github-executorch-ci-checks
    
    Reviewed By: nathanaelsee
    
    Differential Revision: D59011492
    
    fbshipit-source-id: 40e75afdaecefa733bf1748a627877b48b5d949a

--- backends/vulkan/runtime/graph/ops/impl/Clone.cpp
+++ backends/vulkan/runtime/graph/ops/impl/Clone.cpp
@@ -25,14 +25,11 @@ void add_clone_node(
   std::string kernel_name = "clone";
   add_dtype_suffix(kernel_name, *t_out);
 
-  api::utils::uvec3 global_size = t_out->image_extents();
-  api::utils::uvec3 local_size = adaptive_work_group_size(global_size);
-
   graph.execute_nodes().emplace_back(new ExecuteNode(
       graph,
       VK_KERNEL_FROM_STR(kernel_name),
-      global_size,
-      local_size,
+      graph.create_global_wg_size(out),
+      graph.create_local_wg_size(out),
       {{out, api::MemoryAccessType::WRITE}, {in, api::MemoryAccessType::READ}},
       {t_out->texture_limits_ubo()}));
 }
