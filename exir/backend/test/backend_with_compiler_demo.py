bdb73a5c2ccd8977f62edcc638ebaa0e6a43b2a8
Chen Lai <chenlai@meta.com>
2024-06-26 23:59:50 -0700

    update the on device delegate compatibility (#4060)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/4060
    
    As discussed offline, mainly two changes in this diff
    - add a unit test in the demo backend that will throw `DelegateInvalidCompatibility` when getting a wrong version
    - Add doc block for the compatibility error report during init
    
    Reviewed By: mergennachin, kirklandsign
    
    Differential Revision: D58986515
    
    fbshipit-source-id: 0b1da8a770fe564485ceb3a28c3d13831ee37528

--- exir/backend/test/backend_with_compiler_demo.py
+++ exir/backend/test/backend_with_compiler_demo.py
@@ -82,6 +82,7 @@ class BackendWithCompilerDemo(BackendDetails):
     ) -> PreprocessResult:
         processed_bytes = ""
         number_of_instruction = 0
+        version = "0"
         debug_handle_map = {}
         match_ops = [
             exir_ops.edge.aten.sin.default,
@@ -129,7 +130,12 @@ class BackendWithCompilerDemo(BackendDetails):
             debug_handle_map[new_debug_id] = (original_debug_id,)
         return PreprocessResult(
             processed_bytes=bytes(
-                str(number_of_instruction) + "#" + processed_bytes, encoding="utf8"
+                str(number_of_instruction)
+                + "version:"
+                + version
+                + "#"
+                + processed_bytes,
+                encoding="utf8",
             ),
             debug_handle_map=debug_handle_map,
         )
