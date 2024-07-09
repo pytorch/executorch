cd7a41629d05d5f2a7c8926cc86f0ced488e170a
Jorge Pineda <jorgep31415@meta.com>
2024-07-09 09:35:42 -0700

    Update on "[ET-VK] Consolidate MeanToSumDiv export pass into VulkanPreprocess"
    Clean up time. Previous issue fixed by registering `aten.mean.dim` in `supported_ops.py`.
    
    Differential Revision: [D59525641](https://our.internmc.facebook.com/intern/diff/D59525641/)
    
    [ghstack-poisoned]

--- backends/vulkan/vulkan_preprocess.py
+++ backends/vulkan/vulkan_preprocess.py
@@ -14,8 +14,8 @@ from executorch.backends.transforms.fuse_batch_norm_with_conv import (
 )
 from executorch.backends.transforms.fuse_conv_with_clamp import FuseClampPass
 from executorch.backends.transforms.fuse_view_copy import FuseViewCopyTransform
-from executorch.backends.transforms.remove_clone_ops import RemoveCloneOpsTransform
 from executorch.backends.transforms.mean_to_sum_div import MeanToSumDiv
+from executorch.backends.transforms.remove_clone_ops import RemoveCloneOpsTransform
 
 from executorch.backends.vulkan.serialization.vulkan_graph_builder import VkGraphBuilder
 from executorch.backends.vulkan.serialization.vulkan_graph_serialize import (
