d93176ad8fb8ec15cdd042c2734ca27ca25414ee
Jorge Pineda <jorgep31415@meta.com>
2024-07-09 15:32:52 -0700

    Create `containers/` folder in `api/` (#4181)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/4181
    
    ```
    find . -type f -exec sed -i 's/api\/Tensor/api\/containers\/Tensor/g' {} +
    find . -type f -exec sed -i 's/api\/ParamsBuffer/api\/containers\/ParamsBuffer/g' {} +
    find . -type f -exec sed -i 's/api\/StorageBuffer/api\/containers\/StorageBuffer/g' {} +
    ```
    ghstack-source-id: 233085712
    exported-using-ghexport
    
    bypass-github-export-checks
    bypass-github-pytorch-ci-checks
    bypass-github-executorch-ci-checks
    
    Reviewed By: SS-JIA
    
    Differential Revision: D59527912
    
    fbshipit-source-id: 09a152d42619ac649848c923c3ae7c6acf894489

--- backends/vulkan/runtime/graph/containers/SharedObject.h
+++ backends/vulkan/runtime/graph/containers/SharedObject.h
@@ -10,8 +10,7 @@
 
 // @lint-ignore-every CLANGTIDY facebook-hte-BadMemberName
 
-#include <executorch/backends/vulkan/runtime/api/Context.h>
-#include <executorch/backends/vulkan/runtime/api/Tensor.h>
+#include <executorch/backends/vulkan/runtime/api/api.h>
 
 #include <executorch/backends/vulkan/runtime/vk_api/Types.h>
 
