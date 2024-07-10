99a52a7c60ec8f3173e1e2fa17dada9622de40b7
haowhsu-quic <quic_haowhsu@quicinc.com>
2024-07-10 14:55:39 -0700

    Add memory planning pass for fully delegated path (#4176)
    Summary: Pull Request resolved: https://github.com/pytorch/executorch/pull/4176
    
    Reviewed By: tarun292
    
    Differential Revision: D59507586
    
    Pulled By: cccclai
    
    fbshipit-source-id: 8b4c196cfbb7eacaef37bc382dabdd290f0fa89a

--- exir/lowered_backend_module.py
+++ exir/lowered_backend_module.py
@@ -139,6 +139,7 @@ class LoweredBackendModule(torch.nn.Module):
         segment_alignment: int = 4096,
         constant_tensor_alignment: Optional[int] = None,
         delegate_alignment: Optional[int] = None,
+        memory_planning: MemoryPlanningPass = None,
     ) -> bytes:
         """
         Returns a buffer containing the serialized ExecuTorch binary.
@@ -146,7 +147,7 @@ class LoweredBackendModule(torch.nn.Module):
         # TODO(T181463742): avoid calling bytes(..) which incurs large copies.
         out = bytes(
             _serialize_pte_binary(
-                program=self.program(),
+                program=self.program(memory_planning=memory_planning),
                 extract_delegate_segments=extract_delegate_segments,
                 segment_alignment=segment_alignment,
                 constant_tensor_alignment=constant_tensor_alignment,
@@ -157,7 +158,11 @@ class LoweredBackendModule(torch.nn.Module):
 
     # TODO(chenlai): re-consider recapture instead of manually constructing the program because
     # the meta data construction is done manually.
-    def program(self, emit_stacktrace: bool = False) -> Program:
+    def program(
+        self,
+        emit_stacktrace: bool = False,
+        memory_planning: MemoryPlanningPass = None,
+    ) -> Program:
         # Fix autodpes introuces cyclic dependencies:
         # program -> verifier -> lowered_backend_module -> program
         # @manual
@@ -319,9 +324,9 @@ class LoweredBackendModule(torch.nn.Module):
             example_inputs=None,
             verifier=lowered_exported_program.verifier,
         )
-        exported_program = _transform(
-            exported_program, SpecPropPass(), MemoryPlanningPass("greedy")
-        )
+        if memory_planning is None:
+            memory_planning = MemoryPlanningPass("greedy")
+        exported_program = _transform(exported_program, SpecPropPass(), memory_planning)
         emitted_program = emit_program(
             exported_program, emit_stacktrace=emit_stacktrace
         ).program
