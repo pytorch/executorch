b6ac61ddcc94923749b64feaec969c707c6e724b
Esteban Padilla Cerdio <estebanpadilla@meta.com>
2024-07-11 09:41:17 -0700

    RegCount concurrency calculation (#4173)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/4173
    
    This project adds an internal implementation of https://github.com/microsoft/ArchProbe.
    
    This stack introduces a kernel that can be used to get the number of available registers on a mobile GPU by gradually increasing the number of accessed elements and detecting dramatic drops in performance. See [this paper](https://www.microsoft.com/en-us/research/uploads/prod/2022/02/mobigpu_mobicom22_camera.pdf), page 4, for more information.
    
    This diff finds the number of concurrency groups available for full registry usage, and half registry usage.
    
    On a Samsung Galaxy S22, the latency graphs for full and half registry usage look like this:
    
    Using all available registers:
     {F1751911855}
    
    Using half available registers:
    {F1751912469}
    
    Reviewed By: SS-JIA
    
    Differential Revision: D59497314

--- backends/vulkan/tools/gpuinfo/src/app.cpp
+++ backends/vulkan/tools/gpuinfo/src/app.cpp
@@ -21,9 +21,13 @@ void reg_count() {
   const double COMPENSATE = 0.01;
   const double THRESHOLD = 3;
 
+  const uint32_t NGRP_MIN = 1;
+  const uint32_t NGRP_MAX = 64;
+  const uint32_t NGRP_STEP = 1;
+
   uint32_t NITER;
 
-  auto bench = [&](uint32_t nthread, uint32_t ngrp, uint32_t nreg) {
+  auto bench = [&](uint32_t ngrp, uint32_t nreg) {
     size_t len = sizeof(float);
     StorageBuffer buffer(context(), vkapi::kFloat, len);
     ParamsBuffer params(context(), int32_t(len));
@@ -35,8 +39,8 @@ void reg_count() {
       context()->submit_compute_job(
           VK_KERNEL_FROM_STR(shader_name),
           pipeline_barrier,
-          {nthread, ngrp, 1},
-          {nthread, 1, 1},
+          {1, ngrp, 1},
+          {1, 1, 1},
           {SV(NITER)},
           VK_NULL_HANDLE,
           0,
@@ -47,7 +51,7 @@ void reg_count() {
   };
 
   std::cout << "Calculating NITER..." << std::endl;
-  ensure_min_niter(1000, NITER, [&]() { return bench(1, 1, NREG_MIN); });
+  ensure_min_niter(1000, NITER, [&]() { return bench(1, NREG_MIN); });
   std::cout << "NITER," << NITER << std::endl;
 
   uint32_t nreg_max;
@@ -55,7 +59,7 @@ void reg_count() {
   DtJumpFinder<5> dj(COMPENSATE, THRESHOLD);
   uint32_t nreg = NREG_MIN;
   for (; nreg <= NREG_MAX; nreg += NREG_STEP) {
-    double time = bench(1, 1, nreg);
+    double time = bench(1, nreg);
     std::cout << "Testing nreg=\t" << nreg << "\tTime=\t" << time << std::endl;
     if (dj.push(time)) {
       nreg -= NREG_STEP;
@@ -69,6 +73,51 @@ void reg_count() {
   } else {
     std::cout << nreg_max << " registers are available at most" << std::endl;
   }
+
+  auto find_ngrp_by_nreg = [&](const uint32_t nreg) {
+    DtJumpFinder<5> dj(COMPENSATE, THRESHOLD);
+    for (auto ngrp = NGRP_MIN; ngrp <= NGRP_MAX; ngrp += NGRP_STEP) {
+      auto time = bench(ngrp, nreg);
+      std::cout << "Testing occupation (nreg=" << nreg << "); ngrp=" << ngrp
+                << ", time=" << time << " us" << std::endl;
+
+      if (dj.push(time)) {
+        ngrp -= NGRP_STEP;
+        std::cout << "Using " << nreg << " registers can have " << ngrp
+                  << " concurrent single-thread workgroups" << std::endl;
+        return ngrp;
+      }
+    }
+    std::cout
+        << "Unable to conclude a maximum number of concurrent single-thread workgroups when "
+        << nreg << " registers are occupied" << std::endl;
+    return (uint32_t)1;
+  };
+
+  uint32_t ngrp_full, ngrp_half;
+  ngrp_full = find_ngrp_by_nreg(nreg_max);
+  ngrp_half = find_ngrp_by_nreg(nreg_max / 2);
+
+  std::string reg_ty;
+
+  if (ngrp_full * 1.5 < ngrp_half) {
+    std::cout << "All physical threads in an sm share " << nreg_max
+              << " registers" << std::endl;
+    reg_ty = "Pooled";
+
+  } else {
+    std::cout << "Each physical thread has " << nreg_max << " registers"
+              << std::endl;
+    reg_ty = "Dedicated";
+  }
+
+  std::cout << "\n\nNITER," << NITER << std::endl;
+  std::cout << "Max registers," << nreg_max << std::endl;
+  std::cout << "Concurrent full single thread workgroups," << ngrp_full
+            << std::endl;
+  std::cout << "Concurrent half single thread workgroups," << ngrp_half
+            << std::endl;
+  std::cout << "Register type," << reg_ty << std::endl;
 }
 
 int main(int argc, const char** argv) {
