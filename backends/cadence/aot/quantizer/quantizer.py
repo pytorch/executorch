be8a51c60ef64880b0ba3ef30e91014ab50aeee9
Matthias Cremon <matthiascremon@meta.com>
2024-07-12 14:25:34 -0700

    Add support for quantized bmm (#4047)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/4047
    
    The current quantizer only captures "fake" bmm from matmuls with specific shapes. Add support for `torch.bmm` as well
    
    Reviewed By: dulinriley, zonglinpengmeta, hsharma35
    
    Differential Revision: D58959269
    
    fbshipit-source-id: cb36eede25047a144bb0334da1847ca4381f7929

--- backends/cadence/aot/quantizer/quantizer.py
+++ backends/cadence/aot/quantizer/quantizer.py
@@ -4,11 +4,14 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import List
+# pyre-strict
+
+from typing import List, Optional, Tuple, Union
 
 import torch
 from executorch.backends.cadence.aot.quantizer.patterns import (
     AddmmPattern,
+    BmmPattern,
     Conv1dPattern,
     Conv2dPattern,
     LayerNormFunctionalPattern,
@@ -16,18 +19,20 @@ from executorch.backends.cadence.aot.quantizer.patterns import (
     LinearFunctionalPattern,
     LinearPattern,
     MatmulPattern,
+    QuantizationPattern,
     ReluPattern,
 )
 from executorch.backends.cadence.aot.quantizer.utils import (
     is_annotated,
     no_outside_users,
 )
+from pyre_extensions import assert_is_instance
 
 from torch import fx
 
 from torch.ao.quantization.observer import HistogramObserver, MinMaxObserver
 from torch.ao.quantization.pt2e.graph_utils import find_sequential_partitions
-from torch.ao.quantization.quantizer import Quantizer
+from torch.ao.quantization.quantizer import DerivedQuantizationSpec, Quantizer
 from torch.ao.quantization.quantizer.composable_quantizer import ComposableQuantizer
 from torch.ao.quantization.quantizer.xnnpack_quantizer_utils import (
     OperatorConfig,
@@ -55,16 +60,18 @@ wgt_qspec = QuantizationSpec(
     observer_or_fake_quant_ctr=MinMaxObserver,
 )
 
-bias_qspec = None
+bias_qspec: Optional[QuantizationSpec] = None
 
 
 class CadenceGenericQuantizer(Quantizer):
-    def __init__(self, pattern, quantization_config):
+    def __init__(
+        self, pattern: QuantizationPattern, quantization_config: QuantizationConfig
+    ) -> None:
         super().__init__()
         self.pattern = pattern
         self.quantization_config = quantization_config
 
-    def annotate(self, model):
+    def annotate(self, model: torch.fx.GraphModule) -> torch.fx.GraphModule:
         fused_partitions = find_sequential_partitions(
             model,
             self.pattern.partition_types(),
@@ -94,25 +101,40 @@ class CadenceGenericQuantizer(Quantizer):
                 continue
 
             for output, *custom_spec in anchors.output:
-                output.meta["quantization_annotation"] = QuantizationAnnotation(
-                    output_qspec=custom_spec[0] if custom_spec else output_act_qspec,
-                    _annotated=True,
+                assert_is_instance(output, fx.Node).meta["quantization_annotation"] = (
+                    QuantizationAnnotation(
+                        # pyre-ignore[6]: incompatible parameter type
+                        output_qspec=(
+                            custom_spec[0] if custom_spec else output_act_qspec
+                        ),
+                        _annotated=True,
+                    )
                 )
 
-            def annotate_inputs(inputs, spec):
+            def annotate_inputs(
+                inputs: Union[
+                    List[Tuple[fx.Node, int]],
+                    List[Tuple[fx.Node, int, DerivedQuantizationSpec],],
+                ],
+                spec: Optional[QuantizationSpec],
+            ) -> None:
                 for node, idx, *custom_spec in inputs:
-                    annotation = node.meta.get(
+                    _node = assert_is_instance(node, fx.Node)
+                    annotation = _node.meta.get(
                         "quantization_annotation",
                         QuantizationAnnotation(_annotated=True),
                     )
-                    annotation.input_qspec_map[node.args[idx]] = (
+                    # pyre-ignore[6]: incompatible parameter type
+                    annotation.input_qspec_map[_node.args[idx]] = (
                         custom_spec[0] if custom_spec else spec
                     )
-                    node.meta["quantization_annotation"] = annotation
+                    _node.meta["quantization_annotation"] = annotation
 
             annotate_inputs(anchors.inputs, input_act_qspec)
             annotate_inputs(anchors.weights, weight_qspec)
+            # pyre-ignore[6]: incompatible parameter type
             annotate_inputs(anchors.biases, bias_qspec)
+        return model
 
     def validate(self, model: fx.GraphModule) -> None:
         pass
@@ -123,7 +145,7 @@ class CadenceGenericQuantizer(Quantizer):
 
 
 class CadenceQuantizer(ComposableQuantizer):
-    def __init__(self):
+    def __init__(self) -> None:
         static_qconfig = QuantizationConfig(
             act_qspec,
             act_qspec,
@@ -133,6 +155,7 @@ class CadenceQuantizer(ComposableQuantizer):
         super().__init__(
             [
                 CadenceGenericQuantizer(AddmmPattern(), static_qconfig),
+                CadenceGenericQuantizer(BmmPattern(), static_qconfig),
                 CadenceGenericQuantizer(Conv1dPattern(), static_qconfig),
                 CadenceGenericQuantizer(Conv2dPattern(), static_qconfig),
                 CadenceGenericQuantizer(LayerNormPattern(), static_qconfig),
