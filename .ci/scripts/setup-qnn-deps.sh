4f1b7bb66d85c7855e2880c4fac1ecec9fb54d0e
Chen Lai <chenlai@meta.com>
2024-07-16 13:42:27 -0700

    add script to install qnn

---
+++ .ci/scripts/setup-qnn-deps.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+set -ex
+
+install_qnn() {
+  echo "Start installing qnn."
+
+  curl -Os --retry 3 "https://softwarecenter.qualcomm.com/api/download/software/qualcomm_neural_processing_sdk/v2.23.0.24.06.24.zip" --output v2.23.0.24.06.24.zip
+  echo "Finishing downloading qnn sdk."
+  unzip -qo "v2.23.0.24.06.24.zip"
+  echo "Finishing unzip qnn sdk."
+
+  QNN_INSTALLATION_DIR=/opt/qnn
+  mkdir -p "${QNN_INSTALLATION_DIR}"
+
+  pushd /tmp
+  # The NDK installation is cached on ossci-android S3 bucket
+  curl -Os --retry 3 "https://softwarecenter.qualcomm.com/api/download/software/qualcomm_neural_processing_sdk/v2.23.0.24.06.24.zip"
+  unzip -qo "v2.23.0.24.06.24.zip"
+
+  # Print the content for manual verification
+  ls -lah "qairt"
+  mv "qairt"/* "${NDK_INSTALLATION_DIR}"
+  echo "Finishing installing qnn."
+
+  popd
+}
+
+install_qnn
