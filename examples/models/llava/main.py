c672405605534ecd89b1c59b9b7eb71a0c71248a
Mengwei Liu <larryliu@meta.com>
2024-07-18 10:36:28 -0700

    Update base for Update on "[llava][6/N] Add export_llava.py"
    Summary: As titled. Pending CI job
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    Differential Revision: [D59901269](https://our.internmc.facebook.com/intern/diff/D59901269)
    
    [ghstack-poisoned]

--- examples/models/llava/main.py
+++ examples/models/llava/main.py
@@ -7,7 +7,7 @@
 import torch
 
 from model import LlavaModel
-
+import logging
 
 def main():
 
@@ -15,7 +15,7 @@ def main():
     llava = llava_model.get_eager_model()
 
     prompt_before_image, resized, prompt_after_image = llava_model.get_example_inputs()
-    print(f"Prompt: {llava_model.prompt}")
+    logging.info(f"Prompt: {llava_model.prompt}")
     preprocessed = llava.image_preprocess(resized)
     with torch.inference_mode():
         output_ids = llava_model.model.generate(
@@ -31,7 +31,7 @@ def main():
     outputs = llava_model.tokenizer.batch_decode(output_ids, skip_special_tokens=True)[
         0
     ].strip()
-    print(f"Reference output: {outputs}")
+    logging.info(f"Reference output: {outputs}")
 
     # comparing with llava result
     # prefill_logits = llava.prefill(prompt_before_image, resized, prompt_after_image)
@@ -50,6 +50,14 @@ def main():
     #         torch.tensor([new_tokens[i]]), torch.tensor([context_len + i])
     #     )
     #     new_tokens.append(torch.argmax(logits[-1, :]))
+    prefill_logits = llava.prefill(prompt_before_image, resized, prompt_after_image)
+    context_len = prefill_logits.shape[1]
+    logging.info(prefill_logits)
+    new_tokens = [torch.argmax(prefill_logits[..., -1, :]).item()]
+    i = 0
+    logging.info(i, llava_model.tokenizer.decode(new_tokens[i]))
+    logits = llava.step(torch.tensor([new_tokens[i]]), torch.tensor([context_len + i]))
+    logging.info(logits)
 
 
 if __name__ == "__main__":
