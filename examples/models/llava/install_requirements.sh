5865a576dba8e93231ee99ade7cecc880ebc96b6
Guang Yang <guangyang@fb.com>
2024-07-19 14:25:29 -0700

    Update Hugging Face packages to latest versions (#4306)
    Summary: Pull Request resolved: https://github.com/pytorch/executorch/pull/4306
    
    Reviewed By: kirklandsign
    
    Differential Revision: D59977405
    
    Pulled By: guangy10
    
    fbshipit-source-id: 2ce889e6f49ade545a668244db8aab6e7f7bef01

--- examples/models/llava/install_requirements.sh
+++ examples/models/llava/install_requirements.sh
@@ -20,11 +20,12 @@ pip install bitsandbytes -I
 # numpy needs to be pin to 1.24. 1.26.4 will error out
 pip install numpy==1.24
 
-# Newer transformer will give TypeError: LlavaLlamaForCausalLM.forward() got an unexpected keyword argument 'cache_position'
-pip install transformers==4.37.2
-
 # The deps of llava can have different versions than deps of ExecuTorch.
 # For example, torch version required from llava is older than ExecuTorch.
 # To make both work, recover ExecuTorch's original dependencies by rerunning
 # the install_requirements.sh.
 bash -x ./install_requirements.sh --pybind xnnpack
+
+# Newer transformer will give TypeError: LlavaLlamaForCausalLM.forward() got an unexpected keyword argument 'cache_position'
+pip install timm==0.6.13
+pip install transformers==4.38.2
