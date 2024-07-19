d58e58df219c4e6e1a3ba0df8a4e2f50cf0618e2
Mengwei Liu <larryliu@fb.com>
2024-07-19 15:52:16 -0700

    Lint

--- examples/models/llava/test/test_llava.py
+++ examples/models/llava/test/test_llava.py
@@ -6,6 +6,7 @@
 
 import logging
 import unittest
+
 import torch
 
 from ..model import LlavaModel
@@ -13,15 +14,22 @@ from ..model import LlavaModel
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
 class TestLlava(unittest.TestCase):
     def setUp(self):
         self.llava_model = LlavaModel()
         self.llava = self.llava_model.get_eager_model()
-        self.prompt_before_image, self.resized, self.prompt_after_image = self.llava_model.get_inputs_for_prefill()
+        self.prompt_before_image, self.resized, self.prompt_after_image = (
+            self.llava_model.get_inputs_for_prefill()
+        )
 
     def test_prefill_logits(self):
-        prefill_logits = self.llava.prefill(self.prompt_before_image, self.resized, self.prompt_after_image)
-        prefill_logits_ref = self.llava.prefill_ref(self.prompt_before_image, self.resized, self.prompt_after_image)[0]
+        prefill_logits = self.llava.prefill(
+            self.prompt_before_image, self.resized, self.prompt_after_image
+        )
+        prefill_logits_ref = self.llava.prefill_ref(
+            self.prompt_before_image, self.resized, self.prompt_after_image
+        )[0]
         self.assertTrue(torch.allclose(prefill_logits, prefill_logits_ref, atol=3e-2))
 
     def test_generated_output(self):
@@ -38,10 +46,14 @@ class TestLlava(unittest.TestCase):
                 use_cache=True,
             )
 
-        ref_outputs = self.llava_model.tokenizer.batch_decode(output_ids, skip_special_tokens=True)[0].strip()
+        ref_outputs = self.llava_model.tokenizer.batch_decode(
+            output_ids, skip_special_tokens=True
+        )[0].strip()
 
         # being tested, using llama_transformer
-        prefill_logits = self.llava.prefill(self.prompt_before_image, self.resized, self.prompt_after_image)
+        prefill_logits = self.llava.prefill(
+            self.prompt_before_image, self.resized, self.prompt_after_image
+        )
         context_len = prefill_logits.shape[1]
         new_tokens = [torch.argmax(prefill_logits[..., -1, :]).item()]
         for i in range(4):
@@ -49,6 +61,8 @@ class TestLlava(unittest.TestCase):
                 torch.tensor([new_tokens[i]]), torch.tensor([context_len + i])
             )
             new_tokens.append(torch.argmax(logits[-1, :]).item())
-        
-        outputs = self.llava_model.tokenizer.batch_decode(torch.tensor([new_tokens]), skip_special_tokens=True)[0].strip()
+
+        outputs = self.llava_model.tokenizer.batch_decode(
+            torch.tensor([new_tokens]), skip_special_tokens=True
+        )[0].strip()
         self.assertEqual(outputs, ref_outputs)
