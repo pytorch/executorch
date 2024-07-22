13eb862a06ad733e3feb73b7db7f165224ceaefc
Mengwei Liu <larryliu@fb.com>
2024-07-22 16:22:29 -0700

    Try

--- examples/models/llava/install_requirements.sh
+++ examples/models/llava/install_requirements.sh
@@ -8,7 +8,15 @@
 set -x
 
 # install llava from the submodule
-pip install --force-reinstall -e examples/third-party/LLaVA
+# TODO: This doesn't work for macos, on python 3.12, because torch 2.1.2 is missing.
+# manually install dependencies so we don't have conflicts on torch
+pip install tokenizers==0.15.1 sentencepiece==0.1.99    \
+    shortuuid accelerate==0.21.0 peft                   \
+    pydantic markdown2[all]  scikit-learn==1.2.2        \
+    requests httpx==0.24.0 uvicorn fastapi              \
+    einops==0.6.1 einops-exts==0.0.4 timm==0.6.13
+
+pip install --force-reinstall -e examples/third-party/LLaVA --no-deps
 
 # not included in the pip install package, but needed in llava
 pip install protobuf
@@ -18,7 +26,7 @@ pip install protobuf
 pip install bitsandbytes -I
 
 # numpy needs to be pin to 1.24. 1.26.4 will error out
-pip install numpy==1.24
+pip install numpy==1.26.4
 
 # The deps of llava can have different versions than deps of ExecuTorch.
 # For example, torch version required from llava is older than ExecuTorch.
