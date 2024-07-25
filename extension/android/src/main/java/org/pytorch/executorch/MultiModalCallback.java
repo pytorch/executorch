f9ee05fa97673046f2fef70aff1b6d20cf611bee
Mengwei Liu <larryliu@fb.com>
2024-07-25 14:34:16 -0700

    [Test only] multimodal android binding
    [ghstack-poisoned]

---
+++ extension/android/src/main/java/org/pytorch/executorch/MultiModalCallback.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package org.pytorch.executorch;
+
+import com.facebook.jni.annotations.DoNotStrip;
+
+public interface MultiModalCallback {
+  /**
+   * Called when a new result is available from JNI. Users will keep getting onResult() invocations
+   * until generate() finishes.
+   *
+   * @param result Last generated token
+   */
+  @DoNotStrip
+  public void onResult(String result);
+
+  /**
+   * Called when the statistics for the generate() is available.
+   *
+   * @param tps Tokens/second for generated tokens.
+   */
+  @DoNotStrip
+  public void onStats(float tps);
+}
