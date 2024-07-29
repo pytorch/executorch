2755af8664e2c6a6c64bc9461ca25c67cc69b227
winskuo-quic <quic_winskuo@quicinc.com>
2024-07-29 15:03:31 -0700

    Qualcomm AI Engine Direct - Fix UT example script hang when exception happened (#4355)
    Summary:
    - Fix UT Example Script hang when exceptions happened during execution. While the main process is waiting for child process to return a message, the child process exits without exceptions properly caught.
    - Remove RemoveRedundancy Pass during quantizer to resolve memory format issues while quantizing.
    - Prevent constant being double dequant in AnnotateQuantAttrs Pass
    
    Pull Request resolved: https://github.com/pytorch/executorch/pull/4355
    
    Reviewed By: kirklandsign
    
    Differential Revision: D60177584
    
    Pulled By: cccclai
    
    fbshipit-source-id: fc4d277b8eef05bd42c4eae2b9aa67236f53cc32

--- examples/qualcomm/oss_scripts/esrgan.py
+++ examples/qualcomm/oss_scripts/esrgan.py
@@ -40,50 +40,7 @@ def get_instance(repo: str):
     return model.model.eval()
 
 
-if __name__ == "__main__":
-    parser = setup_common_args_and_variables()
-
-    parser.add_argument(
-        "-a",
-        "--artifact",
-        help="path for storing generated artifacts by this example. Default ./esrgan",
-        default="./esrgan",
-        type=str,
-    )
-
-    parser.add_argument(
-        "-r",
-        "--hr_ref_dir",
-        help="Path to the high resolution images",
-        default="",
-        type=str,
-    )
-
-    parser.add_argument(
-        "-l",
-        "--lr_dir",
-        help="Path to the low resolution image inputs",
-        default="",
-        type=str,
-    )
-
-    parser.add_argument(
-        "-d",
-        "--default_dataset",
-        help="If specified, download and use B100 dataset by torchSR API",
-        action="store_true",
-        default=False,
-    )
-
-    parser.add_argument(
-        "--oss_repo",
-        help="Path to cloned https://github.com/ai-forever/Real-ESRGAN",
-        type=str,
-        required=True,
-    )
-
-    args = parser.parse_args()
-
+def main(args):
     skip_node_id_set, skip_node_op_set = parse_skip_delegation_node(args)
 
     # ensure the working directory exist.
@@ -174,3 +131,56 @@ if __name__ == "__main__":
     else:
         print(f"Average of PSNR is: {avg_PSNR}")
         print(f"Average of SSIM is: {avg_SSIM}")
+
+
+if __name__ == "__main__":
+    parser = setup_common_args_and_variables()
+
+    parser.add_argument(
+        "-a",
+        "--artifact",
+        help="path for storing generated artifacts by this example. Default ./esrgan",
+        default="./esrgan",
+        type=str,
+    )
+
+    parser.add_argument(
+        "-r",
+        "--hr_ref_dir",
+        help="Path to the high resolution images",
+        default="",
+        type=str,
+    )
+
+    parser.add_argument(
+        "-l",
+        "--lr_dir",
+        help="Path to the low resolution image inputs",
+        default="",
+        type=str,
+    )
+
+    parser.add_argument(
+        "-d",
+        "--default_dataset",
+        help="If specified, download and use B100 dataset by torchSR API",
+        action="store_true",
+        default=False,
+    )
+
+    parser.add_argument(
+        "--oss_repo",
+        help="Path to cloned https://github.com/ai-forever/Real-ESRGAN",
+        type=str,
+        required=True,
+    )
+
+    args = parser.parse_args()
+    try:
+        main(args)
+    except Exception as e:
+        if args.ip and args.port != -1:
+            with Client((args.ip, args.port)) as conn:
+                conn.send(json.dumps({"Error": str(e)}))
+        else:
+            raise Exception(e)
