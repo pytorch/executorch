a567abfd0853c0c59302173cb11c727d5fae3416
Chirag Modi <cmodi@meta.com>
2024-07-30 15:38:18 -0700

    Porting over ET MultiModal Demo App (#4455)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/4455
    
    Adding ET demo app for multimodal support. This is the first diff that supports Llama3.
    
    This includes major changes to the existing Llama Demo app with having the following features:
    1. Llama3 support
    2. Settings activity
    3. UI/UX improvements to the mainactivity
    4. Ability to add multi-images in prep for multimodal support.
    5. Metrics
    
    Note: You'll need to build the `executorch-llama.aar` and have it placed in the `app/libs` folder.
    
    Reviewed By: kirklandsign
    
    Differential Revision: D60416605
    
    fbshipit-source-id: 262329c30e1ec28c3905da5c040dc661307f8666

--- examples/demo-apps/android/LlamaDemo/app/src/main/java/com/example/executorchllamademo/MessageAdapter.java
+++ examples/demo-apps/android/LlamaDemo/app/src/main/java/com/example/executorchllamademo/MessageAdapter.java
@@ -8,33 +8,86 @@
 
 package com.example.executorchllamademo;
 
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+import java.util.ArrayList;
 
 public class MessageAdapter extends ArrayAdapter<Message> {
-  public MessageAdapter(android.content.Context context, int resource) {
+
+  private final ArrayList<Message> savedMessages;
+
+  public MessageAdapter(
+      android.content.Context context, int resource, ArrayList<Message> savedMessages) {
     super(context, resource);
+    this.savedMessages = savedMessages;
   }
 
   @Override
   public View getView(int position, View convertView, ViewGroup parent) {
     Message currentMessage = getItem(position);
+    int layoutIdForListItem;
 
-    int layoutIdForListItem =
-        currentMessage.getIsSent() ? R.layout.sent_message : R.layout.received_message;
+    if (currentMessage.getMessageType() == MessageType.SYSTEM) {
+      layoutIdForListItem = R.layout.system_message;
+    } else {
+      layoutIdForListItem =
+          currentMessage.getIsSent() ? R.layout.sent_message : R.layout.received_message;
+    }
     View listItemView =
         LayoutInflater.from(getContext()).inflate(layoutIdForListItem, parent, false);
-    TextView messageTextView = listItemView.findViewById(R.id.message_text);
-    messageTextView.setText(currentMessage.getText());
+    if (currentMessage.getMessageType() == MessageType.IMAGE) {
+      ImageView messageImageView = listItemView.requireViewById(R.id.message_image);
+      messageImageView.setImageURI(Uri.parse(currentMessage.getImagePath()));
+      TextView messageTextView = listItemView.requireViewById(R.id.message_text);
+      messageTextView.setVisibility(View.GONE);
+    } else {
+      TextView messageTextView = listItemView.requireViewById(R.id.message_text);
+      messageTextView.setText(currentMessage.getText());
+    }
 
+    String metrics = "";
+    TextView tokensView;
     if (currentMessage.getTokensPerSecond() > 0) {
-      TextView tokensView = listItemView.findViewById(R.id.tokens_per_second);
-      tokensView.setText("" + currentMessage.getTokensPerSecond() + " t/s");
+      metrics = String.format("%.2f", currentMessage.getTokensPerSecond()) + "t/s  ";
+    }
+
+    if (currentMessage.getTotalGenerationTime() > 0) {
+      metrics = metrics + (float) currentMessage.getTotalGenerationTime() / 1000 + "s  ";
+    }
+
+    if (currentMessage.getTokensPerSecond() > 0 || currentMessage.getTotalGenerationTime() > 0) {
+      tokensView = listItemView.requireViewById(R.id.generation_metrics);
+      tokensView.setText(metrics);
+      TextView separatorView = listItemView.requireViewById(R.id.bar);
+      separatorView.setVisibility(View.VISIBLE);
+    }
+
+    if (currentMessage.getTimestamp() > 0) {
+      TextView timestampView = listItemView.requireViewById(R.id.timestamp);
+      timestampView.setText(currentMessage.getFormattedTimestamp());
     }
 
     return listItemView;
+  }
+
+  @Override
+  public void add(Message msg) {
+    super.add(msg);
+    savedMessages.add(msg);
+  }
+
+  @Override
+  public void clear() {
+    super.clear();
+    savedMessages.clear();
+  }
+
+  public ArrayList<Message> getSavedMessages() {
+    return savedMessages;
   }
 }
