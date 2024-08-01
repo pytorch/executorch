ac4da32e74d96905d0a0d7014c17c6a48d95b1c7
Lucy Qiu <lfq@meta.com>
2024-08-01 11:56:01 -0700

    update sampler reference (#4508)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/4508
    
    fix build https://www.internalfb.com/sandcastle/workflow/3607383301527440057
    
    Reviewed By: JacobSzwejbka
    
    Differential Revision: D60602001

--- extension/llm/sampler/test/test_sampler.cpp
+++ extension/llm/sampler/test/test_sampler.cpp
@@ -6,7 +6,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-#include <executorch/examples/models/llama2/sampler/sampler.h>
+#include <executorch/extension/llm/sampler/sampler.h>
 
 #include <gtest/gtest.h>
 #include <torch/torch.h>
