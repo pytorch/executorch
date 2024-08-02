c3cdbe9a5f80777bc309a748a2657a9105659862
Songhao Jia <gasoonjia@fb.com>
2024-08-01 17:18:28 -0700

    b&c start ops | add dim order sanity check
    Differential Revision: D59824515

--- kernels/portable/cpu/op_bitwise_not.cpp
+++ kernels/portable/cpu/op_bitwise_not.cpp
@@ -33,6 +33,8 @@ Tensor& bitwise_not_out(RuntimeContext& ctx, const Tensor& in, Tensor& out) {
       "Failed to resize output tensor.");
 
   ET_KERNEL_CHECK(ctx, tensors_have_same_dtype(in, out), InvalidArgument, out);
+  ET_KERNEL_CHECK(
+      ctx, tensors_have_same_dim_order(in, out), InvalidArgument, out);
 
   if (in.scalar_type() == exec_aten::ScalarType::Bool) {
     apply_unary_map_fn(
