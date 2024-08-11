8b9afafacb054f3257261ef756ca8fb31e4f9c05
Chen Lai <chenlai@fb.com>
2024-08-11 13:42:54 -0700

    fix sdk download

--- .ci/scripts/setup-qnn-deps.sh
+++ .ci/scripts/setup-qnn-deps.sh
@@ -13,22 +13,18 @@ install_qnn() {
   mkdir -p "${QNN_INSTALLATION_DIR}"
 
   pushd /tmp
-  curl -Os --retry 3 "https://softwarecenter.qualcomm.com/api/download/software/qualcomm_neural_processing_sdk/v2.23.0.24.06.24.zip"
+  wget get "https://softwarecenter.qualcomm.com/api/download/software/qualcomm_neural_processing_sdk/v2.23.0.24.06.24.zip"
   echo "Finishing downloading qnn sdk."
   unzip -qo "v2.23.0.24.06.24.zip"
   echo "Finishing unzip qnn sdk."
 
 
-  pushd /tmp
-  # The NDK installation is cached on ossci-android S3 bucket
-  curl -Os --retry 3 "https://softwarecenter.qualcomm.com/api/download/software/qualcomm_neural_processing_sdk/v2.23.0.24.06.24.zip"
-  unzip -qo "v2.23.0.24.06.24.zip"
-
   # Print the content for manual verification
   ls -lah "qairt"
-  mv "qairt"/* "${NDK_INSTALLATION_DIR}"
-  echo "Finishing installing qnn."
+  mv "qairt"/* "${QNN_INSTALLATION_DIR}"
+  echo "Finishing installing qnn '${QNN_INSTALLATION_DIR}' ."
 
+  ls -lah "${QNN_INSTALLATION_DIR}"
   popd
 }
 
