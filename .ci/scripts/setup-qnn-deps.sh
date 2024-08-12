03919c86d29e9cdb46f2c2296e57acb87686f9c4
Chen Lai <chenlai@fb.com>
2024-08-11 18:37:26 -0700

    fix qnn path

--- .ci/scripts/setup-qnn-deps.sh
+++ .ci/scripts/setup-qnn-deps.sh
@@ -12,20 +12,18 @@ install_qnn() {
   QNN_INSTALLATION_DIR=/tmp/qnn
   mkdir -p "${QNN_INSTALLATION_DIR}"
 
-  pushd /tmp
-  curl -Lo v2.23.0.24.06.24.zip "https://softwarecenter.qualcomm.com/api/download/software/qualcomm_neural_processing_sdk/v2.23.0.24.06.24.zip"
+  curl -Lo /tmp/v2.23.0.24.06.24.zip "https://softwarecenter.qualcomm.com/api/download/software/qualcomm_neural_processing_sdk/v2.23.0.24.06.24.zip"
   echo "Finishing downloading qnn sdk."
-  unzip -qo "v2.23.0.24.06.24.zip"
+  unzip -qo /tmp/v2.23.0.24.06.24.zip -d /tmp
   echo "Finishing unzip qnn sdk."
 
 
   # Print the content for manual verification
-  ls -lah "qairt"
-  mv "qairt"/* "${QNN_INSTALLATION_DIR}"
+  ls -lah "/tmp/qairt"
+  mv "/tmp/qairt"/* "${QNN_INSTALLATION_DIR}"
   echo "Finishing installing qnn '${QNN_INSTALLATION_DIR}' ."
 
   ls -lah "${QNN_INSTALLATION_DIR}"
-  popd
 }
 
 install_qnn
