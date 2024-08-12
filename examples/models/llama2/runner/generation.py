d0192e59a9b673dbf38b2825db351fc207ae6771
lucylq <lfq@meta.com>
2024-08-12 13:11:48 -0700

    add llama sdpa to generation script

--- examples/models/llama2/runner/generation.py
+++ examples/models/llama2/runner/generation.py
@@ -19,6 +19,11 @@ from executorch.examples.models.llama2.tokenizer.tiktoken import (
     Message,
     Tokenizer,
 )
+
+from executorch.extension.pybindings import portable_lib  # noqa
+# Note: import this after portable_lib
+from executorch.extension.llm.custom_ops import sdpa_with_kv_cache  # noqa
+
 from executorch.extension.pybindings.portable_lib import _load_for_executorch
 
 
