d51ec6d2709dea75b66d987e55dd8e7899b6746e
winskuo-quic <143469905+winskuo-quic@users.noreply.github.com>
2024-08-14 01:33:16 +0800

    Qualcomm AI Engine Direct - File Structure Refactoring
    Differential Revision: D61195190
    
    Pull Request resolved: https://github.com/pytorch/executorch/pull/4626

--- examples/qualcomm/scripts/mobilenet_v2.py
+++ examples/qualcomm/scripts/mobilenet_v2.py
@@ -14,7 +14,7 @@ import numpy as np
 import torch
 from executorch.backends.qualcomm.quantizer.quantizer import QuantDtype
 from executorch.examples.models.mobilenet_v2 import MV2Model
-from executorch.examples.qualcomm.scripts.utils import (
+from executorch.examples.qualcomm.utils import (
     build_executorch_binary,
     make_output_dir,
     parse_skip_delegation_node,
