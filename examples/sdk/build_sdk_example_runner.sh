73e46adfc4cb25a5e1f163226fb91d4f2607d5ee
Gyanendra Sinha <gyanendra_sinha@apple.com>
2024-08-14 18:53:37 -0700

    [Core ML] Implement intermediate tensor logging
    Differential Revision: D60838372
    
    Pull Request resolved: https://github.com/pytorch/executorch/pull/4557

--- examples/sdk/build_sdk_example_runner.sh
+++ examples/sdk/build_sdk_example_runner.sh
@@ -17,15 +17,46 @@ readonly EXECUTORCH_ROOT="${SCRIPT_DIR}/../.."
 # Allow overriding the number of build jobs. Default to 9.
 export CMAKE_BUILD_PARALLEL_LEVEL="${CMAKE_BUILD_PARALLEL_LEVEL:-9}"
 
+BUILD_COREML=OFF
+
+usage() {
+  echo "Builds sdk example runner."
+  echo "Options:"
+  echo "  --coreml             Include this flag to enable Core ML backend when building the SDK."
+  exit 0
+}
+
+for arg in "$@"; do
+  case $arg in
+      -h|--help) usage ;;
+      --coreml) BUILD_COREML=ON ;;
+      *)
+  esac
+done
+
 main() {
   cd "${EXECUTORCH_ROOT}"
 
   rm -rf cmake-out
-  cmake -DCMAKE_INSTALL_PREFIX=cmake-out \
+
+  if [[ "${BUILD_COREML}" == "ON" ]]; then
+    cmake -DCMAKE_INSTALL_PREFIX=cmake-out \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DEXECUTORCH_BUILD_SDK=ON \
+        -DEXECUTORCH_ENABLE_EVENT_TRACER=ON \
+        -DEXECUTORCH_BUILD_COREML=ON \
+        -Dprotobuf_BUILD_TESTS=OFF \
+        -Dprotobuf_BUILD_EXAMPLES=OFF \
+        -DEXECUTORCH_ENABLE_LOGGING=ON \
+        -Bcmake-out .
+  else
+   cmake -DCMAKE_INSTALL_PREFIX=cmake-out \
       -DCMAKE_BUILD_TYPE=Release \
       -DEXECUTORCH_BUILD_SDK=ON \
       -DEXECUTORCH_ENABLE_EVENT_TRACER=ON \
       -Bcmake-out .
+  fi
+
   cmake --build cmake-out --target install --config Release
 
   local example_dir=examples/sdk
@@ -34,6 +65,7 @@ main() {
   rm -rf ${build_dir}
   cmake -DCMAKE_PREFIX_PATH="${cmake_prefix_path}" \
       -DCMAKE_BUILD_TYPE=Release \
+      -DEXECUTORCH_BUILD_COREML=$BUILD_COREML \
       -B"${build_dir}" \
       "${example_dir}"
   cmake --build "${build_dir}" --config Release
