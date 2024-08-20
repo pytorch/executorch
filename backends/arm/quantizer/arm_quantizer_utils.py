17b7cb00c48e313844715a3a23c438283620d859
Erik Lundell <erik.lundell@arm.com>
2024-08-20 12:03:46 +0200

    Improve logic for getting submodules from target name
    With just getattr, some targets that contain indexes,
    for example something like module.layers.0.sequential[...],
    fail.
    
    Signed-off-by: Erik Lundell <erik.lundell@arm.com>
    Change-Id: I55660c60fac871f68c19346701ddf880f095cb00

--- backends/arm/quantizer/arm_quantizer_utils.py
+++ backends/arm/quantizer/arm_quantizer_utils.py
@@ -102,12 +102,19 @@ def is_input_ok_for_quantization(input_act: Node, gm: GraphModule):
     )
 
 
+def get_node_target(module: torch.nn.Module | GraphModule, target_str: str):
+    targets = target_str.split(".")
+    for target in targets[:-1]:
+        module = module.get_submodule(target)
+    return getattr(module, targets[-1])
+
+
 def is_input_large_scalar(node: Node, gm: GraphModule):
     """Check if input is a large scalar value. So that we can skip quantization for the node
     since histc op (in HistogramObserver) only works for values up to certain upper bound
     """
     if node.op == "get_attr" and isinstance(node.target, str):
-        tensor = getattr(gm, node.target)
+        tensor = get_node_target(gm, node.target)
         # torch.histc works until this upper bound
         HISTC_UPPER_BOUND = 3.4028235e15
         return tensor.numel() == 1 and abs(tensor.item()) > HISTC_UPPER_BOUND
