588ff22f7c01414e00dbf70147964acba6c6889b
Erik Lundell <erik.lundell@arm.com>
2024-08-20 11:03:23 +0200

    Implement dumping operator distribution for TOSA graph
    Change-Id: I946e8487ad185d9994049ddcdcf7b08153c2597b
    Signed-off-by: Erik Lundell <erik.lundell@arm.com>

--- backends/arm/test/misc/test_debug_feats.py
+++ backends/arm/test/misc/test_debug_feats.py
@@ -127,29 +127,28 @@ class TestNumericalDiffPrints(unittest.TestCase):
             self.fail()
 
 
-class TestDumpOperatorsAndDtypes(unittest.TestCase):
-    def test_dump_ops_and_dtypes(self):
-        model = Linear(20, 30)
-        (
-            ArmTester(
-                model,
-                example_inputs=model.get_inputs(),
-                compile_spec=common.get_tosa_compile_spec(),
-            )
-            .quantize()
-            .dump_dtype_distribution()
-            .dump_operator_distribution()
-            .export()
-            .dump_dtype_distribution()
-            .dump_operator_distribution()
-            .to_edge()
-            .dump_dtype_distribution()
-            .dump_operator_distribution()
-            .partition()
-            .dump_dtype_distribution()
-            .dump_operator_distribution()
+def test_dump_ops_and_dtypes():
+    model = Linear(20, 30)
+    (
+        ArmTester(
+            model,
+            example_inputs=model.get_inputs(),
+            compile_spec=common.get_tosa_compile_spec(),
         )
-        # Just test that there are no execeptions.
+        .quantize()
+        .dump_dtype_distribution()
+        .dump_operator_distribution()
+        .export()
+        .dump_dtype_distribution()
+        .dump_operator_distribution()
+        .to_edge()
+        .dump_dtype_distribution()
+        .dump_operator_distribution()
+        .partition()
+        .dump_dtype_distribution()
+        .dump_operator_distribution()
+    )
+    # Just test that there are no execptions.
 
 
 class TestCollateTosaTests(unittest.TestCase):
@@ -186,3 +185,42 @@ class TestCollateTosaTests(unittest.TestCase):
 
         os.environ.pop("TOSA_TESTCASES_BASE_PATH")
         shutil.rmtree("test_collate_tosa_tests", ignore_errors=True)
+
+
+def test_dump_tosa_ops(capsys):
+    model = Linear(20, 30)
+    (
+        ArmTester(
+            model,
+            example_inputs=model.get_inputs(),
+            compile_spec=common.get_tosa_compile_spec(),
+        )
+        .quantize()
+        .export()
+        .to_edge()
+        .partition()
+        .dump_operator_distribution()
+    )
+    captured = capsys.readouterr()
+    assert "Partition operators:" in captured.out
+    assert "TOSA operators:" in captured.out
+
+
+def test_fail_dump_tosa_ops(capsys):
+    class Add(torch.nn.Module):
+        def forward(self, x):
+            return x + x
+
+    model = Add()
+    compile_spec = common.get_u55_compile_spec()
+    (
+        ArmTester(model, example_inputs=(torch.ones(5),), compile_spec=compile_spec)
+        .quantize()
+        .export()
+        .to_edge()
+        .partition()
+        .dump_operator_distribution()
+    )
+    captured = capsys.readouterr()
+    assert "Partition operators:" in captured.out
+    assert "Can not get operator distribution for Vela command stream." in captured.out
