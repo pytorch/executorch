997fff9b9a290cbbb29e318916795c4a8d36cd41
Lunwen He <lunwenh@meta.com>
2024-08-21 15:31:16 -0700

    Make it runnable via buck (#4811)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/4811
    
    update the buck target and refactor the code to make it runnable via buck
    
    Reviewed By: dvorjackz
    
    Differential Revision: D61567500

--- examples/models/phi-3-mini/export_phi-3-mini.py
+++ examples/models/phi-3-mini/export_phi-3-mini.py
@@ -28,7 +28,7 @@ from transformers import Phi3ForCausalLM
 from .phi_3_mini import Phi3Mini
 
 
-def main(args) -> None:
+def export(args) -> None:
     torch.manual_seed(0)
 
     if args.context_length == "4k":
@@ -90,7 +90,7 @@ def main(args) -> None:
         file.write(et_program.buffer)
 
 
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-c",
@@ -113,4 +113,8 @@ if __name__ == "__main__":
         default="phi-3-mini.pte",
         help="Override the output filename of the saved pte model file.",
     )
-    main(parser.parse_args())
+    export(parser.parse_args())
+
+
+if __name__ == "__main__":
+    main()
