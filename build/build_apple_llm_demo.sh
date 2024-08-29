f05e3b038be4bdee069d0497fd20d5664fc4d748
Huy Do <huydhn@gmail.com>
2024-08-28 19:01:22 -0700

    Add a benchmark test for iOS LLaMA app

--- build/build_apple_llm_demo.sh
+++ build/build_apple_llm_demo.sh
@@ -11,14 +11,66 @@ TOKENIZER="${1:-bpe}"
 ARTIFACTS_DIR_NAME="$2"
 
 APP_PATH="examples/demo-apps/apple_ios/LLaMA/LLaMA"
+TEST_RESOURCE_PATH="examples/demo-apps/apple_ios/LLaMA/LLaMATests/Resources"
 
-xcodebuild build -project "${APP_PATH}.xcodeproj" \
-  -scheme LLaMA \
-  -destination platform="iOS" \
-  -allowProvisioningUpdates \
-  DEVELOPMENT_TEAM=78E7V7QP35 \
-  CODE_SIGN_STYLE=Manual \
-  PROVISIONING_PROFILE_SPECIFIER=iLLaMA \
-  CODE_SIGN_IDENTITY="iPhone Distribution" \
-  CODE_SIGNING_REQUIRED=No \
-  CODE_SIGNING_ALLOWED=No
+mkdir -p "${TEST_RESOURCE_PATH}"
+# Copy all existing model
+cp *.pte *.bin "${TEST_RESOURCE_PATH}/"
+
+if [[ "${TOKENIZER}" = "bpe" ]]; then
+  xcodebuild build -project "${APP_PATH}.xcodeproj" \
+    -scheme LLaMA \
+    -destination platform="iOS" \
+    -allowProvisioningUpdates \
+    DEVELOPMENT_TEAM=78E7V7QP35 \
+    CODE_SIGN_STYLE=Manual \
+    PROVISIONING_PROFILE_SPECIFIER=iLLaMA \
+    CODE_SIGN_IDENTITY="iPhone Distribution" \
+    CODE_SIGNING_REQUIRED=No \
+    CODE_SIGNING_ALLOWED=No \
+    GCC_PREPROCESSOR_DEFINITIONS='$GCC_PREPROCESSOR_DEFINITIONS ET_USE_TIKTOKEN=0 '
+else
+  xcodebuild build -project "${APP_PATH}.xcodeproj" \
+    -scheme LLaMA \
+    -destination platform="iOS" \
+    -allowProvisioningUpdates \
+    DEVELOPMENT_TEAM=78E7V7QP35 \
+    CODE_SIGN_STYLE=Manual \
+    PROVISIONING_PROFILE_SPECIFIER=iLLaMA \
+    CODE_SIGN_IDENTITY="iPhone Distribution" \
+    CODE_SIGNING_REQUIRED=No \
+    CODE_SIGNING_ALLOWED=No
+fi
+
+# The hack to figure out where the xctest package locates
+BUILD_DIR=$(xcodebuild -showBuildSettings -project "$APP_PATH.xcodeproj" -json | jq -r ".[0].buildSettings.BUILD_DIR")
+
+# Prepare the demo app
+MODE="Debug"
+PLATFORM="iphoneos"
+pushd "${BUILD_DIR}/${MODE}-${PLATFORM}"
+
+rm -rf Payload && mkdir Payload
+APP_NAME=LLaMA
+
+ls -lah
+cp -r "${APP_NAME}.app" Payload && zip -vr "${APP_NAME}.ipa" Payload
+
+popd
+
+# Prepare the test suite
+pushd "${BUILD_DIR}"
+
+ls -lah
+zip -vr "${MOCK_APP_NAME}.xctestrun.zip" *.xctestrun
+
+popd
+
+if [[ -n "${ARTIFACTS_DIR_NAME}" ]]; then
+  mkdir -p "${ARTIFACTS_DIR_NAME}"
+  # Prepare all the artifacts to upload
+  cp "${BUILD_DIR}/${MODE}-${PLATFORM}/${MOCK_APP_NAME}.ipa" "${ARTIFACTS_DIR_NAME}/"
+  cp "${BUILD_DIR}/${MOCK_APP_NAME}.xctestrun.zip" "${ARTIFACTS_DIR_NAME}/"
+
+  ls -lah "${ARTIFACTS_DIR_NAME}/"
+fi
---
+++ examples/demo-apps/apple_ios/LLaMA/LLaMATests/LLaMATests.swift
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import XCTest
+import LLaMARunner
+
+final class LLaMATests: XCTestCase {
+  func testExample() throws {
+    guard
+      let modelPath = Bundle(for: type(of: self))
+        .path(forResource: "model", ofType: "pte")
+    else {
+      XCTFail("Failed to get model path")
+      return
+    }
+      
+    guard
+      let tokenizerPath = Bundle(for: type(of: self))
+        .path(forResource: "tokenizer", ofType: "bin")
+    else {
+      XCTFail("Failed to get tokenizer path")
+      return
+    }
+      
+    let runner = Runner(modelPath: modelPath, tokenizerPath: tokenizerPath)
+    do {
+      try runner.load()
+    } catch let loadError {
+      XCTFail("Failed to load the model: \(loadError)")
+    }
+    XCTAssertTrue(runner.isloaded())
+  }
+}
