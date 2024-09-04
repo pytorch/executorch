59533b2caaeeaa9faf96ccb6adc44bb569c92af7
Manuel Candales <mcandales@meta.com>
2024-09-04 06:55:48 -0700

    Add op: narrow_copy.out (#5047)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/5047
    
    Refactor utilities used by slice_copy and slice_scatter into slice_util.h, and use for narrow_copy
    
    Differential Revision: D62156680

--- kernels/portable/cpu/util/copy_ops_util.cpp
+++ kernels/portable/cpu/util/copy_ops_util.cpp
@@ -411,33 +411,6 @@ void get_select_copy_out_target_size(
   }
 }
 
-bool check_slice_copy_args(
-    const Tensor& in,
-    int64_t dim,
-    int64_t step,
-    Tensor& out) {
-  ET_LOG_AND_RETURN_IF_FALSE(in.dim() > 0);
-  ET_LOG_AND_RETURN_IF_FALSE(tensors_have_same_dtype(in, out));
-  ET_LOG_AND_RETURN_IF_FALSE(tensor_has_dim(in, dim));
-  ET_LOG_MSG_AND_RETURN_IF_FALSE(
-      step > 0, "slice step must be greater than zero");
-  return true;
-}
-
-void get_slice_copy_out_target_size(
-    const Tensor& in,
-    int64_t dim,
-    int64_t num_values,
-    exec_aten::SizesType* out_sizes,
-    size_t* out_ndim) {
-  *out_ndim = in.dim();
-
-  for (size_t d = 0; d < in.dim(); ++d) {
-    out_sizes[d] = in.size(d);
-  }
-  out_sizes[dim] = num_values;
-}
-
 bool check_split_with_sizes_copy_args(
     const Tensor& in,
     exec_aten::ArrayRef<int64_t> split_sizes,
