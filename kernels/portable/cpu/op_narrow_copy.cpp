2f2568a85b639904556b8cb1e701417584efe269
Manuel Candales <42380156+manuelcandales@users.noreply.github.com>
2024-09-04 14:38:49 -0400

    Add op: narrow_copy.out
    Differential Revision: D62156680
    
    Pull Request resolved: https://github.com/pytorch/executorch/pull/5047

--- kernels/portable/cpu/op_narrow_copy.cpp
+++ kernels/portable/cpu/op_narrow_copy.cpp
@@ -16,43 +16,38 @@ namespace native {
 
 using Tensor = exec_aten::Tensor;
 
-Tensor& slice_copy_Tensor_out(
+Tensor& narrow_copy_out(
     RuntimeContext& ctx,
     const Tensor& in,
     int64_t dim,
-    exec_aten::optional<int64_t> start_val,
-    exec_aten::optional<int64_t> end_val,
-    int64_t step,
+    int64_t start,
+    int64_t length,
     Tensor& out) {
   (void)ctx;
 
   ET_KERNEL_CHECK(
-      ctx, check_slice_copy_args(in, dim, step, out), InvalidArgument, out);
+      ctx,
+      check_narrow_copy_args(in, dim, start, length, out),
+      InvalidArgument,
+      out);
 
   if (dim < 0) {
     dim += in.dim();
   }
 
-  // If user do not set value to end_val, set end to in.size(dim) (largest
-  // value available)
-  int64_t end = end_val.has_value() ? end_val.value() : in.size(dim);
-  // If user do not set value to start_val, set start to 0 (smallest value
-  // available)
-  int64_t start = start_val.has_value() ? start_val.value() : 0;
-
-  int64_t length = adjust_slice_indices(in.size(dim), &start, &end, step);
-
   // @lint-ignore CLANGTIDY facebook-hte-CArray
   Tensor::SizesType target_sizes[kTensorDimensionLimit];
   size_t target_ndim = 0;
-  get_slice_copy_out_target_size(in, dim, length, target_sizes, &target_ndim);
+  get_narrow_copy_out_target_size(in, dim, length, target_sizes, &target_ndim);
   ET_KERNEL_CHECK(
       ctx,
       resize_tensor(out, {target_sizes, target_ndim}) == Error::Ok,
       InvalidArgument,
       out);
 
-  compute_slice(in, dim, start, length, step, out);
+  if (length != 0) {
+    compute_slice(in, dim, start, length, 1, out);
+  }
 
   return out;
 }
