721308079aae214f020376453396b795866f61a8
Anthony Shoumikhin <shoumikhin@meta.com>
2024-09-09 20:38:31 -0700

    Let the app check "aatp/data" subdir for AWS. (#5208)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/5208
    
    Also handle same model name in multiple directories. Also, use separate build flags for debug and release.
    
    Reviewed By: huydhn, guangy10
    
    Differential Revision: D62409615

--- extension/apple/Benchmark/Tests/Tests.mm
+++ extension/apple/Benchmark/Tests/Tests.mm
@@ -22,82 +22,105 @@ using namespace ::executorch::runtime;
 @implementation Tests
 
 + (void)initialize {
-  if (self == [Tests class]) {
-    NSString *modelsDir = [[NSBundle bundleForClass:[self class]].resourcePath
-        stringByAppendingPathComponent:@"Models"];
-    NSArray *models =
-        [NSFileManager.defaultManager contentsOfDirectoryAtPath:modelsDir
-                                                          error:nil];
-    for (NSString *model in models) {
-      NSString *modelName = model.stringByDeletingPathExtension;
-      NSString *modelPath = [modelsDir stringByAppendingPathComponent:model];
-      XCTAssertGreaterThan(modelPath.length, 0);
-
-      SEL testLoadSelector = NSSelectorFromString(
-          [NSString stringWithFormat:@"test_load_%@", modelName]);
-      IMP testLoadImplementation = imp_implementationWithBlock(^(id _self) {
-        auto __block module = std::make_unique<Module>(modelPath.UTF8String);
-        [_self
-            measureWithMetrics:@[ [XCTClockMetric new], [XCTMemoryMetric new] ]
-                       options:XCTMeasureOptions.defaultOptions
-                         block:^{
-                           XCTAssertEqual(module->load_method("forward"),
-                                          Error::Ok);
-                         }];
-      });
-      class_addMethod(
-          [self class], testLoadSelector, testLoadImplementation, "v@:");
-
-      SEL testForwardSelector = NSSelectorFromString(
-          [NSString stringWithFormat:@"test_forward_%@", modelName]);
-      IMP testForwardImplementation = imp_implementationWithBlock(^(id _self) {
-        auto __block module = std::make_unique<Module>(modelPath.UTF8String);
-        XCTAssertEqual(module->load_method("forward"), Error::Ok);
-
-        const auto method_meta = module->method_meta("forward");
-        XCTAssertEqual(method_meta.error(), Error::Ok);
-
-        const auto num_inputs = method_meta->num_inputs();
-        XCTAssertGreaterThan(num_inputs, 0);
-
-        std::vector<std::vector<uint8_t>> buffers;
-        buffers.reserve(num_inputs);
-        std::vector<TensorPtr> tensors;
-        tensors.reserve(num_inputs);
-        std::vector<EValue> __block inputs;
-        inputs.reserve(num_inputs);
-
-        for (auto index = 0; index < num_inputs; ++index) {
-          auto input_tag = method_meta->input_tag(index);
-          XCTAssertEqual(input_tag.error(), Error::Ok);
-
-          switch (*input_tag) {
-          case Tag::Tensor: {
-            const auto tensor_meta = method_meta->input_tensor_meta(index);
-            XCTAssertEqual(tensor_meta.error(), Error::Ok);
-
-            const auto sizes = tensor_meta->sizes();
-            buffers.emplace_back(tensor_meta->nbytes(),
-                                 0b01010101); // Set all bytes to be non-zero.
-            tensors.emplace_back(from_blob(buffers.rbegin()->data(),
-                                           {sizes.begin(), sizes.end()},
-                                           tensor_meta->scalar_type()));
-            inputs.emplace_back(tensors.back());
-          } break;
-          default:
-            XCTFail("Unsupported tag %i at input %d", *input_tag, index);
-          }
+  if (self != [self class]) {
+    return;
+  }
+  for (NSBundle *bundle in @[
+         [NSBundle mainBundle],
+         [NSBundle bundleForClass:[self class]],
+       ]) {
+    for (NSString *directory in @[
+           @"Models",
+           @"aatp/data",
+         ]) {
+      NSString *directoryPath =
+          [bundle.resourcePath stringByAppendingPathComponent:directory];
+      NSArray *filePaths =
+          [NSFileManager.defaultManager contentsOfDirectoryAtPath:directoryPath
+                                                            error:nil];
+      for (NSString *filePath in filePaths) {
+        if (![filePath hasSuffix:@".pte"]) {
+          continue;
         }
-        [_self
-            measureWithMetrics:@[ [XCTClockMetric new], [XCTMemoryMetric new] ]
-                       options:XCTMeasureOptions.defaultOptions
-                         block:^{
-                           XCTAssertEqual(module->forward(inputs).error(),
-                                          Error::Ok);
-                         }];
-      });
-      class_addMethod(
-          [self class], testForwardSelector, testForwardImplementation, "v@:");
+        NSString *modelPath =
+            [directoryPath stringByAppendingPathComponent:filePath];
+        NSString *directoryName =
+            [directory stringByReplacingOccurrencesOfString:@"/"
+                                                 withString:@"_"]
+                .lowercaseString;
+        NSString *modelName =
+            modelPath.lastPathComponent.stringByDeletingPathExtension;
+
+        SEL testLoadSelector = NSSelectorFromString([NSString
+            stringWithFormat:@"test_load_%@_%@", directoryName, modelName]);
+        IMP testLoadImplementation = imp_implementationWithBlock(^(id _self) {
+          auto __block module = std::make_unique<Module>(modelPath.UTF8String);
+          [_self measureWithMetrics:@[
+            [XCTClockMetric new],
+            [XCTMemoryMetric new],
+          ]
+                            options:XCTMeasureOptions.defaultOptions
+                              block:^{
+                                XCTAssertEqual(module->load_method("forward"),
+                                               Error::Ok);
+                              }];
+        });
+        class_addMethod(
+            [self class], testLoadSelector, testLoadImplementation, "v@:");
+
+        SEL testForwardSelector = NSSelectorFromString([NSString
+            stringWithFormat:@"test_forward_%@_%@", directoryName, modelName]);
+        IMP testForwardImplementation = imp_implementationWithBlock(^(
+            id _self) {
+          auto __block module = std::make_unique<Module>(modelPath.UTF8String);
+          XCTAssertEqual(module->load_method("forward"), Error::Ok);
+
+          const auto method_meta = module->method_meta("forward");
+          XCTAssertEqual(method_meta.error(), Error::Ok);
+
+          const auto num_inputs = method_meta->num_inputs();
+          XCTAssertGreaterThan(num_inputs, 0);
+
+          std::vector<TensorPtr> __block tensors;
+          tensors.reserve(num_inputs);
+          std::vector<EValue> __block inputs;
+          inputs.reserve(num_inputs);
+
+          for (auto index = 0; index < num_inputs; ++index) {
+            const auto input_tag = method_meta->input_tag(index);
+            XCTAssertEqual(input_tag.error(), Error::Ok);
+
+            switch (*input_tag) {
+            case Tag::Tensor: {
+              const auto tensor_meta = method_meta->input_tensor_meta(index);
+              XCTAssertEqual(tensor_meta.error(), Error::Ok);
+
+              const auto sizes = tensor_meta->sizes();
+              tensors.emplace_back(make_tensor_ptr(
+                  tensor_meta->scalar_type(),
+                  {sizes.begin(), sizes.end()},
+                  std::vector<uint8_t>(tensor_meta->nbytes(), 0b01010101)));
+              inputs.emplace_back(tensors.back());
+            } break;
+            default:
+              XCTFail("Unsupported tag %i at input %d", *input_tag, index);
+            }
+          }
+          [_self measureWithMetrics:@[
+            [XCTClockMetric new],
+            [XCTMemoryMetric new],
+          ]
+                            options:XCTMeasureOptions.defaultOptions
+                              block:^{
+                                XCTAssertEqual(module->forward(inputs).error(),
+                                               Error::Ok);
+                              }];
+        });
+        class_addMethod([self class],
+                        testForwardSelector,
+                        testForwardImplementation,
+                        "v@:");
+      }
     }
   }
 }
