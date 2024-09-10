3839723781ce0661d254ef4f4c7963153028fb3e
Per Åstrand <per@users.noreply.github.com>
2024-09-10 05:29:19 +0200

    Add pass to convert special case of mean.dim to averagepool2d
    Differential Revision: D62034655
    
    Pull Request resolved: https://github.com/pytorch/executorch/pull/4900

--- backends/arm/operators/op_mean_dim.py
+++ backends/arm/operators/op_mean_dim.py
@@ -11,7 +11,6 @@ from executorch.backends.arm.operators.node_visitor import (
     register_node_visitor,
 )
 from executorch.backends.arm.tosa_mapping import TosaArg
-from executorch.backends.arm.tosa_utils import build_avg_pool_2d_common
 
 
 @register_node_visitor
@@ -29,30 +28,5 @@ class MeanDimVisitor(NodeVisitor):
         output: TosaArg,
         is_quant_node: bool,
     ) -> None:
-
-        input_tensor = inputs[0]
-        dim = node.args[1]
-        keep_dim = node.args[2]
-
-        # mean.dim(-1, -2) is the same as avg_pool2d when just computing mean over HW dimensions.
-        # Since tosa doesn't have mean.dim operation, lowers it to average pooling instead.
-        if dim == [-1, -2]:
-            if keep_dim is True:
-                # Given the shape format of input is (N, C, H, W)
-                kernel_size = [input_tensor.shape[2], input_tensor.shape[3]]
-                stride = [1, 1]
-                padding = [0, 0, 0, 0]
-
-                build_avg_pool_2d_common(
-                    node,
-                    tosa_graph,
-                    input_tensor,
-                    kernel_size,
-                    stride,
-                    padding,
-                    is_quant_node,
-                    output,
-                )
-                return
 
         raise AssertionError("unsupported")
