22240f3cdfbc29e64dda577ded6caf4b231815f1
Scott Wolchok <swolchok@fb.com>
2024-09-11 14:30:04 -0700

    [ExecuTorch] Clean commit of FFHT dependency
    Pull Request resolved: https://github.com/pytorch/executorch/pull/5185
    
    This is https://github.com/FALCONN-LIB/FFHT .
    ghstack-source-id: 241722288
    @exported-using-ghexport
    
    Differential Revision: [D60194972](https://our.internmc.facebook.com/intern/diff/D60194972/)

---
+++ extension/llm/custom_ops/spinquant/third-party/FFHT/fht_impl.h
@@ -0,0 +1,32 @@
+#ifndef _FHT_IMPL_H__
+#define _FHT_IMPL_H__
+
+#include "fast_copy.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __AVX__
+#include "fht_avx.c"
+#define VECTOR_WIDTH (32u)
+#else
+#include "fht_sse.c"
+#define VECTOR_WIDTH (16u)
+#endif
+
+int fht_float_oop(float *in, float *out, int log_n) {
+    fast_copy(out, in, sizeof(float) << log_n);
+    return fht_float(out, log_n);
+}
+
+int fht_double_oop(double *in, double *out, int log_n) {
+    fast_copy(out, in, sizeof(double) << log_n);
+    return fht_double(out, log_n);
+}
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif // ifndef _FHT_IMPL_H__
