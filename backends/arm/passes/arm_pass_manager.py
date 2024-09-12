095114ad07d90b3c10fe6dba7d0d721b8164b3fd
Erik Lundell <erik.lundell@arm.com>
2024-09-12 15:21:16 +0200

    Add CastInt64ToInt32Pass
    Integer placeholders, for example in this examples
    x = torch.Tensor([1.]) + 1
    Are by default lowered as int64 tensors.
    
    As int64 is not a valid TOSA dtype, we cast these
    tensors to int32.
    
    Reference:
    https://www.mlplatform.org/tosa/tosa_spec.html#_supported_number_formats
    
    Signed-off-by: Erik Lundell <erik.lundell@arm.com>
    Change-Id: Ice59625b7fd68ff3a544ee4648fe05f69b9333e6

--- backends/arm/passes/arm_pass_manager.py
+++ backends/arm/passes/arm_pass_manager.py
@@ -11,6 +11,7 @@ import torch
 from executorch.backends.arm.passes.annotate_channels_last_dim_order_pass import (
     AnnotateChannelsLastDimOrder,
 )
+from executorch.backends.arm.passes.cast_int64_pass import CastInt64ToInt32Pass
 from executorch.backends.arm.passes.convert_expand_copy_to_repeat import (
     ConvertExpandCopyToRepeatPass,
 )
@@ -36,6 +37,7 @@ class ArmPassManager(PassManager):
         self, exported_program: ExportedProgram, compile_spec: list[CompileSpec]
     ):
         """Apply passes before transforming program to backend"""
+        self.add_pass(CastInt64ToInt32Pass(exported_program))
         self.add_pass(SizeAdjustConv2DPass())
         self.add_pass(RemoveClonePass())
         self.add_pass(ConvertExpandCopyToRepeatPass())
