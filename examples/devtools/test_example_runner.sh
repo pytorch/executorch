3ad2f1604f477a5e807f1fe7d7556fef4c962807
Olivia Liu <olivialpx@meta.com>
2024-09-11 23:04:56 -0700

    Rename the "executorch/examples/sdk" folder to "executorch/examples/devtools" (#5207)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/5207
    
    This is part of the bigger renaming "sdk" -> "devtools" task.
    Renaming principles:
    1. Replace "sdk" in the file paths to "devtools", that is, "fbcode/executorch/examples/**sdk**" -> "fbcode/executorch/examples/**devtools**";
    2. When parent path has the word "devtools" in it, then the child paths do not include "devtools" anymore *to reduce redundancy*, e.g., "fbcode/executorch/examples/**sdk/sdk_example_runner**" -> "fbcode/executorch/examples/**devtools/example_runner**"
    
    Reviewed By: dbort
    
    Differential Revision: D62409715
    
    fbshipit-source-id: 00ee0dc540349d85ba344d07e0000dcc5205b74d

--- examples/devtools/test_example_runner.sh
+++ examples/devtools/test_example_runner.sh
@@ -5,7 +5,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-# Test the end-to-end flow of building sdk_example_runner and use it to run
+# Test the end-to-end flow of building devtools/example_runner and use it to run
 # an actual model.
 
 
@@ -14,7 +14,7 @@ set -e
 # shellcheck source=/dev/null
 source "$(dirname "${BASH_SOURCE[0]}")/../../.ci/scripts/utils.sh"
 
-cmake_install_executorch_sdk_lib() {
+cmake_install_executorch_devtools_lib() {
   echo "Installing libexecutorch.a, libportable_kernels.a, libetdump.a, libbundled_program.a"
   rm -rf cmake-out
 
@@ -27,10 +27,10 @@ cmake_install_executorch_sdk_lib() {
   cmake --build cmake-out -j9 --target install --config Release
 }
 
-test_cmake_sdk_example_runner() {
+test_cmake_devtools_example_runner() {
   echo "Exporting MobilenetV2"
-  ${PYTHON_EXECUTABLE} -m examples.sdk.scripts.export_bundled_program --model_name="mv2"
-  local example_dir=examples/sdk
+  ${PYTHON_EXECUTABLE} -m examples.devtools.scripts.export_bundled_program --model_name="mv2"
+  local example_dir=examples/devtools
   local build_dir=cmake-out/${example_dir}
   CMAKE_PREFIX_PATH="${PWD}/cmake-out/lib/cmake/ExecuTorch;${PWD}/cmake-out/third-party/gflags"
   rm -rf ${build_dir}
@@ -44,8 +44,8 @@ test_cmake_sdk_example_runner() {
   echo "Building ${example_dir}"
   cmake --build ${build_dir} -j9 --config Release
 
-  echo 'Running sdk_example_runner'
-  ${build_dir}/sdk_example_runner --bundled_program_path="./mv2_bundled.bpte"
+  echo 'Running example_runner'
+  ${build_dir}/example_runner --bundled_program_path="./mv2_bundled.bpte"
 }
 
 if [[ -z $PYTHON_EXECUTABLE ]];
@@ -58,5 +58,5 @@ then
   BUCK=buck2
 fi
 
-cmake_install_executorch_sdk_lib
-test_cmake_sdk_example_runner
+cmake_install_executorch_devtools_lib
+test_cmake_devtools_example_runner
