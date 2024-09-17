a336001cc11a27e377262387c005ae59ad200084
Nathanael See <nathanaelsee@meta.com>
2024-09-17 12:32:59 -0700

    Integrate axis mapping into binary op (#5408)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/5408
    
    Update binary op to support axis mapped textures.
    
    Reviewed By: SS-JIA
    
    Differential Revision: D62622013
    
    fbshipit-source-id: 070ce40e22f4fca4d438d8dd3a33887bee8fc78a

--- backends/vulkan/test/op_tests/cases.py
+++ backends/vulkan/test/op_tests/cases.py
@@ -49,6 +49,7 @@ def get_binary_elementwise_inputs():
             ((S, S1, S2), (S, S1, S2)),
             ((S, S1, S2), (S, S1, 1), 2.0),
             ((S, S1, S2), (S, 1, S2), 2.0),
+            ((XS, S, S1, S2), (XS, S, 1, 1), 2.0),
         ]
     )
     test_suite.layouts = [
