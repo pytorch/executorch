f4728f40bba4f23a802881e7c13bb88f73392aea
Zingo Andersen <zingo.andersen@arm.com>
2024-09-23 21:14:45 -0700

    Add all relevant testcases for Arm Ethos-U85 (#5346)
    Summary:
    Add separate tests for Ethos-U85 to all backend operator tests.
    Updated ethos-u-vela version to support more operators.
    
    Signed-off-by: Per Åstrand <[per.astrand@arm.com](mailto:per.astrand@arm.com)>
    Signed-off-by: Tom Allsop <[tom.allsop@arm.com](mailto:tom.allsop@arm.com)>
    
    Pull Request resolved: https://github.com/pytorch/executorch/pull/5346
    
    Reviewed By: manuelcandales
    
    Differential Revision: D62875027
    
    Pulled By: digantdesai
    
    fbshipit-source-id: 3bf238d81957258ee93ae235d575beff8a575191

--- examples/arm/aot_arm_compiler.py
+++ examples/arm/aot_arm_compiler.py
@@ -214,7 +214,11 @@ if __name__ == "__main__":
         edge = edge.to_backend(
             ArmPartitioner(
                 ArmCompileSpecBuilder()
-                .ethosu_compile_spec("ethos-u55-128")
+                .ethosu_compile_spec(
+                    "ethos-u55-128",
+                    system_config="Ethos_U55_High_End_Embedded",
+                    memory_mode="Shared_Sram",
+                )
                 .set_permute_memory_format(
                     args.model_name in MODEL_NAME_TO_MODEL.keys()
                 )
