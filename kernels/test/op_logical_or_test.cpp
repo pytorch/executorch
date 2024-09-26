071ce021bf09381dafe5f5f853f5487df9b76017
Scott Wolchok <swolchok@fb.com>
2024-09-26 13:53:43 -0700

    [ExecuTorch] Support bf16 for binary logical ops
    Differential Revision: [D63486223](https://our.internmc.facebook.com/intern/diff/D63486223/)
    
    [ghstack-poisoned]

--- kernels/test/op_logical_or_test.cpp
+++ kernels/test/op_logical_or_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * Copyright (c) Meta Platforms, Inc. andaffiliates.
  * All rights reserved.
  *
  * This source code is licensed under the BSD-style license found in the
@@ -7,22 +7,25 @@
  */
 
 #include <executorch/kernels/test/FunctionHeaderWrapper.h> // Declares the operator
-#include <executorch/kernels/test/TestUtil.h>
-#include <executorch/runtime/core/exec_aten/exec_aten.h>
-#include <executorch/runtime/core/exec_aten/testing_util/tensor_factory.h>
-#include <executorch/runtime/core/exec_aten/testing_util/tensor_util.h>
+#include <executorch/kernels/test/BinaryLogicalOpTest.h>
 
 #include <gtest/gtest.h>
 
-using namespace ::testing;
-using exec_aten::ScalarType;
 using exec_aten::Tensor;
-using torch::executor::testing::TensorFactory;
 
-class OpLogicalOrTest : public OperatorTest {
+class OpLogicalOrTest : public torch::executor::testing::BinaryLogicalOpTest {
  protected:
   Tensor&
-  op_logical_or_out(const Tensor& self, const Tensor& other, Tensor& out) {
+  op_out(const Tensor& self, const Tensor& other, Tensor& out) override {
     return torch::executor::aten::logical_or_outf(context_, self, other, out);
   }
+
+  double op_reference(double x, double y) const override {
+    uint64_t lhs, rhs;
+    std::memcpy(&lhs, &x, sizeof(lhs));
+    std::memcpy(&rhs, &y, sizeof(rhs));
+    return lhs || rhs;
+  }
 };
+
+IMPLEMENT_BINARY_LOGICAL_OP_TEST(OpLogicalOrTest)
