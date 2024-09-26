19ff6a6d202aa5aa5e52f833480a606986875547
Chen Lai <chenlai@fb.com>
2024-09-26 13:25:01 -0700

    Make export file compatible with buck
    Part of the export.py isn't working well with buck, move the function from `__main__.py` to a standalone `main` function so it can be called
    
    Differential Revision: [D63476613](https://our.internmc.facebook.com/intern/diff/D63476613/)
    
    [ghstack-poisoned]

--- examples/apple/coreml/scripts/export.py
+++ examples/apple/coreml/scripts/export.py
@@ -13,22 +13,29 @@ import coremltools as ct
 import executorch.exir as exir
 
 import torch
+from coremltools.optimize.torch.quantization.quantization_config import (
+    LinearQuantizerConfig,
+    QuantizationScheme,
+)
 
 from executorch.backends.apple.coreml.compiler import CoreMLBackend
 
 from executorch.backends.apple.coreml.partition import CoreMLPartitioner
+from executorch.backends.apple.coreml.quantizer import CoreMLQuantizer
 from executorch.devtools.etrecord import generate_etrecord
 from executorch.exir import to_edge
 
 from executorch.exir.backend.backend_api import to_backend
+from torch.ao.quantization.quantize_pt2e import convert_pt2e, prepare_pt2e
+
 from torch.export import export
 
 REPO_ROOT = pathlib.Path(__file__).resolve().parent.parent.parent.parent.parent
 EXAMPLES_DIR = REPO_ROOT / "examples"
 sys.path.append(str(EXAMPLES_DIR.absolute()))
 
-from models import MODEL_NAME_TO_MODEL
-from models.model_factory import EagerModelFactory
+from executorch.examples.models import MODEL_NAME_TO_MODEL
+from executorch.examples.models.model_factory import EagerModelFactory
 
 # Script to export a model with coreml delegation.
 
@@ -74,6 +81,13 @@ def parse_args() -> argparse.ArgumentParser:
     parser.add_argument("--generate_etrecord", action=argparse.BooleanOptionalAction)
     parser.add_argument("--save_processed_bytes", action=argparse.BooleanOptionalAction)
 
+    parser.add_argument(
+        "--quantize",
+        action=argparse.BooleanOptionalAction,
+        required=False,
+        help="Quantize CoreML model",
+    )
+
     args = parser.parse_args()
     return args
 
@@ -82,7 +96,7 @@ def partition_module_to_coreml(module):
     module = module.eval()
 
 
-def lower_module_to_coreml(module, compile_specs):
+def lower_module_to_coreml(module, compile_specs, example_inputs):
     module = module.eval()
     edge = to_edge(export(module, example_inputs), compile_config=_EDGE_COMPILE_CONFIG)
     # All of the subsequent calls on the edge_dialect_graph generated above (such as delegation or
@@ -109,9 +123,10 @@ def export_lowered_module_to_executorch_program(lowered_module, example_inputs):
     return exec_prog
 
 
-def save_executorch_program(exec_prog, model_name, compute_unit):
+def save_executorch_program(exec_prog, model_name, compute_unit, quantize):
     buffer = exec_prog.buffer
-    filename = f"{model_name}_coreml_{compute_unit}.pte"
+    data_type = "quantize" if quantize else "fp"
+    filename = f"{model_name}_coreml_{compute_unit}_{data_type}.pte"
     print(f"Saving exported program to {filename}")
     with open(filename, "wb") as file:
         file.write(buffer)
@@ -141,7 +156,7 @@ def generate_compile_specs_from_args(args):
     )
 
 
-if __name__ == "__main__":
+def main():
     args = parse_args()
 
     if args.model_name not in MODEL_NAME_TO_MODEL:
@@ -167,6 +182,23 @@ if __name__ == "__main__":
     if args.use_partitioner:
         model.eval()
         exir_program_aten = torch.export.export(model, example_inputs)
+        if args.quantize:
+            quantization_config = LinearQuantizerConfig.from_dict(
+                {
+                    "global_config": {
+                        "quantization_scheme": QuantizationScheme.affine,
+                        "activation_dtype": torch.quint8,
+                        "weight_dtype": torch.qint8,
+                        "weight_per_channel": True,
+                    }
+                }
+            )
+
+            quantizer = CoreMLQuantizer(quantization_config)
+            model = prepare_pt2e(model, quantizer)  # pyre-fixme[6]
+            model(*example_inputs)
+            exir_program_aten = convert_pt2e(model)
+
         edge_program_manager = exir.to_edge(exir_program_aten)
         edge_copy = copy.deepcopy(edge_program_manager)
         partitioner = CoreMLPartitioner(
@@ -179,6 +211,7 @@ if __name__ == "__main__":
     else:
         lowered_module, edge_copy = lower_module_to_coreml(
             module=model,
+            example_inputs=example_inputs,
             compile_specs=compile_specs,
         )
         exec_program = export_lowered_module_to_executorch_program(
@@ -186,10 +219,16 @@ if __name__ == "__main__":
             example_inputs,
         )
 
-    save_executorch_program(exec_program, args.model_name, args.compute_unit)
+    save_executorch_program(
+        exec_program, args.model_name, args.compute_unit, args.quantize
+    )
     generate_etrecord(f"{args.model_name}_coreml_etrecord.bin", edge_copy, exec_program)
 
     if args.save_processed_bytes and lowered_module is not None:
         save_processed_bytes(
             lowered_module.processed_bytes, args.model_name, args.compute_unit
         )
+
+
+if __name__ == "__main__":
+    main()
