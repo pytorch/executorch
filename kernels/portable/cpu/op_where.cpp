ae39cce0c959503d3f74a4ac57f9ab5612265788
Scott Wolchok <swolchok@fb.com>
2024-10-03 09:01:01 -0700

    [ExecuTorch] Simplify function pointers for apply_ternary_elementwise_fn
    Pull Request resolved: https://github.com/pytorch/executorch/pull/5833
    
    Cleaning up some of the required boilerplate. I updated op_clamp and op_where, but continued to not optimize op_where for size/build time. Ideal usage optimizing for size/build time would look like op_clamp.
    ghstack-source-id: 246132259
    @exported-using-ghexport
    
    Differential Revision: [D63790004](https://our.internmc.facebook.com/intern/diff/D63790004/)

--- kernels/portable/cpu/op_where.cpp
+++ kernels/portable/cpu/op_where.cpp
@@ -56,21 +56,10 @@ Tensor& where_out(
           b,
           cond,
           out,
-          [](const void* a_ptr) {
-            return static_cast<CTYPE_OUT>(
-                *reinterpret_cast<const CTYPE_A*>(a_ptr));
-          },
-          [](const void* b_ptr) {
-            return static_cast<CTYPE_OUT>(
-                *reinterpret_cast<const CTYPE_B*>(b_ptr));
-          },
-          [](const void* c_ptr) {
-            return static_cast<CTYPE_OUT>(
-                *reinterpret_cast<const uint8_t*>(c_ptr));
-          },
-          [](CTYPE_OUT result, void* out) {
-            *reinterpret_cast<CTYPE_OUT*>(out) = result;
-          });
+          internal::load_and_convert<CTYPE_OUT, CTYPE_A>,
+          internal::load_and_convert<CTYPE_OUT, CTYPE_B>,
+          internal::load_and_convert<CTYPE_OUT, uint8_t>,
+          internal::convert_and_store<CTYPE_OUT, CTYPE_OUT>);
     });
   });
 
