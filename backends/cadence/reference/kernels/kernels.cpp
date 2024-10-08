d56b46065991bff7852229c8d2cf7849b2a7677b
Zonglin Peng <zonglinpeng@fb.com>
2024-10-08 10:08:05 -0700

    make kernel header a full path

--- backends/cadence/reference/kernels/kernels.cpp
+++ backends/cadence/reference/kernels/kernels.cpp
@@ -6,7 +6,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-#include <kernels.h>
+#include <executorch/backends/cadence/reference/kernels/kernels.h>
 #include <math.h>
 #include <algorithm>
 #include <cstring>
