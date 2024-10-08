513d166339d41512cf68ea2f9ccec86f9c7d86bb
Stephen Jia <ssjia@meta.com>
2024-10-08 10:33:18 -0700

    Miscellaneous fixes for Vulkan docs (#5972)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/5972
    
    ## Context
    
    Implement various fixes to Vulkan delegate while performing QA for Vulkan docs. I elected to package everything into this one diff so that it is easy to cherrypick into the 0.4 release.
    
    imported-using-ghimport
    
    Test Plan: Imported from OSS
    
    Reviewed By: jorgep31415
    
    Differential Revision: D64022818
    
    Pulled By: SS-JIA
    
    fbshipit-source-id: 35782970e9db1ab33154ccbae2e10c77d911c041

--- backends/vulkan/partitioner/vulkan_partitioner.py
+++ backends/vulkan/partitioner/vulkan_partitioner.py
@@ -48,6 +48,10 @@ class VulkanSupportedOperators(OperatorSupportBase):
     def __init__(self, require_dynamic_shape: bool = False) -> None:
         super().__init__()
         self.require_dynamic_shapes = require_dynamic_shape
+        # The tensor dim limit is to guard against tensors with one or more
+        # large dimensions, which cannot be represented by an image texture due
+        # to the texture axis limits.
+        self.tensor_dim_limit = 16384
 
     # pyre-ignore
     def node_val_is_compatible(self, node_val: Any) -> bool:
@@ -67,6 +71,10 @@ class VulkanSupportedOperators(OperatorSupportBase):
             # bool dtype not currently supported
             if node_val.dtype == torch.bool:
                 return False
+
+            for dim in node_val.shape:
+                if dim > self.tensor_dim_limit:
+                    return False
 
         if isinstance(node_val, (list, tuple)):
             for item in node_val:
@@ -100,11 +108,14 @@ class VulkanSupportedOperators(OperatorSupportBase):
         if len(node.users) != 1:
             return False
 
-        if list(node.users.keys())[0].target in [
+        first_user = list(node.users.keys())[0]
+        if first_user.target in [
             exir_ops.edge.aten.mm.default,
             exir_ops.edge.aten.addmm.default,
         ]:
-            return True
+            # Only mark this node if the overall linear op is valid
+            if self.all_args_compatible(first_user):
+                return True
 
         return False
 
