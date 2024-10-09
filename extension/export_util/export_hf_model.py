4ec9b757a60b04b3970da9760353f70f02e5a8aa
helunwencser <lwhecser@gmail.com>
2024-10-09 15:09:36 -0700

    Remove logic for appending or prepending tokens (#4920)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/4920
    
    The runner should not prepend or append any tokens to the prompt. Users should make sure that the prompt is properly formatted as the model's chat template.
    
    This PR removes the logic for appending or prepending tokens.
    
    Test Plan:
    CI
    
    imported-using-ghimport
    
    Reviewed By: iseeyuan
    
    Differential Revision: D61824091
    
    Pulled By: helunwencser
    
    fbshipit-source-id: f2f25c17dbcfb499e6865649dac8e020d9f5d802

--- extension/export_util/export_hf_model.py
+++ extension/export_util/export_hf_model.py
@@ -85,8 +85,6 @@ def main() -> None:
             "get_head_dim": model.config.hidden_size / model.config.num_attention_heads,
             "get_max_batch_size": model.generation_config.cache_config.batch_size,
             "get_max_seq_len": model.generation_config.cache_config.max_cache_len,
-            "get_n_bos": 1,
-            "get_n_eos": 1,
             "get_n_kv_heads": model.config.num_key_value_heads,
             "get_n_layers": model.config.num_hidden_layers,
             "get_vocab_size": model.config.vocab_size,
