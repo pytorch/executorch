60755cc1fe8b0742ce38ef0476683dc8f451e3de
Tarun Karuturi <tkaruturi@meta.com>
2024-10-09 13:46:04 -0700

    Move vulkan.passes to vulkan._passes (#5919)
    Summary:
    Changing vulkan.passes to vulkan._passes to indicate that these passes are not covered under the API stability guarantee.
    
    Pull Request resolved: https://github.com/pytorch/executorch/pull/5919
    
    Reviewed By: helunwencser
    
    Differential Revision: D63926849
    
    fbshipit-source-id: bf135c46c6718bc37afa640cf51d004891516575

--- backends/transforms/fuse_conv_with_clamp.py
+++ backends/transforms/fuse_conv_with_clamp.py
@@ -7,7 +7,9 @@
 import sys
 
 import torch
-from executorch.backends.vulkan.passes.custom_ops_defs import conv_with_clamp_op  # noqa
+from executorch.backends.vulkan._passes.custom_ops_defs import (  # noqa
+    conv_with_clamp_op,
+)
 
 from executorch.exir.dialects._ops import ops as exir_ops
 from executorch.exir.pass_base import ExportPass, PassResult
