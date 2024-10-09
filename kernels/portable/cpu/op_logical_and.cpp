78946bf290f7a96291eff041d67682c4642efdb5
Manuel Candales <manuelcandales@gmail.com>
2024-10-08 19:51:35 -0700

    Update base for Update on "[ET][Portable][Build Size] Move to dtype_utils"
    Differential Revision: [D63994875](https://our.internmc.facebook.com/intern/diff/D63994875/)
    
    [ghstack-poisoned]

--- kernels/portable/cpu/op_logical_and.cpp
+++ kernels/portable/cpu/op_logical_and.cpp
@@ -26,6 +26,7 @@ Tensor& logical_and_out(
     const Tensor& a,
     const Tensor& b,
     Tensor& out) {
+  // @lint-ignore CLANGTIDY facebook-hte-CArray
   static constexpr const char op_name[] = "logical_and.out";
   return internal::logical_tensor_out<op_name>(logical_and, ctx, a, b, out);
 }
--- kernels/portable/cpu/op_mul.cpp
+++ kernels/portable/cpu/op_mul.cpp
@@ -41,6 +41,7 @@ Tensor& mul_out(
   // Compute Dtype
   ScalarType compute_type = utils::get_compute_type(common_type);
 
+  // @lint-ignore CLANGTIDY facebook-hte-CArray
   static constexpr const char op_name[] = "mul.out";
 
   ET_KERNEL_CHECK(
@@ -89,6 +90,7 @@ Tensor& mul_scalar_out(
   // Compute Dtype
   ScalarType compute_type = utils::get_compute_type(common_type);
 
+  // @lint-ignore CLANGTIDY facebook-hte-CArray
   static constexpr const char op_name[] = "mul.Scalar_out";
 
   ET_SWITCH_REALB_TYPES(compute_type, ctx, op_name, CTYPE_COMPUTE, [&]() {
