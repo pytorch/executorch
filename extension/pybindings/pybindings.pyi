da1d2ca3d647b744e516dd8b62c4e2aa0d884e0a
Mengwei Liu <larryliu@meta.com>
2024-10-09 00:54:29 -0700

    Add mapping from C++ program::verification to Python (#5915)
    Summary:
    As titled. This enables
    `portable_lib._load_for_executorch[_from_buffer]` to accept `Program::Verification` argument.
    
    See added test, now we can do something like:
    
    ```
    from executorch.extension.pybindings.portable_lib import Verification
    module = load_fn(
      exported_program.buffer,
      enable_etdump=False,
      debug_buffer_size=0,
      program_verification=Verification.Minimal,
    )
    ```
    
    Pull Request resolved: https://github.com/pytorch/executorch/pull/5915
    
    Test Plan: See unit test
    
    Reviewed By: dbort
    
    Differential Revision: D63987538
    
    Pulled By: larryliu0820
    
    fbshipit-source-id: b68d8d1149e2d46b90544679707f420179e72b19

--- extension/pybindings/pybindings.pyi
+++ extension/pybindings/pybindings.pyi
@@ -7,9 +7,21 @@
 # pyre-strict
 from __future__ import annotations
 
-from typing import Any, Dict, List, Optional, Sequence, Tuple
+from typing import Any, Dict, Enum, List, Optional, Sequence, Tuple
 
 from executorch.exir._warnings import experimental
+
+@experimental("This API is experimental and subject to change without notice.")
+class Verification(Enum):
+    """Verification maps C++ Program::Verification to Python.
+
+    .. warning::
+
+        This API is experimental and subject to change without notice.
+    """
+
+    Minimal: ...
+    InternalConsistency: ...
 
 @experimental("This API is experimental and subject to change without notice.")
 class ExecuTorchModule:
@@ -125,7 +137,10 @@ class MethodMeta:
 
 @experimental("This API is experimental and subject to change without notice.")
 def _load_for_executorch(
-    path: str, enable_etdump: bool = False, debug_buffer_size: int = 0
+    path: str,
+    enable_etdump: bool = False,
+    debug_buffer_size: int = 0,
+    program_verification: Verification = Verification.InternalConsistency,
 ) -> ExecuTorchModule:
     """Load an ExecuTorch Program from a file.
 
@@ -148,7 +163,10 @@ def _load_for_executorch(
 
 @experimental("This API is experimental and subject to change without notice.")
 def _load_for_executorch_from_buffer(
-    buffer: bytes, enable_etdump: bool = False, debug_buffer_size: int = 0
+    buffer: bytes,
+    enable_etdump: bool = False,
+    debug_buffer_size: int = 0,
+    program_verification: Verification = Verification.InternalConsistency,
 ) -> ExecuTorchModule:
     """Same as _load_for_executorch, but takes a byte buffer instead of a file path.
 
