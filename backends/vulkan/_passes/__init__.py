1a0c2c7320bf7069e161566c70608d431b807cfb
Stephen Jia <ssjia@meta.com>
2024-10-09 19:13:00 -0700

    Update `RemoveLocalScalarDenseOpsTransform` to tag scalar tensors as well (#6069)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/6069
    
    ## Context
    
    See the new docstrings added to `remove_local_scalar_dense_ops` for more details on what the pass is trying to achieve.
    
    The goal is to mark tensors that are consumed as scalars via `tensor[0].item()` as "scalar tensors" that will be represented as a `SymInt` object in the vulkan delegate instead of a regular `Tensor` object.
    
    This diff also adds an `__init__.py` file to the `_passes` folder to make it easier to include Vulkan passes from one place.
    ghstack-source-id: 247163956
    exported-using-ghexport
    
    Reviewed By: jorgep31415
    
    Differential Revision: D64139867
    
    fbshipit-source-id: 88ba420e107654d7eadb2cbca78a3750a51f74b0

---
+++ backends/vulkan/_passes/__init__.py
@@ -0,0 +1,7 @@
+from executorch.backends.vulkan._passes.remove_local_scalar_dense_ops import (
+    RemoveLocalScalarDenseOpsTransform,
+)
+
+__all__ = [
+    "RemoveLocalScalarDenseOpsTransform",
+]
