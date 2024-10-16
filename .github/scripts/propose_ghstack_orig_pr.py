a247f83d3f299755f44721d6fd987cdae37daae9
Hansong Zhang <hsz@meta.com>
2024-10-15 23:25:44 -0700

    Update
    [ghstack-poisoned]

--- .github/scripts/propose_ghstack_orig_pr.py
+++ .github/scripts/propose_ghstack_orig_pr.py
@@ -52,14 +52,17 @@ def extract_stack_from_body(pr_body: str) -> List[int]:
     """
 
     prs = []
+    ghstack_begin = "Stack from [ghstack](https://github.com/ezyang/ghstack) (oldest at bottom):"
+    ghstack_begin_seen = False
     for line in pr_body.splitlines():
+        if ghstack_begin in line:
+            ghstack_begin_seen = True
+        if not ghstack_begin_seen:
+            continue
         match = re.match(r"\*(?:.*?)? #(\d+)", line)
         if match:
             # It's a bullet followed by an integer.
             prs.append(int(match.group(1)))
-        elif line.strip() == "":
-            # End of the stack.
-            break
     return list(reversed(prs))
 
 
@@ -69,7 +72,7 @@ def get_pr_stack_from_number(pr_number: int, repo: Repository) -> List[int]:
     if not pr_stack:
         raise Exception(
             f"Could not find PR stack in body of #{pr_number}. "
-            + "Please make sure that the PR was created with ghexport."
+            + "Please make sure that the PR was created with ghstack."
         )
 
     return pr_stack
@@ -83,16 +86,12 @@ def create_prs_for_orig_branch(pr_stack: List[int], repo: Repository):
         pr = repo.get_pull(pr_stack[i])
         if not pr.is_merged():
             print("The PR (and stack above) is not merged yet, skipping")
-            return
+            # return
         # Check for invariant: For the current PR, it must be gh/user/x/base <- gh/user/x/head
         assert pr.base.ref.replace("base", "head") == pr.head.ref
         # The PR we want to create is then "branch_to_merge" <- gh/user/x/orig
         # gh/user/x/orig is the clean diff between gh/user/x/base <- gh/user/x/head
         orig_branch_merge_head = pr.base.ref.replace("base", "orig")
-        if repo.get_pulls(head=orig_branch_merge_head).totalCount > 0:
-            # There is already a PR for this diff, so we can skip it
-            print(f"PR for {orig_branch_merge_head} already exists, skipping")
-            continue
         bot_metadata = f"""This PR was created by the merge bot to help merge the original PR into the main branch.
 ghstack PR number: https://github.com/pytorch/executorch/pull/{pr.number}
 ^ Please use this as the source of truth for the PR number to reference in comments
@@ -102,12 +101,19 @@ Merge bot PR base: https://github.com/pytorch/executorch/tree/{orig_branch_merge
 Merge bot PR head: https://github.com/pytorch/executorch/tree/{orig_branch_merge_head}
 \nOriginal PR body:\n
         """
-        repo.create_pull(
-            base=orig_branch_merge_base,
-            head=orig_branch_merge_head,
-            title=pr.title,
-            body=bot_metadata + pr.body,
-        )
+
+        existing_orig_pr = repo.get_pulls(head="pytorch:" + orig_branch_merge_head, base=orig_branch_merge_base, state="open")
+        if existing_orig_pr.totalCount > 0:
+            for p in existing_orig_pr:
+                print(f"PR for {orig_branch_merge_head} already exists {p.number}")
+            # We don't need to create/edit because the head PR is merged and orig is finalized.
+        else:
+            repo.create_pull(
+                base=orig_branch_merge_base,
+                head=orig_branch_merge_head,
+                title=pr.title,
+                body=bot_metadata + pr.body,
+            )
         # Advance the base for the next PR
         orig_branch_merge_base = orig_branch_merge_head
 
