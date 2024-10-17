d7f66f075c9293cd9ffa983cae2780ed3a087677
Erik Lundell <erik.lundell@arm.com>
2024-10-17 11:52:37 -0700

    Fix scalar arithemetic and add test cases (#6224)
    Summary:
    Add UnsquezeScalarPlaceholders pass to make scalars rank 1 Add MatchShapesPass to guarantee same rank for all inputs for ops that require it.
    Additional fixes to make Scalar tests pass
    
    Map which cases work and which don't.
    
    Signed-off-by: Erik Lundell <erik.lundell@arm.com>
    Change-Id: I4ea5e189e26cf7aff391ec153d525b2fb61aa16f
    
    Fix shape issues
    
    Change-Id: I0b8588cd5f8b284c25e806bb83bc788067d5b649
    
    Pull Request resolved: https://github.com/pytorch/executorch/pull/6224
    
    Reviewed By: mergennachin
    
    Differential Revision: D64427014
    
    Pulled By: digantdesai
    
    fbshipit-source-id: 5295e9ffab1d848b111e0cb01aa0ce9142c20781

--- backends/arm/test/misc/test_lifted_tensor.py
+++ backends/arm/test/misc/test_lifted_tensor.py
@@ -3,40 +3,119 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+import operator
 import unittest
+from typing import Union
 
 import torch
 from executorch.backends.arm.test import common
 from executorch.backends.arm.test.tester.arm_tester import ArmTester
+from parameterized import parameterized
 
 
 class LiftedTensor(torch.nn.Module):
 
-    def __init__(self):
+    test_data = [
+        # (operator, test_data, length)
+        (operator.add, (torch.randn(2, 2), 2)),
+        (operator.truediv, (torch.ones(2, 2), 2)),
+        (operator.mul, (torch.randn(2, 2), 2)),
+        (operator.sub, (torch.rand(2, 2), 2)),
+    ]
+
+    def __init__(self, op: callable):
         super().__init__()
+        self.op = op
         self.lifted_tensor = torch.Tensor([[1, 2], [3, 4]])
 
     def forward(self, x: torch.Tensor, length) -> torch.Tensor:
         sliced = self.lifted_tensor[:, :length]
-        return sliced + x
+        return self.op(sliced, x)
+
+
+class LiftedScalarTensor(torch.nn.Module):
+    test_data = [
+        # (operator, test_data)
+        (operator.add, (torch.randn(2, 2),), 1.0),
+        (operator.truediv, (torch.randn(4, 2),), 1.0),
+        (operator.mul, (torch.randn(1, 2),), 2.0),
+        (operator.sub, (torch.randn(3),), 1.0),
+    ]
+
+    def __init__(self, op: callable, arg1: Union[int, float, torch.tensor]):
+        super().__init__()
+        self.op = op
+        self.arg1 = arg1
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.op(x, self.arg1)
 
 
 class TestLiftedTensor(unittest.TestCase):
     """Tests the ArmPartitioner with a placeholder of type lifted tensor."""
 
-    def test_partition_lifted_tensor(self):
+    @parameterized.expand(LiftedTensor.test_data)
+    def test_partition_lifted_tensor_tosa_MI(self, op, data):
         tester = (
             ArmTester(
-                LiftedTensor(),
-                example_inputs=(torch.ones(2, 2), 2),
+                LiftedTensor(op),
+                example_inputs=data,
                 compile_spec=common.get_tosa_compile_spec(),
             )
             .export()
             .to_edge()
-            .dump_artifact()
         )
         signature = tester.get_artifact().exported_program().graph_signature
         assert len(signature.lifted_tensor_constants) > 0
         tester.partition()
         tester.to_executorch()
-        tester.run_method_and_compare_outputs((torch.ones(2, 2), 2))
+        tester.run_method_and_compare_outputs(data)
+
+    @parameterized.expand(LiftedTensor.test_data)
+    def test_partition_lifted_tensor_tosa_BI(self, op, data):
+        tester = (
+            ArmTester(
+                LiftedTensor(op),
+                example_inputs=data,
+                compile_spec=common.get_tosa_compile_spec(),
+            )
+            .quantize()
+            .export()
+            .to_edge()
+        )
+        signature = tester.get_artifact().exported_program().graph_signature
+        assert len(signature.lifted_tensor_constants) == 0
+        tester.partition()
+        tester.to_executorch()
+        tester.run_method_and_compare_outputs(data)
+
+    @parameterized.expand(LiftedScalarTensor.test_data)
+    def test_partition_lifted_scalar_tensor_tosa_MI(self, op, data, arg1):
+        (
+            ArmTester(
+                LiftedScalarTensor(op, arg1),
+                example_inputs=(data),
+                compile_spec=common.get_tosa_compile_spec(),
+            )
+            .export()
+            .to_edge()
+            .partition()
+            .to_executorch()
+            .run_method_and_compare_outputs(data)
+        )
+
+    @parameterized.expand(LiftedScalarTensor.test_data)
+    def test_partition_lifted_scalar_tensor_tosa_BI(self, op, data, arg1):
+        (
+            ArmTester(
+                LiftedScalarTensor(op, arg1),
+                example_inputs=(data),
+                compile_spec=common.get_tosa_compile_spec(),
+            )
+            .quantize()
+            .export()
+            .to_edge()
+            .partition()
+            .to_executorch()
+            .run_method_and_compare_outputs(data)
+        )
