d8773aed7e5a1864c5e31864cf3d602c01eb1623
pytorchbot <soumith+bot@pytorch.org>
2024-10-23 19:24:03 -0700

    [ET-VK] Implement generic reduction shader + mean, sum, amax, amin (#6473)
    Pull Request resolved: https://github.com/pytorch/executorch/pull/6457
    
    ## Context
    
    Introduce a generic shader to compute reduction along a single dim, and `keepdim = True`. With the generic shader template, `mean`, `sum`, `amin`, and `amax` can be implemented.
    ghstack-source-id: 249709743
    @exported-using-ghexport
    
    Differential Revision: [D64840504](https://our.internmc.facebook.com/intern/diff/D64840504/)
    
    ---------
    
    Co-authored-by: Stephen Jia <ssjia@meta.com>

--- backends/vulkan/partitioner/supported_ops.py
+++ backends/vulkan/partitioner/supported_ops.py
@@ -89,6 +89,10 @@ SUPPORTS_DYNAMIC_SHAPE = [
     # Reduction
     exir_ops.edge.aten._log_softmax.default,
     exir_ops.edge.aten._softmax.default,
+    exir_ops.edge.aten.mean.dim,
+    exir_ops.edge.aten.sum.dim_IntList,
+    exir_ops.edge.aten.amax.default,
+    exir_ops.edge.aten.amin.default,
     # 2D Pooling
     exir_ops.edge.aten.avg_pool2d.default,
     exir_ops.edge.aten.max_pool2d_with_indices.default,
@@ -101,9 +105,6 @@ SUPPORTS_DYNAMIC_SHAPE = [
 ]
 
 NO_DYNAMIC_SHAPE = [
-    # Reduction
-    exir_ops.edge.aten.mean.dim,
-    exir_ops.edge.aten.sum.dim_IntList,
     # Normalization
     exir_ops.edge.aten._native_batch_norm_legit_no_training.default,
     exir_ops.edge.aten.native_layer_norm.default,
