918306d61ba458298069c9feb3faca19bd9221bb
Lunwen He <lunwenh@meta.com>
2024-10-25 16:09:09 -0700

    add the ability to run eager runner via buck (#6506)
    Summary:
    
    This is helpful when we want to debug in eager mode using buck.
    
    Differential Revision: D64730344

--- examples/models/llama/runner/eager.py
+++ examples/models/llama/runner/eager.py
@@ -9,14 +9,13 @@ import json
 from typing import Optional
 
 import torch
-
-from examples.models.llama.llama_transformer import ModelArgs
 from executorch.examples.models.llama.export_llama_lib import (
     _prepare_for_llama_export,
     build_args_parser as _build_args_parser,
 )
+from executorch.examples.models.llama.llama_transformer import ModelArgs
 from executorch.examples.models.llama.runner.generation import LlamaRunner
-from executorch.extension.llm.export import LLMEdgeManager
+from executorch.extension.llm.export.builder import LLMEdgeManager
 
 
 class EagerLlamaRunner(LlamaRunner):
@@ -43,8 +42,8 @@ class EagerLlamaRunner(LlamaRunner):
 
     def forward(
         self,
-        tokens: Optional[torch.LongTensor] = None,
-        input_pos: Optional[torch.LongTensor] = None,
+        tokens: torch.Tensor,
+        input_pos: Optional[torch.Tensor] = None,
     ) -> torch.Tensor:
         return self.model.forward(tokens=tokens, input_pos=input_pos)
 
