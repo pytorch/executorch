f24016d559d8aa3cebdb09d5442ca039314c2356
lucylq <lfq@meta.com>
2024-10-28 15:39:28 -0700

    Introduce data schema to store raw tensors
    [ghstack-poisoned]

---
+++ exir/schema_data.py
@@ -0,0 +1,45 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+
+from dataclasses import dataclass
+from typing import List, Optional
+
+from executorch.exir.scalar_type import ScalarType
+
+# Note: check executorch/schema/data.fbs for explanations of these fields.
+
+
+@dataclass
+class TensorMetadata:
+    fully_qualified_name: str
+    scalar_type: ScalarType
+    dim_sizes: List[int]
+    dim_order: List[bytes]
+
+    offset: int
+    size: int
+
+
+@dataclass
+class TensorSegment:
+    segment_index: int
+    tensor_metadata: List[TensorMetadata]
+
+
+@dataclass
+class DataSegment:
+    offset: int
+    size: int
+
+
+@dataclass
+class Data:
+    version: int
+    tensor_alignment: int
+    tensor_segments: List[TensorSegment]
+    data_segments: List[DataSegment]
