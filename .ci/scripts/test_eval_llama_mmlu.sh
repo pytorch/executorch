b1f66785c4ecc21eb922fa5025b6941e78e399f0
Jack Zhang <dvorjackz@gmail.com>
2024-10-28 08:11:06 -0700

    Fix tests again

--- .ci/scripts/test_eval_llama_mmlu.sh
+++ .ci/scripts/test_eval_llama_mmlu.sh
@@ -35,7 +35,7 @@ run_and_verify() {
         exit 1
     fi
     $PYTHON_EXECUTABLE -m examples.models.llama.eval_llama \
-	--model llama2
+	--model llama2 \
 	-c stories110M.pt \
 	-p params.json \
 	-t tokenizer.model \
