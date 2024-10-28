6a5622dd5abe73fd95bc5aa4f784a4c3754f6a80
Lunwen He <lwhecser@gmail.com>
2024-10-28 12:18:33 -0700

    upgrade lm_eval to 0.4.5
    [ghstack-poisoned]

--- examples/models/llama/evaluate/eager_eval.py
+++ examples/models/llama/evaluate/eager_eval.py
@@ -31,7 +31,7 @@ class EagerEvalWrapper(eval_wrapper):
         use_kv_cache: bool = False,
     ):
         device = "cuda" if torch.cuda.is_available() else "cpu"
-        super().__init__(device=device)
+        super().__init__(device=device, pretrained="gpt2")
         self._model = model
         self._tokenizer = tokenizer
         self._device = torch.device(device)
