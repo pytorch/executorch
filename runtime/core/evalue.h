bdc1b82f70b9d9de401f37670a7af6afb46d21f8
Tarun Karuturi <tkaruturi@meta.com>
2024-11-03 02:50:21 -0800

    Make EValue template constructor MSVC friendly
    Differential Revision: D65393407

--- runtime/core/evalue.h
+++ runtime/core/evalue.h
@@ -242,17 +242,20 @@ struct EValue {
   // Template constructor that allows construction from types that can be
   // dereferenced to produce a type that EValue can be implicitly constructed
   // from.
-  template <typename T>
-  /*implicit*/ EValue(
-      T&& value,
-      typename std::enable_if<std::is_convertible<
-          decltype(*std::forward<T>(value)),
-          EValue>::value>::type* = 0) {
+template <typename T,
+    typename = typename std::enable_if<
+        std::is_convertible<
+            decltype(*std::forward<T>(std::declval<T>())), // declval to simulate forwarding
+            EValue
+        >::value
+    >::type>
+/*implicit*/ EValue(T&& value) {
     ET_CHECK_MSG(value != nullptr, "Pointer is null.");
     // Note that this ctor does not initialize this->tag directly; it is set by
     // moving in the new value.
     moveFrom(*std::forward<T>(value));
-  }
+}
+
 
   // Delete constructor for raw pointers to ensure they cannot be used.
   template <typename T>
