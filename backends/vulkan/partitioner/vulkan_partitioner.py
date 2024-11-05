cefe51594f717e8c9d089d4b0a55de7dc9479c56
pytorchbot <soumith+bot@pytorch.org>
2024-11-05 12:18:45 -0800

    [ET-VK] Refine paritioner to account for storage type and memory layout (#6668)
    Pull Request resolved: https://github.com/pytorch/executorch/pull/6635
    
    ## Context
    
    There are a variety of ways that tensors can be represented in Vulkan. The two main descriptors for how a tensor is laid out in memory is:
    
    1. Storage Type (buffer or texture)
    2. Memory Layout (which dim is packed along a texel, which dim has a stride of 1, etc.)
    
    Due to the differences between buffers and textures, and the differences between different memory layouts, an implementation for an operator may only support a specific set of (storage type, memory layout) combinations.
    
    Furthermore, if an operator implementation supports multiple (storage type, memory layout) combinations, there may be a "preferred" setting which results in optimal performance.
    
    These changes lay the foundation for the implementation of a memory metadata tagging graph transform, which will make sure that all tensors participating in an operator call is has a valid/optimal (storage type, memory layout) setting, and insert transition operators to transfer input tensors to the correct memory settings when necessary.
    
    An additional change that is required arises from the fact that in Vulkan, there is a limit on texture and buffer sizes. Therefore, the partitioner needs to account for the storage types and memory layouts supported by the operator implementation, and check if all tensors participating in a computation can be represented with some storage type, memory layout combination supported by the implementation.
    
    
    ## Changes
    
    Improvements to the operator registry:
    
    * Introduce utility functions to check the optimal and enabled storage types and memory layouts for an operator
    
    Improvements to the Partitioner:
    
    * Account for the storage types and memory layouts supported by an operator when deciding if a node should be partitioned
    * Improved logic for fusable ops (i.e. the permute/transpose before a mm which can be fused into linear) to check if the final target op is supported in Vulkan, and only partition those nodes if so. Otherwise, don't partition it so that it can be fused by another backend.
    ghstack-source-id: 251883705
    @exported-using-ghexport
    
    Differential Revision: [D65428843](https://our.internmc.facebook.com/intern/diff/D65428843/)
    
    Co-authored-by: Stephen Jia <ssjia@meta.com>

--- backends/vulkan/partitioner/vulkan_partitioner.py
+++ backends/vulkan/partitioner/vulkan_partitioner.py
@@ -9,12 +9,23 @@
 import logging
 from typing import Any, Callable, Dict, final, List, Mapping, Optional, Tuple
 
-import executorch.backends.vulkan.serialization.vulkan_graph_schema as vk_graph_schema
+import executorch.backends.vulkan.utils as utils
 
 import torch
 
-from executorch.backends.vulkan.op_registry import vulkan_supported_ops
+from executorch.backends.vulkan.op_registry import (
+    get_op_features,
+    has_impl,
+    OpFeatures,
+    vulkan_supported_ops,
+)
+
+from executorch.backends.vulkan.serialization.vulkan_graph_schema import (
+    VkMemoryLayout,
+    VkStorageType,
+)
 from executorch.backends.vulkan.vulkan_preprocess import VulkanBackend
+
 from executorch.exir.backend.compile_spec_schema import CompileSpec
 from executorch.exir.backend.partitioner import (
     DelegationSpec,
@@ -24,7 +35,6 @@ from executorch.exir.backend.partitioner import (
 from executorch.exir.backend.utils import tag_constant_data
 from executorch.exir.dialects._ops import ops as exir_ops
 
-from torch._subclasses.fake_tensor import FakeTensor
 from torch.export.exported_program import ExportedProgram
 from torch.fx.passes.infra.partitioner import CapabilityBasedPartitioner
 
@@ -40,104 +50,140 @@ logger.setLevel(logging.INFO)
 
 
 class VulkanSupportedOperators(OperatorSupportBase):
-    def __init__(self, require_dynamic_shape: bool = False) -> None:
+    def __init__(
+        self, texture_limits: utils.ImageExtents, require_dynamic_shape: bool = False
+    ) -> None:
         super().__init__()
         self.require_dynamic_shapes = require_dynamic_shape
-        # The tensor dim limit is to guard against tensors with one or more
-        # large dimensions, which cannot be represented by an image texture due
-        # to the texture axis limits.
-        self.tensor_dim_limit = 16384
+        self.texture_limits: utils.ImageExtents = texture_limits
 
-    # pyre-ignore
-    def node_val_is_compatible(self, node_val: Any) -> bool:
-        # Skip nodes that don't have a value
-        if node_val is None:
-            return True
+    def op_node_is_compatible(
+        self, node: torch.fx.Node, features: Optional[OpFeatures] = None
+    ) -> Tuple[bool, str]:
+        """
+        Check if a given node is compatible with the Vulkan delegate's implementation
+        of the operator called by the node. Each tensor argument participating in the
+        operator call must be able to be represented with a (storage type, memory layout)
+        combination that is supported by the operator implementation.
+        """
+        target = node.target
+        # Account for custom operators
+        if node.target == torch.ops.higher_order.auto_functionalized:
+            first_arg = node.args[0]
+            assert isinstance(first_arg, torch._ops.OpOverload)
+            target = first_arg.name()
 
-        # TODO(ssjia) support symbolic ints
-        if isinstance(node_val, torch.SymInt):
-            return False
+        # Extract the features for the node's operator, if no override was provided
+        if features is None:
+            if not has_impl(target):
+                return False, "no operator implementation"
+            features = get_op_features(target)
 
-        if isinstance(node_val, FakeTensor):
-            # Vulkan currently only supports tensors of up to 4D
-            if len(node_val.shape) > 4:
-                return False
-
-            # bool dtype not currently supported
-            if node_val.dtype == torch.bool:
-                return False
-
-            for dim in node_val.shape:
-                if dim > self.tensor_dim_limit:
-                    return False
-
-        if isinstance(node_val, (list, tuple)):
-            for item in node_val:
-                if not self.node_val_is_compatible(item):
-                    return False
-
-        return True
-
-    def all_args_compatible(self, node: torch.fx.Node) -> bool:
-        node_val = node.meta.get("val", None)
-        if not self.node_val_is_compatible(node_val):
-            return False
-
+        valid_texture_layouts = utils.possible_node_memory_layouts(
+            node, self.texture_limits
+        )
         for arg in node.args:
-            if not isinstance(arg, torch.fx.Node):
-                continue
+            if isinstance(arg, torch.fx.Node) and utils.is_tensor_node(arg):
+                arg_texture_layouts = utils.possible_node_memory_layouts(
+                    arg, self.texture_limits
+                )
+                valid_texture_layouts = valid_texture_layouts.intersection(
+                    arg_texture_layouts
+                )
 
-            arg_val = arg.meta.get("val", None)
-            if not self.node_val_is_compatible(arg_val):
-                return False
+        # If there are no valid texture memory layouts, then buffer storage must be
+        # supported by the operator implementation.
+        if len(valid_texture_layouts) == 0:
+            # TODO: once memory metadata tagging pass is implemented, check that the
+            # op impl supports buffers instead
+            return False, "requires buffer representation"
 
-        return True
+        op_available_layouts = features.supported_memory_layouts(
+            VkStorageType.TEXTURE_3D
+        )
 
-    def is_linear_permute(self, node: torch.fx.Node) -> bool:
+        is_compatible = any(
+            layout in op_available_layouts for layout in valid_texture_layouts
+        )
+        if not is_compatible:
+            return False, "Required texutre memory layout not supported"
+
+        return is_compatible, "Op is compatible"
+
+    def node_is_compatible(
+        self, node: torch.fx.Node, features: Optional[OpFeatures] = None
+    ) -> Tuple[bool, str]:
+        # TODO(ssjia) support symbolic ints
+        if utils.is_symint_node(node):
+            return False, "symint node not supported yet"
+        elif utils.is_tensor_node(node):
+            return self.op_node_is_compatible(node, features=features)
+
+        return False, f"Unsupported node type: {node.format_node()}"
+
+    def is_linear_permute(self, node: torch.fx.Node) -> Tuple[bool, bool]:
+        """
+        Detect if a node is a permute/transpose that precedes a call to a `mm` or
+        `addmm` operator. This node can be fused with the `mm` or `addmm` to produce a
+        `linear` operator.
+
+        This function returns two bool values:
+        1. The first indicates if this node can be fused into a linear node
+        2. The second indicates if the overall linear op can be executed with Vulkan
+
+        The node will be partitioned only if both are true.
+        """
         if node.target not in [
             exir_ops.edge.aten.t_copy.default,
             exir_ops.edge.aten.permute_copy.default,
         ]:
-            return False
+            return False, False
 
         if len(node.users) != 1:
-            return False
+            return False, False
 
         first_user = list(node.users.keys())[0]
         if first_user.target in [
             exir_ops.edge.aten.mm.default,
             exir_ops.edge.aten.addmm.default,
         ]:
-            # Only mark this node if the overall linear op is valid
-            if self.all_args_compatible(first_user):
-                return True
+            # Only mark this node if the target linear op is valid
+            if self.node_is_compatible(first_user)[0]:
+                return True, True
+            else:
+                return True, False
 
-        return False
+        return False, False
 
-    def is_in_local_scalar_dense_chain(self, node: torch.fx.Node) -> bool:
+    def is_in_local_scalar_dense_chain(self, node: torch.fx.Node) -> Tuple[bool, bool]:
         """
         Scalar tensors are usually converted to scalar values in the graph via`
         scalar_tensor[0].item()` in Python, which translates to a chain of
         `local_scalar_dense(torch.select.int(scalar_tensor, 0, 0))` in the graph.
         This function marks the entire chain as supported by the Vulkan delegate.
 
-        Later, within vulkan_preprocess there will be a graph transform which
-        replaces the chain with passing in the scalar tensor directly.
+        Later, within vulkan_preprocess there will be a graph transform which replaces
+        the chain with passing in the scalar tensor directly.
+
+        Similar to the `is_linear_permute` function, this function has 2 return values.
         """
         if node.target == exir_ops.edge.aten.select_copy.int:
             if len(node.users) != 1:
-                return False
+                return False, False
             # pyre-ignore
             if node.args[0].meta["val"].numel() != 1:
-                return False
+                return False, False
 
-            user = list(node.users.keys())[0]
-            return user.target == torch.ops.aten._local_scalar_dense.default
+            local_scalar_dense = list(node.users.keys())[0]
+            if local_scalar_dense.target != torch.ops.aten._local_scalar_dense.default:
+                return False, False
+
+            return self.is_in_local_scalar_dense_chain(local_scalar_dense)
 
         if node.target == torch.ops.aten._local_scalar_dense.default:
-            return True
+            return True, all(self.node_is_compatible(user)[0] for user in node.users)
 
-        return False
+        return False, False
 
     def log_skip(self, node: torch.fx.Node, reason: str) -> None:
         if node.op == "call_function":
@@ -148,26 +194,35 @@ class VulkanSupportedOperators(OperatorSupportBase):
     def is_node_supported(
         self, submodules: Mapping[str, torch.nn.Module], node: torch.fx.Node
     ) -> bool:
-        r = self._is_node_supported(submodules, node)
+        r = self._is_node_supported(node)
         return r
 
-    def _is_node_supported(
-        self, submodules: Mapping[str, torch.nn.Module], node: torch.fx.Node
-    ) -> bool:
+    def _is_node_supported(self, node: torch.fx.Node) -> bool:
         target = node.target
         if node.target == torch.ops.higher_order.auto_functionalized:
             first_arg = node.args[0]
             assert isinstance(first_arg, torch._ops.OpOverload)
             target = first_arg.name()
 
-        if self.is_linear_permute(node):
+        is_linear_permute, target_linear_is_compatible = self.is_linear_permute(node)
+        if is_linear_permute and target_linear_is_compatible:
             return True
+        elif is_linear_permute:
+            # Skip so that the permute can be fused into a linear by another backend
+            self.log_skip(node, "permute node of non compatible linear node")
+            return False
 
-        if self.is_in_local_scalar_dense_chain(node):
+        is_in_local_scalar_dense_chain, dst_node_is_compatible = (
+            self.is_in_local_scalar_dense_chain(node)
+        )
+        if is_in_local_scalar_dense_chain and dst_node_is_compatible:
             return True
+        elif is_in_local_scalar_dense_chain:
+            self.log_skip(node, "local scalar dense of incompatible op node")
+            return False
 
         if target not in vulkan_supported_ops:
-            self.log_skip(node, "not in vulkan_supported_ops")
+            self.log_skip(node, "no operator implementation")
             return False
 
         features = vulkan_supported_ops[target]
@@ -180,18 +235,37 @@ class VulkanSupportedOperators(OperatorSupportBase):
             self.log_skip(node, "no dynamic shape support")
             return False
 
-        return self.all_args_compatible(node)
+        is_compatible, reason = self.node_is_compatible(node, features=features)
+        if not is_compatible:
+            self.log_skip(node, reason)
+
+        return is_compatible
 
 
 def parse_compile_options(compile_options: Dict[str, Any]) -> List[CompileSpec]:
     compile_specs = []
 
     for key, value in compile_options.items():
-        if isinstance(
-            value, (vk_graph_schema.VkStorageType, vk_graph_schema.VkMemoryLayout)
-        ):
+        if isinstance(value, (VkStorageType, VkMemoryLayout)):
             value_bytes = int(value).to_bytes(4, byteorder="little")
             compile_specs.append(CompileSpec(key, value_bytes))
+
+        if key == "texture_limits":
+            compile_specs.append(
+                CompileSpec(
+                    "texture_limits_x", int(value[0]).to_bytes(4, byteorder="little")
+                )
+            )
+            compile_specs.append(
+                CompileSpec(
+                    "texture_limits_y", int(value[1]).to_bytes(4, byteorder="little")
+                )
+            )
+            compile_specs.append(
+                CompileSpec(
+                    "texture_limits_z", int(value[2]).to_bytes(4, byteorder="little")
+                )
+            )
 
         # Unhandled options are ignored
 
@@ -200,7 +274,10 @@ def parse_compile_options(compile_options: Dict[str, Any]) -> List[CompileSpec]:
 
 @final
 class VulkanPartitioner(Partitioner):
-    def __init__(self, compile_options: Optional[Dict[str, Any]] = None) -> None:
+    def __init__(
+        self,
+        compile_options: Optional[Dict[str, Any]] = None,
+    ) -> None:
         self.options: Dict[str, Any] = {}
         if compile_options is not None:
             self.options = compile_options
@@ -218,9 +295,15 @@ class VulkanPartitioner(Partitioner):
         # subgraphs containing the nodes with the tags
         partition_tags = {}
 
+        texture_limits: utils.ImageExtents = self.options.get(
+            "texture_limits", utils.DEFAULT_TEXTURE_LIMITS
+        )
         capability_partitioner = CapabilityBasedPartitioner(
             exported_program.graph_module,
-            VulkanSupportedOperators(self.options.get("require_dynamic_shapes", False)),
+            VulkanSupportedOperators(
+                texture_limits,
+                require_dynamic_shape=self.options.get("require_dynamic_shapes", False),
+            ),
             allows_single_node_partition=True,
         )
         partition_list = capability_partitioner.propose_partitions()
