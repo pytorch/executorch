2230f91e9c0944c643ce06f92c809ca35335e428
Jack Zhang <dvorjackz@gmail.com>
2024-11-06 10:14:25 -0800

    Lint

--- examples/models/llama/export_llama_lib.py
+++ examples/models/llama/export_llama_lib.py
@@ -594,19 +594,14 @@ def _export_llama(modelname, args) -> LLMEdgeManager:  # noqa: C901
     pt2e_quant_params, quantizers, quant_dtype = get_quantizer_and_quant_params(args)
 
     # export_to_edge
-    builder_exported = (
-        _prepare_for_llama_export(modelname, args)
-        .export()
-    )
+    builder_exported = _prepare_for_llama_export(modelname, args).export()
 
     if args.export_only:
         exit()
 
-    builder_exported_to_edge = (
-        builder_exported
-        .pt2e_quantize(quantizers)
-        .export_to_edge()
-    )
+    builder_exported_to_edge = builder_exported.pt2e_quantize(
+        quantizers
+    ).export_to_edge()
 
     modelname = builder_exported_to_edge.modelname
 
