2bc8bfb247dd8fca7705f6dfb603c6dab50d0c39
Kimish Patel <kimishpatel@fb.com>
2024-11-08 11:17:28 -0800

    Refactor op_div for shreability with optimized op_div
    Pull Request resolved: https://github.com/pytorch/executorch/pull/6713
    
    
    ghstack-source-id: 252584479
    @exported-using-ghexport
    
    Differential Revision: [D65606664](https://our.internmc.facebook.com/intern/diff/D65606664/)

--- kernels/portable/cpu/op_div_impl.h
+++ kernels/portable/cpu/op_div_impl.h
@@ -6,46 +6,41 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-#include <executorch/kernels/portable/cpu/op_div_impl.h>
+#include <executorch/kernels/portable/cpu/scalar_utils.h>
+#include <executorch/kernels/portable/cpu/util/elementwise_util.h>
+#include <executorch/kernels/portable/cpu/util/math_util.h>
+#include <executorch/runtime/kernel/kernel_includes.h>
+#include <executorch/runtime/platform/assert.h>
 
 namespace torch {
 namespace executor {
 namespace native {
 
-Tensor& div_out(
+Tensor& div_out_impl(
     KernelRuntimeContext& ctx,
     const Tensor& a,
     const Tensor& b,
-    Tensor& out) {
-  return div_out_impl(ctx, a, b, out);
-}
+    Tensor& out);
 
-Tensor& div_out_mode(
+Tensor& div_out_mode_impl(
     KernelRuntimeContext& ctx,
     const Tensor& a,
     const Tensor& b,
     exec_aten::optional<exec_aten::string_view> mode,
-    Tensor& out) {
-  return div_out_mode_impl(ctx, a, b, mode, out);
-}
+    Tensor& out);
 
-Tensor& div_scalar_out(
+Tensor& div_scalar_out_impl(
     KernelRuntimeContext& ctx,
     const Tensor& a,
     const Scalar& b,
-    Tensor& out) {
-  return div_scalar_out_impl(ctx, a, b, out);
-}
+    Tensor& out);
 
-Tensor& div_scalar_mode_out(
+Tensor& div_scalar_mode_out_impl(
     KernelRuntimeContext& ctx,
     const Tensor& a,
     const Scalar& b,
     exec_aten::optional<exec_aten::string_view> mode,
-    Tensor& out) {
-  return div_scalar_mode_out_impl(ctx, a, b, mode, out);
-}
-
+    Tensor& out);
 } // namespace native
 } // namespace executor
 } // namespace torch
