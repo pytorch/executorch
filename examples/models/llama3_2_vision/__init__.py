e5428de267a07b0e927ebc9c2b4f9129aafdddc0
Jack Zhang <dvorjackz@gmail.com>
2024-11-14 09:45:52 -0800

    Move to subdir

--- examples/models/llama3_2_vision/__init__.py
+++ examples/models/llama3_2_vision/__init__.py
@@ -4,6 +4,6 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .model import Llama3_2Decoder
+from .text_decoder.model import Llama3_2Decoder
 
 __all__ = [Llama3_2Decoder]
