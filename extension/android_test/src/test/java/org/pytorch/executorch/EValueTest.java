d877519113f5905ed9e04891c2e8a7b4197a174d
Hansong Zhang <hsz@meta.com>
2024-11-21 10:27:49 -0800

    [Android] Remove runtime internal evalue list types
    List types are used in runtime only. Exported model inputs and outputs won't
    use them. Therefore, we remove them from Java public API.

--- extension/android_test/src/test/java/org/pytorch/executorch/EValueTest.java
+++ extension/android_test/src/test/java/org/pytorch/executorch/EValueTest.java
@@ -67,70 +67,6 @@ public class EValueTest {
   }
 
   @Test
-  public void testBoolListValue() {
-    boolean[] value = {true, false, true};
-    EValue evalue = EValue.listFrom(value);
-    assertTrue(evalue.isBoolList());
-    assertTrue(Arrays.equals(value, evalue.toBoolList()));
-  }
-
-  @Test
-  public void testIntListValue() {
-    long[] value = {Long.MIN_VALUE, 0, Long.MAX_VALUE};
-    EValue evalue = EValue.listFrom(value);
-    assertTrue(evalue.isIntList());
-    assertTrue(Arrays.equals(value, evalue.toIntList()));
-  }
-
-  @Test
-  public void testDoubleListValue() {
-    double[] value = {Double.MIN_VALUE, 0.1d, 0.01d, 0.001d, Double.MAX_VALUE};
-    EValue evalue = EValue.listFrom(value);
-    assertTrue(evalue.isDoubleList());
-    assertTrue(Arrays.equals(value, evalue.toDoubleList()));
-  }
-
-  @Test
-  public void testTensorListValue() {
-    long[][] data = {{1, 2, 3}, {1, 2, 3, 4, 5, 6}};
-    long[][] shape = {{1, 3}, {2, 3}};
-    Tensor[] tensors = {Tensor.fromBlob(data[0], shape[0]), Tensor.fromBlob(data[1], shape[1])};
-
-    EValue evalue = EValue.listFrom(tensors);
-    assertTrue(evalue.isTensorList());
-
-    assertTrue(Arrays.equals(evalue.toTensorList()[0].shape, shape[0]));
-    assertTrue(Arrays.equals(evalue.toTensorList()[0].getDataAsLongArray(), data[0]));
-
-    assertTrue(Arrays.equals(evalue.toTensorList()[1].shape, shape[1]));
-    assertTrue(Arrays.equals(evalue.toTensorList()[1].getDataAsLongArray(), data[1]));
-  }
-
-  @Test
-  @SuppressWarnings("unchecked")
-  public void testOptionalTensorListValue() {
-    long[][] data = {{1, 2, 3}, {1, 2, 3, 4, 5, 6}};
-    long[][] shape = {{1, 3}, {2, 3}};
-
-    EValue evalue =
-        EValue.listFrom(
-            Optional.<Tensor>empty(),
-            Optional.of(Tensor.fromBlob(data[0], shape[0])),
-            Optional.of(Tensor.fromBlob(data[1], shape[1])));
-    assertTrue(evalue.isOptionalTensorList());
-
-    assertTrue(!evalue.toOptionalTensorList()[0].isPresent());
-
-    assertTrue(evalue.toOptionalTensorList()[1].isPresent());
-    assertTrue(Arrays.equals(evalue.toOptionalTensorList()[1].get().shape, shape[0]));
-    assertTrue(Arrays.equals(evalue.toOptionalTensorList()[1].get().getDataAsLongArray(), data[0]));
-
-    assertTrue(evalue.toOptionalTensorList()[2].isPresent());
-    assertTrue(Arrays.equals(evalue.toOptionalTensorList()[2].get().shape, shape[1]));
-    assertTrue(Arrays.equals(evalue.toOptionalTensorList()[2].get().getDataAsLongArray(), data[1]));
-  }
-
-  @Test
   public void testAllIllegalCast() {
     EValue evalue = EValue.optionalNone();
     assertTrue(evalue.isNone());
@@ -171,46 +107,6 @@ public class EValueTest {
     assertFalse(evalue.isString());
     try {
       evalue.toStr();
-      fail("Should have thrown an exception");
-    } catch (IllegalStateException e) {
-    }
-
-    // try bool list
-    assertFalse(evalue.isBoolList());
-    try {
-      evalue.toBoolList();
-      fail("Should have thrown an exception");
-    } catch (IllegalStateException e) {
-    }
-
-    // try int list
-    assertFalse(evalue.isIntList());
-    try {
-      evalue.toIntList();
-      fail("Should have thrown an exception");
-    } catch (IllegalStateException e) {
-    }
-
-    // try double list
-    assertFalse(evalue.isDoubleList());
-    try {
-      evalue.toBool();
-      fail("Should have thrown an exception");
-    } catch (IllegalStateException e) {
-    }
-
-    // try Tensor list
-    assertFalse(evalue.isTensorList());
-    try {
-      evalue.toTensorList();
-      fail("Should have thrown an exception");
-    } catch (IllegalStateException e) {
-    }
-
-    // try optional Tensor list
-    assertFalse(evalue.isOptionalTensorList());
-    try {
-      evalue.toOptionalTensorList();
       fail("Should have thrown an exception");
     } catch (IllegalStateException e) {
     }
