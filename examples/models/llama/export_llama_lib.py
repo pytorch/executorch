e1b15640c79bc48cd5637a6554adc85be0bb8a9d
Manuel Candales <mcandales@fb.com>
2024-11-22 12:12:34 -0500

    Add torchao mps lowbit ops to llama runner

--- examples/models/llama/export_llama_lib.py
+++ examples/models/llama/export_llama_lib.py
@@ -600,7 +600,7 @@ def get_quantizer_and_quant_params(args):
 
 def _qmode_type(value):
     choices = ["int8", "8da4w", "8da4w-gptq", "vulkan_4w"]
-    patterns = [r"torchao:8da(\d+)w"]
+    patterns = [r"torchao:8da(\d+)w", r"torchao:fpa(\d+)w"]
 
     if value in choices:
         return value
