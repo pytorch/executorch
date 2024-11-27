408b405af0e18beea2604f724258344f5e415f81
neuropilot-captain <neuropilot-captain@mediatek.com>
2024-11-27 11:36:22 +0800

    update build script

--- backends/mediatek/scripts/mtk_build.sh
+++ backends/mediatek/scripts/mtk_build.sh
@@ -33,6 +33,7 @@ rm -rf cmake-android-out && mkdir cmake-android-out && cd cmake-android-out
 cmake -DBUCK2="$BUCK_PATH" \
       -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK/build/cmake/android.toolchain.cmake" \
       -DANDROID_ABI=arm64-v8a \
+      -DANDROID_PLATFORM=android-26 \
       -DEXECUTORCH_BUILD_NEURON=ON \
       -DNEURON_BUFFER_ALLOCATOR_LIB="$NEURON_BUFFER_ALLOCATOR_LIB" \
       ..
