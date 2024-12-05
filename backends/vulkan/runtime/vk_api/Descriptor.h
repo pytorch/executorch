3c1a2cf94884dc2bbecb7e099ff23369e778d13c
trivedivivek <5340687+trivedivivek@users.noreply.github.com>
2024-12-05 14:18:43 -0600

    [ET-VK] Using a single GPU buffer for all tensor uniforms.
    Differential Revision: D65841750
    
    Pull Request resolved: https://github.com/pytorch/executorch/pull/7015

--- backends/vulkan/runtime/vk_api/Descriptor.h
+++ backends/vulkan/runtime/vk_api/Descriptor.h
@@ -33,7 +33,7 @@ struct BufferBindInfo final {
   VkDeviceSize range;
 
   BufferBindInfo();
-  BufferBindInfo(const VulkanBuffer& buffer_p);
+  BufferBindInfo(const VulkanBuffer& buffer_p, const uint32_t offset_p = 0u);
 };
 
 struct ParamsBindList final {
