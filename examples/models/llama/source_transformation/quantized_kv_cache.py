a7494fcd0242fe94d9cde25b4460bca55306dec9
Kimish Patel <kimishpatel@fb.com>
2024-12-05 09:02:53 -0800

    [Executorch] Add quantized kv cache to oss ci
    Pull Request resolved: https://github.com/pytorch/executorch/pull/6997
    
    Fixes to make sure quantized kv cache works in oss
    ghstack-source-id: 256718639
    @exported-using-ghexport
    
    //unit-test-arm broken in trunk plus complains about internal failure
    @bypass-github-export-checks
    @exported-using-ghexport
    
    Differential Revision: [D66269487](https://our.internmc.facebook.com/intern/diff/D66269487/)

--- examples/models/llama/source_transformation/quantized_kv_cache.py
+++ examples/models/llama/source_transformation/quantized_kv_cache.py
@@ -10,6 +10,7 @@ from enum import Enum
 import torch
 import torch.nn as nn
 from executorch.examples.models.llama.llama_transformer import KVCache
+
 from torch.ao.quantization.fx._decomposed import quantized_decomposed_lib  # noqa: F401
 
 
@@ -221,6 +222,33 @@ class QuantizedKVCache(nn.Module):
 
 
 def replace_kv_cache_with_quantized_kv_cache(module):
+    try:
+        op = torch.ops.quantized_decomposed.quantize_per_token.out
+        assert op is not None
+    except:
+        import glob
+
+        import executorch
+        from executorch.extension.pybindings import portable_lib  # noqa # usort: skip
+
+        # Ideally package is installed in only one location but usage of
+        # PYATHONPATH can result in multiple locations.
+        # ATM this is mainly used in CI for qnn runner. Will need to revisit this
+        executorch_package_path = executorch.__path__[-1]
+        libs = list(
+            glob.glob(
+                f"{executorch_package_path}/**/libquantized_ops_aot_lib.*",
+                recursive=True,
+            )
+        )
+        assert len(libs) == 1, f"Expected 1 library but got {len(libs)}"
+        logging.info(f"Loading custom ops library: {libs[0]}")
+        torch.ops.load_library(libs[0])
+        op = torch.ops.quantized_decomposed.quantize_per_token.out
+        assert op is not None
+    # This is needed to ensure that custom ops are registered
+    from executorch.extension.llm.custom_ops import custom_ops  # noqa: F401
+
     logging.warning(
         "Replacing KVCache with QuantizedKVCache. This modifies the model in place."
     )
