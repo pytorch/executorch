36086de22d4c638ac23d282a8c2ccce4d31746fd
Måns Nilsson <mans.nilsson@arm.com>
2024-12-09 13:25:25 +0100

    Move to TOSA v0.80.1
    Change-Id: I24986e3014cb0fd58e558cea0c06b2e8031da308

--- backends/arm/test/ops/test_avg_pool.py
+++ backends/arm/test/ops/test_avg_pool.py
@@ -56,7 +56,7 @@ class TestAvgPool2d(unittest.TestCase):
                 module,
                 example_inputs=test_data,
                 compile_spec=common.get_tosa_compile_spec(
-                    "TOSA-0.80.0+MI", permute_memory_to_nhwc=True
+                    "TOSA-0.80+MI", permute_memory_to_nhwc=True
                 ),
             )
             .export()
@@ -79,7 +79,7 @@ class TestAvgPool2d(unittest.TestCase):
                 module,
                 example_inputs=test_data,
                 compile_spec=common.get_tosa_compile_spec(
-                    "TOSA-0.80.0+BI", permute_memory_to_nhwc=True
+                    "TOSA-0.80+BI", permute_memory_to_nhwc=True
                 ),
             )
             .quantize(Quantize(quantizer, get_symmetric_quantization_config()))
