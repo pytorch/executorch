f3dc62cc874e62bc0b98fcf081aa678a30b19c20
ynimmaga <yamini.nimmagadda@intel.com>
2024-12-10 18:12:32 -0800

    Added standalone build script for openvino backend

--- backends/openvino/scripts/build.sh
+++ backends/openvino/scripts/build.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Define the directory where CMakeLists.txt is located
-EXECUTORCH_ROOT=$(realpath "$(dirname "$0")/../..")
+EXECUTORCH_ROOT=$(realpath "$(dirname "$0")/../../..")
 echo EXECUTORCH_ROOT=${EXECUTORCH_ROOT}
 
 main() {
@@ -29,25 +29,12 @@ main() {
     # Build the project
     cmake --build cmake-openvino-out --target install --config Release -j5
 
-    ## Build example
-    local example_dir=examples/openvino
-    local example_build_dir="${build_dir}/${example_dir}"
-    local cmake_prefix_path="${PWD}/${build_dir}/lib/cmake/ExecuTorch;${PWD}/${build_dir}/third-party/gflags;"
-    rm -rf "${example_build_dir}"
-
-    ## OpenVINO original
-    cmake -DCMAKE_PREFIX_PATH="${cmake_prefix_path}" \
-          -DCMAKE_FIND_ROOT_PATH_MODE_PACKAGE=BOTH \
-          -B"${example_build_dir}" \
-          $EXECUTORCH_ROOT/$example_dir
-
-    cmake --build "${example_build_dir}" -j5
-
     # Switch back to the original directory
     cd - > /dev/null
 
     # Print a success message
     echo "Build successfully completed."
+
 }
 
 main "$@"
