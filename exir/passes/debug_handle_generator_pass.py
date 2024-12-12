27bb639c8f8836bf743ec89b27c7069680b16dfb
gasoonjia <gasoonjia@icloud.com>
2024-12-12 10:18:24 -0800

    restucture et debug handle
    Pull Request resolved: https://github.com/pytorch/executorch/pull/7197
    
    This diff formats the debug handle generation process in et stack by. extracting bfs graph tracing process.
    
    
    ghstack-source-id: 257377901
    @exported-using-ghexport
    
    Differential Revision: [D66622890](https://our.internmc.facebook.com/intern/diff/D66622890/)

--- exir/passes/debug_handle_generator_pass.py
+++ exir/passes/debug_handle_generator_pass.py
@@ -4,12 +4,11 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from executorch.exir.graph_module import get_control_flow_submodules
+from executorch.exir.graph_module import bfs_trace_with_node_process
 from executorch.exir.pass_base import ExportPass
 from torch.export import ExportedProgram
 from torch.fx import GraphModule
 from torch.fx.passes.infra.pass_base import PassResult
-
 
 class DebugHandleGeneratorPass(ExportPass):
     def call(self, graph_module: GraphModule) -> PassResult:
@@ -17,19 +16,15 @@ class DebugHandleGeneratorPass(ExportPass):
         to executorch backend, that has a canonical set of quantized operators
         """
 
-        queue = [graph_module]
         index = 1
-        # bfs to traverse all modules including control flow submodules to attached debug handle id
-        while queue:
-            current_graph_module = queue.pop(0)
-            for node in current_graph_module.graph.nodes:
-                node.meta["debug_handle"] = index
-                index += 1
-            control_flow_submodules = [
-                submodule
-                for _, submodule, _ in get_control_flow_submodules(current_graph_module)
-            ]
-            queue.extend(control_flow_submodules)
+
+        def _extract_debug_handles_from_node(node):
+            nonlocal index
+            node.meta["debug_handle"] = index
+            index += 1
+
+        bfs_trace_with_node_process(graph_module, _extract_debug_handles_from_node)
+
         return PassResult(graph_module, True)
 
 
@@ -38,28 +33,18 @@ def generate_missing_debug_handles(ep: ExportedProgram):
     This pass is used to generate missing debug handles for the graph module and its submodules.
     """
 
-    def get_control_flow_submodules_list(graph_module):
-        return [
-            submodule for _, submodule, _ in get_control_flow_submodules(graph_module)
-        ]
-
     max_handle = 0
-    queue = [ep.graph_module]
 
-    while queue:
-        current_graph_module = queue.pop(0)
-        for node in current_graph_module.graph.nodes:
-            if "debug_handle" in node.meta:
-                max_handle = max(max_handle, node.meta["debug_handle"])
-        control_flow_submodules = get_control_flow_submodules_list(current_graph_module)
-        queue.extend(control_flow_submodules)
+    def _extract_max_debug_handle(node):
+        nonlocal max_handle
+        if "debug_handle" in node.meta:
+            max_handle = max(max_handle, node.meta["debug_handle"])
 
-    queue = [ep.graph_module]
-    while queue:
-        current_graph_module = queue.pop(0)
-        for node in current_graph_module.graph.nodes:
-            if node.meta.get("debug_handle", 0) in (0, None):
-                node.meta["debug_handle"] = max_handle + 1
-                max_handle += 1
-        control_flow_submodules = get_control_flow_submodules_list(current_graph_module)
-        queue.extend(control_flow_submodules)
+    def _insert_new_debug_handles(node):
+        nonlocal max_handle
+        if node.meta.get("debug_handle", 0) in (0, None):
+            node.meta["debug_handle"] = max_handle + 1
+            max_handle += 1
+
+    bfs_trace_with_node_process(ep.graph_module, _extract_max_debug_handle)
+    bfs_trace_with_node_process(ep.graph_module, _insert_new_debug_handles)
