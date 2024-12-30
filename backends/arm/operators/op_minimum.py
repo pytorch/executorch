e9426bcae84adc5a2845c8e053b2b4d213617ca7
Oscar Andersson <oscar.andersson@arm.com>
2024-12-30 10:10:13 +0100

    Add missing __init__.py to passes
    backends/arm/_passes missed an init file which caused pyre to not find
    imports from _passes.
    
    Signed-off-by: Oscar Andersson <oscar.andersson@arm.com>
    Change-Id: I80e0c2071844ec0fefeb9fd44f9471ed1be32228

--- backends/arm/operators/op_minimum.py
+++ backends/arm/operators/op_minimum.py
@@ -11,10 +11,7 @@ import executorch.backends.arm.tosa_quant_utils as tqutils
 
 import serializer.tosa_serializer as ts  # type: ignore
 
-# pyre-fixme[21]: 'Could not find a module corresponding to import `executorch.backends.arm._passes.fold_qdq_with_annotated_qparams_pass`.'
-from executorch.backends.arm._passes.fold_qdq_with_annotated_qparams_pass import (
-    get_input_qparams,
-)
+from executorch.backends.arm._passes import get_input_qparams
 from executorch.backends.arm.operators.node_visitor import (
     NodeVisitor,
     register_node_visitor,
@@ -45,9 +42,7 @@ class MinVisitor(NodeVisitor):
         scale_back = 1.0
         min_output = output
         if inputs[0].dtype == ts.DType.INT8:
-            input_qparams = get_input_qparams(  # pyre-ignore[16]: 'Module `executorch.backends.arm` has no attribute `_passes`.'
-                node
-            )
+            input_qparams = get_input_qparams(node)
             assert (
                 len(input_qparams) == 2
             ), f"Both inputs needs to have quantization information for {node}"
