e66cdaf514e15242692073db1271aae4657f2033
pssrawat <34485295+pssrawat@users.noreply.github.com>
2025-01-04 00:47:16 +0530

    [Xnnpack] Accept default padding value for torch.constant_pad_nd
    Differential Revision: D67756862
    
    Pull Request resolved: https://github.com/pytorch/executorch/pull/7469

--- backends/xnnpack/test/ops/test_static_constant_pad.py
+++ backends/xnnpack/test/ops/test_static_constant_pad.py
@@ -114,6 +114,40 @@ class TestStaticConstantPad(unittest.TestCase):
         )
         self._test_static_constant_pad_functional(inputs)
 
+    def test_constant_pad_nd(self):
+        class ConstantPad(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+
+            def forward(self, x, y, z):
+                pad_6 = (1, 2, 3, 4, 5, 6)
+                pad_4 = (1, 2, 3, 4)
+                pad_2 = (1, 2)
+                a = torch.constant_pad_nd(input=x, pad=pad_6)
+                b = torch.constant_pad_nd(input=y, pad=pad_4)
+                c = torch.constant_pad_nd(input=z, pad=pad_2)
+
+                return (a + a, b + b, c + c)
+
+        inputs = (
+            torch.randn(size=(5, 4, 3, 2)),
+            torch.randn(size=(5, 3, 2)),
+            torch.randn(size=(4, 3)),
+        )
+        (
+            Tester(ConstantPad(), inputs)
+            .export()
+            .check_count({"torch.ops.aten.constant_pad_nd.default": 3})
+            .to_edge_transform_and_lower()
+            .check_count({"torch.ops.higher_order.executorch_call_delegate": 1})
+            .check_not(
+                ["executorch_exir_dialects_edge__ops_aten_constant_pad_nd_default"]
+            )
+            .to_executorch()
+            .serialize()
+            .run_method_and_compare_outputs()
+        )
+
     def test_qs8_static_constant_pad_functional(self):
         class Pad(torch.nn.Module):
             def __init__(self):
