26be4004cbed43735c122c01fc069e98477dd73d
Stephen Jia <ssjia@meta.com>
2025-01-10 13:05:06 -0800

    [ET-VK][ez] Misc fixes related to extension support checking
    ## Context
    
    Follow up from https://github.com/pytorch/executorch/pull/7576. Apply two "fixes" that were missed in the first diff:
    
    1. Check device capability for Prepacking nodes as well
    2. Remove conditional skips during generated operator correctness tests; rely on the device capability check to determine if a skip is needed.
    
    Differential Revision: [D68035430](https://our.internmc.facebook.com/intern/diff/D68035430/)
    
    [ghstack-poisoned]

--- backends/vulkan/test/op_tests/utils/gen_computegraph.py
+++ backends/vulkan/test/op_tests/utils/gen_computegraph.py
@@ -633,30 +633,6 @@ for (int i=0; i<out.size(); i++) {{
 
         return graph_exec
 
-    def gen_conditional_skips(self, skip_str: str = "GTEST_SKIP();") -> str:
-        fp16_skip = f"if (!{self.graph}{self.dot}context()->adapter_ptr()->has_full_float16_buffers_support()) {{\n"
-        fp16_skip += f"  {skip_str}\n"
-        fp16_skip += "}"
-        fp16_skip = re.sub(r"^", "  ", fp16_skip, flags=re.M) + "\n"
-
-        int8_skip = f"if (!{self.graph}{self.dot}context()->adapter_ptr()->has_full_int8_buffers_support()) {{\n"
-        int8_skip += f"  {skip_str};\n"
-        int8_skip += "}\n"
-
-        skips = ""
-
-        skips += "if (test_dtype == at::kHalf) {\n"
-        skips += fp16_skip
-        skips += "}\n"
-
-        for _, dtype in self.suite_def.arg_dtype.items():
-            if dtype == "at::kChar" or dtype == "at::kQInt8":
-                skips += int8_skip
-                continue
-
-        skips += "\n"
-        return skips
-
     def gen_op_check_fn(self) -> str:
         op_name = self.f.func.name.unambiguous_name()
         if self.suite_def.test_name_suffix is not None:
@@ -667,7 +643,6 @@ for (int i=0; i<out.size(); i++) {{
             op_check_fn = self.gen_decl(f"prepacked_check_{op_name}") + " {\n"
 
         op_check_fn_body = ""
-        op_check_fn_body += self.gen_conditional_skips()
         op_check_fn_body += self.gen_graph_build_code()
         op_check_fn_body += self.gen_graph_exec_code()
 
