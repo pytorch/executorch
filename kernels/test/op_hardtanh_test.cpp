d463d026725c1d16e47e76e3ab7f87a6829f5805
Manuel Candales <mcandales@meta.com>
2025-01-14 10:15:56 -0800

    Enable Half/BF16: abs, full, gelu, hardtanh, logit, neg, sign (#5856)
    Summary: Pull Request resolved: https://github.com/pytorch/executorch/pull/5856
    
    Reviewed By: swolchok
    
    Differential Revision: D63863399

--- kernels/test/op_hardtanh_test.cpp
+++ kernels/test/op_hardtanh_test.cpp
@@ -30,7 +30,31 @@ class OpHardTanhTest : public OperatorTest {
     return torch::executor::aten::hardtanh_outf(
         context_, self, min_val, max_val, out);
   }
+
+  template <ScalarType DTYPE>
+  void test_dtype() {
+    TensorFactory<DTYPE> tf;
+
+    Tensor in = tf.make({2, 3}, {0, 1, 2, 3, 4, 5});
+    Scalar min_val = 1;
+    Scalar max_val = 4;
+    Tensor out = tf.zeros({2, 3});
+    Tensor expected = tf.make({2, 3}, {1, 1, 2, 3, 4, 4});
+
+    Tensor ret = op_hardtanh_out(in, min_val, max_val, out);
+
+    EXPECT_TENSOR_EQ(out, ret);
+    EXPECT_TENSOR_EQ(out, expected);
+  }
 };
+
+TEST_F(OpHardTanhTest, AllRealHBF16Input) {
+#define TEST_KERNEL(INPUT_CTYPE, INPUT_DTYPE) \
+  test_dtype<ScalarType::INPUT_DTYPE>();
+
+  ET_FORALL_REALHBF16_TYPES(TEST_KERNEL);
+#undef TEST_KERNEL
+}
 
 TEST_F(OpHardTanhTest, SanityCheck) {
   TensorFactory<ScalarType::Float> tf;
