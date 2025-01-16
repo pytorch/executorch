a4def9f821880781069add5f1da09b6259f6b4bf
Oscar Andersson <87121123+oscarandersson8218@users.noreply.github.com>
2025-01-16 09:48:23 +0100

    Make ArmPassManager aware of TosaSpecification (#7668)
    - Pass TosaSpecifcation to ArmPassManager. Based on this the
     PassManager can decide which passes should be run.
    - Also adds docstrings and renames some passes.
    
    Signed-off-by: Oscar Andersson <oscar.andersson@arm.com>

--- backends/arm/test/ops/test_max_pool.py
+++ backends/arm/test/ops/test_max_pool.py
@@ -19,6 +19,7 @@ from executorch.backends.arm.quantizer.arm_quantizer import (
 )
 from executorch.backends.arm.test import common, conftest
 from executorch.backends.arm.test.tester.arm_tester import ArmTester
+from executorch.backends.arm.tosa_specification import TosaSpecification
 
 from executorch.backends.xnnpack.test.tester.tester import Quantize
 from executorch.exir.backend.backend_details import CompileSpec
@@ -86,15 +87,11 @@ class TestMaxPool2d(unittest.TestCase):
     def _test_maxpool2d_tosa_BI_pipeline(
         self, module: torch.nn.Module, test_data: Tuple[torch.tensor]
     ):
-        quantizer = ArmQuantizer().set_io(get_symmetric_quantization_config())
+        tosa_spec = TosaSpecification.create_from_string("TOSA-0.80+BI")
+        compile_spec = common.get_tosa_compile_spec(tosa_spec)
+        quantizer = ArmQuantizer(tosa_spec).set_io(get_symmetric_quantization_config())
         (
-            ArmTester(
-                module,
-                example_inputs=test_data,
-                compile_spec=common.get_tosa_compile_spec(
-                    "TOSA-0.80+BI",
-                ),
-            )
+            ArmTester(module, example_inputs=test_data, compile_spec=compile_spec)
             .quantize(Quantize(quantizer, get_symmetric_quantization_config()))
             .export()
             .check_count({"torch.ops.aten.max_pool2d.default": 1})
@@ -118,7 +115,8 @@ class TestMaxPool2d(unittest.TestCase):
         compile_spec: CompileSpec,
         test_data: Tuple[torch.tensor],
     ):
-        quantizer = ArmQuantizer().set_io(get_symmetric_quantization_config())
+        tosa_spec = TosaSpecification.create_from_compilespecs(compile_spec)
+        quantizer = ArmQuantizer(tosa_spec).set_io(get_symmetric_quantization_config())
         tester = (
             ArmTester(
                 module,
