84dfaa99e8e103f9617eb5307372623b00fd4087
Per Åstrand <per.astrand@arm.com>
2025-01-16 15:04:58 +0100

    [Arm] Update TOSA reference model to v0.80.1 tag
    Update TOSA reference model to latest tag on 0.80 branch to get fixes
    for numpy==2.0.0
    
    Signed-off-by: Per Åstrand <per.astrand@arm.com>
    Change-Id: I18de47807bd307b3919f44a67c36d42da0759f49

--- examples/arm/setup.sh
+++ examples/arm/setup.sh
@@ -2,7 +2,7 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # All rights reserved.
 #
-# Copyright 2023-2024 Arm Limited and/or its affiliates.
+# Copyright 2023-2025 Arm Limited and/or its affiliates.
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
@@ -88,7 +88,7 @@ ethos_u_base_rev="24.08"
 
 # tosa reference model
 tosa_reference_model_url="https://review.mlplatform.org/tosa/reference_model"
-tosa_reference_model_rev="c5570b79e90c3a36ab8c4ddb8ee3fbc2cd3f7c38"
+tosa_reference_model_rev="v0.80.1"
 
 # vela
 vela_repo_url="https://review.mlplatform.org/ml/ethos-u/ethos-u-vela"
