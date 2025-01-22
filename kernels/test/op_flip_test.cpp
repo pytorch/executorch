67957f67fc904b475d6ec4af35a47d055e224e48
Scott Wolchok <swolchok@meta.com>
2025-01-22 09:33:20 -0800

    Update
    [ghstack-poisoned]

--- kernels/test/op_flip_test.cpp
+++ kernels/test/op_flip_test.cpp
@@ -38,13 +38,12 @@ class OpFlipOutTest : public ::testing::Test {
   void test_1d_dtype() {
     TensorFactory<DTYPE> tf;
 
-    Tensor input =
-      tf.make({4, 1, 3}, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
+    Tensor input = tf.make({4, 1, 3}, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
     int64_t dims_data[1] = {-1};
     IntArrayRef dims = IntArrayRef(dims_data, 1);
     Tensor out = tf.zeros({4, 1, 3});
     Tensor out_expected =
-      tf.make({4, 1, 3}, {3, 2, 1, 6, 5, 4, 9, 8, 7, 12, 11, 10});
+        tf.make({4, 1, 3}, {3, 2, 1, 6, 5, 4, 9, 8, 7, 12, 11, 10});
     op_flip_out(input, dims, out);
     EXPECT_TENSOR_CLOSE(out, out_expected);
   }
