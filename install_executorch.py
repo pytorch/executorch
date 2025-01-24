565dc28027f2c01f7a73522eb4e6e6e7d42f5aa2
Huy Do <huydhn@gmail.com>
2025-01-23 18:40:52 -0800

    Revert "don't touch CMAKE_BUILD_ARGS in install_executorch.py (#7716)"
    This reverts commit 57a09f49027ab05515afef4315d0a797657222f3.

--- install_executorch.py
+++ install_executorch.py
@@ -43,6 +43,7 @@ def main(args):
 
     EXECUTORCH_BUILD_PYBIND = ""
     CMAKE_ARGS = os.getenv("CMAKE_ARGS", "")
+    CMAKE_BUILD_ARGS = os.getenv("CMAKE_BUILD_ARGS", "")
     use_pytorch_nightly = True
 
     parser = argparse.ArgumentParser()
@@ -115,6 +116,7 @@ def main(args):
     # Set environment variables
     os.environ["EXECUTORCH_BUILD_PYBIND"] = EXECUTORCH_BUILD_PYBIND
     os.environ["CMAKE_ARGS"] = CMAKE_ARGS
+    os.environ["CMAKE_BUILD_ARGS"] = CMAKE_BUILD_ARGS
 
     # Run the pip install command
     subprocess.run(
