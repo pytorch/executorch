78b48ad4fe96c6aabbd9257d9d400effb11434bb
Zonglin Peng <zonglinpeng@meta.com>
2025-01-28 13:02:55 -0800

    link hifi ops to jarvis (#8012)
    Summary:
    Pull Request resolved: https://github.com/pytorch/executorch/pull/8012
    
    List of changed/added ops are:
    op_atan2.cpp
    op_cat.cpp
    op_clamp.cpp
    op_full.cpp
    op_mean.cpp
    op_permute_copy.cpp
    op_pow.cpp
    op_remainder.cpp
    op_softmax.cpp
    op_where.cpp
    quantized_relu.cpp
    
    Reviewed By: hsharma35
    
    Differential Revision: D68471314

--- backends/cadence/hifi/operators/op_softmax.cpp
+++ backends/cadence/hifi/operators/op_softmax.cpp
@@ -8,11 +8,11 @@
 
 #include <cmath>
 
+#include <executorch/backends/cadence/hifi/kernels/kernels.h>
 #include <executorch/kernels/portable/cpu/util/activation_ops_util.h>
 #include <executorch/kernels/portable/cpu/util/functional_util.h>
 #include <executorch/kernels/portable/cpu/util/reduce_util.h>
 #include <executorch/runtime/kernel/kernel_includes.h>
-#include "kernels.h"
 
 using executorch::aten::ScalarType;
 using executorch::aten::Tensor;
@@ -24,7 +24,7 @@ namespace impl {
 namespace HiFi {
 namespace native {
 
-Tensor& softmax_out(
+Tensor& _softmax_out(
     KernelRuntimeContext& ctx,
     const Tensor& in,
     int64_t dim,
