f4786ace5d8661431432282d7cb79397cda50369
per held <pergoransson@gmail.com>
2025-01-28 19:22:55 +0100

    Lintrunner: Enable mypy testing on backends/arm (#7776)
    Migration from pyre to mypy in the lintrunner by enabling mypy for
    backends/arm.
    
    But, choosing to ignore the directory backends/arm/test.
    
    Adding ignores all over the place. These needs to be fixed properly in
    the future, but now we will start to catch new things trying to sneak
    in.
    
    Change-Id: Ie7f73d5688aaec3b32dca9f0cd042da94c06f487

--- backends/arm/operators/op_repeat.py
+++ backends/arm/operators/op_repeat.py
@@ -1,11 +1,11 @@
-# Copyright 2024 Arm Limited and/or its affiliates.
+# Copyright 2024-2025 Arm Limited and/or its affiliates.
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
 # pyre-unsafe
 
-import serializer.tosa_serializer as ts
+import serializer.tosa_serializer as ts  # type: ignore
 import torch
 from executorch.backends.arm.operators.node_visitor import (
     NodeVisitor,
