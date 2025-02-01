a4d7458a6065716a666358c07dc9e2e1dd6d6c52
ynimmaga <yamini.nimmagadda@intel.com>
2025-02-01 08:50:41 -0800

    Added license headers to the openvino files

--- backends/openvino/runtime/OpenvinoBackend.cpp
+++ backends/openvino/runtime/OpenvinoBackend.cpp
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) Intel Corporation
+ *
+ * Licensed under the BSD License (the "License"); you may not use this file
+ * except in compliance with the License. See the license file in the root
+ * directory of this source tree for more details.
+ */
+
 #include <cstring>
 #include <memory>
 #include <iostream>
