e5301303c148d2491f4c0bbdcf7b031d52fbb1ab
shewu-quic <quic_shewu@quicinc.com>
2025-02-03 12:08:03 +0800

    fix wrong import

--- examples/qualcomm/oss_scripts/llama/model/static_llama.py
+++ examples/qualcomm/oss_scripts/llama/model/static_llama.py
@@ -12,10 +12,8 @@ from typing import List, Optional, Tuple
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from executorch.examples.models.llama.llama_transformer import (
-    ModelArgs,
-    precompute_freqs_cis,
-)
+from executorch.examples.models.llama.model_args import ModelArgs
+from executorch.examples.models.llama.rope import precompute_freqs_cis
 
 
 def apply_rotary_emb_single(
