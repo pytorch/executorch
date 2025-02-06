da3a668cef6e27f32f4bd17b8c4e71f39ca53f63
Huy Do <huydhn@gmail.com>
2025-02-06 15:37:45 -0800

    Oh darn

--- .ci/docker/common/install_conda.sh
+++ .ci/docker/common/install_conda.sh
@@ -37,9 +37,9 @@ install_python() {
 
   # From https://github.com/pytorch/pytorch/blob/main/.ci/docker/common/install_conda.sh
   if [[ $(uname -m) == "aarch64" ]]; then
-    conda_install "openblas==0.3.28=*openmp*"
+    conda_install -y "openblas==0.3.28=*openmp*"
   else
-    conda_install mkl=2021.4.0 mkl-include=2021.4.0
+    conda_install -y mkl=2021.4.0 mkl-include=2021.4.0
   fi
 
   popd
