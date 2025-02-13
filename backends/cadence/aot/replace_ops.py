e32cc731f2c37d51da7987324c4e15790078a32a
pytorchbot <pytorchbot@pytorch.com>
2025-02-13 11:35:10 +0000

    2025-02-13 nightly release (8ad15f30e502707150ef4dbc556b302e2948227d)

--- backends/cadence/aot/replace_ops.py
+++ backends/cadence/aot/replace_ops.py
@@ -1,4 +1,8 @@
-# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
 
 
 # This file contains all the functions that replace one op with another in the
