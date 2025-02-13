49530338ca23c7434fc91460932d6ce36cc00f7a
Cavus Mustafa <mustafa.cavus@intel.com>
2025-02-12 18:08:55 -0800

    Removed comment which was added for debugging

--- backends/openvino/tests/ops/base_openvino_op_test.py
+++ backends/openvino/tests/ops/base_openvino_op_test.py
@@ -77,7 +77,7 @@ class BaseOpenvinoOpTest(unittest.TestCase):
                     exec_prog.write_to_file(file)
 
                 # Save inputs into a temporary file
-                #self.generate_inputs(tmp_dir, "input_list.txt", [sample_inputs], input_list)
+                self.generate_inputs(tmp_dir, "input_list.txt", [sample_inputs], input_list)
                 self.make_output_dir(output_dir)
 
                 # Start a subprocess to execute model with openvino_executor_runner
