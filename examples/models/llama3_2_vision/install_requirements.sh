613256a49ff9a347342caae17b85df7d7824937b
Mengwei Liu <larryliu@meta.com>
2025-02-19 18:17:46 -0800

    Try fixing unittest macos
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- examples/models/llama3_2_vision/install_requirements.sh
+++ examples/models/llama3_2_vision/install_requirements.sh
@@ -5,7 +5,9 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-NIGHTLY_VERSION="dev20250115"
+NIGHTLY_VERSION="dev20250219"
 
 # Install torchtune nightly for model definitions.
 pip install --pre torchtune==0.6.0.${NIGHTLY_VERSION} --extra-index-url https://download.pytorch.org/whl/nightly/cpu --no-cache-dir
+
+pip install tiktoken
