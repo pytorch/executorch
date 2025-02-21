pytorch/executorch

commit 1b5b3ee5282f4b1e10bbb2662450a5802803b6e5

Refactor and add Llama Python library build (#8107)
Summary:

As title. To use static llama export outside QC dir.

Reviewed By: cccclai

Differential Revision: D68937637

examples/qualcomm/oss_scripts/llama/TARGETS
@@ -15,6 +15,24 @@ python_library(
     ],
 )
 
+python_library(
+    name = "llama_lib",
+    srcs = ["llama.py"],
+    deps = [
+        "//caffe2:torch",
+        "//executorch/backends/qualcomm/partition:partition",
+        "//executorch/backends/qualcomm/quantizer:quantizer",
+        "//executorch/devtools:lib",
+        "//executorch/examples/models:models",
+        "//executorch/examples/qualcomm/oss_scripts/llama:static_llama",
+        "//executorch/examples/qualcomm:utils",
+        "//executorch/extension/export_util:export_util",
+        "//executorch/extension/llm/custom_ops:model_sharding_py",
+        "//executorch/extension/llm/export:export_lib",
+        "//executorch/extension/pybindings:aten_lib",
+    ],
+)
+
 python_binary(
     name = "llama",
     srcs = ["llama.py"],

examples/qualcomm/oss_scripts/llama/llama.py
@@ -819,7 +819,7 @@ def inference(args, quant_attrs, pte_filename, runtime_tokenizer_path, pre_gen_p
             logging.info(f"Results[{idx}]:\n{output}")
 
 
-def main():
+def _build_parser():
     parser = setup_common_args_and_variables()
     parser.add_argument(
         "-a",
@@ -944,7 +944,13 @@ def main():
         type=str,
     )
 
-    args = parser.parse_args()
+    return parser
+
+
+def main(args) -> None:
+    parser = _build_parser()
+
+    args = parser.parse_args(args)
     if args.compile_only and args.pre_gen_pte:
         exit("Cannot set both compile_only and pre_gen_pte as true")
 
@@ -1035,4 +1041,4 @@ def main():
 
 # flake8: noqa: C901
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])

commit d85acc7799aab73f3e342a518214c2ea32800d89

fix export llama to qnn (#8608)
Summary:

Regression from https://github.com/pytorch/executorch/pull/8107, it causes buck run python binary fails. Then https://github.com/pytorch/executorch/pull/7691 introduces dependency in source transformation

Reviewed By: larryliu0820, kirklandsign

Differential Revision: D69942429

examples/models/llama/TARGETS
@@ -96,6 +96,9 @@ runtime.command_alias(
 
 runtime.python_library(
     name = "source_transformation",
+    visibility = [
+        "//executorch/examples/...",
+    ],
     srcs = [
         "source_transformation/apply_spin_quant_r1_r2.py",
         "source_transformation/attention.py",

examples/qualcomm/oss_scripts/llama/TARGETS
@@ -19,6 +19,7 @@ python_library(
     name = "llama_lib",
     srcs = ["llama.py"],
     deps = [
+        "//executorch/examples/models/llama:source_transformation",
         "//caffe2:torch",
         "//executorch/backends/qualcomm/partition:partition",
         "//executorch/backends/qualcomm/quantizer:quantizer",

examples/qualcomm/oss_scripts/llama/llama.py
@@ -1039,10 +1039,7 @@ def _build_parser():
     return parser
 
 
-def main(args) -> None:
-    parser = _build_parser()
-
-    args = parser.parse_args(args)
+def export_llama(args) -> None:
     if args.compile_only and args.pre_gen_pte:
         exit("Cannot set both compile_only and pre_gen_pte as true")
 
@@ -1143,6 +1140,12 @@ def main(args) -> None:
             raise Exception(e)
 
 
+def main():
+    parser = _build_parser()
+    args = parser.parse_args()
+    export_llama(args)
+
+
 # flake8: noqa: C901
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()