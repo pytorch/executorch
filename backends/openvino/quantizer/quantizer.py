ac8df240446391a8ba38be165f6bf76aad684728
Cavus Mustafa <mustafa.cavus@intel.com>
2025-02-25 12:16:25 -0800

    License comment line update

--- backends/openvino/quantizer/quantizer.py
+++ backends/openvino/quantizer/quantizer.py
@@ -1,8 +1,8 @@
 # Copyright (c) Intel Corporation
 #
 # Licensed under the BSD License (the "License"); you may not use this file
-# except in compliance with the License. See the license file in the root
-# directory of this source tree for more details.
+# except in compliance with the License. See the license file found in the
+# LICENSE file in the root directory of this source tree.
 
 from collections import defaultdict
 from enum import Enum
