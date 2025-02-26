0ff7e47ba1e582c259a7ce077d4fac5d5d12a908
Martin Yuan <myuan@meta.com>
2025-02-26 15:42:57 -0800

    Mimi export using definition in transformers

--- examples/models/moshi/mimi/mimi_export.py
+++ examples/models/moshi/mimi/mimi_export.py
@@ -2,7 +2,8 @@ from transformers import MimiModel
 import torch
 import torch.nn as nn
 from torch.export import export, export_for_training, ExportedProgram
-
+from typing import List, Optional, Tuple, Union
+from transformers.cache_utils import Cache
 
 mimi: nn.Module = MimiModel.from_pretrained("kyutai/mimi")
 # print(mimi)
@@ -29,10 +30,34 @@ class MimiDecode(nn.Module):
         super().__init__()
         self.mimi_model = mimi
 
-    def forward(self, x):
-        return self.mimi_model.decode(x)
+    def forward(self,
+            input_values: torch.Tensor,
+            past_key_values: Optional[Union[Cache, List[torch.FloatTensor]]] = None,
+            return_dict: Optional[bool] = None,
+            use_code: bool = False,
+            requires_grad: bool = False,
+        ):
+        embeddings = input_values
+        embeddings = self.mimi_model.upsample(embeddings)
+
+        # pyre-fixme[29]: `Union[Tensor, Module]` is not a function.
+        decoder_outputs = self.mimi_model.decoder_transformer(
+            embeddings.transpose(1, 2),
+            past_key_values=past_key_values,
+            return_dict=return_dict,
+        )
+        if return_dict:
+            past_key_values = decoder_outputs.get("past_key_values")
+        elif len(decoder_outputs) > 1:
+            past_key_values = decoder_outputs[1]
+        embeddings = decoder_outputs[0].transpose(1, 2)
+        # pyre-fixme[29]: `Union[Tensor, Module]` is not a function.
+        outputs = self.mimi_model.decoder(embeddings)
+        return outputs
+
 
 mimi_decode = MimiDecode(mimi)
-decode_input = torch.ones(1, 32, 125, dtype=float)
+decode_input = torch.ones(1, 512, 125, dtype=torch.float32)
+decode_output = mimi_decode(decode_input)
 exported_decode = export_for_training(mimi_decode, (decode_input,), strict=False).module()
 
---
+++ examples/models/moshi/mimi_test.py
@@ -0,0 +1,163 @@
+# Copyright (c) Kyutai, all rights reserved.
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+import argparse
+import random
+import time
+
+from huggingface_hub import hf_hub_download
+import numpy as np
+import sphn
+import torch
+from torch.profiler import profile, ProfilerActivity
+
+from moshi.models import loaders
+
+import torch.nn as nn
+
+from executorch.examples.models.llama.llama_transformer import Transformer
+
+from executorch.examples.models.llama.model_args import ModelArgs
+
+from torch.export import export, export_for_training, ExportedProgram
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--mimi-weight", type=str)
+parser.add_argument("--hf-repo", type=str, default=loaders.DEFAULT_REPO)
+parser.add_argument(
+    "--device", type=str, default="cuda" if torch.cuda.device_count() else "cpu"
+)
+parser.add_argument("--profile", action="store_true")
+args = parser.parse_args()
+
+
+def seed_all(seed):
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)  # for multi-GPU setups
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
+seed_all(42424242)
+
+
+print("loading mimi")
+if args.mimi_weight is None:
+    args.mimi_weight = hf_hub_download(args.hf_repo, loaders.MIMI_NAME)
+mimi = loaders.get_mimi(args.mimi_weight, args.device)
+# print("mimi loaded")
+# emb = torch.load('emb.pt')
+# (emb,) = mimi.decoder_transformer(emb)
+# print(emb)
+
+# model_args: ModelArgs = ModelArgs(
+#     dim=512,
+#     n_layers=8,
+#     n_heads=8,
+#     max_seq_len=250,
+#     max_context_len=250,
+#     max_batch_size=1,
+#     use_kv_cache=True,
+#     use_sdpa_with_kv_cache_op=True,
+#     apply_embedding=False,
+#     apply_output=False,
+# )
+# et_transformer = Transformer(model_args)
+# print(et_transformer)
+# print("end of experiment")
+
+
+def mimi_streaming_test(mimi, max_duration_sec=10.0):
+    pcm_chunk_size = int(mimi.sample_rate / mimi.frame_rate)
+    # # wget https://github.com/metavoiceio/metavoice-src/raw/main/assets/bria.mp3
+    # sample_pcm, sample_sr = sphn.read("/Users/myuan/src/moshi0/src/moshi/data/bria-24khz.mp3")
+    # sample_rate = mimi.sample_rate
+    # print("loaded pcm", sample_pcm.shape, sample_sr)
+    # sample_pcm = sphn.resample(
+    #     sample_pcm, src_sample_rate=sample_sr, dst_sample_rate=sample_rate
+    # )
+    # sample_pcm = torch.tensor(sample_pcm, device=args.device)
+    # max_duration_len = int(sample_rate * max_duration_sec)
+    # if sample_pcm.shape[-1] > max_duration_len:
+    #     sample_pcm = sample_pcm[..., :max_duration_len]
+    # print("resampled pcm", sample_pcm.shape, sample_sr)
+    # sample_pcm = sample_pcm[None].to(device=args.device)
+    #
+    sample_pcm = torch.ones(1,1,240000)
+
+    print("streaming encoding...")
+    start_time = time.time()
+    all_codes = []
+
+    def run_loop():
+        for start_idx in range(0, sample_pcm.shape[-1], pcm_chunk_size):
+            end_idx = min(sample_pcm.shape[-1], start_idx + pcm_chunk_size)
+            chunk = sample_pcm[..., start_idx:end_idx]
+            codes = mimi.encode(chunk)
+            if codes.shape[-1]:
+                print(start_idx, codes.shape, end="\r")
+                all_codes.append(codes)
+
+    if args.profile:
+        with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA]) as prof:
+            run_loop()
+        prof.export_chrome_trace("trace.json")
+    else:
+        run_loop()
+    all_codes_th = torch.cat(all_codes, dim=-1)
+    print(f"codes {all_codes_th.shape} generated in {time.time() - start_time:.2f}s")
+    print("streaming decoding...")
+    all_pcms = []
+    # with mimi.streaming(1):
+    #     for i in range(all_codes_th.shape[-1]):
+    #         codes = all_codes_th[..., i : i + 1]
+    #         pcm = mimi.decode(codes)
+    #         print(i, pcm.shape, end="\r")
+    #         all_pcms.append(pcm)
+    # all_pcms = torch.cat(all_pcms, dim=-1)
+    # print("pcm", all_pcms.shape, all_pcms.dtype)
+    # sphn.write_wav("streaming_out.wav", all_pcms[0, 0].cpu().numpy(), sample_rate)
+    pcm_ref = mimi.decode(all_codes_th)
+
+    class MimiEncode(nn.Module):
+        def __init__(self, mimi: nn.Module):
+            super().__init__()
+            self.mimi_model = mimi
+
+        def forward(self, x):
+            return self.mimi_model.encode(x)
+
+    mimi_encode = MimiEncode(mimi)
+    chunk = sample_pcm[..., 0:pcm_chunk_size]
+    out = mimi_encode(chunk)
+    exported_encode = export_for_training(mimi_encode, (chunk,), strict=False).module()
+    class MimiDecode(nn.Module):
+        def __init__(self, mimi: nn.Module):
+            super().__init__()
+            self.mimi_model = mimi
+
+        def forward(self, x):
+            return self.mimi_model.decode(x)
+
+    mimi_decode = MimiDecode(mimi)
+
+    exported_decode = export_for_training(mimi_decode, (all_codes_th,), strict=False).module()
+
+
+    emb = mimi.decode_latent(all_codes_th)
+    emb = mimi._to_encoder_framerate(emb)
+    (emb,) = mimi.decoder_transformer(emb)
+    with mimi._context_for_encoder_decoder:
+        pcm = mimi.decoder(emb)
+
+    print("pcm", pcm.shape, pcm.dtype)
+    sphn.write_wav("roundtrip_out.wav", pcm[0, 0].cpu().numpy(), sample_rate)
+
+
+with torch.no_grad():
+    mimi_streaming_test(mimi)
