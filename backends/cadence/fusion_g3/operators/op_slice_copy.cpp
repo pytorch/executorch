bdd6e421812ed81f93255bbd8e3168d4f0efca94
cmadhira@cadence.com <cmadhira@global.cadence.com>
2025-02-26 18:07:55 +0530

    resolved linter error in slice_copy

--- backends/cadence/fusion_g3/operators/op_slice_copy.cpp
+++ backends/cadence/fusion_g3/operators/op_slice_copy.cpp
@@ -102,7 +102,7 @@ Tensor& slice_copy_Tensor_out(
        (out.scalar_type() == ScalarType::UInt32) ||
        (out.scalar_type() == ScalarType::UInt16) ||
        (out.scalar_type() == ScalarType::Byte) ||
-       (out.scalar_type() == ScalarType::Float))){
+       (out.scalar_type() == ScalarType::Float))) {
     XT_KERNEL_CHECK(
         ctx,
         out,
