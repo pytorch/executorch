08467a1da71f0a635974bab8f337cd5fe638f7bb
Mengwei Liu <larryliu@fb.com>
2025-03-07 12:18:44 -0800

    mkdir when destination directory doesn't exist
    Fixes #8980
    
    Make sure we mkdir if destination directory doesn't exist. I have no
    idea why it doesn't work before
    
    [ghstack-poisoned]

--- setup.py
+++ setup.py
@@ -463,8 +463,11 @@ class InstallerBuildExt(build_ext):
         dst_file: Path = ext.dst_path(self)
 
         # Ensure that the destination directory exists.
-        self.mkpath(os.fspath(dst_file.parent))
-
+        if not dst_file.parent.exists():
+            print(f"DEBUG 1: dest parent directory {dst_file.parent}, exists? {dst_file.parent.exists()}")
+            self.mkpath(os.fspath(dst_file.parent))
+            print(f"DEBUG 2: dest parent directory {dst_file.parent}, exists? {dst_file.parent.exists()}")
+            
         # Copy the file.
         self.copy_file(os.fspath(src_file), os.fspath(dst_file))
 
