db0bd83bc58267d99998fd2029315797a8db8bcb
Hansong Zhang <hsz@fb.com>
2025-03-07 14:42:48 -0800

    Docker

---
+++ .ci/docker/common/install_java.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+set -ex
+
+apt-get install -y --no-install-recommends \
+        openjdk-17-jdk \
+        ca-certificates-java \
+        ant
