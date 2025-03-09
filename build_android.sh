11e4a53ebd27a8f282e30d4ecec402da78925f10
jathu <jathu.satkunarajah@gmail.com>
2025-03-09 15:30:04 -0700

    wip, [Mar 09, 2025 03:38:03 PM] update

---
+++ build_android.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -eu
+
+
+export ANDROID_NDK="/opt/android_ndk/r17fb2"
+# android_abis=("arm64-v8a" "x86_64")
+android_abis=("x86_64")
+cmake_jobs=$(( $(sysctl -n hw.ncpu) - 1 ))
+
+for abi in "${android_abis[@]}"; do
+    echo "abi: ${abi}"
+    out_dir="cmake-out-android-${abi}"
+    mkdir -p "${out_dir}"
+    echo "out_dir: ${abi}"
+
+  cmake . -DCMAKE_INSTALL_PREFIX="${out_dir}" \
+    -DCMAKE_TOOLCHAIN_FILE="${ANDROID_NDK}/build/cmake/android.toolchain.cmake" \
+    -DANDROID_ABI="${abi}" \
+    -DEXECUTORCH_BUILD_XNNPACK=ON \
+    -DEXECUTORCH_BUILD_EXTENSION_DATA_LOADER=ON \
+    -DEXECUTORCH_BUILD_EXTENSION_MODULE=ON \
+    -DEXECUTORCH_BUILD_EXTENSION_RUNNER_UTIL=ON \
+    -DEXECUTORCH_BUILD_EXTENSION_TENSOR=ON \
+    -B"${out_dir}"
+
+  cmake --build "${out_dir}" -j "${cmake_jobs}" --target install
+done
