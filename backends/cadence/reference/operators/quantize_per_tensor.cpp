1e84bea5bb7bd446479482d545256237080901ff
Zonglin Peng <zonglinpeng@fb.com>
2025-03-10 11:20:07 -0700

    lint

--- backends/cadence/reference/operators/quantize_per_tensor.cpp
+++ backends/cadence/reference/operators/quantize_per_tensor.cpp
@@ -5,6 +5,7 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
 #include <executorch/backends/cadence/reference/kernels/kernels.h>
 #include <executorch/runtime/kernel/kernel_includes.h>
 
