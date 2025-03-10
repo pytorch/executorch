f5cd7a4f5c22e067edfa42634f81a245c03e3635
Per Åstrand <per.astrand@arm.com>
2025-03-10 16:55:07 +0100

    Arm backend: Move _transpose to backend dialect
    Signed-off-by: Per Åstrand <per.astrand@arm.com>
    Change-Id: I9e2b03cfafba03decc364034ef3d8c06b73b93ae

--- backends/arm/tosa/dialect/__init__.py
+++ backends/arm/tosa/dialect/__init__.py
@@ -3,4 +3,8 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from executorch.backends.arm.tosa.dialect.ops import rescale, table  # noqa F401
+from executorch.backends.arm.tosa.dialect.ops import (  # noqa F401
+    rescale,
+    table,
+    transpose,
+)
