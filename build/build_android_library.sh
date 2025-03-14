ad8dc4bb0ee4d49f4f45dbf46897ef0308ba0615
Hansong Zhang <hsz@fb.com>
2025-03-14 12:07:04 -0700

    Simplify stuff

--- build/build_android_library.sh
+++ build/build_android_library.sh
@@ -137,37 +137,6 @@ build_aar() {
   popd
 }
 
-build_android_demo_apps() {
-  mkdir -p examples/demo-apps/android/LlamaDemo/app/libs
-  cp ${BUILD_AAR_DIR}/executorch.aar examples/demo-apps/android/LlamaDemo/app/libs
-  pushd examples/demo-apps/android/LlamaDemo
-  ANDROID_HOME="${ANDROID_SDK:-/opt/android/sdk}" ./gradlew build assembleAndroidTest
-  popd
-
-  mkdir -p extension/benchmark/android/benchmark/app/libs
-  cp ${BUILD_AAR_DIR}/executorch.aar extension/benchmark/android/benchmark/app/libs
-  pushd extension/benchmark/android/benchmark
-  ANDROID_HOME="${ANDROID_SDK:-/opt/android/sdk}" ./gradlew build assembleAndroidTest
-  popd
-}
-
-collect_artifacts_to_be_uploaded() {
-  ARTIFACTS_DIR_NAME="$1"
-  DEMO_APP_DIR="${ARTIFACTS_DIR_NAME}/llm_demo"
-  # The app directory is named using its build flavor as a suffix.
-  mkdir -p "${DEMO_APP_DIR}"
-  # Collect the app and its test suite
-  cp examples/demo-apps/android/LlamaDemo/app/build/outputs/apk/debug/*.apk "${DEMO_APP_DIR}"
-  cp examples/demo-apps/android/LlamaDemo/app/build/outputs/apk/androidTest/debug/*.apk "${DEMO_APP_DIR}"
-  # Collect AAR
-  cp "${BUILD_AAR_DIR}/executorch.aar" "${DEMO_APP_DIR}"
-  # Collect MiniBench APK
-  MINIBENCH_APP_DIR="${ARTIFACTS_DIR_NAME}/minibench"
-  mkdir -p "${MINIBENCH_APP_DIR}"
-  cp extension/benchmark/android/benchmark/app/build/outputs/apk/debug/*.apk "${MINIBENCH_APP_DIR}"
-  cp extension/benchmark/android/benchmark/app/build/outputs/apk/androidTest/debug/*.apk "${MINIBENCH_APP_DIR}"
-}
-
 main() {
   if [[ -z "${BUILD_AAR_DIR:-}" ]]; then
     BUILD_AAR_DIR="$(mktemp -d)"
@@ -185,10 +154,6 @@ main() {
     build_android_native_library ${ANDROID_ABI}
   done
   build_aar
-  # build_android_demo_apps
-  # if [ -n "$ARTIFACTS_DIR_NAME" ]; then
-    # collect_artifacts_to_be_uploaded ${ARTIFACTS_DIR_NAME}
-  # fi
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
