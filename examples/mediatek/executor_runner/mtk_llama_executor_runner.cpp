39d88943984a3c486f5389b13e046c5e13ee1996
Mergen Nachin <mnachin@meta.com>
2025-03-14 11:20:17 -0400

    Fix MediaTek license headers

--- examples/mediatek/executor_runner/mtk_llama_executor_runner.cpp
+++ examples/mediatek/executor_runner/mtk_llama_executor_runner.cpp
@@ -7,16 +7,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-/* Copyright Statement:
- *
- * This software/firmware and related documentation ("MediaTek Software") are
- * protected under relevant copyright laws. The information contained herein
- * is confidential and proprietary to MediaTek Inc. and/or its licensors.
- * Without the prior written permission of MediaTek inc. and/or its licensors,
- * any reproduction, modification, use or disclosure of MediaTek Software,
- * and information contained herein, in whole or in part, shall be strictly
- * prohibited.
- */
 /* MediaTek Inc. (C) 2024. All rights reserved.
  *
  * BY OPENING THIS FILE, RECEIVER HEREBY UNEQUIVOCALLY ACKNOWLEDGES AND AGREES
