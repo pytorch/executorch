35672e9fbe9e6204632944bac7064fcf5941a2ad
Anthony Shoumikhin <anthony@shoumikh.in>
2025-03-18 11:12:45 -0700

    Use resolve_buck script to find buck executable when building Apple frameworks

--- build/build_apple_frameworks.sh
+++ build/build_apple_frameworks.sh
@@ -205,7 +205,7 @@ echo "Exporting headers"
 
 mkdir -p "$HEADERS_PATH"
 
-BUCK2=$(find $SOURCE_ROOT_DIR -type f -path '*/buck2-bin/buck2-*' | head -n 1)
+BUCK2=$("$PYTHON" "$SOURCE_ROOT_DIR/tools/cmake/resolve_buck.py" --cache_dir="buck2-bin")
 if [[ -z "$BUCK2" ]]; then
   echo "Could not find buck2 executable in any buck2-bin directory under $SOURCE_ROOT_DIR"
   BUCK2=$(which buck2)
