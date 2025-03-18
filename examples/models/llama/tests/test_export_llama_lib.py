f125d7437853b588ba0b20ec138f6d59b4b61f23
Jack <32371937+jackzhxng@users.noreply.github.com>
2025-03-18 16:53:09 -0700

    Only check unwanted ops

--- examples/models/llama/tests/test_export_llama_lib.py
+++ examples/models/llama/tests/test_export_llama_lib.py
@@ -11,42 +11,15 @@ import torch
 from executorch.devtools.backend_debug import get_delegation_info
 from executorch.examples.models.llama.export_llama_lib import _export_llama, build_args_parser
 
-
-# Ops expected to be found in the default exported llama_transformer. Obtained through
-# print_delegation_info from the backend_debug module, which is displayed with
-# export_llama under --verbose.
-BASE_EXPECTED_OPS = {
-    "sym_size": 1,
-    "alloc": 288,
-    "aten_embedding_default": 1,
-    "aten_select_copy_int": 12,
-    "_local_scalar_dense": 11,
-    "add": 1,
-    "aten_slice_copy_tensor": 23,
-    "aten_mul_tensor": 83,
-    "aten_mean_dim": 11,
-    "aten_add_tensor": 31,
-    "aten_rsqrt_default": 11,
-    "aten_linear_default": 36,
-    "aten_view_copy_default": 40,
-    "aten_squeeze_copy_dims": 20,
-    "aten_sub_tensor": 10,
-    "aten_unsqueeze_copy_default": 20,
-    "aten_cat_default": 10,
-    "update_cache": 10,
-    "llama_custom_sdpa_default": 5,
-    "aten_sigmoid_default": 5,
-}
-
 UNWANTED_OPS = [
     "aten_permute_copy_default",
+    "aten_transpose_copy_default",
 ]
 
 class ExportLlamaLibTest(unittest.TestCase):
     def test_has_expected_ops_and_op_counts(self):
         """
-        Tests that the presence of expected ops and counts for each op are
-        do not change.
+        Checks the presence of unwanted expensive ops.
 
         Serves as a proxy for a performance regression test, as performance
         is directly tied to which and how many of each ops are in the graph.
@@ -73,9 +46,5 @@ class ExportLlamaLibTest(unittest.TestCase):
         delegation_info = get_delegation_info(graph_module)
 
         for op, op_info in delegation_info.delegation_by_operator.items():
-            self.assertTrue(op in BASE_EXPECTED_OPS)
             self.assertTrue(op not in UNWANTED_OPS)
-            self.assertEqual(op_info.non_delegated, BASE_EXPECTED_OPS[op])
-
-        self.assertEqual(len(delegation_info.delegation_by_operator), len(BASE_EXPECTED_OPS))
         
