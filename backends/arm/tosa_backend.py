5bf723158265f9abd8aae7a295f1ebbe0fc53171
Mengwei Liu <larryliu0820@users.noreply.github.com>
2025-03-18 14:32:11 -0700

    Revert "Arm backend: Add tosa_spec and et-version info to .tosa files (#9308)"
    This reverts commit 28673ad8c5a807756a610fddea344539988c2b4b.

--- backends/arm/tosa_backend.py
+++ backends/arm/tosa_backend.py
@@ -12,7 +12,6 @@
 #
 import logging
 import os
-from importlib.metadata import version
 from typing import cast, final, List
 
 import serializer.tosa_serializer as ts  # type: ignore
@@ -126,12 +125,10 @@ class TOSABackend(BackendDetails):
 
         if artifact_path:
             tag = _get_first_delegation_tag(graph_module)
-            et_version = version("executorch")
             dbg_tosa_dump(
                 tosa_graph,
                 artifact_path,
-                suffix="{}".format(f"_{tag}" if tag else "")
-                + (f"_{tosa_spec}" + (f"_{et_version}")),
+                suffix="{}".format(f"_{tag}" if tag else ""),
             )
 
         # Serialize and return the TOSA flatbuffer.
