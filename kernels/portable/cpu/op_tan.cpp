ed37d72c38d9c56b9d7f1f6db5a0ca764877bda5
Scott Wolchok <swolchok@meta.com>
2025-03-19 19:57:02 -0700

    Implement unary_ufunc functions using elementwise_util
    One less set of independent implementations to worry about going forward (e.g., we don't have to vectorize these separately from elementwise_util and they get all benefits of elementwise_util).
    
    ghstack-source-id: a15b66e4a1bc0142ce38d437edabde37e43e30da
    ghstack-comment-id: 2735017402
    Pull Request resolved: https://github.com/pytorch/executorch/pull/9386

--- kernels/portable/cpu/op_tan.cpp
+++ kernels/portable/cpu/op_tan.cpp
@@ -15,7 +15,9 @@ namespace executor {
 namespace native {
 
 Tensor& tan_out(KernelRuntimeContext& ctx, const Tensor& in, Tensor& out) {
-  return internal::unary_ufunc_realhbbf16_to_floathbf16(std::tan, ctx, in, out);
+  static constexpr const char op_name[] = "tan.out";
+  return internal::unary_ufunc_realhbbf16_to_floathbf16<op_name>(
+      [](auto x) { return std::tan(x); }, ctx, in, out);
 }
 
 } // namespace native
