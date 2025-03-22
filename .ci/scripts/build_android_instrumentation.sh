6f6dd0b5c8f23eda8a6b4b9970eb8bef67c35cf9
Hansong Zhang <hsz@fb.com>
2025-03-21 22:01:23 -0700

    Run android emulator test for aar

--- .ci/scripts/build_android_instrumentation.sh
+++ .ci/scripts/build_android_instrumentation.sh
@@ -13,27 +13,21 @@ fi
 which "${PYTHON_EXECUTABLE}"
 
 build_android_test() {
-  mkdir -p extension/android/executorch_android/src/androidTest/resources
-  cp extension/module/test/resources/add.pte extension/android/executorch_android/src/androidTest/resources
-  pushd extension/android
+  mkdir -p "${BUILD_AAR_DIR}"/executorch_android/src/androidTest/resources
+  cp extension/module/test/resources/add.pte "${BUILD_AAR_DIR}"/executorch_android/src/androidTest/resources
+
+  pushd "${BUILD_AAR_DIR}"
   ANDROID_HOME="${ANDROID_SDK:-/opt/android/sdk}" ./gradlew :executorch_android:testDebugUnitTest
   ANDROID_HOME="${ANDROID_SDK:-/opt/android/sdk}" ./gradlew :executorch_android:assembleAndroidTest
+  if [ -n "$ARTIFACTS_DIR_NAME" ]; then
+    mkdir -p "${ARTIFACTS_DIR_NAME}/library_test_dir"
+    cp executorch_android/build/outputs/apk/androidTest/debug/executorch_android-debug-androidTest.apk "${ARTIFACTS_DIR_NAME}/library_test_dir"
+  fi
   popd
-}
-
-collect_artifacts_to_be_uploaded() {
-  ARTIFACTS_DIR_NAME="$1"
-  # Collect Java library test
-  JAVA_LIBRARY_TEST_DIR="${ARTIFACTS_DIR_NAME}/library_test_dir"
-  mkdir -p "${JAVA_LIBRARY_TEST_DIR}"
-  cp extension/android/executorch_android/build/outputs/apk/androidTest/debug/*.apk "${JAVA_LIBRARY_TEST_DIR}"
 }
 
 main() {
   build_android_test
-  if [ -n "$ARTIFACTS_DIR_NAME" ]; then
-    collect_artifacts_to_be_uploaded ${ARTIFACTS_DIR_NAME}
-  fi
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
