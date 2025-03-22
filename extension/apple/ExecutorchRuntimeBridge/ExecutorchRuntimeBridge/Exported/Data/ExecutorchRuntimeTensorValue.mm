a4f2ab97796fc797ea7574fc94102aa885a6962a
Berker Soyluoglu <bsoyluoglu@meta.com>
2025-03-21 17:46:28 -0700

    Remove tuple from module implementation
    Differential Revision: D71658527
    
    Pull Request resolved: https://github.com/pytorch/executorch/pull/9519

--- extension/apple/ExecutorchRuntimeBridge/ExecutorchRuntimeBridge/Exported/Data/ExecutorchRuntimeTensorValue.mm
+++ extension/apple/ExecutorchRuntimeBridge/ExecutorchRuntimeBridge/Exported/Data/ExecutorchRuntimeTensorValue.mm
@@ -71,25 +71,31 @@ using torch::executor::ScalarType;
   return [self initWithData:floatVector shape:shapeVector];
 }
 
-- (nullable ModelRuntimeTensorValueBridgingTuple *)floatRepresentationAndReturnError:(NSError * _Nullable * _Nullable)error
+- (NSArray<NSNumber *> *)shape
 {
+  const auto sizes = _tensor->sizes();
+  std::vector<int32_t> tensorSizes(sizes.begin(), sizes.end());
+
+  NSMutableArray<NSNumber *> *sizesArray = [[NSMutableArray alloc] initWithCapacity:tensorSizes.size()];
+  for (int &tensorSize : tensorSizes) {
+    [sizesArray addObject:@(tensorSize)];
+  }
+
+  return sizesArray;
+}
+
+- (NSArray<NSNumber *> * _Nullable)floatArrayAndReturnError:(NSError * _Nullable * _Nullable)error {
   if (_tensor->scalar_type() == torch::executor::ScalarType::Float) {
-    const auto *tensorPtr = _tensor->data<float>();
-    const auto sizes = _tensor->sizes();
-    std::vector<float> tensorVec(tensorPtr, tensorPtr + _tensor->numel());
-    std::vector<int32_t> tensorSizes(sizes.begin(), sizes.end());
+      const auto *tensorPtr = _tensor->data<float>();
+      const auto sizes = _tensor->sizes();
+      std::vector<float> tensorVec(tensorPtr, tensorPtr + _tensor->numel());
+      std::vector<int32_t> tensorSizes(sizes.begin(), sizes.end());
 
-    NSMutableArray<NSNumber *> *floatArray = [[NSMutableArray alloc] initWithCapacity:tensorVec.size()];
-    for (float &i : tensorVec) {
-      [floatArray addObject:@(i)];
-    }
-
-    NSMutableArray<NSNumber *> *sizesArray = [[NSMutableArray alloc] initWithCapacity:tensorSizes.size()];
-    for (int &tensorSize : tensorSizes) {
-      [sizesArray addObject:@(tensorSize)];
-    }
-
-    return [[ModelRuntimeTensorValueBridgingTuple alloc] initWithFloatArray:floatArray shape:sizesArray];
+      NSMutableArray<NSNumber *> *floatArray = [[NSMutableArray alloc] initWithCapacity:tensorVec.size()];
+      for (float &i : tensorVec) {
+        [floatArray addObject:@(i)];
+      }
+    return floatArray;
   }
 
   if (error) {
