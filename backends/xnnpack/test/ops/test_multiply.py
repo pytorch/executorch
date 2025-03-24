821551fe0dc94ca3840a88907ee597d0ab5af6a7
Zuby A <git.zuby@gmail.com>
2025-03-23 21:34:36 -0700

    Add test for mixed dtype mul ops

--- backends/xnnpack/test/ops/test_multiply.py
+++ backends/xnnpack/test/ops/test_multiply.py
@@ -31,15 +31,24 @@ class TestMul(unittest.TestCase):
             z = x * y
             return torch.nn.functional.relu(z)
 
-    def _test_mul(self, inputs):
-        (
+    def _test_mul(self, inputs, mixed_dtype=False):
+        tester = (
             Tester(self.Mul(), inputs)
             .export()
             .check_count({"torch.ops.aten.mul.Tensor": 1})
             .to_edge_transform_and_lower()
-            .check_count({"torch.ops.higher_order.executorch_call_delegate": 1})
-            .check_not(["executorch_exir_dialects_edge__ops_aten_mul_Tensor"])
-            .to_executorch()
+        )
+
+        if mixed_dtype:
+            # Inverse check for mixed-dtype: original node remains and no delegate node
+            tester.check_count({"executorch_exir_dialects_edge__ops_aten_mul_Tensor": 1})
+            tester.check_not(["torch.ops.higher_order.executorch_call_delegate"])
+        else:
+            tester.check_count({"torch.ops.higher_order.executorch_call_delegate": 1})
+            tester.check_not(["executorch_exir_dialects_edge__ops_aten_mul_Tensor"])
+
+        (
+            tester.to_executorch()
             .serialize()
             .run_method_and_compare_outputs()
         )
@@ -144,3 +153,19 @@ class TestMul(unittest.TestCase):
             .serialize()
             .run_method_and_compare_outputs()
         )
+
+    def test_fp32_mul_with_mixed_dtype(self):
+        test_cases = [
+            torch.bfloat16,
+            torch.float16,
+            torch.int8,
+        ]
+        for dtype in test_cases:
+            with self.subTest(dtype=str(dtype)):
+                inputs = (
+                    torch.randn(1, 1, 4, 4).to(torch.float32),
+                    torch.randn(1, 1, 4, 4).to(dtype),
+                )
+                # Set mixed_dtype=True to verify that
+                # no delegate node is inserted and the original node remains in the graph
+                self._test_mul(inputs, mixed_dtype=True)
