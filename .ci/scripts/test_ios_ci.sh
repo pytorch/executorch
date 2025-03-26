e23096a7fe9c16e34a3b883e05899e0c660572a5
Huy Do <huydhn@gmail.com>
2025-03-26 13:54:31 -0700

    Use SECRET_GITHUB_TOKEN

--- .ci/scripts/test_ios_ci.sh
+++ .ci/scripts/test_ios_ci.sh
@@ -36,7 +36,7 @@ say() {
 
 say "Cloning the Demo App"
 
-git clone --depth 1 https://${GITHUB_TOKEN}:github.com/pytorch-labs/executorch-examples.git
+git clone --depth 1 https://${SECRET_GITHUB_TOKEN}:github.com/pytorch-labs/executorch-examples.git
 
 say "Installing CoreML Backend Requirements"
 
