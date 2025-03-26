265b9b784eef08acf36e5140f9e51005dc210505
Erik Lundell <erik.lundell@arm.com>
2025-03-26 15:27:50 +0100

    Arm backend: Extend convolution support check to 3d (#9640)
    Add conv3d tests, though most are skipped since conv3d support is not
    yet implemented.
    
    Signed-off-by: Erik Lundell <erik.lundell@arm.com>

--- backends/arm/test/ops/test_conv3d.py
+++ backends/arm/test/ops/test_conv3d.py
@@ -1,4 +1,4 @@
-# Copyright 2024-2025 Arm Limited and/or its affiliates.
+# Copyright 2025 Arm Limited and/or its affiliates.
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
@@ -6,6 +6,7 @@
 
 from typing import List, Tuple, Union
 
+import pytest
 import torch
 from executorch.backends.arm.test import common
 from executorch.backends.arm.test.tester.test_pipeline import (
@@ -16,13 +17,13 @@ from executorch.backends.arm.test.tester.test_pipeline import (
     TosaPipelineMI,
 )
 
-aten_op = "torch.ops.aten.conv2d.default"
+aten_op = "torch.ops.aten.conv3d.default"
 exir_op = "executorch_exir_dialects_edge__ops_aten_convolution_default"
 
 
-class Conv2d(torch.nn.Module):
+class Conv3d(torch.nn.Module):
     """
-    Creates one or many chained 2D-convolutions. For multiple convolutions, the
+    Creates one or many chained 3D-convolutions. For multiple convolutions, the
     respective parameteres are provided as lists.
     """
 
@@ -30,6 +31,7 @@ class Conv2d(torch.nn.Module):
         self,
         height=8,
         width=8,
+        depth=8,
         nbr_conv=1,  # Number of chained convs
         in_channels: Union[List, int, None] = None,
         out_channels: Union[List, int, None] = None,
@@ -49,10 +51,10 @@ class Conv2d(torch.nn.Module):
         # Handle default values
         in_channels = [2] * nbr_conv if in_channels is None else in_channels
         out_channels = [1 * nbr_conv] if out_channels is None else out_channels
-        kernel_size = [(3, 3)] * nbr_conv if kernel_size is None else kernel_size
-        stride = [(2, 2)] * nbr_conv if stride is None else stride
-        padding = [(1, 1)] * nbr_conv if padding is None else padding
-        dilation = [(1, 1)] * nbr_conv if dilation is None else dilation
+        kernel_size = [(3, 3, 1)] * nbr_conv if kernel_size is None else kernel_size
+        stride = [(2, 2, 1)] * nbr_conv if stride is None else stride
+        padding = [(1, 1, 1)] * nbr_conv if padding is None else padding
+        dilation = [(1, 1, 1)] * nbr_conv if dilation is None else dilation
         groups = [1] * nbr_conv if groups is None else groups
         bias = [True] * nbr_conv if bias is None else bias
         padding_mode = ["zeros"] * nbr_conv if padding_mode is None else padding_mode
@@ -82,6 +84,7 @@ class Conv2d(torch.nn.Module):
         self.in_channels = in_channels
         self.height = height
         self.width = width
+        self.depth = depth
         self.dtype = dtype
 
         # Build chain of convs
@@ -89,7 +92,7 @@ class Conv2d(torch.nn.Module):
             setattr(
                 self,
                 f"conv_{i}",
-                torch.nn.Conv2d(
+                torch.nn.Conv3d(
                     in_channels=in_channels[i],
                     out_channels=out_channels[i],
                     kernel_size=kernel_size[i],
@@ -104,9 +107,9 @@ class Conv2d(torch.nn.Module):
 
     def get_inputs(self):
         return (
-            torch.randn(self.batches, self.in_channels[0], self.height, self.width).to(
-                self.dtype
-            ),
+            torch.randn(
+                self.batches, self.in_channels[0], self.height, self.width, self.depth
+            ).to(self.dtype),
         )
 
     def forward(self, x):
@@ -116,10 +119,10 @@ class Conv2d(torch.nn.Module):
         return x
 
 
-conv2d_2x2_3x2x40x40_nobias = Conv2d(
+conv3d_2x2_3x2x40x40_nobias = Conv3d(
     in_channels=2,
     out_channels=3,
-    kernel_size=(2, 2),
+    kernel_size=(2, 2, 2),
     stride=1,
     bias=False,
     padding=0,
@@ -128,10 +131,10 @@ conv2d_2x2_3x2x40x40_nobias = Conv2d(
     batches=3,
 )
 
-conv2d_3x3_1x3x256x256_st1 = Conv2d(
+conv3d_3x3_1x3x256x256_st1 = Conv3d(
     in_channels=3,
     out_channels=10,
-    kernel_size=(3, 3),
+    kernel_size=(3, 3, 3),
     stride=1,
     padding=0,
     width=256,
@@ -139,10 +142,10 @@ conv2d_3x3_1x3x256x256_st1 = Conv2d(
     batches=1,
 )
 
-conv2d_3x3_1x3x12x12_st2_pd1 = Conv2d(
+conv3d_3x3_1x3x12x12_st2_pd1 = Conv3d(
     in_channels=3,
     out_channels=4,
-    kernel_size=(3, 3),
+    kernel_size=(3, 3, 3),
     stride=2,
     padding=1,
     width=12,
@@ -150,10 +153,10 @@ conv2d_3x3_1x3x12x12_st2_pd1 = Conv2d(
     batches=1,
 )
 
-conv2d_1x1_1x2x128x128_st1 = Conv2d(
+conv3d_1x1_1x2x128x128_st1 = Conv3d(
     in_channels=2,
     out_channels=1,
-    kernel_size=(1, 1),
+    kernel_size=(1, 1, 1),
     stride=1,
     padding=0,
     width=128,
@@ -161,10 +164,10 @@ conv2d_1x1_1x2x128x128_st1 = Conv2d(
     batches=1,
 )
 
-conv2d_2x2_1x1x14x13_st2 = Conv2d(
+conv3d_2x2_1x1x14x13_st2 = Conv3d(
     in_channels=1,
     out_channels=1,
-    kernel_size=(2, 2),
+    kernel_size=(2, 2, 2),
     stride=2,
     padding=0,
     width=14,
@@ -172,10 +175,10 @@ conv2d_2x2_1x1x14x13_st2 = Conv2d(
     batches=1,
 )
 
-conv2d_5x5_3x2x128x128_st1 = Conv2d(
+conv3d_5x5_3x2x128x128_st1 = Conv3d(
     in_channels=2,
     out_channels=3,
-    kernel_size=(5, 5),
+    kernel_size=(5, 5, 5),
     stride=1,
     padding=0,
     width=128,
@@ -183,10 +186,10 @@ conv2d_5x5_3x2x128x128_st1 = Conv2d(
     batches=3,
 )
 
-conv2d_3x3_1x3x224x224_st2_pd1 = Conv2d(
+conv3d_3x3_1x3x224x224_st2_pd1 = Conv3d(
     in_channels=3,
     out_channels=16,
-    kernel_size=(3, 3),
+    kernel_size=(3, 3, 3),
     stride=2,
     padding=1,
     width=224,
@@ -194,10 +197,10 @@ conv2d_3x3_1x3x224x224_st2_pd1 = Conv2d(
     batches=1,
 )
 
-conv2d_5x5_1x3x14x15_st3_pd1 = Conv2d(
+conv3d_5x5_1x3x14x15_st3_pd1 = Conv3d(
     in_channels=3,
     out_channels=16,
-    kernel_size=(5, 5),
+    kernel_size=(5, 5, 5),
     stride=3,
     padding=1,
     width=14,
@@ -205,10 +208,10 @@ conv2d_5x5_1x3x14x15_st3_pd1 = Conv2d(
     batches=1,
 )
 
-conv2d_7x7_1x3x16x16_st2_pd1_dl2 = Conv2d(
+conv3d_7x7_1x3x16x16_st2_pd1_dl2 = Conv3d(
     in_channels=3,
     out_channels=3,
-    kernel_size=(7, 7),
+    kernel_size=(7, 7, 7),
     stride=2,
     padding=1,
     dilation=2,
@@ -217,10 +220,10 @@ conv2d_7x7_1x3x16x16_st2_pd1_dl2 = Conv2d(
     batches=1,
 )
 
-conv2d_7x7_1x3x15x15_st1_pd0_dl1 = Conv2d(
+conv3d_7x7_1x3x15x15_st1_pd0_dl1 = Conv3d(
     in_channels=3,
     out_channels=3,
-    kernel_size=(7, 7),
+    kernel_size=(7, 7, 7),
     stride=1,
     padding=0,
     dilation=1,
@@ -229,10 +232,10 @@ conv2d_7x7_1x3x15x15_st1_pd0_dl1 = Conv2d(
     batches=1,
 )
 
-conv2d_5x5_1x3x14x14_st5_pd0_dl1 = Conv2d(
+conv3d_5x5_1x3x14x14_st5_pd0_dl1 = Conv3d(
     in_channels=3,
     out_channels=3,
-    kernel_size=(5, 5),
+    kernel_size=(5, 5, 5),
     stride=5,
     padding=0,
     dilation=1,
@@ -241,10 +244,10 @@ conv2d_5x5_1x3x14x14_st5_pd0_dl1 = Conv2d(
     batches=1,
 )
 
-conv2d_5x5_1x3x9x9_st5_pd0_dl1 = Conv2d(
+conv3d_5x5_1x3x9x9_st5_pd0_dl1 = Conv3d(
     in_channels=3,
     out_channels=3,
-    kernel_size=(5, 5),
+    kernel_size=(5, 5, 5),
     stride=5,
     padding=0,
     dilation=1,
@@ -253,10 +256,10 @@ conv2d_5x5_1x3x9x9_st5_pd0_dl1 = Conv2d(
     batches=1,
 )
 
-conv2d_3x3_1x3x8x9_st3_pd0_dl1 = Conv2d(
+conv3d_3x3_1x3x8x9_st3_pd0_dl1 = Conv3d(
     in_channels=3,
     out_channels=3,
-    kernel_size=(3, 3),
+    kernel_size=(3, 3, 3),
     stride=3,
     padding=0,
     dilation=1,
@@ -265,10 +268,10 @@ conv2d_3x3_1x3x8x9_st3_pd0_dl1 = Conv2d(
     batches=1,
 )
 
-conv2d_3x3_1x3x9x8_st3_pd0_dl1 = Conv2d(
+conv3d_3x3_1x3x9x8_st3_pd0_dl1 = Conv3d(
     in_channels=3,
     out_channels=3,
-    kernel_size=(3, 3),
+    kernel_size=(3, 3, 3),
     stride=3,
     padding=0,
     dilation=1,
@@ -277,10 +280,10 @@ conv2d_3x3_1x3x9x8_st3_pd0_dl1 = Conv2d(
     batches=1,
 )
 
-conv2d_3x4_1x3x7x7_st3_pd0_dl1 = Conv2d(
+conv3d_3x4_1x3x7x7_st3_pd0_dl1 = Conv3d(
     in_channels=3,
     out_channels=3,
-    kernel_size=(3, 4),
+    kernel_size=(3, 4, 3),
     stride=3,
     padding=0,
     dilation=1,
@@ -289,10 +292,10 @@ conv2d_3x4_1x3x7x7_st3_pd0_dl1 = Conv2d(
     batches=1,
 )
 
-conv2d_4x3_1x3x7x7_st3_pd0_dl1 = Conv2d(
+conv3d_4x3_1x3x7x7_st3_pd0_dl1 = Conv3d(
     in_channels=3,
     out_channels=3,
-    kernel_size=(4, 3),
+    kernel_size=(4, 3, 3),
     stride=3,
     padding=0,
     dilation=1,
@@ -301,64 +304,31 @@ conv2d_4x3_1x3x7x7_st3_pd0_dl1 = Conv2d(
     batches=1,
 )
 
-two_conv2d_nobias = Conv2d(
-    nbr_conv=2,
-    width=256,
-    height=256,
-    in_channels=[3, 10],
-    out_channels=[10, 15],
-    kernel_size=[(5, 5), (5, 5)],
-    stride=[1, 1],
-    padding=[0, 0],
-    bias=[False, False],
-    batches=1,
-)
-
-two_conv2d = Conv2d(
-    nbr_conv=2,
-    width=256,
-    height=256,
-    in_channels=[3, 10],
-    out_channels=[10, 15],
-    kernel_size=[(5, 5), (5, 5)],
-    stride=[1, 1],
-    padding=[0, 0],
-    bias=[True, True],
-    batches=1,
-)
-
-# Shenanigan to get a nicer output when test fails. With unittest it looks like:
-# FAIL: test_convolution_2d_tosa_BI_2_3x3_1x3x12x12_st2_pd1
 test_modules = {
-    "2x2_3x2x40x40_nobias": conv2d_2x2_3x2x40x40_nobias,
-    "3x3_1x3x256x256_st1": conv2d_3x3_1x3x256x256_st1,
-    "3x3_1x3x12x12_st2_pd1": conv2d_3x3_1x3x12x12_st2_pd1,
-    "1x1_1x2x128x128_st1": conv2d_1x1_1x2x128x128_st1,
-    "2x2_1x1x14x13_st2_needs_adjust_pass": conv2d_2x2_1x1x14x13_st2,
-    "5x5_1x3x14x15_st3_pd1_needs_adjust_pass": conv2d_5x5_1x3x14x15_st3_pd1,
-    "7x7_1x3x16x16_st2_pd1_dl2_needs_adjust_pass": conv2d_7x7_1x3x16x16_st2_pd1_dl2,
-    "7x7_1x3x15x15_st1_pd0_dl1_needs_adjust_pass": conv2d_7x7_1x3x15x15_st1_pd0_dl1,
-    "5x5_1x3x14x14_st5_pd0_dl1_needs_adjust_pass": conv2d_5x5_1x3x14x14_st5_pd0_dl1,
-    "5x5_1x3x9x9_st5_pd0_dl1_needs_adjust_pass": conv2d_5x5_1x3x9x9_st5_pd0_dl1,
-    "3x3_1x3x9x8_st3_pd0_dl1_needs_adjust_pass": conv2d_3x3_1x3x9x8_st3_pd0_dl1,
-    "3x3_1x3x8x9_st3_pd0_dl1_needs_adjust_pass": conv2d_3x3_1x3x8x9_st3_pd0_dl1,
-    "3x4_1x3x7x7_st3_pd0_dl1_needs_adjust_pass": conv2d_3x4_1x3x7x7_st3_pd0_dl1,
-    "4x3_1x3x7x7_st3_pd0_dl1_needs_adjust_pass": conv2d_4x3_1x3x7x7_st3_pd0_dl1,
-    "5x5_3x2x128x128_st1": conv2d_5x5_3x2x128x128_st1,
-    "3x3_1x3x224x224_st2_pd1": conv2d_3x3_1x3x224x224_st2_pd1,
-    "two_conv2d_nobias": two_conv2d_nobias,
-    "two_conv2d": two_conv2d,
+    "2x2_3x2x40x40_nobias": conv3d_2x2_3x2x40x40_nobias,
+    "3x3_1x3x256x256_st1": conv3d_3x3_1x3x256x256_st1,
+    "3x3_1x3x12x12_st2_pd1": conv3d_3x3_1x3x12x12_st2_pd1,
+    "1x1_1x2x128x128_st1": conv3d_1x1_1x2x128x128_st1,
+    "2x2_1x1x14x13_st2_needs_adjust_pass": conv3d_2x2_1x1x14x13_st2,
+    "5x5_1x3x14x15_st3_pd1_needs_adjust_pass": conv3d_5x5_1x3x14x15_st3_pd1,
+    "7x7_1x3x16x16_st2_pd1_dl2_needs_adjust_pass": conv3d_7x7_1x3x16x16_st2_pd1_dl2,
+    "7x7_1x3x15x15_st1_pd0_dl1_needs_adjust_pass": conv3d_7x7_1x3x15x15_st1_pd0_dl1,
+    "5x5_1x3x14x14_st5_pd0_dl1_needs_adjust_pass": conv3d_5x5_1x3x14x14_st5_pd0_dl1,
+    "5x5_1x3x9x9_st5_pd0_dl1_needs_adjust_pass": conv3d_5x5_1x3x9x9_st5_pd0_dl1,
+    "3x3_1x3x9x8_st3_pd0_dl1_needs_adjust_pass": conv3d_3x3_1x3x9x8_st3_pd0_dl1,
+    "3x3_1x3x8x9_st3_pd0_dl1_needs_adjust_pass": conv3d_3x3_1x3x8x9_st3_pd0_dl1,
+    "3x4_1x3x7x7_st3_pd0_dl1_needs_adjust_pass": conv3d_3x4_1x3x7x7_st3_pd0_dl1,
+    "4x3_1x3x7x7_st3_pd0_dl1_needs_adjust_pass": conv3d_4x3_1x3x7x7_st3_pd0_dl1,
+    "5x5_3x2x128x128_st1": conv3d_5x5_3x2x128x128_st1,
+    "3x3_1x3x224x224_st2_pd1": conv3d_3x3_1x3x224x224_st2_pd1,
 }
 
-fvp_xfails = {
-    "2x2_3x2x40x40_nobias": "MLETORCH-520: Numerical issues on FVP.",
-    "5x5_3x2x128x128_st1": "MLETORCH-520: Numerical issues on FVP.",
-}
 input_t = Tuple[torch.Tensor]
 
 
 @common.parametrize("test_module", test_modules)
-def test_convolution_2d_tosa_MI(test_module):
+@pytest.mark.skip  # Not implemented, skip until it is.
+def test_convolution_3d_tosa_MI(test_module):
     pipeline = TosaPipelineMI[input_t](
         test_module, test_module.get_inputs(), aten_op, exir_op
     )
@@ -366,7 +336,8 @@ def test_convolution_2d_tosa_MI(test_module):
 
 
 @common.parametrize("test_module", test_modules)
-def test_convolution_2d_tosa_BI(test_module):
+@pytest.mark.skip  # Not implemented, skip until it is.
+def test_convolution_3d_tosa_BI(test_module):
     pipeline = TosaPipelineBI[input_t](
         test_module, test_module.get_inputs(), aten_op, exir_op
     )
@@ -375,33 +346,17 @@ def test_convolution_2d_tosa_BI(test_module):
 
 
 @common.parametrize("test_module", test_modules)
-def test_convolution_2d_u55_BI(test_module):
-    pipeline = EthosU55PipelineBI[input_t](
-        test_module, test_module.get_inputs(), aten_op, exir_op, run_on_fvp=False
-    )
-    pipeline.run()
-
-
-@common.parametrize("test_module", test_modules)
-def test_convolution_2d_u85_BI(test_module):
-    pipeline = EthosU85PipelineBI[input_t](
-        test_module, test_module.get_inputs(), aten_op, exir_op, run_on_fvp=False
-    )
-    pipeline.run()
-
-
-@common.parametrize("test_module", test_modules, fvp_xfails)
-@common.SkipIfNoCorstone300
-def test_convolution_2d_u55_BI_on_fvp(test_module):
+@pytest.mark.skip  # Not implemented, skip until it is.
+def test_convolution_3d_u55_BI(test_module):
     pipeline = EthosU55PipelineBI[input_t](
         test_module, test_module.get_inputs(), aten_op, exir_op, run_on_fvp=True
     )
     pipeline.run()
 
 
-@common.parametrize("test_module", test_modules, fvp_xfails)
-@common.SkipIfNoCorstone320
-def test_convolution_2d_u85_BI_on_fvp(test_module):
+@common.parametrize("test_module", test_modules)
+@pytest.mark.skip  # Not implemented, skip until it is.
+def test_convolution_3d_u85_BI(test_module):
     pipeline = EthosU85PipelineBI[input_t](
         test_module, test_module.get_inputs(), aten_op, exir_op, run_on_fvp=True
     )
@@ -409,31 +364,21 @@ def test_convolution_2d_u85_BI_on_fvp(test_module):
 
 
 reject_suite = {
-    "large_stride": Conv2d(
+    "large_stride": Conv3d(
         in_channels=1,
         out_channels=1,
-        kernel_size=(2, 4),
-        stride=(2, 4),
+        kernel_size=(2, 2, 1),
+        stride=(2, 4, 2),
         padding=1,
         width=10,
         height=14,
         batches=1,
     ),
-    "large_kernel_height": Conv2d(
+    "large_kernel_z": Conv3d(
         in_channels=1,
         out_channels=1,
-        kernel_size=(2, 65),
-        stride=(1, 1),
-        padding=0,
-        width=70,
-        height=70,
-        batches=1,
-    ),
-    "large_kernel": Conv2d(
-        in_channels=1,
-        out_channels=1,
-        kernel_size=(70, 60),
-        stride=(1,),
+        kernel_size=(2, 2, 2),
+        stride=1,
         padding=0,
         width=80,
         height=80,
@@ -443,8 +388,8 @@ reject_suite = {
 
 
 @common.parametrize("module", reject_suite)
-def test_reject_convolution_2d_u55_BI(
-    module: Conv2d,
+def test_reject_convolution_3d_u55_BI(
+    module: Conv3d,
 ):
     OpNotSupportedPipeline(
         module,
