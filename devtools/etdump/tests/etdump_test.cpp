a8db6f3036fe86c5ae976a38a3ebf054b13e14fa
dillondesilva <dillon.desilva@geeksdiary.com>
2025-03-31 11:00:48 +1100

    Updated etdump_test to include intermediate_output log tests with failing condition before running logs with correct values

--- devtools/etdump/tests/etdump_test.cpp
+++ devtools/etdump/tests/etdump_test.cpp
@@ -531,6 +531,42 @@ TEST_F(ProfilerETDumpTest, LogDelegateIntermediateOutput) {
         etdump_gen[i]->set_data_sink(&file_data_sink.get());
       }
 
+      // Only a valid name or delegate debug index should be passed in. If valid
+      // entries are passed in for both then the test should assert out.
+      Result<bool> log_tensor_result = etdump_gen[i]->log_intermediate_output_delegate(
+          "test_event_tensor",
+          static_cast<torch::executor::DebugHandle>(2589),
+          tf.ones({3, 2}));
+
+      std::vector<Tensor> tensors = {tf.ones({5, 4}), tf.ones({7, 6})};
+      Result<bool> log_tensor_list_result = etdump_gen[i]->log_intermediate_output_delegate(
+          nullptr,
+          static_cast<torch::executor::DebugHandle>(-1),
+          ArrayRef<Tensor>(tensors.data(), tensors.size()));
+      
+      Result<bool> log_int_result = etdump_gen[i]->log_intermediate_output_delegate(
+          "test_event_tensor",
+          static_cast<torch::executor::DebugHandle>(2589),
+          10);
+
+      Result<bool> log_double_result = etdump_gen[i]->log_intermediate_output_delegate(
+          "test_event_tensor",
+          static_cast<torch::executor::DebugHandle>(2589),
+          29.82);
+
+      Result<bool> log_bool_result = etdump_gen[i]->log_intermediate_output_delegate(
+          nullptr,
+          static_cast<torch::executor::DebugHandle>(-1),
+          29.82);
+
+      ASSERT_EQ(log_tensor_result.error(), Error::InvalidArgument);
+      ASSERT_EQ(log_tensor_list_result.error(), Error::InvalidArgument);
+      ASSERT_EQ(log_int_result.error(), Error::InvalidArgument);
+      ASSERT_EQ(log_double_result.error(), Error::InvalidArgument);
+      ASSERT_EQ(log_bool_result.error(), Error::InvalidArgument);
+
+      // Now we check log intermediate output delegate with valid args
+
       // Log a tensor
       etdump_gen[i]->log_intermediate_output_delegate(
           "test_event_tensor",
@@ -538,7 +574,6 @@ TEST_F(ProfilerETDumpTest, LogDelegateIntermediateOutput) {
           tf.ones({3, 2}));
 
       // Log a tensor list
-      std::vector<Tensor> tensors = {tf.ones({5, 4}), tf.ones({7, 6})};
       etdump_gen[i]->log_intermediate_output_delegate(
           "test_event_tensorlist",
           static_cast<torch::executor::DebugHandle>(-1),
@@ -709,13 +744,6 @@ TEST_F(ProfilerETDumpTest, LogDelegateEvents) {
 
     // Only a valid name or delegate debug index should be passed in. If valid
     // entries are passed in for both then the test should assert out.
-    TensorFactory<ScalarType::Float> tf;
-    Result<bool> failed_intermediate_output = etdump_gen[i]->log_intermediate_output_delegate(
-        "test_event_tensor",
-        static_cast<torch::executor::DebugHandle>(2589),
-        tf.ones({3, 2}));
-    
-    ASSERT_EQ(failed_intermediate_output.error(), Error::InvalidArgument);
     ET_EXPECT_DEATH(
       etdump_gen[i]->start_profiling_delegate("test_event", 1),
       "Only name or delegate_debug_index can be valid. Check DelegateMappingBuilder documentation for more details."
