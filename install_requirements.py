4264ef68ebba1ab409643b0b5c00b211028a1b7b
Zuby Afzal <git.zuby@gmail.com>
2025-04-03 07:58:46 -0700

    Move and update print statement for Intel-based macOS check

--- install_requirements.py
+++ install_requirements.py
@@ -61,11 +61,17 @@ def python_is_compatible():
 # PyTorch stopped building macOS x86_64 binaries starting with version 2.3.0 (January 2024).
 def is_intel_mac_os():
     # Returns True if running on macOS with an Intel-based CPU.
-    return sys.platform.lower().startswith("darwin") and platform.machine().lower() in (
+    if platform.system().lower() == "darwin" and platform.machine().lower() in (
         "x86",
         "x86_64",
         "i386",
-    )
+    ):
+        print(
+            "ERROR: ExecuTorch does not support Intel-based macOS platforms.",
+            file=sys.stderr,
+        )
+        return True
+    return False
 
 
 # The pip repository that hosts nightly torch packages.
@@ -164,10 +170,6 @@ if __name__ == "__main__":
     # Before doing anything, cd to the directory containing this script.
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     if is_intel_mac_os():
-        print(
-            "ERROR: This platform is not supported (Intel-based macOS).",
-            file=sys.stderr,
-        )
         sys.exit(1)
     if not python_is_compatible():
         sys.exit(1)
