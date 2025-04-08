6791707f1da27ff06e3fb7a672ee42497877e0ad
dlyakhov <daniil.lyakhov@intel.com>
2025-04-08 16:33:43 +0200

    WIP video input

--- backends/openvino/scripts/openvino_build.sh
+++ backends/openvino/scripts/openvino_build.sh
@@ -51,13 +51,14 @@ main() {
         # Set parameters to configure the project with CMake
         # Note: Add any additional configuration options you need here
         export CMAKE_ARGS="-DEXECUTORCH_BUILD_OPENVINO=ON \
+                           -DEXECUTORCH_BUILD_XNNPACK=ON \
                            -DEXECUTORCH_BUILD_EXTENSION_DATA_LOADER=ON \
                            -DEXECUTORCH_BUILD_EXTENSION_MODULE=ON \
                            -DEXECUTORCH_BUILD_EXTENSION_RUNNER_UTIL=ON \
                            -DEXECUTORCH_ENABLE_LOGGING=ON \
                            -DEXECUTORCH_BUILD_EXTENSION_TENSOR=ON \
                            -DEXECUTORCH_BUILD_PYBIND=ON"
-        export CMAKE_BUILD_ARGS="--target openvino_backend"
+        export CMAKE_BUILD_ARGS="--target openvino_backend xnnpack_backend"
 
         # Build the package
         pip install . --no-build-isolation
