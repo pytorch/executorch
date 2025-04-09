f51c1270fd7b8041a21a72cdaf1bc44f4719d07a
dlyakhov <daniil.lyakhov@intel.com>
2025-04-09 19:12:06 +0200

    WIP quantization

--- examples/yolo12_/build.sh
+++
@@ -1,4 +0,0 @@
-rm -r build
-mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="/home/dlyakhov/Projects/executorch_ov/executorch/cmake-out/" -DUSE_XNNPACK_BACKEND=ON -DUSE_OPENVINO_BACKEND=ON ..
-make -j 30
