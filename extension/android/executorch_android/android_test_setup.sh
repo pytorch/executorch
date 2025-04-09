8d1c4bef4564aa4d76da685e61647283c4be511e
Hansong Zhang <hsz@fb.com>
2025-04-08 17:16:57 -0700

    Fix the script

--- extension/android/executorch_android/android_test_setup.sh
+++ extension/android/executorch_android/android_test_setup.sh
@@ -12,4 +12,17 @@ if [[ -z "${PYTHON_EXECUTABLE:-}" ]]; then
 fi
 which "${PYTHON_EXECUTABLE}"
 
-cp ../../../extension/module/test/resources/add.pte src/androidTest/resources
+BASEDIR=$(dirname "$0")
+cp "${BASEDIR}/../../../extension/module/test/resources/add.pte" "${BASEDIR}/src/androidTest/resources"
+
+pushd "${BASEDIR}/../../../"
+curl -Ls "https://huggingface.co/karpathy/tinyllamas/resolve/main/stories110M.pt" --output stories110M.pt
+curl -Ls "https://raw.githubusercontent.com/karpathy/llama2.c/master/tokenizer.model" --output tokenizer.model
+# Create params.json file
+touch params.json
+echo '{"dim": 768, "multiple_of": 32, "n_heads": 12, "n_layers": 12, "norm_eps": 1e-05, "vocab_size": 32000}' > params.json
+python -m examples.models.llama.export_llama -c stories110M.pt -p params.json -X -kv
+
+cp *.pte "${BASEDIR}/src/androidTest/resources/stories.pte"
+cp *.bin "${BASEDIR}/src/androidTest/resources/tokenizer.bin"
+popd
