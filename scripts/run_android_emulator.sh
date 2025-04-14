f31573a80888042d0d3f4674dde3b281fda5f768
Hansong Zhang <hsz@fb.com>
2025-04-14 15:28:43 -0700

    skip LLM

--- scripts/run_android_emulator.sh
+++ scripts/run_android_emulator.sh
@@ -18,25 +18,12 @@ $ADB_PATH wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; d
 echo "List all running emulators"
 $ADB_PATH devices
 
-adb uninstall com.example.executorchllamademo || true
-adb uninstall com.example.executorchllamademo.test || true
-adb install -t app-debug.apk
-adb install -t app-debug-androidTest.apk
-
-adb shell mkdir -p /data/local/tmp/llama
-adb push model.pte /data/local/tmp/llama
-adb push tokenizer.bin /data/local/tmp/llama
-adb logcat -c
-adb shell am instrument -w -r com.example.executorchllamademo.test/androidx.test.runner.AndroidJUnitRunner >result.txt 2>&1
-adb logcat -d > logcat.txt
-cat logcat.txt
-grep -q FAILURES result.txt || cat result.txt
-
 adb uninstall org.pytorch.executorch.test || true
 adb install -t android-test-debug-androidTest.apk
 
 adb logcat -c
-adb shell am instrument -w -r org.pytorch.executorch.test/androidx.test.runner.AndroidJUnitRunner >result.txt 2>&1
+adb shell am instrument -w -r org.pytorch.executorch.test/androidx.test.runner.AndroidJUnitRunner \
+  -e class org.pytorch.executorch.ModuleInstrumentationTest >result.txt 2>&1
 adb logcat -d > logcat.txt
 cat logcat.txt
 grep -q FAILURES result.txt || cat result.txt
