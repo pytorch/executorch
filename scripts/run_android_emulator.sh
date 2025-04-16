22cd82e94a04543e6a36be270a70c9af4621ffcf
Hansong Zhang <hsz@fb.com>
2025-04-16 15:56:06 -0700

    40 second data non llm

--- scripts/run_android_emulator.sh
+++ scripts/run_android_emulator.sh
@@ -24,7 +24,8 @@ for i in {1..40}; do
 done &
 
 adb logcat -c
-adb shell am instrument -w -r \
+adb shell am instrument -w -r -e \
+  class org.pytorch.executorch.ModuleInstrumentationTest,org.pytorch.executorch.ModuleE2ETest \
   org.pytorch.executorch.test/androidx.test.runner.AndroidJUnitRunner >result.txt 2>&1
 adb logcat -d > logcat.txt
 adb shell dumpsys meminfo
