361608edb5fbf8466a7b65e9efb56776ac543d85
haowhsu-quic <quic_haowhsu@quicinc.com>
2025-04-16 15:09:53 +0800

    Qualcomm AI Engine Direct - add more profile event
    Summary
    - add more profile event for important metrics. e.g. RPC execution time
    - change test cases accordingly

--- backends/qualcomm/tests/test_qnn_delegate.py
+++ backends/qualcomm/tests/test_qnn_delegate.py
@@ -2494,7 +2494,7 @@ class TestQNNFloatingPointUtils(TestQNN):
             module,
             sample_input,
             expected_partitions=1,
-            expected_profile_events=24,
+            expected_profile_events=34,
         )
 
     def test_qnn_backend_shared_buffer(self):
@@ -3109,7 +3109,7 @@ class TestQNNQuantizedUtils(TestQNN):
             module,
             sample_input,
             expected_partitions=1,
-            expected_profile_events=25,
+            expected_profile_events=35,
         )
 
     def test_qnn_backend_shared_buffer(self):
