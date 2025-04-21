f1a1e17b9363b994eb98f76b3d4163a70062da23
Anthony Shoumikhin <anthony@shoumikh.in>
2025-04-20 21:28:57 -0700

    test

--- scripts/check_urls.sh
+++ scripts/check_urls.sh
@@ -7,19 +7,51 @@
 
 set -x
 
-ua='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
-accept_hdr='text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
-url='https://wiki.mozilla.org/Abstract_Interpretation'
+ua="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
+accept_hdr="text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+lang_hdr="en-US,en;q=0.9"
+enc_hdr="gzip, deflate, br"
+cache_hdr="no-cache"
+conn_hdr="keep-alive"
 
-curl -s -o /dev/null -w '%{http_code}\n' -L "$url"
+url="https://wiki.mozilla.org/Abstract_Interpretation"
 
-curl -s -o /dev/null -w '%{http_code}\n' -L -A "$ua" "$url"
+curl -s -o /dev/null -w '%{http_code}\n' \
+  -I \
+  -A "$ua" \
+  -H "Accept: $accept_hdr" \
+  -H "Accept-Language: $lang_hdr" \
+  -H "Accept-Encoding: $enc_hdr" \
+  -H "Connection: $conn_hdr" \
+  -H "Cache-Control: $cache_hdr" \
+  "$url"
 
-curl -s -o /dev/null -w '%{http_code}\n' -L -A "$ua" -H "Accept: $accept_hdr" "$url"
+curl -s -o /dev/null -w '%{http_code}\n' \
+  --range 0-0 \
+  -A "$ua" \
+  -H "Accept: $accept_hdr" \
+  -H "Accept-Language: $lang_hdr" \
+  -H "Accept-Encoding: $enc_hdr" \
+  -H "Connection: $conn_hdr" \
+  -H "Cache-Control: $cache_hdr" \
+  "$url"
 
-curl -s -o /dev/null -w '%{http_code}\n' -L -A "$ua" -H "Referer: https://$(echo $url | awk -F/ '{print $3}')/" "$url"
+wget --spider --server-response --user-agent="$ua" "$url" 2>&1 \
+  | awk '/^  HTTP\// { print $2 }'
 
-curl -4 -s -o /dev/null -w '%{http_code}\n' -L -A "$ua" "$url"
+wget --spider --server-response --user-agent="$ua" --method=GET "$url" 2>&1 \
+  | awk '/^  HTTP\// { print $2 }'
+
+curl -s -o /dev/null -w '%{http_code}\n' \
+  --http1.1 \
+  -I \
+  -A "$ua" \
+  -H "Accept: $accept_hdr" \
+  -H "Accept-Language: $lang_hdr" \
+  -H "Accept-Encoding: $enc_hdr" \
+  -H "Connection: $conn_hdr" \
+  -H "Cache-Control: $cache_hdr" \
+  "$url"
 
 set -euo pipefail
 
