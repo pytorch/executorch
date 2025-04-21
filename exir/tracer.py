21f5382a40ea6130988f1ad2967a84ca475bb21f
Scott Roy <scroy@meta.com>
2025-04-21 11:38:23 -0700

    New embedding quant fusion
    Summary:
    The diff adds new quant fusion passes to recognize 2, 4, and 8 bit quantized embeedings (per group and per channel) and fuses them to ExecuTorch kernels.  This makes torchao's quantize_ integrate with ExecuTorch:
    
    
    ```
     quantize_(
        model,
        IntxWeightOnlyConfig(weight_dtype=torch.int4, granularity=PerGroup(32)),
        lambda m, fqn: isinstance(m, torch.nn.Embedding)
    )
    
    # lower model to executorch
    ```
    
    For the model to lower, we need to run QuantFusionPass.  For subbyte, we also need to run constant_prop_pass.  (See new unit tests for examples).  In follow-up diffs, we will enable these passes by default in to_executorch before the memory passing and out-variant passes.
    
    Differential Revision: D73381542

--- exir/tracer.py
+++ exir/tracer.py
@@ -642,6 +642,25 @@ def _default_decomposition_table(
     additional_decomps = get_decompositions(additional_decomp_ops)
     decomps.update(additional_decomps)
     # pyre-fixme[7]: Expected `Dict[OpOverload, typing.Callable[..., executorch.exir....
+
+    never_decompose = []
+    try:
+        # Do not decompose torchao quant primitives
+        # They have decompositions registered for inductor/CUDA, but in ExecuTorch we
+        # just pattern match them and lower to delegates
+        import torchao
+        never_decompose.extend(
+        [
+            torch.ops.torchao.quantize_affine.default,
+            torch.ops.torchao.dequantize_affine.default,
+            torch.ops.torchao.choose_qparams_affine.default,
+        ]
+    )
+    except:
+        pass
+
+    for op in never_decompose:
+        decomps.pop(op, None)
     return decomps
 
 
