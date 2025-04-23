bbff5b53b5237604c1121523f931fffb820f91fe
Hansong Zhang <hsz@fb.com>
2025-04-23 11:35:05 -0700

    Revert "Revert "Minibench refactor (#10376)" (#10405)"
    This reverts commit 98c2c538b0e8f5fdde97829048d5b5edc01c0251.

--- extension/benchmark/android/benchmark/app/src/main/java/org/pytorch/minibench/LlmBenchmark.java
+++ extension/benchmark/android/benchmark/app/src/main/java/org/pytorch/minibench/LlmBenchmark.java
@@ -8,57 +8,33 @@
 
 package org.pytorch.minibench;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.system.ErrnoException;
-import android.system.Os;
 import android.util.Log;
-import com.google.gson.Gson;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class LlmBenchmarkActivity extends Activity implements ModelRunnerCallback {
-  ModelRunner mModelRunner;
+public class LlmBenchmark implements LlmModelRunnerCallback {
+  LlmModelRunner mLlmModelRunner;
 
   String mPrompt;
   StatsInfo mStatsInfo;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+  List<BenchmarkMetric> mResults;
+  BenchmarkActivity mActivity;
 
-    Intent intent = getIntent();
-
-    File modelDir = new File(intent.getStringExtra("model_dir"));
-    File model =
-        Arrays.stream(modelDir.listFiles())
-            .filter(file -> file.getName().endsWith(".pte"))
-            .findFirst()
-            .get();
-    String tokenizerPath = intent.getStringExtra("tokenizer_path");
-
-    float temperature = intent.getFloatExtra("temperature", 0.8f);
-    mPrompt = intent.getStringExtra("prompt");
-    if (mPrompt == null) {
-      mPrompt = "The ultimate answer";
-    }
-
-    try {
-      Os.setenv("ADSP_LIBRARY_PATH", getApplicationInfo().nativeLibraryDir, true);
-    } catch (ErrnoException e) {
-      finish();
-    }
-
+  LlmBenchmark(
+      BenchmarkActivity activity,
+      String modelFile,
+      String tokenizerPath,
+      String prompt,
+      float temperature,
+      List<BenchmarkMetric> results) {
+    mResults = results;
+    mActivity = activity;
     mStatsInfo = new StatsInfo();
-    mStatsInfo.modelName = model.getName().replace(".pte", "");
-    mModelRunner = new ModelRunner(model.getPath(), tokenizerPath, temperature, this);
+    mStatsInfo.modelName = modelFile.substring(modelFile.lastIndexOf('/') + 1).replace(".pte", "");
+    mPrompt = prompt;
+    mLlmModelRunner = new LlmModelRunner(modelFile, tokenizerPath, temperature, this);
     mStatsInfo.loadStart = System.nanoTime();
   }
 
@@ -72,7 +48,7 @@ public class LlmBenchmarkActivity extends Activity implements ModelRunnerCallbac
       return;
     }
     mStatsInfo.generateStart = System.nanoTime();
-    mModelRunner.generate(mPrompt);
+    mLlmModelRunner.generate(mPrompt);
   }
 
   @Override
@@ -99,33 +75,26 @@ public class LlmBenchmarkActivity extends Activity implements ModelRunnerCallbac
 
     final BenchmarkMetric.BenchmarkModel benchmarkModel =
         BenchmarkMetric.extractBackendAndQuantization(mStatsInfo.modelName);
-    final List<BenchmarkMetric> results = new ArrayList<>();
     // The list of metrics we have atm includes:
     // Load status
-    results.add(new BenchmarkMetric(benchmarkModel, "load_status", mStatsInfo.loadStatus, 0));
+    mResults.add(new BenchmarkMetric(benchmarkModel, "load_status", mStatsInfo.loadStatus, 0));
     // Model load time
-    results.add(
+    mResults.add(
         new BenchmarkMetric(
             benchmarkModel,
-            "model_load_time(ms)",
+            "llm_model_load_time(ms)",
             (mStatsInfo.loadEnd - mStatsInfo.loadStart) * 1e-6,
             0.0f));
     // LLM generate time
-    results.add(
+    mResults.add(
         new BenchmarkMetric(
             benchmarkModel,
             "generate_time(ms)",
             (mStatsInfo.generateEnd - mStatsInfo.generateStart) * 1e-6,
             0.0f));
     // Token per second
-    results.add(new BenchmarkMetric(benchmarkModel, "token_per_sec", mStatsInfo.tps, 0.0f));
-
-    try (FileWriter writer = new FileWriter(getFilesDir() + "/benchmark_results.json")) {
-      Gson gson = new Gson();
-      writer.write(gson.toJson(results));
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+    mResults.add(new BenchmarkMetric(benchmarkModel, "token_per_sec", mStatsInfo.tps, 0.0f));
+    mActivity.writeResult();
   }
 }
 
