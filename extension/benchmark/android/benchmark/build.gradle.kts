c818c122294ed4df80c36be648eefc9a9e078cc0
Hansong <107070759+kirklandsign@users.noreply.github.com>
2025-04-23 11:17:45 -0700

    Revert "Minibench refactor (#10376)" (#10405)
    This reverts commit a634653f29ec010facd0ea2d9fdca2057428d1e4.
    
    Broke llama benchmarking.

--- extension/benchmark/android/benchmark/build.gradle.kts
+++ extension/benchmark/android/benchmark/build.gradle.kts
@@ -7,6 +7,4 @@
  */
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins { id("com.android.application") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
-}
+plugins { id("com.android.application") version "8.1.0" apply false }
