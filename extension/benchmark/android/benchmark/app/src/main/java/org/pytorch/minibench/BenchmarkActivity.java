740bbac0e6b1697526963c61cac746bdea8d5988
Hansong Zhang <hsz@fb.com>
2025-04-22 21:21:41 -0700

    Linter

--- extension/benchmark/android/benchmark/app/src/main/java/org/pytorch/minibench/BenchmarkActivity.java
+++ extension/benchmark/android/benchmark/app/src/main/java/org/pytorch/minibench/BenchmarkActivity.java
@@ -16,9 +16,8 @@ import android.os.HandlerThread;
 import android.os.Looper;
 import android.system.ErrnoException;
 import android.system.Os;
-
+import android.util.Log;
 import com.google.gson.Gson;
-
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -28,100 +27,111 @@ import java.util.List;
 
 public class BenchmarkActivity extends Activity {
 
-    File mModel;
-    int mNumIter;
-    int mNumWarmupIter;
-    String mTokenizerPath;
-    float mTemperature;
-    String mPrompt;
+  File mModel;
+  int mNumIter;
+  int mNumWarmupIter;
+  String mTokenizerPath;
+  float mTemperature;
+  String mPrompt;
 
-    HandlerThread mHandlerThread;
-    BenchmarkHandler mHandler;
+  HandlerThread mHandlerThread;
+  BenchmarkHandler mHandler;
 
-    List<BenchmarkMetric> mResult;
+  List<BenchmarkMetric> mResult;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-        try {
-            Os.setenv("ADSP_LIBRARY_PATH", getApplicationInfo().nativeLibraryDir, true);
-        } catch (ErrnoException e) {
-            finish();
-        }
-
-        Intent intent = getIntent();
-        File modelDir = new File(intent.getStringExtra("model_dir"));
-        File model =
-                Arrays.stream(modelDir.listFiles())
-                        .filter(file -> file.getName().endsWith(".pte"))
-                        .findFirst()
-                        .get();
-
-        int numIter = intent.getIntExtra("num_iter", 50);
-        int numWarmupIter = intent.getIntExtra("num_warm_up_iter", 10);
-        String tokenizerPath = intent.getStringExtra("tokenizer_path");
-        float temperature = intent.getFloatExtra("temperature", 0.8f);
-        String prompt = intent.getStringExtra("prompt");
-
-        mModel = model;
-        mNumIter = numIter;
-        mNumWarmupIter = numWarmupIter;
-        mTokenizerPath = tokenizerPath;
-        mTemperature = temperature;
-        mPrompt = prompt;
-        if (mPrompt == null) {
-            mPrompt = "The ultimate answer";
-        }
-        mResult = new ArrayList<>();
-
-        mHandlerThread = new HandlerThread("ModelRunner");
-        mHandlerThread.start();
-        mHandler = new BenchmarkHandler(mHandlerThread.getLooper(), this);
-
-        mHandler.sendEmptyMessage(BenchmarkHandler.MESSAGE_RUN_BENCHMARK);
+    try {
+      Os.setenv("ADSP_LIBRARY_PATH", getApplicationInfo().nativeLibraryDir, true);
+    } catch (ErrnoException e) {
+      finish();
     }
 
-    void writeResult() {
-        try (FileWriter writer = new FileWriter(getFilesDir() + "/benchmark_results.json")) {
-            Gson gson = new Gson();
-            writer.write(gson.toJson(mResult));
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            finish();
-        }
+    Intent intent = getIntent();
+    File modelDir = new File(intent.getStringExtra("model_dir"));
+    File model =
+        Arrays.stream(modelDir.listFiles())
+            .filter(file -> file.getName().endsWith(".pte"))
+            .findFirst()
+            .get();
+
+    int numIter = intent.getIntExtra("num_iter", 50);
+    int numWarmupIter = intent.getIntExtra("num_warm_up_iter", 10);
+    String tokenizerPath = intent.getStringExtra("tokenizer_path");
+    float temperature = intent.getFloatExtra("temperature", 0.8f);
+    String prompt = intent.getStringExtra("prompt");
+
+    mModel = model;
+    mNumIter = numIter;
+    mNumWarmupIter = numWarmupIter;
+    mTokenizerPath = tokenizerPath;
+    mTemperature = temperature;
+    mPrompt = prompt;
+    if (mPrompt == null) {
+      mPrompt = "The ultimate answer";
     }
+    mResult = new ArrayList<>();
+
+    mHandlerThread = new HandlerThread("ModelRunner");
+    mHandlerThread.start();
+    mHandler = new BenchmarkHandler(mHandlerThread.getLooper(), this);
+
+    mHandler.sendEmptyMessage(BenchmarkHandler.MESSAGE_RUN_BENCHMARK);
+  }
+
+  void writeResult() {
+    try (FileWriter writer = new FileWriter(getFilesDir() + "/benchmark_results.json")) {
+      Gson gson = new Gson();
+      writer.write(gson.toJson(mResult));
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      finish();
+    }
+  }
 }
 
 class BenchmarkHandler extends Handler {
-    public static int MESSAGE_RUN_BENCHMARK = 1;
-    public static int MESSAGE_LLM_RUN_BENCHMARK = 2;
+  public static int MESSAGE_RUN_BENCHMARK = 1;
+  public static int MESSAGE_LLM_RUN_BENCHMARK = 2;
 
-    ModelRunner mModelRunner;
-    BenchmarkActivity mBenchmarkActivity;
+  ModelRunner mModelRunner;
+  BenchmarkActivity mBenchmarkActivity;
 
-    LlmModelRunner mLlmModelRunner;
-    LlmBenchmark mLlmBenchmark;
+  LlmModelRunner mLlmModelRunner;
+  LlmBenchmark mLlmBenchmark;
 
-    public BenchmarkHandler(Looper looper, BenchmarkActivity benchmarkActivity) {
-        super(looper);
-        mModelRunner = new ModelRunner();
-        mBenchmarkActivity = benchmarkActivity;
+  public BenchmarkHandler(Looper looper, BenchmarkActivity benchmarkActivity) {
+    super(looper);
+    mModelRunner = new ModelRunner();
+    mBenchmarkActivity = benchmarkActivity;
+  }
+
+  @Override
+  public void handleMessage(android.os.Message msg) {
+    if (msg.what == MESSAGE_RUN_BENCHMARK) {
+      mModelRunner.runBenchmark(
+          mBenchmarkActivity.mModel,
+          mBenchmarkActivity.mNumWarmupIter,
+          mBenchmarkActivity.mNumIter,
+          mBenchmarkActivity.mResult);
+
+      if (mBenchmarkActivity.mTokenizerPath == null) {
+        mBenchmarkActivity.writeResult();
+      } else {
+        this.sendEmptyMessage(MESSAGE_LLM_RUN_BENCHMARK);
+      }
+    } else if (msg.what == MESSAGE_LLM_RUN_BENCHMARK) {
+      mLlmBenchmark =
+          new LlmBenchmark(
+              mBenchmarkActivity,
+              mBenchmarkActivity.mModel.getName(),
+              mBenchmarkActivity.mTokenizerPath,
+              mBenchmarkActivity.mPrompt,
+              mBenchmarkActivity.mTemperature,
+              mBenchmarkActivity.mResult);
     }
-
-    @Override
-    public void handleMessage(android.os.Message msg) {
-        if (msg.what == MESSAGE_RUN_BENCHMARK) {
-            mModelRunner.runBenchmark(mBenchmarkActivity.mModel, mBenchmarkActivity.mNumWarmupIter, mBenchmarkActivity.mNumIter, mBenchmarkActivity.mResult);
-
-            if (mBenchmarkActivity.mTokenizerPath == null) {
-                mBenchmarkActivity.writeResult();
-            } else {
-                this.sendEmptyMessage(MESSAGE_LLM_RUN_BENCHMARK);
-            }
-        } else if (msg.what == MESSAGE_LLM_RUN_BENCHMARK) {
-            mLlmBenchmark = new LlmBenchmark(mBenchmarkActivity, mBenchmarkActivity.mModel.getPath(), mBenchmarkActivity.mTokenizerPath, mBenchmarkActivity.mPrompt, mBenchmarkActivity.mTemperature, mBenchmarkActivity.mResult);
-        }
-    }
+  }
 }
--- extension/benchmark/android/benchmark/app/src/main/java/org/pytorch/minibench/LlmModelRunner.java
+++ extension/benchmark/android/benchmark/app/src/main/java/org/pytorch/minibench/LlmModelRunner.java
@@ -12,89 +12,86 @@ import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Looper;
 import android.os.Message;
-
 import org.pytorch.executorch.extension.llm.LlmCallback;
 import org.pytorch.executorch.extension.llm.LlmModule;
 
-/**
- * A helper class to handle all model running logic within this class.
- */
+/** A helper class to handle all model running logic within this class. */
 public class LlmModelRunner implements LlmCallback {
-    LlmModule mModule = null;
+  LlmModule mModule = null;
 
-    String mModelFilePath = "";
-    String mTokenizerFilePath = "";
+  String mModelFilePath = "";
+  String mTokenizerFilePath = "";
 
-    LlmModelRunnerCallback mCallback = null;
+  LlmModelRunnerCallback mCallback = null;
 
-    HandlerThread mHandlerThread = null;
-    Handler mHandler = null;
+  HandlerThread mHandlerThread = null;
+  Handler mHandler = null;
 
-    /**
-     * ] Helper class to separate between UI logic and model runner logic. Automatically handle
-     * generate() request on worker thread.
-     *
-     * @param modelFilePath
-     * @param tokenizerFilePath
-     * @param callback
-     */
-    LlmModelRunner(
-            String modelFilePath,
-            String tokenizerFilePath,
-            float temperature,
-            LlmModelRunnerCallback callback) {
-        mModelFilePath = modelFilePath;
-        mTokenizerFilePath = tokenizerFilePath;
-        mCallback = callback;
+  /**
+   * ] Helper class to separate between UI logic and model runner logic. Automatically handle
+   * generate() request on worker thread.
+   *
+   * @param modelFilePath
+   * @param tokenizerFilePath
+   * @param callback
+   */
+  LlmModelRunner(
+      String modelFilePath,
+      String tokenizerFilePath,
+      float temperature,
+      LlmModelRunnerCallback callback) {
+    mModelFilePath = modelFilePath;
+    mTokenizerFilePath = tokenizerFilePath;
+    mCallback = callback;
 
-        mModule = new LlmModule(mModelFilePath, mTokenizerFilePath, 0.8f);
-        mHandlerThread = new HandlerThread("LlmModelRunner");
-        mHandlerThread.start();
-        mHandler = new LlmModelRunnerHandler(mHandlerThread.getLooper(), this);
+    mModule = new LlmModule(mModelFilePath, mTokenizerFilePath, 0.8f);
+    mHandlerThread = new HandlerThread("LlmModelRunner");
+    mHandlerThread.start();
+    mHandler = new LlmModelRunnerHandler(mHandlerThread.getLooper(), this);
 
-        mHandler.sendEmptyMessage(LlmModelRunnerHandler.MESSAGE_LOAD_MODEL);
-    }
+    mHandler.sendEmptyMessage(LlmModelRunnerHandler.MESSAGE_LOAD_MODEL);
+  }
 
-    int generate(String prompt) {
-        Message msg = Message.obtain(mHandler, LlmModelRunnerHandler.MESSAGE_GENERATE, prompt);
-        msg.sendToTarget();
-        return 0;
-    }
+  int generate(String prompt) {
+    Message msg = Message.obtain(mHandler, LlmModelRunnerHandler.MESSAGE_GENERATE, prompt);
+    msg.sendToTarget();
+    return 0;
+  }
 
-    void stop() {
-        mModule.stop();
-    }
+  void stop() {
+    mModule.stop();
+  }
 
-    @Override
-    public void onResult(String result) {
-        mCallback.onTokenGenerated(result);
-    }
+  @Override
+  public void onResult(String result) {
+    mCallback.onTokenGenerated(result);
+  }
 
-    @Override
-    public void onStats(String result) {
-        mCallback.onStats(result);
-    }
+  @Override
+  public void onStats(String result) {
+    mCallback.onStats(result);
+  }
 }
 
 class LlmModelRunnerHandler extends Handler {
-    public static int MESSAGE_LOAD_MODEL = 1;
-    public static int MESSAGE_GENERATE = 2;
+  public static int MESSAGE_LOAD_MODEL = 1;
+  public static int MESSAGE_GENERATE = 2;
 
-    private final LlmModelRunner mLlmModelRunner;
+  private final LlmModelRunner mLlmModelRunner;
 
-    public LlmModelRunnerHandler(Looper looper, LlmModelRunner llmModelRunner) {
-        super(looper);
-        mLlmModelRunner = llmModelRunner;
+  public LlmModelRunnerHandler(Looper looper, LlmModelRunner llmModelRunner) {
+    super(looper);
+    mLlmModelRunner = llmModelRunner;
+  }
+
+  @Override
+  public void handleMessage(android.os.Message msg) {
+    if (msg.what == MESSAGE_LOAD_MODEL) {
+      int status = mLlmModelRunner.mModule.load();
+      mLlmModelRunner.mCallback.onModelLoaded(status);
+    } else if (msg.what == MESSAGE_GENERATE) {
+      mLlmModelRunner.mModule.generate((String) msg.obj, mLlmModelRunner);
+      mLlmModelRunner.mCallback.onGenerationStopped();
     }
-
-    @Override
-    public void handleMessage(android.os.Message msg) {
-        if (msg.what == MESSAGE_LOAD_MODEL) {
-            int status = mLlmModelRunner.mModule.load();
-            mLlmModelRunner.mCallback.onModelLoaded(status);
-        } else if (msg.what == MESSAGE_GENERATE) {
-            mLlmModelRunner.mModule.generate((String) msg.obj, mLlmModelRunner);
-            mLlmModelRunner.mCallback.onGenerationStopped();
-        }
-    }
+  }
 }
