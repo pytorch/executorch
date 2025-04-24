65e32bf5f4a9b4d91d4e6ddf7d9c59986c1e379a
Martin Pavella <martin.pavella@nxp.com>
2025-04-24 09:11:18 +0200

    NXP backend: Update licence headers in node converters.

--- backends/nxp/backend/ir/converter/node_converters/ops_converters/add_tensor_converter.py
+++ backends/nxp/backend/ir/converter/node_converters/ops_converters/add_tensor_converter.py
@@ -1,5 +1,4 @@
-# Copyright (c) 2025 NXP
-# All rights reserved.
+# Copyright 2025 NXP
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
