c9532ed801a8751418ef9705733b5969689ba433
Anthony Shoumikhin <anthony@shoumikh.in>
2025-04-27 22:11:25 -0700

    Update lint_xrefs.sh

--- scripts/lint_xrefs.sh
+++ scripts/lint_xrefs.sh
@@ -23,8 +23,8 @@ while IFS=: read -r filepath link; do
     status=1
   fi
 done < <(
-  git --no-pager grep --no-color -I -o -E \
-    '\[[^]]+\]\([^[:space:])]*/[^[:space:])]*\)|href="[^"]*/[^"]*"|src="[^"]*/[^"]*"' \
+  git --no-pager grep --no-color -I -P -o \
+    '(?!.*@lint-ignore)(?:\[[^]]+\]\([^[:space:])]*/[^[:space:])]*\)|href="[^"]*/[^"]*"|src="[^"]*/[^"]*")' \
     -- '*' \
     ':(exclude).*' \
     ':(exclude)**/.*' \
@@ -33,7 +33,6 @@ done < <(
     ':(exclude)**/*.xml' \
     ':(exclude,glob)**/third-party/**' \
     ':(exclude,glob)**/third_party/**' \
-  | grep -v '@lint-ignore' \
   | grep -Ev 'https?://' \
   | sed -E \
       -e 's#([^:]+):\[[^]]+\]\(([^)]+)\)#\1:\2#' \
