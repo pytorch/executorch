1ac9393b3280731567336fc6d22cd7cc33e84f6b
Lefteris Gilmaz <lefterisgilmaz@macbookpro.home>
2025-05-03 23:07:53 +0100

    [executorch][android] Add Runtime.java to centralize native library loading

--- extension/android/executorch_android/src/main/java/org/pytorch/executorch/extension/llm/LlmModule.java
+++ extension/android/executorch_android/src/main/java/org/pytorch/executorch/extension/llm/LlmModule.java
@@ -13,6 +13,7 @@ import com.facebook.jni.annotations.DoNotStrip;
 import com.facebook.soloader.nativeloader.NativeLoader;
 import com.facebook.soloader.nativeloader.SystemDelegate;
 import org.pytorch.executorch.annotations.Experimental;
+import org.pytorch.executorch.Runtime;
 
 /**
  * LlmModule is a wrapper around the Executorch LLM. It provides a simple interface to generate text
@@ -27,11 +28,9 @@ public class LlmModule {
   public static final int MODEL_TYPE_TEXT_VISION = 2;
 
   static {
-    if (!NativeLoader.isInitialized()) {
-      NativeLoader.init(new SystemDelegate());
+    if (!Runtime.isInitialized()) {
+        throw new IllegalStateException("ExecuTorch runtime not initialized.");
     }
-    NativeLoader.loadLibrary("executorch");
-  }
 
   private final HybridData mHybridData;
   private static final int DEFAULT_SEQ_LEN = 128;
