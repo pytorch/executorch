665eba48d5b3ae54c8636de4c9b7b1ec749d3187
jathu <jathu.satkunarajah@gmail.com>
2025-05-06 08:51:18 -0700

    create macro

---
+++ tools/cmake/common/preset.cmake
@@ -0,0 +1,25 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# Enforce config names to always start with EXECUTORCH_, else raise an error.
+function(enforce_executorch_config_name NAME)
+  if(NOT "${NAME}" MATCHES "^EXECUTORCH_")
+    message(FATAL_ERROR "Config name '${NAME}' must start with EXECUTORCH_")
+  endif()
+endfunction()
+
+# Define an overridable config.
+#   1) If the config is already defined in the process, then store that in cache
+#   2) If the config is NOT set, then store the default value in cache
+macro(define_overridable_config NAME DESCRIPTION DEFAULT_VALUE)
+  enforce_executorch_config_name(${NAME})
+
+  if(DEFINED ${NAME})
+    set(${NAME} ${${NAME}} CACHE STRING ${DESCRIPTION} FORCE)
+  else()
+    set(${NAME} ${DEFAULT_VALUE} CACHE STRING ${DESCRIPTION})
+  endif()
+endmacro()
