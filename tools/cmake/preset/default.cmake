83be7d3d001796d4517d248e3d9fc009ba212497
jathu <jathu.satkunarajah@gmail.com>
2025-05-10 15:36:32 -0700

    move EXECUTORCH_PAL_DEFAULT

--- tools/cmake/preset/default.cmake
+++ tools/cmake/preset/default.cmake
@@ -14,10 +14,36 @@ endif()
 
 # MARK: - Overridable Options
 
-define_overridable_option(EXECUTORCH_ENABLE_LOGGING "Build with ET_LOG_ENABLED" BOOL ${_is_build_type_debug})
-define_overridable_option(EXECUTORCH_BUILD_COREML "Build the Core ML backend" BOOL OFF)
+define_overridable_option(
+  EXECUTORCH_ENABLE_LOGGING
+  "Build with ET_LOG_ENABLED"
+  BOOL ${_is_build_type_debug}
+)
+define_overridable_option(
+  EXECUTORCH_BUILD_COREML
+  "Build the Core ML backend"
+  BOOL OFF
+)
 define_overridable_option(
   EXECUTORCH_FLATBUFFERS_MAX_ALIGNMENT
   "Exir lets users set the alignment of tensor data embedded in the flatbuffer, and some users need an alignment larger than the default, which is typically 32."
   STRING 1024
 )
+define_overridable_option(
+  EXECUTORCH_PAL_DEFAULT
+  "Which PAL default implementation to use. Choices: posix, minimal"
+  STRING "posix"
+)
+define_overridable_option(
+  EXECUTORCH_PAL_DEFAULT_FILE_PATH
+  "PAL implementation file path"
+  STRING "${PROJECT_SOURCE_DIR}/runtime/platform/default/${EXECUTORCH_PAL_DEFAULT}.cpp"
+)
+
+
+# MARK: - Validations
+# At this point all the options should be configured with their final value.
+
+if(NOT EXISTS ${EXECUTORCH_PAL_DEFAULT_FILE_PATH})
+  message(FATAL_ERROR "PAL default implementation (EXECUTORCH_PAL_DEFAULT=${EXECUTORCH_PAL_DEFAULT}) file not found: ${EXECUTORCH_PAL_DEFAULT_FILE_PATH}. Choices: posix, minimal")
+endif()
