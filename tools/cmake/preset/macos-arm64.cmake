12010a4c92de1094347a5075b3d5b5fcc11ee3e8
jathu <jathu.satkunarajah@gmail.com>
2025-05-14 15:46:19 -0700

    wip ∫ May 15, 2025 08:50:48 AM ∫ a7473b6fe

--- tools/cmake/preset/macos-arm64.cmake
+++ tools/cmake/preset/macos-arm64.cmake
@@ -4,4 +4,12 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+include(${PROJECT_SOURCE_DIR}/tools/cmake/preset/pybind-macos.cmake)
+
 set_overridable_option(EXECUTORCH_BUILD_COREML ON)
+set_overridable_option(EXECUTORCH_BUILD_EXECUTOR_RUNNER ON)
+set_overridable_option(EXECUTORCH_XNNPACK_SHARED_WORKSPACE ON)
+set_overridable_option(EXECUTORCH_BUILD_EXTENSION_APPLE ON)
+set_overridable_option(EXECUTORCH_BUILD_EXTENSION_DATA_LOADER ON)
+set_overridable_option(EXECUTORCH_BUILD_EXTENSION_MODULE ON)
+set_overridable_option(EXECUTORCH_BUILD_EXTENSION_TENSOR ON)
