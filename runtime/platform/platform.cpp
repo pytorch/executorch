20fd1b20d33b2c872eabb99374c3b912af6da597
Gregory Comer <gjcomer@meta.com>
2025-05-20 23:03:02 -0700

    Introduce PAL function table (#10675)
    Summary:
    
    This is the implementation of the PAL changes described in https://github.com/pytorch/executorch/discussions/10432. This PR introduces a struct (`pal_table`) to hold function pointers to the PAL function implementations. There is a singleton instance of this struct, which is initialized with the weak/strong et_pal_ functions - maintaining backwards compatibility with the existing override mechanism.
    
    I've then added wrapper functions for the PAL into the executorch::runtime namespace which dispatch through the function table. It is intended that callers use these functions instead of the "raw" et_pal_ methods in order to correctly dispatch through the function table. I also update ET callers to do this.
    
    Differential Revision: D74121895

---
+++ runtime/platform/platform.cpp
@@ -0,0 +1,162 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <executorch/runtime/platform/log.h>
+#include <executorch/runtime/platform/platform.h>
+#include <cstdlib>
+
+namespace executorch {
+namespace runtime {
+
+namespace {
+/**
+ * The singleton instance of the PAL function table.
+ */
+pal_table pal_impl = {
+    et_pal_init,
+    et_pal_abort,
+    et_pal_current_ticks,
+    et_pal_ticks_to_ns_multiplier,
+    et_pal_emit_log_message,
+    et_pal_allocate,
+    et_pal_free,
+    __FILE__};
+
+/**
+ * Tracks whether the PAL has been overridden. This is used to warn when
+ * multiple callers override the PAL.
+ */
+bool is_pal_overridden = false;
+} // namespace
+
+pal_table pal_table::create(
+    et_pal_emit_log_message_t emit_log_message,
+    const char* source_filename) {
+  return pal_table::create(
+      nullptr, // init
+      nullptr, // abort
+      nullptr, // current_ticks
+      nullptr, // ticks_to_ns_multiplier
+      emit_log_message,
+      nullptr, // allocate
+      nullptr, // free
+      source_filename);
+}
+
+pal_table pal_table::create(
+    et_pal_init_t init,
+    et_pal_abort_t abort,
+    et_pal_current_ticks_t current_ticks,
+    et_pal_ticks_to_ns_multiplier_t ticks_to_ns_multiplier,
+    et_pal_emit_log_message_t emit_log_message,
+    et_pal_allocate_t allocate,
+    et_pal_free_t free,
+    const char* source_filename) {
+  return pal_table{
+      init,
+      abort,
+      current_ticks,
+      ticks_to_ns_multiplier,
+      emit_log_message,
+      allocate,
+      free,
+      source_filename};
+}
+
+/**
+ * Override the PAL functions with user implementations. Any null entries in the
+ * table are unchanged and will keep the default implementation.
+ */
+bool register_pal(pal_table impl) {
+  if (is_pal_overridden) {
+    ET_LOG(
+        Error,
+        "register_pal() called multiple times. Subsequent calls will override the previous implementation. Previous implementation was registered from %s.",
+        impl.source_filename != nullptr ? impl.source_filename : "unknown");
+  }
+  is_pal_overridden = true;
+
+  if (impl.abort != nullptr) {
+    pal_impl.abort = impl.abort;
+  }
+
+  if (impl.current_ticks != nullptr) {
+    pal_impl.current_ticks = impl.current_ticks;
+  }
+
+  if (impl.ticks_to_ns_multiplier != nullptr) {
+    pal_impl.ticks_to_ns_multiplier = impl.ticks_to_ns_multiplier;
+  }
+
+  if (impl.emit_log_message != nullptr) {
+    pal_impl.emit_log_message = impl.emit_log_message;
+  }
+
+  if (impl.allocate != nullptr) {
+    pal_impl.allocate = impl.allocate;
+  }
+
+  if (impl.free != nullptr) {
+    pal_impl.free = impl.free;
+  }
+
+  if (impl.init != nullptr) {
+    pal_impl.init = impl.init;
+    if (pal_impl.init != nullptr) {
+      pal_impl.init();
+    }
+  }
+
+  return true;
+}
+
+const pal_table* get_pal_impl() {
+  return &pal_impl;
+}
+
+void pal_init() {
+  pal_impl.init();
+}
+
+ET_NORETURN void pal_abort() {
+  pal_impl.abort();
+  // This should be unreachable, but in case the PAL implementation doesn't
+  // abort, force it here.
+  std::abort();
+}
+
+et_timestamp_t pal_current_ticks() {
+  return pal_impl.current_ticks();
+}
+
+et_tick_ratio_t pal_ticks_to_ns_multiplier() {
+  return pal_impl.ticks_to_ns_multiplier();
+}
+
+void pal_emit_log_message(
+    et_timestamp_t timestamp,
+    et_pal_log_level_t level,
+    const char* filename,
+    const char* function,
+    size_t line,
+    const char* message,
+    size_t length) {
+  pal_impl.emit_log_message(
+      timestamp, level, filename, function, line, message, length);
+}
+
+void* pal_allocate(size_t size) {
+  return pal_impl.allocate(size);
+}
+
+void pal_free(void* ptr) {
+  pal_impl.free(ptr);
+}
+
+} // namespace runtime
+} // namespace executorch
