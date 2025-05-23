9a77eb816d254040f9cfa569cee9497a84ad65cd
Vivek Trivedi <5340687+trivedivivek@users.noreply.github.com>
2025-05-23 14:09:48 -0700

    [ET-VK] Minor tuning for conv2d pw op to improve performance.
    The diff introduces minor tuning for the Conv2d pointwise (PW) operation in the Vulkan backend to improve performance.
    Conv 2d pw now issues a 2D dispatch instead of 1D, where dispatch axis y is now sized based on output texture's batch size.
    
    Differential Revision: [D75251145](https://our.internmc.facebook.com/intern/diff/D75251145/)
    
    ghstack-source-id: 285961874
    Pull Request resolved: https://github.com/pytorch/executorch/pull/11113

--- backends/vulkan/runtime/graph/ops/impl/Convolution.cpp
+++ backends/vulkan/runtime/graph/ops/impl/Convolution.cpp
@@ -398,8 +398,10 @@ void add_conv2d_node(
   utils::uvec3 wg_size = create_conv2d_global_wg_size(
       graph, method, out, weight_data, stride_equals_dilation);
 
-  if (method == Conv2dMethod::Pointwise || method == Conv2dMethod::Depthwise) {
+  if (method == Conv2dMethod::Depthwise) {
     wg_size = {wg_size[0] * wg_size[1] * wg_size[2], 1, 1};
+  } else if (method == Conv2dMethod::Pointwise) {
+    wg_size = {wg_size[0] * wg_size[1], wg_size[2], 1};
   }
 
   vkapi::ParamsBindList param_buffers;
