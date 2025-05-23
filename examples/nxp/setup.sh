a1686721c7dbb02d5ac484218c32ec64392e5e88
Digant Desai <digantdesai@meta.com>
2025-05-22 21:35:53 -0500

    [nxp] trunk.yaml: download submodules recursively

--- examples/nxp/setup.sh
+++ examples/nxp/setup.sh
@@ -6,5 +6,15 @@
 
 set -u
 
+retry_command() {
+    local max_attempts=$2
+    for ((i=0; i<$max_attempts; i++)); do
+        $1 && return 0 || echo "Attempt $((i+1)) failed. Retrying..."
+        sleep 2
+    done
+    echo "All attempts failed."
+    return 1
+}
+
 # Install neutron-converter
-pip install --extra-index-url https://eiq.nxp.com/repository neutron-converter_SDK_25_03
+retry_command "pip install --extra-index-url https://eiq.nxp.com/repository neutron-converter_SDK_25_03" 100 || { echo "Failed"; exit 1; }
