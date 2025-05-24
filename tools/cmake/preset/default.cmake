548d858086cab41738fae10d63c07f464f1ac9eb
jathu <jathu.satkunarajah@gmail.com>
2025-05-23 21:30:41 -0700

    wip ∫ MAY 23 2025 21:30:41

--- tools/cmake/preset/default.cmake
+++ tools/cmake/preset/default.cmake
@@ -210,14 +210,14 @@ define_overridable_option(
   BOOL OFF
 )
 define_overridable_option(
-  EXECUTORCH_BUILD_GFLAGS
-  "Build the gflags library."
-  BOOL ON
-)
-define_overridable_option(
   EXECUTORCH_COREML_BUILD_EXECUTOR_RUNNER
   "Build CoreML executor runner."
   BOOL OFF
+)
+define_overridable_option(
+  EXECUTORCH_BUILD_GFLAGS
+  "Build the gflags library."
+  BOOL ON
 )
 
 if(EXECUTORCH_BUILD_ARM_BAREMETAL)
