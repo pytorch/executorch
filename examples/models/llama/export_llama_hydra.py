2996b2699ab12e9360e544f0cbf311a32667fb28
Jack Zhang <32371937+jackzhxng@users.noreply.github.com>
2025-06-06 15:15:19 -0700

    Introduce hydra framework with backwards compatibility
    Pull Request resolved: https://github.com/pytorch/executorch/pull/11029
    
    
    
    
    @imported-using-ghimport
    
    Differential Revision: [D75263989](https://our.internmc.facebook.com/intern/diff/D75263989/)
    ghstack-source-id: 288807821

---
+++ examples/models/llama/export_llama_hydra.py
@@ -0,0 +1,27 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""
+Run export_llama using the new Hydra CLI.
+"""
+
+import hydra
+
+from executorch.examples.models.llama.config.llm_config import LlmConfig
+from executorch.examples.models.llama.export_llama_lib import export_llama
+from hydra.core.config_store import ConfigStore
+
+cs = ConfigStore.instance()
+cs.store(name="llm_config", node=LlmConfig)
+
+
+@hydra.main(version_base=None, config_name="llm_config")
+def main(llm_config: LlmConfig) -> None:
+    export_llama(llm_config)
+
+
+if __name__ == "__main__":
+    main()
