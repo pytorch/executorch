1dc9fbcb1267a37d4e262512c3f487d05418c1fb
jathu <jathu.satkunarajah@gmail.com>
2025-06-06 16:21:01 -0700

    ✨ Simplify build presets by inferring system-specific settings

--- tools/cmake/preset/apple_common.cmake
+++ tools/cmake/preset/apple_common.cmake
@@ -4,22 +4,28 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-set(CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LANGUAGE_STANDARD "c++${CMAKE_CXX_STANDARD}")
-set(CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LIBRARY "libc++")
+if(CMAKE_GENERATOR STREQUAL "Xcode")
+  set(CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LANGUAGE_STANDARD "c++${CMAKE_CXX_STANDARD}")
+  set(CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LIBRARY "libc++")
 
-set(
-  _compiler_flags
-  "-ffile-prefix-map=${PROJECT_SOURCE_DIR}=/executorch"
-  "-fdebug-prefix-map=${PROJECT_SOURCE_DIR}=/executorch"
-)
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${_compiler_flags}")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${_compiler_flags}")
+  set(
+    _compiler_flags
+    "-ffile-prefix-map=${PROJECT_SOURCE_DIR}=/executorch"
+    "-fdebug-prefix-map=${PROJECT_SOURCE_DIR}=/executorch"
+  )
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${_compiler_flags}")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${_compiler_flags}")
+
+  # Swift requires Xcode
+  set_overridable_option(EXECUTORCH_BUILD_EXTENSION_APPLE ON)
+else()
+  message(WARNING "Building Apple targets without Xcode disables some features.")
+endif()
 
 set_overridable_option(EXECUTORCH_BUILD_XNNPACK ON)
 set_overridable_option(EXECUTORCH_BUILD_COREML ON)
 set_overridable_option(EXECUTORCH_BUILD_MPS ON)
 set_overridable_option(EXECUTORCH_XNNPACK_SHARED_WORKSPACE ON)
-set_overridable_option(EXECUTORCH_BUILD_EXTENSION_APPLE ON)
 set_overridable_option(EXECUTORCH_BUILD_EXTENSION_DATA_LOADER ON)
 set_overridable_option(EXECUTORCH_BUILD_EXTENSION_MODULE ON)
 set_overridable_option(EXECUTORCH_BUILD_EXTENSION_TENSOR ON)
