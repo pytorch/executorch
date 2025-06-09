9c74f890bcd4fb2bf657781ae8c45e00e3e0144b
Oscar Andersson <oscar.andersson@arm.com>
2025-06-09 15:23:01 +0200

    Arm backend: Add support for aten.round
    Adds support and unittest for round-op.
    
    Signed-off-by: Oscar Andersson <oscar.andersson@arm.com>
    Change-Id: Ife5bdcd2e4ab8fe076fea8248ea387168b8e3226

--- backends/arm/_passes/arm_pass_manager.py
+++ backends/arm/_passes/arm_pass_manager.py
@@ -35,6 +35,7 @@ from executorch.backends.arm._passes import (
     DecomposeMaxPool2DPass,
     DecomposeMeanDimPass,
     DecomposeNotEqualPass,
+    DecomposeRoundPass,
     DecomposeSelectPass,
     DecomposeSiluPass,
     DecomposeSoftmaxPass,
@@ -139,6 +140,7 @@ class ArmPassManager(PassManager):
         return self._transform(exported_program.graph_module)
 
     def _tosa_080_MI_pipeline(self, exported_program: ExportedProgram) -> GraphModule:
+        self.add_pass(DecomposeRoundPass())
         self.add_pass(DecomposeSqrtPass())
         self.add_pass(ConvertIntPowToMuls())
         self.add_pass(ReplaceScalarWithTensorArgPassTOSAMI())
@@ -219,6 +221,7 @@ class ArmPassManager(PassManager):
         self.add_pass(InsertCastForOpsWithInt64InputPass())
         self.add_pass(DecomposeEmbeddingPass())
         self.add_pass(DecomposeScaledDotProductAttention())
+        self.add_pass(DecomposeRoundPass())
         self.add_pass(ReplaceScalarWithTensorArgPassTOSABI())
         self.add_pass(ScalarsToAttributePass())
         self.add_pass(DecomposeGroupNormPass())
