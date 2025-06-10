5c135c820a45ee53ea4c411164d03ca94f39c1b0
BujSet <ranganath1000@gmail.com>
2025-06-09 18:47:31 -0700

    Testing fix to relative imports error

--- codegen/tools/gen_oplist.py
+++ codegen/tools/gen_oplist.py
@@ -15,10 +15,26 @@ import yaml
 
 try:
     from executorch.codegen.parse import strip_et_fields
+    from executorch.exir._serialize import _deserialize_pte_binary
+    from executorch.exir.schema import (
+        EValue,
+        KernelCall,
+        OptionalTensorList,
+        Tensor,
+        TensorList
+    )
 except ImportError:
     # If we build from source, executorch.codegen is not available.
     # We can use relative import instead.
     from ..parse import strip_et_fields
+    from ..exir._serialize import _deserialize_pte_binary
+    from ..exir.schema import (
+        EValue,
+        KernelCall,
+        OptionalTensorList,
+        Tensor,
+        TensorList
+    )
 
 from torchgen.gen import LineLoader, parse_native_yaml_struct
 from torchgen.selective_build.operator import SelectiveBuildOperator
