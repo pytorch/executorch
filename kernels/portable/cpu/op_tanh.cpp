3a2df97eabb9285ca2747468c92da29db5c3459a
Jacob Szwejbka <jakeszwe@meta.com>
2025-06-10 14:48:47 -0700

    Revert vectorization stack (#11536)
    ### Summary
    These broke some internal tests. @swolchok said to just revert and he
    would try again
    
    ### Test plan
    ci

--- kernels/portable/cpu/op_tanh.cpp
+++ kernels/portable/cpu/op_tanh.cpp
@@ -15,9 +15,8 @@ namespace executor {
 namespace native {
 
 Tensor& tanh_out(KernelRuntimeContext& ctx, const Tensor& in, Tensor& out) {
-  static constexpr const char op_name[] = "tanh.out";
-  return internal::unary_ufunc_realhbbf16_to_floathbf16<op_name>(
-      [](auto x) { return executorch::math::tanh(x); }, ctx, in, out);
+  return internal::unary_ufunc_realhbbf16_to_floathbf16(
+      std::tanh, ctx, in, out);
 }
 
 } // namespace native
--- shim_et/xplat/executorch/kernels/portable/op_registration_util.bzl
+++ shim_et/xplat/executorch/kernels/portable/op_registration_util.bzl
@@ -533,7 +533,6 @@ ATEN_OPS = (
         name = "op_expm1",
         deps = [
             "//executorch/kernels/portable/cpu/pattern:pattern",
-            "//executorch/kernels/portable/cpu/util:elementwise_util",
         ],
     ),
     op_target(
