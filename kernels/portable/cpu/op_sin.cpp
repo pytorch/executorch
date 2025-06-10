7d6ca43022ee825a33560795cc2df0e02fc7462d
Jacob Szwejbka <jakeszwe@fb.com>
2025-06-10 13:29:11 -0700

    Revert "Implement unary_ufunc functions using elementwise_util (#9386)"
    This reverts commit dffbd426a4c519a4dc1596595578b5fa9e68d585.

--- kernels/portable/cpu/op_sin.cpp
+++ kernels/portable/cpu/op_sin.cpp
@@ -15,9 +15,7 @@ namespace executor {
 namespace native {
 
 Tensor& sin_out(KernelRuntimeContext& ctx, const Tensor& in, Tensor& out) {
-  static constexpr const char op_name[] = "sin.out";
-  return internal::unary_ufunc_realhbbf16_to_floathbf16<op_name>(
-      [](auto x) { return executorch::math::sin(x); }, ctx, in, out);
+  return internal::unary_ufunc_realhbbf16_to_floathbf16(std::sin, ctx, in, out);
 }
 
 } // namespace native
