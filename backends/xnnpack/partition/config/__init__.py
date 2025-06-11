8f97a34679c4eae56d3b6a8f70977f0687ee491b
Gregory Comer <gjcomer@meta.com>
2025-06-11 10:28:17 -0700

    Revert "Milestone3.1: Support tanh op in XNNPACK backend (#11364)" (#11535)
    ### Summary
    This reverts commit deb63ec0ef032363e624777d19e9f2f770b0781c
    (https://github.com/pytorch/executorch/pull/11364).
    
    Edit: May not need to land this as the schema looks BC. Waiting for
    internal tests.
    
    ### Test plan
    CI

--- backends/xnnpack/partition/config/__init__.py
+++ backends/xnnpack/partition/config/__init__.py
@@ -49,7 +49,6 @@ from executorch.backends.xnnpack.partition.config.generic_node_configs import (
     SoftmaxConfig,
     SquareRootConfig,
     SubConfig,
-    TanhConfig,
     UpsampleBilinear2dConfig,
 )
 from executorch.backends.xnnpack.partition.config.node_configs import (
@@ -100,7 +99,6 @@ ALL_PARTITIONER_CONFIGS: List[Type[XNNPartitionerConfig]] = [
     PreluConfig,
     ReciprocalSquareRootConfig,
     ReLUConfig,
-    TanhConfig,
     # SDPAConfig, TODO: D60553559: preserving SDPA for fairseq fails
     SigmoidConfig,
     SliceCopyConfig,
