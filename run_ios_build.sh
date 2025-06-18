82659ecfe6aa19f4e199dbf27b131cb468bd1201
hinriksnaer <hinriksnaer@gmail.com>
2025-06-18 19:55:18 -0400

    added temporary build scripts

---
+++ run_ios_build.sh
@@ -0,0 +1,15 @@
+bash install_executorch.sh --clean
+
+bash install_executorch.sh
+
+pip install -e .
+
+./install_requirements.sh
+
+# CoreML-only requirements:
+./backends/apple/coreml/scripts/install_requirements.sh
+
+# MPS-only requirements:
+./backends/apple/mps/install_requirements.sh
+
+./scripts/build_apple_frameworks.sh --Release --Debug --coreml --mps --xnnpack --custom --optimized --portable --quantized --torchao
