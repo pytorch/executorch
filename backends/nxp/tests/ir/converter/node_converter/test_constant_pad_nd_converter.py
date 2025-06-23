d8c7ee95187e3a687fc77bc94775153351f322e2
Lukas Sztefek <lukas.sztefek@nxp.com>
2025-06-23 14:05:57 +0200

    NXP backend: Improve partitioning and conversion of Pad op

--- backends/nxp/tests/ir/converter/node_converter/test_constant_pad_nd_converter.py
+++ backends/nxp/tests/ir/converter/node_converter/test_constant_pad_nd_converter.py
@@ -7,7 +7,10 @@ import numpy as np
 import pytest
 import torch
 
-from executorch.backends.nxp.tests.executorch_pipeline import to_edge_program
+from executorch.backends.nxp.tests.executorch_pipeline import (
+    to_edge_program,
+    to_quantized_edge_program,
+)
 from executorch.backends.nxp.tests.executors import (
     convert_run_compare,
     ToNCHWPreprocess,
@@ -60,16 +63,10 @@ def test_constant_pad_nd_conversion__default_constant():
         pytest.param([2, 4], list(range(4)), id="2D, padding N, H"),
         pytest.param([2, 4, 6], list(range(2)), id="3D, padding H"),
         pytest.param([2, 4, 6], list(range(4)), id="3D, padding C, H"),
-        pytest.param([2, 4, 6], list(range(6)), id="3D, padding N, C, H"),
         pytest.param([2, 4, 6, 8], list(range(2)), id="4D, padding W"),
         pytest.param([2, 4, 6, 8], list(range(4)), id="4D, padding H, W"),
-        pytest.param([2, 4, 6, 8], list(range(6)), id="4D, padding C, H, W"),
-        pytest.param([2, 4, 6, 8], list(range(8)), id="4D, padding N, C, H, W"),
         pytest.param([1, 2, 3, 4, 5], list(range(2)), id="5D, padding D"),
         pytest.param([1, 2, 3, 4, 5], list(range(4)), id="5D, padding W, D"),
-        pytest.param([1, 2, 3, 4, 5], list(range(6)), id="5D, padding H, W, D"),
-        pytest.param([1, 2, 3, 4, 5], list(range(8)), id="5D, padding C, H, W, D"),
-        pytest.param([1, 2, 3, 4, 5], list(range(10)), id="5D, padding N, C, H, W, D"),
     ],
 )
 def test_constant_pad_nd_conversion__format_less(input_shape, paddings):
@@ -87,13 +84,12 @@ def test_constant_pad_nd_conversion__format_less(input_shape, paddings):
     [
         pytest.param([1, 4, 6, 8], list(range(2)), id="4D, padding W"),
         pytest.param([1, 4, 6, 8], list(range(4)), id="4D, padding H, W"),
-        pytest.param([1, 1, 6, 8], [1, 2, 3, 4, 2, 1], id="4D, padding C, H, W"),
-        # pytest.param([1, 1, 6, 8], [1, 2, 3, 4, 2, 1, 5, 6], id='4D, padding N, C, H, W'),  # Batch size must stay 0.
     ],
 )
 def test_constant_pad_nd_conversion__channels_first(input_shape, paddings):
+    model = ConstantPadNDConvModule(paddings)
     edge_program = to_edge_program(
-        ConstantPadNDConvModule(paddings), input_shape
+        model, input_shape
     ).exported_program()  # Extra `Conv` after the padding.
 
     input_data = np.random.random(input_shape).astype(np.float32)
@@ -104,3 +100,24 @@ def test_constant_pad_nd_conversion__channels_first(input_shape, paddings):
         tflite_input_preprocess=ToNHWCPreprocess(),
         tflite_output_preprocess=ToNCHWPreprocess(),
     )
+
+
+@pytest.mark.parametrize(
+    "input_shape, paddings",
+    [
+        pytest.param([2, 4, 6], list(range(6)), id="3D, padding N, C, H"),
+        pytest.param([2, 4, 6, 8], list(range(6)), id="4D, padding C, H, W"),
+        pytest.param([2, 4, 6, 8], list(range(8)), id="4D, padding N, C, H, W"),
+        pytest.param([1, 2, 3, 4, 5], list(range(6)), id="5D, padding H, W, D"),
+        pytest.param([1, 2, 3, 4, 5], list(range(8)), id="5D, padding C, H, W, D"),
+        pytest.param([1, 2, 3, 4, 5], list(range(10)), id="5D, padding N, C, H, W, D"),
+        pytest.param([1, 1, 6, 8], [1, 2, 3, 4, 2, 1], id="4D, padding C, H, W"),
+    ],
+)
+def test_constant_pad_nd__unsupported_paddings(input_shape, paddings):
+    model = ConstantPadNDModule(paddings)
+    exec_program = to_quantized_edge_program(model, input_shape).exported_program()
+
+    nodes = list(exec_program.graph.nodes)
+    # There is at least one non-delegated Pad node
+    assert any(node.name == "aten_constant_pad_nd_default" for node in nodes)
