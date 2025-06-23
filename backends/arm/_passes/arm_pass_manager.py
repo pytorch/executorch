1c29cd2564b3e4cb20c94f329ae14b6391bfc0ef
Yufeng Shi <yufeng.shi@arm.com>
2025-06-23 17:14:26 +0100

    Arm backend: Match fp32->int32 cast between pytorch and TOSA's CAST
    - Pytorch .to() truncates values toward zeros, while TOSA's CAST
      rounds values to the nearest int. They will show different
      behaviours on fp32->int32 casting.
    - Fix the mismatched behaviour with the following transformation:
      Before:
        output = to_copy(x, torch.int32)
      After:
        is_non_negative = x >= 0
        floor_x = floor(x)
        ceil_x = ceil(x)
        decorated_x = where(is_non_negative, floor_x, ceil_x)
        output = to_copy(decorated_x, torch.int32)
    
    Change-Id: I21286432eeb0a5e2f21865f3ac097051c921a9b3
    Signed-off-by: Yufeng Shi <yufeng.shi@arm.com>

--- backends/arm/_passes/arm_pass_manager.py
+++ backends/arm/_passes/arm_pass_manager.py
@@ -51,6 +51,7 @@ from executorch.backends.arm._passes import (
     DecomposeSqrtPass,
     DecomposeSumPass,
     DecomposeVarPass,
+    DecorateFp32toInt32CastingPass,
     FoldAndAnnotateQParamsPass,
     FuseBatchnorm2DPass,
     FuseConstantArgsPass,
@@ -191,6 +192,9 @@ class ArmPassManager(PassManager):
         self.add_pass(UnsqueezeScalarPlaceholdersPass(exported_program))
         self.add_pass(MatchArgRanksPass(exported_program))
         self.add_pass(DecomposeAvgPool2d())
+        self.add_pass(
+            DecorateFp32toInt32CastingPass()
+        )  # Require that no new fp32->int32 is introduced after this pass
         self.add_pass(ComputeConstantOpsAOT(exported_program))
 
         self.add_pass(DecomposeGroupedConv())
