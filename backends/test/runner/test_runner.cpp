3fca48c8e2d2325d0490c876fb96502b789aab88
Gregory James Comer <gjcomer@meta.com>
2025-06-24 02:36:42 -0700

    Add test runner executable
    ghstack-source-id: 0f132ce6bcb7fae7b62ff536c84cb0a6eac084d7
    ghstack-comment-id: 2999563457
    Pull-Request: https://github.com/pytorch/executorch/pull/11907

---
+++ backends/test/runner/test_runner.cpp
@@ -0,0 +1,7 @@
+/*
+ * This runner is intended to built and run as part of the backend test flow. It takes a
+ * set of inputs from a flat_tensor-format file, runs each case, and then serializes the
+ * outputs to a file, also in flat_tensor format.
+ */
+
+ 
