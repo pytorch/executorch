9050ac5ecdcb75b82b6f59db11ca27d02df8c0f9
Gregory Comer <gregoryjcomer@gmail.com>
2025-06-24 21:59:52 -0700

    Update
    [ghstack-poisoned]

--- backends/xnnpack/test/tester/__init__.py
+++ backends/xnnpack/test/tester/__init__.py
@@ -4,7 +4,6 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-# TODO: Be more delibrate on module structure
 from executorch.backends.xnnpack.test.tester.tester import (
     Export,
     Partition,
@@ -12,7 +11,6 @@ from executorch.backends.xnnpack.test.tester.tester import (
     RunPasses,
     Serialize,
     Tester,
-    ToEdge,
     ToEdge,
     ToEdgeTransformAndLower,
     ToExecutorch,
