3f820bf285b94c0f62a97df0e35434d6a352e4d3
Gregory James Comer <gjcomer@meta.com>
2025-06-24 23:36:36 -0700

    Add tests for activation functions
    ghstack-source-id: 693e5089c45b62c56c4378208890ff45200d4cb6
    ghstack-comment-id: 3003538739
    Pull-Request: https://github.com/pytorch/executorch/pull/11961

---
+++ backends/test/compliance_suite/operators/test_hardswish.py
@@ -0,0 +1,41 @@
+# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+# pyre-strict
+
+from typing import Callable
+
+import torch
+
+from executorch.backends.test.compliance_suite import (
+    dtype_test,
+    operator_test,
+    OperatorTest,
+)
+
+class Model(torch.nn.Module):
+    def __init__(self, inplace=False):
+        super().__init__()
+        self.inplace = inplace
+
+    def forward(self, x):
+        return torch.nn.functional.hardswish(x, inplace=self.inplace)
+
+@operator_test
+class TestHardswish(OperatorTest):
+    @dtype_test
+    def test_hardswish_dtype(self, dtype, tester_factory: Callable) -> None:
+        self._test_op(Model(), ((torch.rand(2, 10)).to(dtype),), tester_factory)
+        
+    def test_hardswish_f32_single_dim(self, tester_factory: Callable) -> None:
+        self._test_op(Model(), (torch.randn(20),), tester_factory)
+
+    def test_hardswish_f32_multi_dim(self, tester_factory: Callable) -> None:
+        self._test_op(Model(), (torch.randn(2, 3, 4, 5),), tester_factory)
+        
+    def test_hardswish_f32_inplace(self, tester_factory: Callable) -> None:
+        self._test_op(Model(inplace=True), (torch.randn(3, 4, 5),), tester_factory)
+    
+    def test_hardswish_f32_boundary_values(self, tester_factory: Callable) -> None:
+        # Test with values that span the hardswish's piecewise regions
+        x = torch.tensor([-5.0, -3.0, -1.0, 0.0, 1.0, 3.0, 5.0])
+        self._test_op(Model(), (x,), tester_factory)
