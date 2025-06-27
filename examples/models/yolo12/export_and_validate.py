4f3a30d9531c263b4b1a49105a03389d080fc7d1
dlyakhov <daniil.lyakhov@intel.com>
2025-06-27 16:27:38 +0200

    Minor

--- examples/models/yolo12/export_and_validate.py
+++ examples/models/yolo12/export_and_validate.py
@@ -30,7 +30,6 @@ from executorch.exir import (
 from executorch.exir.backend.backend_details import CompileSpec
 from executorch.runtime import Runtime
 from torch.export.exported_program import ExportedProgram
-from torch.fx.passes.graph_drawer import FxGraphDrawer
 from torchao.quantization.pt2e.quantize_pt2e import convert_pt2e, prepare_pt2e
 from ultralytics import YOLO
 
