8dc97d5910972222ff71ebf2daa32f676e7aadfd
BujSet <ranganath1000@gmail.com>
2025-06-27 15:37:24 +0000

    Addressing some comments

--- .ci/scripts/setup-arm-zephyr-x86-64-tools.sh
+++ .ci/scripts/setup-arm-zephyr-x86-64-tools.sh
@@ -8,4 +8,4 @@
 # Setup arm example environment (including TOSA tools)
 git config --global user.email "github_executorch@arm.com"
 git config --global user.name "Github Executorch"
-bash examples/arm/setup.sh --i-agree-to-the-contained-eula --user-toolchain-url "https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v0.17.2/toolchain_linux-x86_64_arm-zephyr-eabi.tar.xz" --user-toolchain-dir "arm-zephyr-eabi"
+bash examples/arm/setup.sh --i-agree-to-the-contained-eula  --target-toolchain zephyr
