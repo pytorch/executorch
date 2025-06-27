9200dca45c6cdc1b57006f88b256f8da806f6359
Scott Wolchok <swolchok@meta.com>
2025-06-26 23:28:29 -0700

    Detect release builds by comparing CMAKE_BUILD_TYPE to Debug, not Release
    MinSizeRel and RelWithDebInfo are also release builds.
    
    
    ghstack-source-id: 44bb2f859d56a5a65a893300164f73ec3e8c78a6
    ghstack-comment-id: 3010869785
    Pull-Request-resolved: https://github.com/pytorch/executorch/pull/12045

--- tools/cmake/preset/default.cmake
+++ tools/cmake/preset/default.cmake
@@ -4,7 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-if(CMAKE_BUILD_TYPE STREQUAL "Release")
+if(NOT CMAKE_BUILD_TYPE STREQUAL "Debug")
   set(_is_build_type_release ON)
   set(_is_build_type_debug OFF)
 else()
