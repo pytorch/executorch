3d5c07d20ec9a502545d795703bca28fb4e7cc70
BujSet <ranganath1000@gmail.com>
2025-06-30 14:28:26 +0000

    Removing unnecessary setup script

--- .ci/scripts/setup-arm-baremetal-tools.sh
+++ .ci/scripts/setup-arm-baremetal-tools.sh
@@ -8,4 +8,8 @@
 # Setup arm example environment (including TOSA tools)
 git config --global user.email "github_executorch@arm.com"
 git config --global user.name "Github Executorch"
-bash examples/arm/setup.sh --i-agree-to-the-contained-eula
+if [ "$#" -eq 0 ]; then
+    bash examples/arm/setup.sh --i-agree-to-the-contained-eula
+else
+    bash examples/arm/setup.sh --i-agree-to-the-contained-eula "$#"
+fi
