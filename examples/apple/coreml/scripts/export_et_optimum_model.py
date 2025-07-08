0648b9c25d9bbddac2cd05d700b94c21db4cc20e
Scott Roy <161522778+metascroy@users.noreply.github.com>
2025-07-08 15:00:08 -0700

    up

--- examples/apple/coreml/scripts/export_et_optimum_model.py
+++ examples/apple/coreml/scripts/export_et_optimum_model.py
@@ -1,12 +1,8 @@
-import coremltools as ct
 import argparse
-
-from optimum.exporters.executorch.recipe_registry import register_recipe
 import logging
 from typing import Dict
 
-from tabulate import tabulate
-from torch.export import ExportedProgram
+import coremltools as ct
 
 from executorch.devtools.backend_debug import get_delegation_info
 from executorch.exir import (
@@ -15,6 +11,11 @@ from executorch.exir import (
     ExecutorchProgram,
     to_edge_transform_and_lower,
 )
+
+from optimum.exporters.executorch.recipe_registry import register_recipe
+
+from tabulate import tabulate
+from torch.export import ExportedProgram
 
 COREML_RECIPE = "coreml-et-testing"
 COREML_RECIPE_LLM_KWARGS = {
@@ -32,7 +33,6 @@ COREML_RECIPE_FP16_KWARGS = {
     "compute_precision": ct.precision.FLOAT16,
     "compute_unit": ct.ComputeUnit.ALL,
 }
-
 
 
 @register_recipe(COREML_RECIPE)
@@ -77,10 +77,14 @@ def export_to_executorch_with_coreml(
         ]
         for k in kwargs:
             if k not in valid_kwargs:
-                raise ValueError(f"Invalid keyword argument {k} for CoreML recipe. Valid arguments are {valid_kwargs}")
+                raise ValueError(
+                    f"Invalid keyword argument {k} for CoreML recipe. Valid arguments are {valid_kwargs}"
+                )
 
         compute_unit = kwargs.get("compute_unit", ct.ComputeUnit.ALL)
-        minimum_deployment_target = kwargs.get("minimum_deployment_target", ct.target.iOS15)
+        minimum_deployment_target = kwargs.get(
+            "minimum_deployment_target", ct.target.iOS15
+        )
         compute_precision = kwargs.get("compute_precision", ct.precision.FLOAT16)
         model_type = kwargs.get("model_type", "model")
         model_type = {
@@ -107,7 +111,9 @@ def export_to_executorch_with_coreml(
             },
         }
         if quant_recipe is not None and quant_recipe not in valid_quant_recipes:
-            raise ValueError(f"Invalid quant recipe {quant_recipe}, must be one of {valid_quant_recipes.keys()}")
+            raise ValueError(
+                f"Invalid quant recipe {quant_recipe}, must be one of {valid_quant_recipes.keys()}"
+            )
         op_linear_quantizer_config = valid_quant_recipes.get(quant_recipe, None)
 
         et_progs = {}
@@ -145,8 +151,12 @@ def export_to_executorch_with_coreml(
             logging.debug(
                 f"\nExecuTorch program for {pte_name}.pte: {et_progs[pte_name].exported_program().graph_module}"
             )
-            delegation_info = get_delegation_info(et_progs[pte_name].exported_program().graph_module)
-            logging.debug(f"\nDelegation info Summary for {pte_name}.pte: {delegation_info.get_summary()}")
+            delegation_info = get_delegation_info(
+                et_progs[pte_name].exported_program().graph_module
+            )
+            logging.debug(
+                f"\nDelegation info Summary for {pte_name}.pte: {delegation_info.get_summary()}"
+            )
             logging.debug(
                 f"\nDelegation info for {pte_name}.pte: {tabulate(delegation_info.get_operator_delegation_dataframe(), headers='keys', tablefmt='fancy_grid')}"
             )
@@ -154,7 +164,6 @@ def export_to_executorch_with_coreml(
 
     exported_progs = model.export()
     return _lower_to_executorch(exported_progs, model.metadata, **kwargs)
-
 
 
 # model_id = "HuggingFaceTB/SmolLM2-135M" # works, output questionable
@@ -165,7 +174,6 @@ def export_to_executorch_with_coreml(
 def test_decoder_only_model(model_id):
     from optimum.executorch import ExecuTorchModelForCausalLM
     from transformers import AutoTokenizer
-
 
     prompt = "Simply put, the theory of relativity states"
     tokenizer = AutoTokenizer.from_pretrained(model_id)
@@ -184,11 +192,9 @@ def test_decoder_only_model(model_id):
     print(f"\nGenerated text:\n\t{generated_text}")
 
 
-
-
 # All models fail export with same issue as https://fb.workplace.com/groups/pytorch.edge.users/permalink/1796069037930048/
 # Even if fixed, bypassed they fail lowering to CoreML because symints are passed to signature
-# model_id = "google-bert/bert-base-uncased" 
+# model_id = "google-bert/bert-base-uncased"
 # model_id = "distilbert/distilbert-base-uncased"
 # model_id = "FacebookAI/xlm-roberta-base"
 def test_encoder_only_model(model_id):
@@ -214,14 +220,16 @@ def test_encoder_only_model(model_id):
     exported_outputs = model.forward(inputs["input_ids"], inputs["attention_mask"])
     predicted_masks = tokenizer.decode(exported_outputs[0, 4].topk(5).indices)
     print(f"Predicted masks: {predicted_masks}")
-    assert any(word in predicted_masks for word in ["capital", "center", "heart", "birthplace"])
+    assert any(
+        word in predicted_masks for word in ["capital", "center", "heart", "birthplace"]
+    )
 
 
 # model_id = "openai/whisper-tiny" works
 def test_whisper(model_id):
+    from datasets import load_dataset
     from optimum.executorch import ExecuTorchModelForSpeechSeq2Seq
     from transformers import AutoProcessor, AutoTokenizer
-    from datasets import load_dataset
 
     model_id = "openai/whisper-tiny"
     tokenizer = AutoTokenizer.from_pretrained(model_id)
@@ -232,13 +240,18 @@ def test_whisper(model_id):
     )
 
     processor = AutoProcessor.from_pretrained(model_id)
-    dataset = load_dataset("distil-whisper/librispeech_long", "clean", split="validation")
+    dataset = load_dataset(
+        "distil-whisper/librispeech_long", "clean", split="validation"
+    )
     sample = dataset[0]["audio"]
 
     input_features = processor(
-        sample["array"], return_tensors="pt", truncation=False, sampling_rate=sample["sampling_rate"]
+        sample["array"],
+        return_tensors="pt",
+        truncation=False,
+        sampling_rate=sample["sampling_rate"],
     ).input_features
-    
+
     # Current implementation of the transcibe method accepts up to 30 seconds of audio, therefore I trim the audio here.
     input_features_trimmed = input_features[:, :, :3000].contiguous()
 
@@ -247,14 +260,17 @@ def test_whisper(model_id):
     print(f"Generated transcription: {generated_transcription}")
     print(f"Expected transcription: {expected_text}")
 
+
 # model_id = "google-t5/t5-small"
 def test_t5(model_id):
     from optimum.executorch import ExecuTorchModelForSeq2SeqLM
     from transformers import AutoTokenizer
-    
+
     tokenizer = AutoTokenizer.from_pretrained(model_id)
-    model = ExecuTorchModelForSeq2SeqLM.from_pretrained(model_id, recipe=COREML_RECIPE, recipe_kwargs=COREML_RECIPE_LLM_KWARGS)
-    
+    model = ExecuTorchModelForSeq2SeqLM.from_pretrained(
+        model_id, recipe=COREML_RECIPE, recipe_kwargs=COREML_RECIPE_LLM_KWARGS
+    )
+
     article = (
         " New York (CNN)When Liana Barrientos was 23 years old, she got married in Westchester County, New York. A"
         " year later, she got married again in Westchester County, but to a different man and without divorcing"
@@ -291,12 +307,11 @@ def test_t5(model_id):
     print(f"Expected text:\n\t{expected_text}")
 
 
-
 # model_id = "google/vit-base-patch16-224"
 def test_vit(model_id):
-    from transformers import AutoConfig, AutoModelForImageClassification
-    from optimum.executorch import ExecuTorchModelForImageClassification
     import torch
+    from optimum.executorch import ExecuTorchModelForImageClassification
+    from transformers import AutoConfig, AutoModelForImageClassification
 
     config = AutoConfig.from_pretrained(model_id)
     batch_size = 1
@@ -311,7 +326,9 @@ def test_vit(model_id):
         recipe_kwargs=COREML_RECIPE_FP32_KWARGS,
     )
 
-    eager_model = AutoModelForImageClassification.from_pretrained(model_id).eval().to("cpu")
+    eager_model = (
+        AutoModelForImageClassification.from_pretrained(model_id).eval().to("cpu")
+    )
     with torch.no_grad():
         eager_output = eager_model(pixel_values)
         et_output = et_model.forward(pixel_values)
@@ -334,33 +351,54 @@ def main():
         # Decode: 27 tps
         test_decoder_only_model(model_id="HuggingFaceTB/SmolLM2-135M")
     elif model_name == "llama3":
+        # Generated text: Simply put, the theory of relativity states that the speed of light is the same for all observers, regardless of their relative motion. This means that the speed of light is the same for an observer moving with respect to the source of light as it is for an observer moving with respect to the observer. This is
+        # Decode: 25 tps
         test_decoder_only_model(model_id="NousResearch/Llama-3.2-1B")
     elif model_name == "phi4":
+        # Fails export
+        # RuntimeError: Found a custom (non-ATen) operator whose output has alias annotations: EDGE_DO_NOT_DECOMP::split.Tensor(Tensor(a -> *) self, SymInt split_size, int dim=0) -> Tensor(a)[]. We only support functionalizing operators whose outputs do not have alias annotations (e.g. 'Tensor(a)' is a Tensor with an alias annotation whereas 'Tensor' is a Tensor without. The '(a)' is the alias annotation). The alias annotation specifies that the output Tensor shares storage with an input that has the same annotation. Please check if (1) the output needs to be an output (if not, don't return it), (2) if the output doesn't share storage with any inputs, then delete the alias annotation. (3) if the output indeed shares storage with an input, then add a .clone() before returning it to prevent storage sharing and then delete the alias annotation. Otherwise, please file an issue on GitHub.
         test_decoder_only_model(model_id="microsoft/Phi-4-mini-instruct")
     elif model_name == "qwen3":
+        # Generated text: Simply put, the theory of relativity states that the speed of light is constant in all inertial frames of reference. This is a statement that is true, but it does not imply that the speed of light is constant in all inertial frames of reference. The statement that the speed of light is constant in all inert
+        # Decode: 20 tps
         test_decoder_only_model(model_id="Qwen/Qwen3-0.6B")
     elif model_name == "olmo":
+        # Generated text: Simply put, the theory of relativity states that time and space are not the same. Speed and distance are not the same. Time and distance are not the same. The theory of relativity is a theory of everything. It is a theory of space and time. It is a theory of gravity. It is a
         test_decoder_only_model(model_id="allenai/OLMo-1B-hf")
     elif model_name == "gemma3":
+        # Fails export
+        # torch._dynamo.exc.TorchRuntimeError: Dynamo failed to run FX node with fake tensors: call_function <built-in function scaled_dot_product_attention>(*(FakeTensor(..., size=(1, 4, 1, 256)), FakeTensor(..., size=(1, 4, 2048, 256)), FakeTensor(..., size=(1, 4, 2048, 256))), **{'attn_mask': FakeTensor(..., size=(1, 1, 1, 512)), 'dropout_p': 0.0, 'scale': 0.0625, 'is_causal': False}): got RuntimeError('Attempting to broadcast a dimension of length 512 at -1! Mismatching argument at index 1 had torch.Size([1, 1, 1, 512]); but expected shape should be broadcastable to [1, 4, 1, 2048]')
+        # From user code:
+        #     attn_output = torch.nn.functional.scaled_dot_product_attention(
         test_decoder_only_model(model_id="unsloth/gemma-3-1b-it")
     # Encoder-only models
     elif model_name == "bert":
+        # Fails export; same issue as https://fb.workplace.com/groups/pytorch.edge.users/permalink/1796069037930048/
         test_encoder_only_model(model_id="google-bert/bert-base-uncased")
     elif model_name == "distilbert":
+        # Fails export; same issue as https://fb.workplace.com/groups/pytorch.edge.users/permalink/1796069037930048/
         test_encoder_only_model(model_id="distilbert/distilbert-base-uncased")
     elif model_name == "roberta":
+        # Fails export; same issue as https://fb.workplace.com/groups/pytorch.edge.users/permalink/1796069037930048/
         test_encoder_only_model(model_id="FacebookAI/xlm-roberta-base")
     # Vision models
     elif model_name == "vit":
+        # Works
         test_vit(model_id="google/vit-base-patch16-224")
     # Speech models
     elif model_name == "whisper":
+        # Works
+        # Generated transcription (cut short from expected):
+        # Mr. Quilter is the apostle of the middle classes, and we are glad to welcome his Gospel. Nor is Mr. Quilter's manner less interesting than his matter. He tells us that at this festive season of the year, with Christmas and a roast beef, looming before us,
         test_whisper(model_id="openai/whisper-tiny")
     # Seq2Seq models
     elif model_name == "t5":
+        # Fails CoreML lowering: NotImplementedError: Placeholder val must be a tensor or fake tensor, but got type <class 'torch.SymInt'>, value s70
+        # ET might introduce symbolic variables in graph breaks
         test_t5(model_id="google-t5/t5-small")
     else:
         raise ValueError(f"Invalid model name {model_name}")
+
 
 if __name__ == "__main__":
     main()
