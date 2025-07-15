2bab70b5ff3f8d6d402aa557122b2bb401e2250a
BujSet <ranganath1000@gmail.com>
2025-07-15 18:08:33 +0000

    linting

--- runtime/platform/default/arm_zephyr.cpp
+++ runtime/platform/default/arm_zephyr.cpp
@@ -4,11 +4,11 @@
 void et_pal_init(void) {}
 
 ET_NORETURN void et_pal_abort(void) {
-    _exit(-1);
+  _exit(-1);
 }
 
 et_timestamp_t et_pal_current_ticks(void) {
-    return k_uptime_ticks();
+  return k_uptime_ticks();
 }
 
 et_tick_ratio_t et_pal_ticks_to_ns_multiplier(void) {
@@ -39,9 +39,9 @@ void et_pal_emit_log_message(
 }
 
 void* et_pal_allocate(size_t size) {
-    return k_malloc(size);
+  return k_malloc(size);
 }
 
 void et_pal_free(ET_UNUSED void* ptr) {
-    k_free(ptr);
-} 
+  k_free(ptr);
+}
