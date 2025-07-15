867362f0248e78f985f9eb29eb81aac33f926871
sidart <sidart@devvm22281.ftw0.facebook.com>
2025-07-15 02:30:09 -0700

    Summary: Add Initial support to pico2 (Arm Cortex M)
    Test Plan:
    TBD
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

---
+++ examples/arm/rp_pi/arm-cortex-m0plus-toolchain.cmake
@@ -0,0 +1,33 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+# Copyright 2023-2025 Arm Limited and/or its affiliates.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# cortex-m0plus cmake
+
+if(NOT DEFINED EXECUTORCH_BUILD_ARM_BAREMETAL)
+  # If not defined, assume we're building standalone
+  set(EXECUTORCH_BUILD_ARM_BAREMETAL ON)
+endif()
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR cortex-m0plus)
+
+if(NOT DEFINED CMAKE_C_COMPILER)
+  set(CMAKE_C_COMPILER arm-none-eabi-gcc CACHE STRING "C compiler")
+endif()
+
+if(NOT DEFINED CMAKE_CXX_COMPILER)
+    set(CMAKE_CXX_COMPILER arm-none-eabi-g++ CACHE STRING "C++ compiler")
+endif()
+
+set(CPU_FLAGS "-mcpu=cortex-m0plus -mthumb -mfloat-abi=soft")
+# C flags (no RTTI or exceptions here, since RTTI is C++-only)
+set(CMAKE_C_FLAGS "${CPU_FLAGS} -O2 -ffunction-sections -fdata-sections -fno-exceptions -fno-unwind-tables")
+
+# C++ flags (RTTI-related flags go here)
+set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} -fno-rtti -fno-use-cxa-atexit -ffunction-sections -fdata-sections")
+
+# Linker flags
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--gc-sections -nostartfiles -flto")
---
+++ examples/arm/rp_pi/syscall_stubs.c
@@ -0,0 +1,41 @@
+/* Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ * Copyright 2023-2025 Arm Limited and/or its affiliates.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+/*
+ * We are adding a custom syscall_stubs.c file to provide dummy implementations for syscalls that are not 
+ * available on the Pico platform. This is necessary because the Pico does not have an operating system, 
+ * and therefore does not support standard C library functions like _exit, _sbrk, _read, etc.
+ * By adding these stubs, we can resolve linker errors that occur when building our project for the Pico. 
+ * The stubs will be compiled and linked into our final executable, allowing it to run on the target hardware.
+*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+void *__dso_handle = 0;
+int fnmatch(const char *pattern, const char *string, int flags) { return 1; }
+ssize_t pread(int fd, void *buf, size_t count, off_t offset) { return -1; }
+void _fini(void) {}
+void _exit(int status) { while (1) {} }
+void* _sbrk(ptrdiff_t incr) { return (void*)-1; }
+int _read(int file, char *ptr, int len) { return -1; }
+int _write(int file, char *ptr, int len) { return -1; }
+int _close(int file) { return -1; }
+int _fstat(int file, void *st) { return 0; }
+int _lseek(int file, int ptr, int dir) { return 0; }
+int _isatty(int file) { return 1; }
+int _kill(int pid, int sig) { return -1; }
+int _getpid(void) { return 1; }
+int _open(const char *name, int flags, int mode) { return -1; }
+int _gettimeofday(void *tv, void *tz) { return -1; }
+#ifdef __cplusplus
+}
+#endif
