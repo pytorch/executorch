91e9a52f2d6b3b45e17802d27e3821fd95edbc19
Ethan Ng <ethann@meta.com>
2025-07-16 10:42:03 -0700

    Add unittest for quantized_relu_out, dequantize_per_tensor_out (#12499)
    Summary: Add a unittest for quantized_relu_out, dequantize_per_tensor_out
    
    Reviewed By: hsharma35
    
    Test Plan:
    Imported from GitHub, without a `Test Plan:` line.
    
    Rollback Plan:
    
    Differential Revision: D78175392
    
    Pulled By: ethansfng

---
+++ backends/cadence/hifi/operators/tests/test_op_dequantize_per_tensor_out.cpp
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <gtest/gtest.h>
+#include <sys/times.h>
+
+#include <executorch/kernels/test/TestUtil.h>
+#include <executorch/runtime/core/error.h>
+#include <executorch/runtime/core/exec_aten/exec_aten.h>
+#include <executorch/runtime/core/exec_aten/testing_util/tensor_factory.h>
+#include <executorch/runtime/core/exec_aten/testing_util/tensor_util.h>
+#include <executorch/runtime/platform/runtime.h>
+
+#include <executorch/backends/cadence/hifi/operators/operators.h>
+
+namespace cadence {
+namespace impl {
+namespace HiFi {
+namespace native {
+namespace {
+
+using ::executorch::aten::Scalar;
+using ::executorch::aten::ScalarType;
+using ::executorch::aten::Tensor;
+using ::executorch::aten::TensorImpl;
+using ::executorch::runtime::Error;
+using ::executorch::runtime::KernelRuntimeContext;
+using ::executorch::runtime::runtime_init;
+using ::executorch::runtime::testing::TensorFactory;
+using std::optional;
+using std::string_view;
+
+class HiFiDequantizePerTensorTest : public OperatorTest {
+ public:
+ protected:
+  void dequantize_per_tensor_out(
+      const Tensor& input,
+      double scale,
+      int64_t zero_point,
+      int64_t quant_min,
+      int64_t quant_max,
+      ScalarType dtype,
+      Tensor& out) {
+    return ::cadence::impl::HiFi::native::dequantize_per_tensor_out(
+        context_, input, scale, zero_point, quant_min, quant_max, dtype, out);
+  }
+};
+
+TEST_F(HiFiDequantizePerTensorTest, MultiDimensionalTest) {
+  TensorFactory<ScalarType::Float> tf_float;
+  TensorFactory<ScalarType::Char> tf_chars;
+  const std::vector<int32_t> sizes{2, 3, 5, 6};
+  Tensor quantized_tensor = tf_chars.full(sizes, -128);
+  Tensor output_float = tf_float.zeros(sizes);
+  double dequant_scale = 0.000244140625;
+  int64_t dequant_zero_point = -128;
+  int64_t quant_min = -128;
+  int64_t quant_max = 127;
+
+  dequantize_per_tensor_out(
+      quantized_tensor,
+      dequant_scale,
+      dequant_zero_point,
+      quant_min,
+      quant_max,
+      ScalarType::Float,
+      output_float);
+
+  EXPECT_TENSOR_EQ(output_float, tf_float.zeros(sizes));
+}
+
+TEST_F(HiFiDequantizePerTensorTest, OneDimensionalTest) {
+  TensorFactory<ScalarType::Float> tf_float;
+  TensorFactory<ScalarType::Char> tf_chars;
+  const std::vector<int32_t> sizes{56};
+  Tensor quantized_tensor = tf_chars.full(sizes, -128);
+  Tensor output_float = tf_float.zeros(sizes);
+  double dequant_scale = 0.000244140625;
+  int64_t dequant_zero_point = -128;
+  int64_t quant_min = -128;
+  int64_t quant_max = 127;
+
+  dequantize_per_tensor_out(
+      quantized_tensor,
+      dequant_scale,
+      dequant_zero_point,
+      quant_min,
+      quant_max,
+      ScalarType::Float,
+      output_float);
+
+  EXPECT_TENSOR_EQ(output_float, tf_float.zeros(sizes));
+}
+
+} // namespace
+} // namespace native
+} // namespace HiFi
+} // namespace impl
+} // namespace cadence
