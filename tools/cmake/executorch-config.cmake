b85ed41c905b2650c2d312c41ae1c3cfabcf7596
Scott Wolchok <swolchok@meta.com>
2025-07-17 09:02:56 -0700

    Update
    [ghstack-poisoned]

--- tools/cmake/executorch-config.cmake
+++ tools/cmake/executorch-config.cmake
@@ -25,14 +25,33 @@
 
 cmake_minimum_required(VERSION 3.24)
 include("${CMAKE_CURRENT_LIST_DIR}/Utils.cmake")
-find_package(tokenizers REQUIRED)
+find_package(tokenizers) # not REQUIRED because building it is optional
 
 set(_root "${CMAKE_CURRENT_LIST_DIR}/../../..")
+set(required_lib_list executorch executorch_core portable_kernels)
+set(EXECUTORCH_LIBRARIES)
 set(EXECUTORCH_INCLUDE_DIRS
     ${_root}/include ${_root}/include/executorch/runtime/core/portable_type/c10
     ${_root}/lib
 )
-set(non_exported_lib_list XNNPACK xnnpack-microkernels-prod kleidiai pthreadpool cpuinfo)
+foreach(lib ${required_lib_list})
+  set(lib_var "LIB_${lib}")
+  find_library(
+    ${lib_var} ${lib}
+    HINTS "${_root}/lib"
+    CMAKE_FIND_ROOT_PATH_BOTH
+  )
+  if(NOT ${lib_var})
+    set(EXECUTORCH_FOUND OFF)
+    return()
+  endif()
+  list(APPEND EXECUTORCH_LIBRARIES ${lib})
+endforeach()
+set(EXECUTORCH_FOUND ON)
+
+set(non_exported_lib_list XNNPACK xnnpack-microkernels-prod kleidiai
+                          pthreadpool cpuinfo
+)
 foreach(lib ${non_exported_lib_list})
   # Name of the variable which stores result of the find_library search
   set(lib_var "LIB_${lib}")
@@ -48,14 +67,51 @@ foreach(lib ${non_exported_lib_list})
   else()
     add_library(${lib} STATIC IMPORTED)
     set_target_properties(${lib} PROPERTIES IMPORTED_LOCATION "${${lib_var}}")
-    target_include_directories(
-      ${lib}
-      INTERFACE ${EXECUTORCH_INCLUDE_DIRS}
-    )
+    target_include_directories(${lib} INTERFACE ${EXECUTORCH_INCLUDE_DIRS})
     list(APPEND EXECUTORCH_LIBRARIES ${lib})
   endif()
 endforeach()
 
 include("${CMAKE_CURRENT_LIST_DIR}/ExecuTorchTargets.cmake")
 
-# TODO: does ExecuTorchTargets.cmake set EXECUTORCH_FOUND?
+set(optional_lib_list
+    flatccrt
+    etdump
+    bundled_program
+    extension_data_loader
+    extension_flat_tensor
+    coreml_util
+    coreml_inmemoryfs
+    coremldelegate
+    mpsdelegate
+    neuron_backend
+    qnn_executorch_backend
+    portable_ops_lib
+    custom_ops
+    extension_module
+    extension_module_static
+    extension_runner_util
+    extension_tensor
+    extension_threadpool
+    extension_training
+    xnnpack_backend
+    vulkan_backend
+    optimized_kernels
+    optimized_portable_kernels
+    cpublas
+    eigen_blas
+    optimized_ops_lib
+    optimized_native_cpu_ops_lib
+    quantized_kernels
+    quantized_ops_lib
+    quantized_ops_aot_lib
+)
+foreach(lib ${optional_lib_list})
+  if(TARGET ${lib})
+    list(APPEND EXECUTORCH_LIBRARIES ${lib})
+  else()
+    message("${lib} library is not found.
+             If needed rebuild with the proper options in CMakeLists.txt"
+    )
+  endif()
+endforeach()
