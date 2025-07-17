028955a518c5e1652766a39cbf109a267b5180c4
Vivek Trivedi <5340687+trivedivivek@users.noreply.github.com>
2025-07-16 17:33:11 -0700

    [ET-VK] 4/n Split dispatches between multiple command buffers. Removing flush function call from submit_current_cmd_and_wait.
    Pull Request resolved: https://github.com/pytorch/executorch/pull/12525
    
    This diff removes the `flush` function call from `submit_current_cmd_and_wait` and instead adds it to the `encode_prepack` method.
    
    This change will allow better control over flush which is needed in follow up diffs.
    ghstack-source-id: 296712817
    @exported-using-ghexport
    
    Differential Revision: [D78360036](https://our.internmc.facebook.com/intern/diff/D78360036/)

--- backends/vulkan/runtime/graph/ComputeGraph.cpp
+++ backends/vulkan/runtime/graph/ComputeGraph.cpp
@@ -765,8 +765,6 @@ void ComputeGraph::submit_current_cmd_and_wait(const bool final_use) {
   context_->submit_cmd_to_gpu(fence.get_submit_handle(), final_use);
   fence.wait();
   context_->fences().return_fence(fence);
-
-  context_->flush();
 }
 
 void ComputeGraph::prepack() {
@@ -787,7 +785,12 @@ void ComputeGraph::prepack() {
       // Otherwise, just submit the current command buffer for execution and
       // proceed. This results in lower load latency at the cost of higher peak
       // memory usage.
-      reduce_peak_memory ? submit_current_cmd_and_wait() : submit_current_cmd();
+      if (reduce_peak_memory) {
+        submit_current_cmd_and_wait();
+        context_->flush();
+      } else {
+        submit_current_cmd();
+      }
       staging_nbytes_in_cmd_ = 0;
       context_->set_cmd();
       submitted = true;
@@ -797,6 +800,7 @@ void ComputeGraph::prepack() {
     i++;
   }
   submit_current_cmd_and_wait(/*final_use=*/true);
+  context_->flush();
   staging_nbytes_in_cmd_ = 0;
 }
 
