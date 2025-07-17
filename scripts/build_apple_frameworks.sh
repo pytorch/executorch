9ed0430d8dddde701d023ea2660a7e5f2c31e0d1
Anthony Shoumikhin <anthony@shoumikh.in>
2025-07-17 11:01:49 -0700

    Update build_apple_frameworks.sh

--- scripts/build_apple_frameworks.sh
+++ scripts/build_apple_frameworks.sh
@@ -5,13 +5,6 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-#!/bin/bash
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree.
-
 set -euxo pipefail
 
 MODES=()
