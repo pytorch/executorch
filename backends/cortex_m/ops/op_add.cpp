f6cc26295fe9a04b6357e3cf8eef6988a569a2cb
sidart <sidart@devvm22281.ftw0.facebook.com>
2025-07-17 16:49:08 -0700

    Summary: Initial CMSS-NN Add Op
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

---
+++ backends/cortex_m/ops/op_add.cpp
@@ -0,0 +1,45 @@
+#include <executorch/runtime/kernel/kernel_includes.h>
+#include <cinttypes>
+namespace cortex_m {
+namespace native {
+
+using Tensor = executorch::aten::Tensor;
+using ScalarType = executorch::aten::ScalarType;
+using KernelRuntimeContext = torch::executor::KernelRuntimeContext;
+
+Tensor& add_out(
+    KernelRuntimeContext& ctx,
+    const Tensor& input1,
+    const Tensor& input2,
+    const ScalarType dtype,
+    Tensor& out) {
+  
+  // Ensure input is char type
+  ET_CHECK_MSG(
+      input1.scalar_type() == ScalarType::Char,
+      "input1.scalar_type() %" PRId8 " is not char type",
+      static_cast<int8_t>(input1.scalar_type()));
+
+  ET_CHECK_MSG(
+      input2.scalar_type() == ScalarType::Char,
+      "input2.scalar_type() %" PRId8 " is not char type",
+      static_cast<int8_t>(input2.scalar_type()));
+
+  // Check output dtype is float
+  ET_CHECK_MSG(
+      out.scalar_type() == ScalarType::Float,
+      "out.scalar_type() %" PRId8 " is not float",
+      static_cast<int8_t>(out.scalar_type()));
+
+  // Check dtype is int8 (Char)
+  ET_CHECK_MSG(
+      dtype == ScalarType::Char,
+      "dtype %" PRId8 " is not int8 (Char)",
+      static_cast<int8_t>(dtype));
+  
+
+  return out;
+}
+
+} // namespace native
+} // namespace cortex_m
