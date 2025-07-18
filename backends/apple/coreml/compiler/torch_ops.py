8c330502079b4db59dde04fadd3fad636d722a2e
Scott Roy <161522778+metascroy@users.noreply.github.com>
2025-07-18 10:43:30 -0700

    init

---
+++ backends/apple/coreml/compiler/torch_ops.py
@@ -0,0 +1,19 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# This file registers torch ops that are not yet in coremltools, or are in a more recent version of
+# coremltools than is used by ExecuTorch.  Each op registered here should have a link to a PR in coremltools that adds
+# the op to the coremltools library.
+
+from coremltools.converters.mil.frontend.torch.ops import transpose
+from coremltools.converters.mil.frontend.torch.torch_op_registry import (
+    register_torch_op,
+)
+
+
+@register_torch_op(override=False)
+def transpose_copy(context, node):
+    transpose(context, node)
