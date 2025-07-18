pytorch/executorch

commit 892589088c4e5010a937c4992ba280479479e582

Add deprecation message for constant buffer branch (#12295)
Summary:


Add a warning message before deprecation. There are a bunch of PTE files still using this branch: D77626530

Reviewed By: larryliu0820

Differential Revision: D77965467

runtime/executor/program.cpp
@@ -219,6 +219,9 @@ Result<executorch_flatbuffer::ExecutionPlan*> get_execution_plan(
   } else {
     // The constant data is stored inside the flatbuffer, so this program does
     // not contain a separate segment for it.
+    ET_LOG(
+        Error,
+        "!!PLEASE REGENERATE YOUR PTE FILE!! This branch is deprecated as of ExecuTorch 0.7. Regenerate your PTE to ensure newer versions of the ExecuTorch runtime can support it. See the ExecuTorch deprecation policy at: https://docs.pytorch.org/executorch/stable/api-life-cycle.html#deprecation-policy. Contact the PyTorch Edge team if you have any questions.");
     return Program(
         loader,
         segment_base_offset,

commit 4197bc202fd90c9882df1895e26bd6246517e388

Do not load segment when only placeholder exists (#12646)
Summary:

The deprecation warning should only appear when there are values inside the constant_buffer and nothing inside the constant_segment.

Even when there are no constants in the program, we expect to have one 'DataSegment' storing the placeholder for non-constants. Move this into the 'if' branch.

Fix failing jobs caused by error logging from #12295
Test failures: https://hud.pytorch.org/pr/pytorch/executorch/12631#46239138027

Arm test is using a recently-generated PTE, however hitting the deprecation warning because the pte has no constants.

Reviewed By: larryliu0820

Differential Revision: D78568839

runtime/executor/program.cpp
@@ -172,7 +172,18 @@ Result<executorch_flatbuffer::ExecutionPlan*> get_execution_plan(
   // only offset, the constant segment is empty and does not need to be loaded.
   const auto* constant_segment = flatbuffer_program->constant_segment();
   if (constant_segment != nullptr && constant_segment->offsets() != nullptr &&
-      constant_segment->offsets()->size() > 1) {
+      constant_segment->offsets()->size() > 0) {
+    if (constant_segment->offsets()->size() == 1) {
+      // No constants; the constant segment is empty and does not
+      // need to be loaded.
+      return Program(
+          loader,
+          segment_base_offset,
+          std::move(program_data.get()),
+          flatbuffer_program,
+          /*constant_segment_data=*/FreeableBuffer{},
+          std::move(pte_data_map));
+    }
     // The constant data is inside a separate segment.
     const auto* constant_buffer = flatbuffer_program->constant_buffer();
     ET_CHECK_OR_RETURN_ERROR(