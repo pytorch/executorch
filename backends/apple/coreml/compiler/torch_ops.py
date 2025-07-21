0a436522b292ff89238889a372d8060de3b5a93d
Scott Roy <161522778+metascroy@users.noreply.github.com>
2025-07-21 11:17:17 -0700

    Add ability to add MIL ops to ET that are not yet in coremltools (#12648)
    This introduces torch_ops.py in ET's CoreML compiler. This lets us add
    critical MIL ops that are not yet in ET's version of coremltools.

---
+++ backends/apple/coreml/compiler/torch_ops.py
@@ -0,0 +1,26 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# This file registers torch ops that are not yet in coremltools, or are in a more recent version of
+# coremltools than is used by ExecuTorch.  Each op registered here should have a link to a PR in coremltools that adds
+# the op to the coremltools library.
+
+from coremltools.converters.mil.frontend.torch.ops import transpose, unbind
+from coremltools.converters.mil.frontend.torch.torch_op_registry import (
+    register_torch_op,
+)
+
+
+# https://github.com/apple/coremltools/pull/2556
+@register_torch_op(override=False)
+def transpose_copy(context, node):
+    transpose(context, node)
+
+
+# https://github.com/apple/coremltools/pull/2557
+@register_torch_op(override=False)
+def unbind_copy(context, node):
+    unbind(context, node)
