7c0481efe7bb263a5069735ff7b16d5ab9ec4583
Gregory James Comer <gjcomer@meta.com>
2025-07-21 14:38:58 -0700

    Update
    [ghstack-poisoned]

--- tools/cmake/executorch-config.cmake
+++ tools/cmake/executorch-config.cmake
@@ -57,18 +57,28 @@ set(EXECUTORCH_FOUND ON)
 
 target_link_libraries(executorch INTERFACE executorch_core)
 
-set(lib_list
-    flatccrt
-    etdump
-    bundled_program
-    extension_data_loader
-    extension_flat_tensor
+set(backend_lib_list
     coreml_util
     coreml_inmemoryfs
     coremldelegate
     mpsdelegate
     neuron_backend
     qnn_executorch_backend
+    # Start XNNPACK Lib Deps
+    XNNPACK
+    xnnpack-microkernels-prod
+    kleidiai
+    # End XNNPACK Lib Deps
+    xnnpack_backend
+    vulkan_backend
+)
+
+set(lib_list
+    flatccrt
+    etdump
+    bundled_program
+    extension_data_loader
+    extension_flat_tensor
     portable_ops_lib
     custom_ops
     extension_module
@@ -77,15 +87,8 @@ set(lib_list
     extension_tensor
     extension_threadpool
     extension_training
-    xnnpack_backend
-    # Start XNNPACK Lib Deps
-    XNNPACK
-    xnnpack-microkernels-prod
-    kleidiai
-    # End XNNPACK Lib Deps
     cpuinfo
     pthreadpool
-    vulkan_backend
     optimized_kernels
     optimized_portable_kernels
     cpublas
@@ -96,6 +99,9 @@ set(lib_list
     quantized_ops_lib
     quantized_ops_aot_lib
 )
+
+list(APPEND lib_list ${backend_lib_list})
+
 foreach(lib ${lib_list})
   # Name of the variable which stores result of the find_library search
   set(lib_var "LIB_${lib}")
@@ -198,3 +204,12 @@ if(TARGET xnnpack_backend)
   )
   target_link_options_shared_lib(xnnpack_backend)
 endif()
+
+# An interface target containing all available backends.
+add_library(executorch_backends INTERFACE)
+
+foreach(lib ${backend_lib_list})
+  if(TARGET ${lib})
+    target_link_options(executorch_backends INTERFACE ${lib})
+  endif()
+endforeach()
