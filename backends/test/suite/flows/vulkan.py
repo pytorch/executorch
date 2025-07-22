1105e043f52dc46549004b4e59e7bcf1a6ad2bb3
Gregory James Comer <gjcomer@meta.com>
2025-07-22 16:59:27 -0700

    Update
    [ghstack-poisoned]

---
+++ backends/test/suite/flows/vulkan.py
@@ -0,0 +1,19 @@
+import coremltools
+import functools
+
+from executorch.backends.vulkan.test.tester import VulkanTester
+from executorch.backends.test.suite.flow import TestFlow
+from typing import Any
+
+def _create_vulkan_flow(
+    name: str, 
+    quantize: bool = False, 
+) -> TestFlow:
+    return TestFlow(
+        name,
+        backend="vulkan",
+        tester_factory=VulkanTester,
+        quantize=quantize,
+    )
+
+VULKAN_TEST_FLOW = _create_vulkan_flow("vulkan")
