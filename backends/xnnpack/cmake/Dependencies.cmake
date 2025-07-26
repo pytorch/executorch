2f76bf318cbc6ea6ecc1230d6054dcfdcdeca508
Scott Wolchok <swolchok@meta.com>
2025-07-25 17:02:01 -0700

    Add cmake-format linter
    This uses the adapter I added to lintrunner-adapters in https://github.com/justinchuby/lintrunner-adapters/pull/117 . Since there hasn't been a release yet, I copied the adapter to our repo; we can get rid of it when there's an upstream release of lintrunner-adapters and we upgrade to it.
    
    ghstack-source-id: 2a57ed5b67d6a4ffca38ab210a48f72e46b8002b
    ghstack-comment-id: 3120541492
    Pull-Request: https://github.com/pytorch/executorch/pull/12868

--- backends/xnnpack/cmake/Dependencies.cmake
+++ backends/xnnpack/cmake/Dependencies.cmake
@@ -35,24 +35,25 @@ set(XNNPACK_BUILD_TESTS
 set(XNNPACK_ENABLE_AVXVNNI
     OFF
     CACHE BOOL ""
-  )
-# Work around observed failure: https://github.com/pytorch/executorch/pull/10362#issuecomment-2906391232
+)
+# Work around observed failure:
+# https://github.com/pytorch/executorch/pull/10362#issuecomment-2906391232
 set(XNNPACK_ENABLE_AVX512VNNIGFNI
-  OFF
-  CACHE BOOL "")
+    OFF
+    CACHE BOOL ""
+)
 
 if(EXECUTORCH_XNNPACK_ENABLE_KLEIDI)
-    set(XNNPACK_ENABLE_KLEIDIAI
-        ON
-        CACHE BOOL ""
-    )
+  set(XNNPACK_ENABLE_KLEIDIAI
+      ON
+      CACHE BOOL ""
+  )
 else()
-    set(XNNPACK_ENABLE_KLEIDIAI
-        OFF
-        CACHE BOOL ""
-    )
+  set(XNNPACK_ENABLE_KLEIDIAI
+      OFF
+      CACHE BOOL ""
+  )
 endif()
-
 
 set(XNNPACK_BUILD_ALL_MICROKERNELS
     OFF
