7900b66b29493f33694b06bba9949a9baba8f03e
winskuo-quic <143469905+winskuo-quic@users.noreply.github.com>
2025-08-06 05:29:06 +0800

    Qualcomm AI Engine Direct - Runtime Option (#12297)
    ### Summary
    Supporting following options that can be set during both AOT and
    runtime:
    
    - Log Level
    - Performance Mode
    - Profiling Level
    
    ### Test plan
    
    - Log Level: Check `debug` message prefix exists. 
    - Performance Mode: Ensure QNN SDK prints config log for performance,
    and ensure burst is faster than high power saver.
    - Profiling Level: Turn profiling off in compile spec and add profiling
    flag in runtime, ensure profiler gets expected number of events.

--- backends/qualcomm/tests/utils.py
+++ backends/qualcomm/tests/utils.py
@@ -198,6 +198,8 @@ class TestQNN(unittest.TestCase):
     pre_gen_pte: str = ""
     llama_artifacts: str = ""
     dump_intermediate_outputs: bool = False
+    inference_speed: float = 0.0
+    inference_speed_output_path = "outputs/inference_speed.txt"
 
     def _assert_outputs_equal(self, model_output, ref_output):
         self.assertTrue(len(ref_output) == len(model_output))
@@ -264,6 +266,9 @@ class TestQNN(unittest.TestCase):
         output_encodings: Tuple = (),
         check_io_shape: bool = False,
         op_package_paths: List[str] = None,
+        extra_cmds: str = "",
+        output_callback: Optional[Callable[[str], None]] = None,
+        save_inference_speed: bool = False,
     ):
         with tempfile.TemporaryDirectory() as tmp_dir:
             (
@@ -287,7 +292,9 @@ class TestQNN(unittest.TestCase):
                     torch_to_numpy_dtype_dict,
                 )
 
-                for i, f in enumerate(sorted(os.listdir(output_dir))):
+                for i, f in enumerate(
+                    sorted(f for f in os.listdir(output_dir) if f.endswith(".raw"))
+                ):
                     enc = output_encodings[i] if len(output_encodings) != 0 else None
                     dtype = (
                         ref_outputs[i].numpy().dtype
@@ -368,6 +375,13 @@ class TestQNN(unittest.TestCase):
                 ]
                 if expected_intermediate_events != -1:
                     cmd.append("--dump_intermediate_outputs")
+                cmd += extra_cmds.split()
+
+                if save_inference_speed:
+                    cmd += [
+                        "--performance_output_path",
+                        self.inference_speed_output_path,
+                    ]
 
                 if check_io_shape:
                     shape_info = {
@@ -387,16 +401,19 @@ class TestQNN(unittest.TestCase):
                     cmd,
                     stdout=subprocess.PIPE,
                     stderr=subprocess.STDOUT,
+                    text=True,
                     env=env,
                     cwd=tmp_dir,
                 )
 
+                if output_callback:
+                    output_callback(proc)
                 self.assertEqual(
                     proc.returncode,
                     0,
                     f"The process running qnn_executorch_runner return {proc.returncode}, "
                     "STDOUT=\n"
-                    f"{proc.stdout.decode('utf-8')}",
+                    f"{proc.stdout}",
                 )
 
                 # Verify the outputs
@@ -409,6 +426,13 @@ class TestQNN(unittest.TestCase):
 
                 if expected_intermediate_events != -1:
                     validate_intermediate_tensor()
+
+                if save_inference_speed:
+                    with open(
+                        f"{tmp_dir}/{self.inference_speed_output_path}", "r"
+                    ) as f:
+                        self.inference_speed = float(f.read())
+
             else:
                 adb = SimpleADB(
                     qnn_sdk=os.getenv("QNN_SDK_ROOT"),
@@ -438,7 +462,12 @@ class TestQNN(unittest.TestCase):
                     input_list=input_list,
                     files=op_package_paths,
                 )
-                adb.execute(method_index=method_index)
+                adb.extra_cmds += extra_cmds
+                if save_inference_speed:
+                    adb.extra_cmds += (
+                        f" --performance_output_path {self.inference_speed_output_path}"
+                    )
+                adb.execute(method_index=method_index, output_callback=output_callback)
                 adb.pull(output_path=tmp_dir, callback=post_process)
                 self._assert_outputs_equal(outputs, ref_outputs)
 
@@ -451,6 +480,11 @@ class TestQNN(unittest.TestCase):
                         debug_output_path,
                         callback=validate_intermediate_tensor,
                     )
+                if save_inference_speed:
+                    with open(
+                        f"{tmp_dir}/{self.inference_speed_output_path}", "r"
+                    ) as f:
+                        self.inference_speed = float(f.read())
 
     def lower_module_and_test_output(
         self,
@@ -465,6 +499,9 @@ class TestQNN(unittest.TestCase):
         skip_node_op_set: set = None,
         skip_mutable_buffer: bool = False,
         dynamic_shapes: Dict = None,
+        extra_cmds: str = "",
+        output_callback: Optional[Callable[[str], None]] = None,
+        save_inference_speed: bool = False,
     ):
         delegated_program = to_edge_transform_and_lower_to_qnn(
             module,
@@ -520,6 +557,9 @@ class TestQNN(unittest.TestCase):
                 etrecord_path,
                 expected_profile_events,
                 expected_intermediate_events,
+                extra_cmds=extra_cmds,
+                output_callback=output_callback,
+                save_inference_speed=save_inference_speed,
             )
 
     def get_qdq_module(
