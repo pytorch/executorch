683670f2196a7cbe0511034f0cfda544e67b1ce2
cccclai <chenlai@meta.com>
2025-08-09 10:28:03 -0700

    fix llama buck build (#13169)
    Summary: Some recent changes break the llama buck build
    
    Differential Revision: D79753385

--- examples/models/llama/evaluate/eager_eval.py
+++ examples/models/llama/evaluate/eager_eval.py
@@ -10,6 +10,7 @@ from typing import Optional, Union
 import torch
 
 from lm_eval.models.huggingface import HFLM as eval_wrapper
+from pytorch_tokenizers.hf_tokenizer import HuggingFaceTokenizer
 from pytorch_tokenizers.llama2c import Llama2cTokenizer as SentencePieceTokenizer
 from pytorch_tokenizers.tiktoken import TiktokenTokenizer as Tiktoken
 
@@ -24,7 +25,7 @@ class EagerEvalWrapper(eval_wrapper):
     def __init__(
         self,
         model: nn.Module,
-        tokenizer: Union[SentencePieceTokenizer, Tiktoken],
+        tokenizer: Union[SentencePieceTokenizer, Tiktoken, HuggingFaceTokenizer],
         max_seq_length: Optional[int] = None,
         use_kv_cache: bool = False,
     ):
