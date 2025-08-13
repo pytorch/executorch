808e9eae93237e9581ed848f637c8744f3dbcedf
Abhinay Kukkadapu <abhinayk@meta.com>
2025-08-13 15:26:59 -0700

    Update base for Update on "[executorch] Add coreml quant recipes"
    Fixing tests for stack that got reverted: https://github.com/pytorch/executorch/pull/13265
    
    Adds coreml quant recipes after FP32/16 recipes added in #13121
    
    Recipes added:
    
    PT2E_INT8_STATIC
    PT2E_INT8_WEIGHT_ONLY
    INT4_WEIGHT_ONLY_PER_CHANNEL
    INT4_WEIGHT_ONLY_PER_GROUP
    INT8_WEIGHT_ONLY_PER_CHANNEL
    INT8_WEIGHT_ONLY_PER_GROUP
    CODEBOOK_WEIGHT_ONLY
    
    Differential Revision: [D80206542](https://our.internmc.facebook.com/intern/diff/D80206542/)
    
    [ghstack-poisoned]

--- backends/xnnpack/test/recipes/test_xnnpack_recipes.py
+++ backends/xnnpack/test/recipes/test_xnnpack_recipes.py
@@ -155,7 +155,7 @@ class TestXnnpackRecipes(unittest.TestCase):
                     session, example_inputs, 1e-3
                 )
                 self._compare_eager_unquantized_model_outputs(
-                    session, model, example_inputs, 15
+                    session, model, example_inputs, 14
                 )
 
     def _get_recipe_for_quant_type(self, quant_type: QuantType) -> XNNPackRecipeType:
