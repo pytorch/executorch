92ca856724df792e3066f6c8cd3975fac6945ccd
cad-rlc <cad_rlc_ndsp@cadence.com>
2025-08-13 08:24:47 -0700

    Add iDMA support for softmax over last dimension within the softmax operator
    - Added iDMA support within the softmax operator in vision/operators directory.
    - Modified op_add, op_embedding and op_view_copy based on comments from last PR.
    - Created a build script as an example on how build and run "add" on Vision 130.
    - Replaced IVP_PACKVNRN_2X64W with IVP_PACKVRNRN_2X64W. (Instruction also available on VP6)

--- backends/cadence/vision/operators/op_softmax.cpp
+++ backends/cadence/vision/operators/op_softmax.cpp
@@ -6,12 +6,14 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+#include <idma_init.h>
 #include <api.h>
 #include <executorch/backends/cadence/vision/kernels/kernels.h>
 #include <executorch/kernels/portable/cpu/util/activation_ops_util.h>
 #include <executorch/kernels/portable/cpu/util/functional_util.h>
 #include <executorch/kernels/portable/cpu/util/reduce_util.h>
 #include <executorch/runtime/kernel/kernel_includes.h>
+#include <stdio.h>
 
 using executorch::aten::ScalarType;
 using executorch::aten::Tensor;
@@ -61,6 +63,17 @@ Tensor& _softmax_out(
   constexpr int MaxDim = 5;
 
   bool optimized = true;
+  bool ping_pong_process = false;
+  bool ping_process_pong = false;
+
+  if ((d == in.dim() - 1)){
+    if (size <= IDMA_BUFF_SIZE / 4 && in.dim() != 1){
+      ping_pong_process = true;
+    }
+    else if (size <= IDMA_BUFF_SIZE / 2){
+      ping_process_pong = true;
+    }
+  }
 
   if (out.scalar_type() != ScalarType::Float)
     optimized = false;
@@ -72,73 +85,152 @@ Tensor& _softmax_out(
     const float *ptr_inp = (float *)in.const_data_ptr<float>();
     float *out_data = (float *)out.mutable_data_ptr<float>();
 
-    int num_inp_dims = in.dim();
-    int num_out_dims = num_inp_dims;
+	  /* Channel 0*/
+	  idma_init(0, 0, MAX_BLOCK_16, 8, TICK_CYCLES_1, 0, NULL);
+	  idma_init_loop(0, descbuf[0], IDMA_2D_DESC, 1, NULL, NULL);
 
-    int ptr_inp_shape[MaxDim];
-    int ptr_out_shape[MaxDim];
-    int ptr_permute_vec[MaxDim];
+	  /* Channel 1*/
+	  idma_init(1, 0, MAX_BLOCK_16, 8, TICK_CYCLES_1, 0, NULL);
+	  idma_init_loop(1, descbuf[1], IDMA_2D_DESC, 1, NULL, NULL);
 
-    for (int i = 0; i < num_inp_dims; i++)
-      ptr_inp_shape[i] = in.size(i);
-
-    for (int i = 0; i < num_inp_dims; i++) {
-      if (i == d)
-        ptr_permute_vec[i] = num_inp_dims - 1;
-      else if (i == (num_inp_dims - 1))
-        ptr_permute_vec[num_inp_dims - 1] = d;
-      else
-        ptr_permute_vec[i] = i;
-
-      ptr_out_shape[i] = ptr_inp_shape[ptr_permute_vec[i]];
-
-      if (i != d)
-        outer_size = outer_size * ptr_inp_shape[i];
-    }
-
-    outer_stride = size;
-
-    float* ptr_out =
-        (float*)kernels::allocate_temp_memory(ctx, out.numel() * sizeof(float));
-
-    ET_KERNEL_CHECK(ctx, ptr_out != nullptr, MemoryAllocationFailed, out);
-
-    float* ptr_out1 =
-        (float*)kernels::allocate_temp_memory(ctx, out.numel() * sizeof(float));
-
-    ET_KERNEL_CHECK(ctx, ptr_out1 != nullptr, MemoryAllocationFailed, out);
-
-    tensor_transposef(
-      ptr_out,
-      ptr_out_shape,
-      ptr_inp,
-      ptr_inp_shape,
-      ptr_permute_vec,
-      num_out_dims,
-      num_inp_dims);
-
-    for (size_t outer_idx = 0; outer_idx < outer_size; ++outer_idx) {
-      size_t outer = outer_idx * outer_stride;
-      for (size_t inner_idx = 0; inner_idx < stride; ++inner_idx) {
-        size_t base = outer + inner_idx;
-        
-        float *ptr_in_data = &ptr_out[base];
-        float *ptr_out_data = &ptr_out1[base];
-
-        vsoftmaxf(ptr_out_data, ptr_in_data, size);
+    if (ping_pong_process) {
+      for (int i = 0; i < in.dim(); i++){
+        if (i != d)
+          outer_size *= in.size(i);
       }
+
+      outer_stride = size;
+      stride = size;
+
+      int pp_swap = 0;
+
+	    float32_t *ptr_out = out_data;
+	    float32_t *ptr_in = (float32_t *) ptr_inp;
+
+      idma_copy_2d_desc(0, inpData[pp_swap], ptr_in, 4 * stride, DESC_IDMA_PRIOR_H, 1, 0, 0);
+      pp_swap = 1;
+
+      for (int i = 0; i < (outer_size - 1); i++){ 
+          IDMA_HW_WAIT_ALL(0);
+          ptr_in += outer_stride;
+          idma_copy_2d_desc(0, inpData[pp_swap], ptr_in, 4 * stride, DESC_IDMA_PRIOR_H, 1, 0, 0);
+          pp_swap = pp_swap ^ 1;
+
+          /* PROCESS CALL */
+          vsoftmaxf(outData[pp_swap], inpData[pp_swap], stride);
+
+          IDMA_HW_WAIT_ALL(1);
+          idma_copy_2d_desc(1, ptr_out, outData[pp_swap], 4 * stride, DESC_IDMA_PRIOR_H, 1, 0, 0);
+          ptr_out += outer_stride;
+        }
+
+      IDMA_HW_WAIT_ALL(0);
+      pp_swap = pp_swap ^ 1;
+
+      /* PROCESS CALL */
+      vsoftmaxf(outData[pp_swap], inpData[pp_swap], stride);
+
+      IDMA_HW_WAIT_ALL(1);
+      idma_copy_2d_desc(1, ptr_out, outData[pp_swap], 4 * stride, DESC_IDMA_PRIOR_H, 1, 0, 0);
+
+      IDMA_HW_WAIT_ALL(1);
+
+      return out;
+    } else if (ping_process_pong) {
+      for (int i = 0; i < in.dim(); i++){
+        if (i != d)
+          outer_size *= in.size(i);
+      }
+
+      outer_stride = size;
+      stride = size;
+
+	    float32_t *ptr_out = out_data;
+	    float32_t *ptr_in = (float32_t *) ptr_inp;
+
+	    for (int i = 0; i < outer_size; i++){
+        idma_copy_2d_desc(0, data_dram0, ptr_in, 4 * stride, DESC_IDMA_PRIOR_H, 1, 0, 0);
+        IDMA_HW_WAIT_ALL(0);
+
+		    vsoftmaxf(data_dram1, data_dram0, stride);
+
+        idma_copy_2d_desc(1, ptr_out, data_dram1, 4 * stride, DESC_IDMA_PRIOR_H, 1, 0, 0);        
+        IDMA_HW_WAIT_ALL(1);
+
+        ptr_in += outer_stride;
+		    ptr_out += outer_stride;
+	    }
+
+      return out;
+    } else {
+      int num_inp_dims = in.dim();
+      int num_out_dims = num_inp_dims;
+
+      int ptr_inp_shape[MaxDim];
+      int ptr_out_shape[MaxDim];
+      int ptr_permute_vec[MaxDim];
+
+      for (int i = 0; i < num_inp_dims; i++)
+        ptr_inp_shape[i] = in.size(i);
+
+      for (int i = 0; i < num_inp_dims; i++) {
+        if (i == d)
+          ptr_permute_vec[i] = num_inp_dims - 1;
+        else if (i == (num_inp_dims - 1))
+          ptr_permute_vec[num_inp_dims - 1] = d;
+        else
+          ptr_permute_vec[i] = i;
+
+        ptr_out_shape[i] = ptr_inp_shape[ptr_permute_vec[i]];
+
+        if (i != d)
+          outer_size = outer_size * ptr_inp_shape[i];
+      }
+
+      outer_stride = size;
+
+      float* ptr_out =
+          (float*)kernels::allocate_temp_memory(ctx, out.numel() * sizeof(float));
+
+      ET_KERNEL_CHECK(ctx, ptr_out != nullptr, MemoryAllocationFailed, out);
+
+      float* ptr_out1 =
+          (float*)kernels::allocate_temp_memory(ctx, out.numel() * sizeof(float));
+
+      ET_KERNEL_CHECK(ctx, ptr_out1 != nullptr, MemoryAllocationFailed, out);
+
+      tensor_transposef(
+        ptr_out,
+        ptr_out_shape,
+        ptr_inp,
+        ptr_inp_shape,
+        ptr_permute_vec,
+        num_out_dims,
+        num_inp_dims);
+
+      for (size_t outer_idx = 0; outer_idx < outer_size; ++outer_idx) {
+        size_t outer = outer_idx * outer_stride;
+        for (size_t inner_idx = 0; inner_idx < stride; ++inner_idx) {
+          size_t base = outer + inner_idx;
+        
+          float *ptr_in_data = &ptr_out[base];
+          float *ptr_out_data = &ptr_out1[base];
+
+          vsoftmaxf(ptr_out_data, ptr_in_data, size);
+        }
+      }
+
+      tensor_transposef(
+        out_data,
+        ptr_inp_shape,
+        ptr_out1,
+        ptr_out_shape,
+        ptr_permute_vec,
+        num_out_dims,
+        num_inp_dims);
+
+      return out;
     }
-
-    tensor_transposef(
-      out_data,
-      ptr_inp_shape,
-      ptr_out1,
-      ptr_out_shape,
-      ptr_permute_vec,
-      num_out_dims,
-      num_inp_dims);
-
-    return out;
   }
 
   ET_SWITCH_FLOATHBF16_TYPES(
