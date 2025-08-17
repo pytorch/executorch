6d626f21c3a2a1f572d3a686df52b6acd517ae03
Gregory Comer <gregoryjcomer@gmail.com>
2025-08-16 17:48:08 -0700

    Update
    [ghstack-poisoned]

--- .ci/scripts/test_backend_linux.sh
+++ .ci/scripts/test_backend_linux.sh
@@ -18,11 +18,24 @@ eval "$(conda shell.bash hook)"
 CONDA_ENV=$(conda env list --json | jq -r ".envs | .[-1]")
 conda activate "${CONDA_ENV}"
 
-# Setup swiftshader and Vulkan SDK which are required to build the Vulkan delegate
-source .ci/scripts/setup-vulkan-linux-deps.sh
-
 # CMake options to use, in addition to the defaults.
-EXTRA_BUILD_ARGS="-DEXECUTORCH_BUILD_VULKAN=ON"
+EXTRA_BUILD_ARGS=""
+
+if [[ "$FLOW" == *"qualcomm"*]]
+    # Setup QNN sdk and deps
+    ./install_requirements.sh --use-pt-pinned-commit
+    PYTHON_EXECUTABLE=python bash .ci/scripts/setup-qnn-deps.sh
+    PYTHON_EXECUTABLE=python bash .ci/scripts/build-qnn-sdk.sh
+
+    EXTRA_BUILD_ARGS+=" -DEXECUTORCH_BUILD_QNN=ON"
+fi
+
+if [[ "$FLOW" == *"vulkan"*]]
+    # Setup swiftshader and Vulkan SDK which are required to build the Vulkan delegate
+    source .ci/scripts/setup-vulkan-linux-deps.sh
+    
+    EXTRA_BUILD_ARGS+=" -DEXECUTORCH_BUILD_VULKAN=ON"
+fi
 
 # We need the runner to test the built library.
 PYTHON_EXECUTABLE=python CMAKE_ARGS="$EXTRA_BUILD_ARGS" .ci/scripts/setup-linux.sh --build-tool cmake --build-mode Release
