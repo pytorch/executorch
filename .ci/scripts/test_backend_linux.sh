42a664ed876e18d86161dcf8a52a6027ff2de9b9
Gregory Comer <gregoryjcomer@gmail.com>
2025-08-18 01:54:34 -0700

    Update
    [ghstack-poisoned]

--- .ci/scripts/test_backend_linux.sh
+++ .ci/scripts/test_backend_linux.sh
@@ -31,10 +31,11 @@ if [[ "$FLOW" == *qualcomm* ]]; then
     PYTHON_EXECUTABLE=python bash .ci/scripts/setup-qnn-deps.sh
     PYTHON_EXECUTABLE=python bash .ci/scripts/build-qnn-sdk.sh
     QNN_X86_LIB_DIR=`realpath build-x86/lib/`
-    export LD_LIBRARY_PATH=$QNN_X86_LIB_DIR:${LD_LIBRARY_PATH:-}
+    QNN_SDK_ROOT="/tmp/qnn/2.28.0.241029"
+    export LD_LIBRARY_PATH"=$QNN_X86_LIB_DIR:$QNN_SDK_ROOT/lib/x86_64-linux-clang/:${LD_LIBRARY_PATH:-}"
 
     # TODO Get SDK root from install scripts
-    EXTRA_BUILD_ARGS+=" -DEXECUTORCH_BUILD_QNN=ON -DQNN_SDK_ROOT=/tmp/qnn/2.28.0.241029"
+    EXTRA_BUILD_ARGS+=" -DEXECUTORCH_BUILD_QNN=ON -DQNN_SDK_ROOT=$QNN_SDK_ROOT"
 fi
 
 if [[ "$FLOW" =~ *vulkan* ]]; then
