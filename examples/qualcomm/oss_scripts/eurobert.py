c14d64e65d41d2f111e9d938b340f7022cd6d799
chenweng-quic <168707118+chenweng-quic@users.noreply.github.com>
2025-08-20 07:02:02 +0800

    Qualcomm AI Engine Direct - Remove input_list dependencies (#13411)
    ### Summary
    - Current SimpleADB push logic require each script to prepare
    input_list, and the name must follow the logic. Remove all these
    dependencies.
    
    ### Test plan
    All existing unit tests should already cover this change.
    
    Co-authored-by: Cheng-Hsin Weng <chenweng@qti.qualcomm.com>

--- examples/qualcomm/oss_scripts/eurobert.py
+++ examples/qualcomm/oss_scripts/eurobert.py
@@ -88,7 +88,7 @@ def main(args):
             "This option is for CI to verify the export flow. It uses random input and will result in poor accuracy."
         )
     else:
-        inputs, targets, input_list = get_masked_language_model_dataset(
+        inputs, targets = get_masked_language_model_dataset(
             args.dataset, tokenizer, data_size
         )
 
@@ -130,7 +130,7 @@ def main(args):
     make_output_dir(output_data_folder)
 
     # accuracy analysis
-    adb.push(inputs=inputs, input_list=input_list)
+    adb.push(inputs=inputs)
     adb.execute()
     adb.pull(output_path=args.artifact)
     goldens, predictions = [], []
