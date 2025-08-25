4e316d44e20f8128ec88839e185c1cea32352c13
roman-janik-nxp <roman.janik@nxp.com>
2025-08-25 20:16:36 +0200

    NXP backend: Add support for conversion of Conv1D operator (#13549)
    ### Summary
    Add delegation of `aten.conv1d` to Neutron. Fixes
    `input_shapes` type hint in `to_quantized_edge_program()`. Fixes
    `operators_not_to_delegate` assignment in partitioner.
    
    ### Test plan
    Unit tests provided in
    backends/nxp/tests/ir/converter/node_converter/test_conv_converter.py.
    
    
    cc @digantdesai @JakeStevens @robert-kalmar

--- backends/nxp/tests/executorch_pipeline.py
+++ backends/nxp/tests/executorch_pipeline.py
@@ -104,7 +104,7 @@ def to_quantized_edge_program(
 
 
 def to_quantized_executorch_program(
-    model: torch.nn.Module, input_shapes: tuple[int] | list[tuple[int]]
+    model: torch.nn.Module, input_shapes: tuple[int, ...] | list[tuple[int, ...]]
 ) -> ExecutorchProgramManager:
     edge_program_manager = to_quantized_edge_program(model, input_shapes)
 
@@ -114,7 +114,7 @@ def to_quantized_executorch_program(
 
 
 def to_edge_program(
-    model: nn.Module, input_shapes: tuple[int] | list[tuple[int]]
+    model: nn.Module, input_shapes: tuple[int, ...] | list[tuple[int, ...]]
 ) -> EdgeProgramManager:
     if isinstance(input_shapes, list):
         assert all(isinstance(input_shape, tuple) for input_shape in input_shapes), (
