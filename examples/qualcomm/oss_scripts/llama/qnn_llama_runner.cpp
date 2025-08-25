f154d50d883dee1102205ce51b2bc0c5effa73cc
winskuo-quic <143469905+winskuo-quic@users.noreply.github.com>
2025-08-26 06:38:29 +0800

    Qualcomm AI Engine Direct - Scripts and accuracy improvement for Qwen3_0.6B/1.7B and Qwen 2.5_1.5B (#13544)
    ### Summary
    - Adding static Qwen 2.5 - 1.5B to script.
    - Adding static Qwen 3 0.6B/1.5B to script
    - Adding back `skip_advanced_requant`.
    - Adding prompt + special token for calibration, which helps certain
    models to improve accuracy.
    
    #### Example Scripts:
    `python examples/qualcomm/oss_scripts/llama/llama.py -b build-android -H
    haowhsu-linux -s 5f396958 -m SM8750 --prompt "How many r's in
    strawberries?" --temperature 0 --model_mode kv --max_seq_len 1024 --ptq
    16a8w --decoder_model qwen3-0_6b --tasks wikitext --limit 1 --artifact
    ./qwen3-0_6b`
    
    #### Statistics on SM8750, seq_len=1024
    qwen2 1.5B: ~34tok/sec. QNN on device PPL=9.4 (CPU FP=9.1)
    qwen3 0.6B: ~56tok/sec. QNN on device PPL=16.8 (CPU FP=16.26)
    qwen3 1.7B: ~14tok/sec. QNN on device PPL=14.1 (CPU FP=13.52)
    
    
    ### Test plan
    E2E in test_qnn_delegate.py

--- examples/qualcomm/oss_scripts/llama/qnn_llama_runner.cpp
+++ examples/qualcomm/oss_scripts/llama/qnn_llama_runner.cpp
@@ -9,8 +9,8 @@
 /**
  * @file
  *
- * This tool can run Llama2 110M, Llama3.2 1B / 3B, Qwen2.5 0.5B, Qwen3 0.6B
- * / 1.7B, phi4-mini-instruct, Smollm2 135M with Qualcomm AI Engine Direct.
+ * This tool can run Llama2 110M, Llama3.2 1B / 3B, Qwen2.5 0.5B / 1.5B, Qwen3
+ * 0.6B / 1.7B, phi4-mini-instruct, Smollm2 135M with Qualcomm AI Engine Direct.
  *
  */
 
@@ -104,6 +104,17 @@ std::string get_formatted_prompt(
     case example::DecoderModelVersion::kLlama2:
     case example::DecoderModelVersion::kQwen2_5:
       formatted_prompt.append(prompt);
+      break;
+    case example::DecoderModelVersion::kQwen3:
+      formatted_prompt.append("<|im_start|>user\n");
+      formatted_prompt.append(prompt);
+      formatted_prompt.append("<|im_end|>\n");
+      if (!system_prompt.empty()) {
+        formatted_prompt.append("<|im_start|>system\n");
+        formatted_prompt.append(system_prompt);
+        formatted_prompt.append("<|im_end|>\n");
+      }
+      formatted_prompt.append("<|im_start|>assistant");
       break;
     case example::DecoderModelVersion::kPhi4:
       if (!system_prompt.empty()) {
