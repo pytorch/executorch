1b9b951ec49389b93b02e9fcea9ef9217211d1fd
Gregory James Comer <gjcomer@meta.com>
2025-08-28 14:49:53 -0700

    Update
    [ghstack-poisoned]

--- .ci/scripts/unittest-windows.ps1
+++ .ci/scripts/unittest-windows.ps1
@@ -5,16 +5,16 @@ $PSNativeCommandUseErrorActionPreference = $true
 conda create --yes --quiet -n et python=3.12
 conda activate et
 
-ls -Path "C:\Program Files"
-ls -Path "C:\Program Files (x86)"
-ls -Path "C:\Program Files\Microsoft Visual Studio\2022\"
-ls -Path "C:\Program Files (x86)\Microsoft Visual Studio\2022\"
+#ls -Path "C:\Program Files"
+#ls -Path "C:\Program Files (x86)"
+#ls -Path "C:\Program Files\Microsoft Visual Studio\2022\"
+#ls -Path "C:\Program Files (x86)\Microsoft Visual Studio\2022\"
 
 # Activate the VS environment - this is required for Dynamo to work, as
 # it uses the MSVC compiler.
-$vsInstallPath = "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\"
-Import-Module (Join-Path $vsInstallPath "Common7\Tools\vsdevshell\Microsoft.VisualStudio.DevShell.dll")
-Enter-VsDevShell -VsInstallPath $vsInstallPath -DevCmdArguments "-arch=x64" -SkipAutomaticLocation
+#$vsInstallPath = "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\"
+#Import-Module (Join-Path $vsInstallPath "Common7\Tools\vsdevshell\Microsoft.VisualStudio.DevShell.dll")
+#Enter-VsDevShell -VsInstallPath $vsInstallPath -DevCmdArguments "-arch=x64" -SkipAutomaticLocation
 
 install_executorch.bat
 if ($LASTEXITCODE -ne 0) {
