3a64498740816d46f8de71989f5b71e69fad1918
Gregory James Comer <gjcomer@meta.com>
2025-08-28 14:41:36 -0700

    Update
    [ghstack-poisoned]

--- .ci/scripts/unittest-windows.ps1
+++ .ci/scripts/unittest-windows.ps1
@@ -5,6 +5,11 @@ $PSNativeCommandUseErrorActionPreference = $true
 conda create --yes --quiet -n et python=3.12
 conda activate et
 
+ls -Path "C:\Program Files"
+ls -Path "C:\Program Files (x86)"
+ls -Path "C:\Program Files\Microsoft Visual Studio\2022\"
+ls -Path "C:\Program Files (x86)\Microsoft Visual Studio\2022\"
+
 # Activate the VS environment - this is required for Dynamo to work, as
 # it uses the MSVC compiler.
 $vsInstallPath = "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\"
