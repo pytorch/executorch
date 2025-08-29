c76e310d77a13c17d2327986e8a63c6391dbdb43
Gregory Comer <gregoryjcomer@gmail.com>
2025-08-28 21:18:27 -0700

    Update
    [ghstack-poisoned]

--- .ci/scripts/unittest-windows.ps1
+++ .ci/scripts/unittest-windows.ps1
@@ -1,5 +1,5 @@
 param (
-    [bool]$editable
+    [string]$editable
 )
 
 Set-PSDebug -Trace 1
@@ -14,7 +14,7 @@ conda activate et
 # See https://learn.microsoft.com/en-us/cpp/build/building-on-the-command-line.
 & "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\Launch-VsDevShell.ps1" -Arch amd64
 
-install_executorch.bat --editable:$editable
+install_executorch.bat --editable:$($editable -eq 'true')
 if ($LASTEXITCODE -ne 0) {
     Write-Host "Installation was unsuccessful. Exit code: $LASTEXITCODE."
     exit $LASTEXITCODE
