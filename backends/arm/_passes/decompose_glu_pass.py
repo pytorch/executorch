6073c86fd12da71e4af85093226e85f35d555d41
Martin Lindström <Martin.Lindstroem@arm.com>
2025-09-03 13:08:48 +0200

    Arm backend: Add chronological dependencies for passes
    The passes in the Arm backend have an attribute called
    `_passes_required_after` which is a set specifying which passes must run
    after the pass itself. This patch sets these dependencies for all the
    passes.
    
    Signed-off-by: Martin Lindstroem <Martin.Lindstroem@arm.com>
    Change-Id: Iae7c51e6b138a1a44495cee311f72920cc7a1c7e

--- backends/arm/_passes/decompose_glu_pass.py
+++ backends/arm/_passes/decompose_glu_pass.py
@@ -7,6 +7,7 @@ from typing import Set, Type
 
 import torch
 from executorch.backends.arm._passes import ArmPass
+from executorch.backends.arm._passes.insert_table_ops import InsertTableOpsPass
 from executorch.exir.dialects._ops import ops as exir_ops
 from executorch.exir.pass_base import ExportPass
 
@@ -39,7 +40,7 @@ def get_ops(op):
 class DecomposeGluPass(ArmPass):
     """Decomposes the GLU operator into hadamard product and sigmoid."""
 
-    _passes_required_after: Set[Type[ExportPass]] = set()
+    _passes_required_after: Set[Type[ExportPass]] = {InsertTableOpsPass}
 
     def call_operator(self, op, args, kwargs, meta):
         if op not in [edge_glu, aten_glu]:
