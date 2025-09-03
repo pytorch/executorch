33c8f76de325bbb7d13bba69170917aaf1107d34
Hansong Zhang <107070759+kirklandsign@users.noreply.github.com>
2025-09-03 11:04:34 -0700

    Add qnn deps to LlamaDemo gradle (#13912)
    Now for QNN users, they can use the dependency `implementation
    "com.qualcomm.qti:qnn-runtime:$qnnVersion"` to add the runtime deps
    
    Define `qnnVersion` in gradle.properties

--- examples/demo-apps/android/LlamaDemo/app/build.gradle.kts
+++ examples/demo-apps/android/LlamaDemo/app/build.gradle.kts
@@ -11,6 +11,8 @@ plugins {
   id("org.jetbrains.kotlin.android")
 }
 
+val qnnVersion: String? = project.findProperty("qnnVersion") as? String
+
 android {
   namespace = "com.example.executorchllamademo"
   compileSdk = 34
@@ -61,6 +63,9 @@ dependencies {
   implementation("com.google.android.material:material:1.12.0")
   implementation("androidx.activity:activity:1.9.0")
   implementation("org.json:json:20250107")
+  if (!qnnVersion.isNullOrEmpty()) {
+    implementation("com.qualcomm.qti:qnn-runtime:$qnnVersion")
+  }
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
