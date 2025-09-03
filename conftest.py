b41ddee759cb052ec304abad158a28697daeaa08
Gregory James Comer <gjcomer@meta.com>
2025-09-02 18:16:06 -0700

    Update
    [ghstack-poisoned]

--- conftest.py
+++ conftest.py
@@ -1,5 +1,7 @@
 import sys
 
+collect_ignore_glob = []
+
 # Skip Apple tests on Windows. Note that some Core ML tests can run on Linux, as the AOT flow
 # is available. Tests will manage this internally. However, the coremltools import is not available
 # on Windows and causes collection to fail. The easiest way to manage this seems to be to just
