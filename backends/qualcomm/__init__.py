4a08f84caf136aee50c4f1cf9259674c050fb231
Chen Lai <chenlai@fb.com>
2025-09-03 16:17:38 -0700

    test

--- backends/qualcomm/__init__.py
+++ backends/qualcomm/__init__.py
@@ -3,3 +3,8 @@ import os
 import pathlib
 
 from .scripts.download_qnn_sdk import install_qnn_sdk
+
+print("Running executorch/backends/qualcomm/__init__.py")
+if False:
+    print("shouldn't be here    ")
+    install_qnn_sdk()
