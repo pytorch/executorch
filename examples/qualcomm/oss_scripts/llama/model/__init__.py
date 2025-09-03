cf62b4867a0e7cfd2979517b2526b113c2e83db6
thchenqti <thchen@qti.qualcomm.com>
2025-09-03 16:51:52 +0800

    Qualcomm AI Engine Direct - codegen2-1B

---
+++ examples/qualcomm/oss_scripts/llama/model/__init__.py
@@ -0,0 +1,16 @@
+# Copyright (c) Qualcomm Innovation Center, Inc.
+# All rights reserved
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from .apply_rope import ROTARY_EMB_REGISTRY
+from .feed_forward import FeedForward_REGISTRY
+from .layernorm import NORM_REGISTRY
+
+
+__all__ = [
+    FeedForward_REGISTRY,
+    ROTARY_EMB_REGISTRY,
+    NORM_REGISTRY,
+]
