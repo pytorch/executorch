8140a4392cba88fe1da2432d7c9cee81e632569d
Hansong Zhang <hsz@meta.com>
2025-09-03 10:40:53 -0700

    Fix gradle

--- examples/demo-apps/android/LlamaDemo/app/build.gradle.kts
+++ examples/demo-apps/android/LlamaDemo/app/build.gradle.kts
@@ -11,7 +11,8 @@ plugins {
   id("org.jetbrains.kotlin.android")
 }
 
-def qnnVersion = System.properties['qnnVersion']
+val qnnVersion: String? = project.findProperty("qnnVersion") as? String
+
 
 android {
   namespace = "com.example.executorchllamademo"
@@ -63,8 +64,8 @@ dependencies {
   implementation("com.google.android.material:material:1.12.0")
   implementation("androidx.activity:activity:1.9.0")
   implementation("org.json:json:20250107")
-  if (qnnVersion) {
-    implementation "com.qualcomm.qti:qnn-runtime:$qnnVersion"
+  if (!qnnVersion.isNullOrEmpty()) {
+    implementation("com.qualcomm.qti:qnn-runtime:$qnnVersion")
   }
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
