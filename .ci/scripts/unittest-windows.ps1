6bccb4eb4400ae6ac08520bbdedaf8b9c12530c4
Gregory James Comer <gjcomer@meta.com>
2025-09-03 17:42:29 -0700

    [Windows] Run native unit tests in CI
    ghstack-source-id: 1dca42f99e11914cb3ab537c53e0d276aeb9bc24
    ghstack-comment-id: 3251228740
    Pull-Request: https://github.com/pytorch/executorch/pull/13925

--- .ci/scripts/unittest-windows.ps1
+++ .ci/scripts/unittest-windows.ps1
@@ -6,10 +6,31 @@ Set-PSDebug -Trace 1
 $ErrorActionPreference = 'Stop'
 $PSNativeCommandUseErrorActionPreference = $true
 
-# Run pytest with coverage
-# pytest -n auto --cov=./ --cov-report=xml
-pytest -v --full-trace -c pytest-windows.ini
+# Run pytest
+pytest -v pytest-windows.ini
 if ($LASTEXITCODE -ne 0) {
     Write-Host "Pytest invocation was unsuccessful. Exit code: $LASTEXITCODE."
+    exit $LASTEXITCODE
+}
+
+# Run native unit tests (via ctest)
+New-Item -Path "test-build" -ItemType Directory
+cd "test-build"
+
+cmake .. --preset Windows -DCMAKE_BUILD_TESTS=ON
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "CMake configuration was unsuccessful. Exit code: $LASTEXITCODE."
+    exit $LASTEXITCODE
+}
+
+cmake --build . -j8 --config Release
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "CMake build was unsuccessful. Exit code: $LASTEXITCODE."
+    exit $LASTEXITCODE
+}
+
+ctest -j8
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "CTest run was unsuccessful. Exit code: $LASTEXITCODE."
     exit $LASTEXITCODE
 }
