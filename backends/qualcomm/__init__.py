87eeb65b5d80ecd535c6fd13d43d519fea35d747
Chen Lai <chenlai@fb.com>
2025-09-03 21:11:50 -0700

    add logs

--- backends/qualcomm/__init__.py
+++ backends/qualcomm/__init__.py
@@ -1,16 +1,16 @@
-import ctypes
 import os
-import pathlib
 
 from .scripts.download_qnn_sdk import install_qnn_sdk
 
 print("Running executorch/backends/qualcomm/__init__.py")
 
 env_flag = os.getenv("EXECUTORCH_INSTALL_QNN_SDK", "0").lower()
+print(f"[QCOM init] EXECUTORCH_INSTALL_QNN_SDK = {env_flag!r}")
 
 if env_flag in ("1", "true", "yes"):
-    print("[QCOM init] EXECUTORCH_INSTALL_QNN_SDK is set -> installing Qualcomm SDK...")
+    print(
+        "[QCOM init] EXECUTORCH_INSTALL_QNN_SDK is set -> skipping Qualcomm SDK install."
+    )
 else:
+    print("[QCOM init] SDK install flag not set -> installing Qualcomm SDK...")
     install_qnn_sdk()
-    print(f"[QCOM init] Skipping SDK install (EXECUTORCH_INSTALL_QNN_SDK={env_flag!r})")
-# install_qnn_sdk()
