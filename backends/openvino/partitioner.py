5b8b633a94ca13b672db873e07725363c2e2014c
Cavus Mustafa <mustafa.cavus@intel.com>
2025-09-04 17:18:03 -0700

    formatting fix

--- backends/openvino/partitioner.py
+++ backends/openvino/partitioner.py
@@ -27,7 +27,7 @@ from torch.fx.passes.operator_support import OperatorSupportBase
 
 
 class PatternNode:
-    op_types = {}
+    op_types: dict[str, list] = {}
 
     def __init__(self):
         self.op_types = {}
