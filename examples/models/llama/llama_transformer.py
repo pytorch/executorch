8607c895ac05b88259f3ca6f2c807d215325ab19
Tarek Dakhran <t.dakhran@gmail.com>
2025-09-05 16:35:46 +0200

    model : support LiquidAI LFM2 hybrid family (#13805)
    ### Summary
    
    Add support for [LiquidAI
    LFM2](https://huggingface.co/collections/LiquidAI/lfm2-686d721927015b2ad73eaa38)
    model family. For more information about models, please read [the blog
    post](https://www.liquid.ai/blog/liquid-foundation-models-v2-our-second-series-of-generative-ai-models).
    
    - Support hybrid LFM2-350M, LFM2-700M, and LFM2-1.2B models.
    - Add `ShortConvBlock`.
    - Modify `construct_transformer` to construct hybrid architectures.
    - Move FeedForward to avoid cyclid dependency
    
    Instructions are in `examples/models/lfm2/README.md`.
    
    ### Test plan
    All commands in `README.md` are tests.
    ```
    ❯ python -m examples.models.llama.runner.native \
      --model lfm2_700m \
      --pte lfm2_700m_8da4w.pte \
      --tokenizer ~/.cache/huggingface/hub/models--LiquidAI--LFM2-700M/snapshots/ab260293733f05dd4ce22399bea1cae2cf9b272d/tokenizer.json \
      --tokenizer_config ~/.cache/huggingface/hub/models--LiquidAI--LFM2-700M/snapshots/ab260293733f05dd4ce22399bea1cae2cf9b272d/tokenizer_config.json \
      --prompt "<|startoftext|><|im_start|>user\nWho are you?<|im_end|>\n<|im_start|>assistant\n" \
      --params examples/models/lfm2/config/lfm2_700m_config.json \
      --max_len 128 \
      -kv \
      --temperature 0.3
    
    ...
    I'm an AI designed to assist with generating text based on the prompts you provide. I'm a type of language model, but I don't have a physical form or consciousness. I operate based on complex algorithms and vast amounts of training data. How can I help you today? If you have a specific question or need assistance with something, feel free to ask!
    ...
    ```
    
    ---------
    
    Co-authored-by: Jack <32371937+jackzhxng@users.noreply.github.com>

--- examples/models/llama/llama_transformer.py
+++ examples/models/llama/llama_transformer.py
@@ -12,29 +12,17 @@ from typing import Any, Optional, Tuple, Union
 import torch
 import torch.nn.functional as F
 
+from executorch.examples.models.lfm2.short_conv import ShortConvBlock
 from executorch.examples.models.llama.attention import (
     Attention,
     ATTENTION_REGISTRY,
     ForwardOptions,
 )
-
+from executorch.examples.models.llama.feed_forward import FeedForward
 from executorch.examples.models.llama.model_args import ModelArgs
 from executorch.examples.models.llama.norm import RMSNorm
 from executorch.examples.models.llama.rope import Rope
 from torch import nn
-
-
-class FeedForward(nn.Module):
-    def __init__(self, args: ModelArgs):
-        super().__init__()
-        assert args.hidden_dim is not None
-        hidden_dim: int = args.hidden_dim
-        self.w1 = nn.Linear(args.dim, hidden_dim, bias=False)
-        self.w2 = nn.Linear(hidden_dim, args.dim, bias=False)
-        self.w3 = nn.Linear(args.dim, hidden_dim, bias=False)
-
-    def forward(self, x):
-        return self.w2(F.silu(self.w1(x)) * self.w3(x))
 
 
 class ConditionalFeedForward(nn.Module):
@@ -102,7 +90,7 @@ class TransformerBlock(nn.Module):
         if args.moe:
             self.block_sparse_moe = MOEFeedForward(args)
         else:
-            self.feed_forward = FeedForward(args)
+            self.feed_forward = FeedForward(dim=args.dim, hidden_dim=args.hidden_dim)
         self.attention_norm = RMSNorm(args.dim, eps=args.norm_eps)
         self.ffn_norm = RMSNorm(args.dim, eps=args.norm_eps)
 
@@ -255,8 +243,18 @@ def construct_transformer(model_args: ModelArgs) -> Transformer:
     layers = torch.nn.ModuleList()
     cls = ATTENTION_REGISTRY[model_args.attention_type]
     for layer_id in range(model_args.n_layers):
-        attention = cls(model_args, layer_id, rope, **model_args.attention_kwargs)
-        transformer_block = TransformerBlock(model_args, attention)
-        layers.append(transformer_block)
+        # hybrid models define layer_types
+        if model_args.layer_types and model_args.layer_types[layer_id] == "conv":
+            layers.append(
+                ShortConvBlock(
+                    dim=model_args.dim,
+                    hidden_dim=model_args.hidden_dim,
+                    norm_eps=model_args.norm_eps,
+                )
+            )
+        else:
+            attention = cls(model_args, layer_id, rope, **model_args.attention_kwargs)
+            transformer_block = TransformerBlock(model_args, attention)
+            layers.append(transformer_block)
 
     return Transformer(model_args, layers, rope)
