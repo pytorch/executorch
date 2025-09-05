7f5a06b0e36827a25209bc259e6a45a343a37886
jiseong.oh <jiseong.oh@samsung.com>
2025-09-05 03:16:03 +0000

    Fixed CI issue which Download NDK before compile Exynos backend
    Signed-off-by: jiseong.oh <jiseong.oh@samsung.com>

--- .ci/scripts/setup-samsung-linux-deps.sh
+++ .ci/scripts/setup-samsung-linux-deps.sh
@@ -30,6 +30,19 @@ download_ai_lite_core() {
 }
 
 install_enn_backend() {
+  NDK_INSTALLATION_DIR=/opt/ndk
+  rm -rf "${NDK_INSTALLATION_DIR}" && mkdir -p "${NDK_INSTALLATION_DIR}"
+  ANDROID_NDK_VERSION=r27b
+
+  pushd .
+  cd /tmp
+  curl -Os --retry 3 "https://ossci-android.s3.amazonaws.com/android-ndk-${ANDROID_NDK_VERSION}-linux.zip"
+  unzip -qo "android-ndk-${ANDROID_NDK_VERSION}-linux.zip"
+
+  # Print the content for manual verification
+  ls -lah "android-ndk-${ANDROID_NDK_VERSION}"
+  mv "android-ndk-${ANDROID_NDK_VERSION}"/* "${NDK_INSTALLATION_DIR}"
+  popd
   # build Exynos backend
   export ANDROID_NDK_ROOT=${ANDROID_NDK_ROOT:-/opt/ndk}
   bash backends/samsung/build.sh --build all
