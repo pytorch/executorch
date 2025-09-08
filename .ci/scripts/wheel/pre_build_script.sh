03472bd525c72f1d4982729040cb8d94741d9acb
Mergen Nachin <mnachin@meta.com>
2025-09-08 12:18:47 -0400

    Remove duplicate libatomic installation check
    Removed duplicate installation of libatomic for aarch64.

--- .ci/scripts/wheel/pre_build_script.sh
+++ .ci/scripts/wheel/pre_build_script.sh
@@ -9,14 +9,14 @@ set -euxo pipefail
 
 # This script is run before building ExecuTorch binaries
 
+# On aarch64, we need to install libatomic explicitly for meta-pytorch/tokenizers to work.
+if [[ "$(uname -m)" == "aarch64" ]]; then
+    sudo dnf install -y libatomic
+fi
+
 # Manually install build requirements because `python setup.py bdist_wheel` does
 # not install them. TODO(dbort): Switch to using `python -m build --wheel`,
 # which does install them. Though we'd need to disable build isolation to be
 # able to see the installed torch package.
 
 "${GITHUB_WORKSPACE}/${REPOSITORY}/install_requirements.sh"  --example
-
-# On aarch64, we need to install libatomic explicitly for meta-pytorch/tokenizers to work.
-if [[ "$(uname -m)" == "aarch64" ]]; then
-    sudo dnf install -y libatomic
-fi
