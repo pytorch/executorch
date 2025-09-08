0c7c5d358b2b8a1450d025f5dbc68c408b4e8336
Gregory James Comer <gjcomer@meta.com>
2025-09-08 15:22:42 -0700

    Update
    [ghstack-poisoned]

--- .ci/scripts/wheel/test_windows.py
+++ .ci/scripts/wheel/test_windows.py
@@ -5,13 +5,43 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+import os
+import subprocess
 import test_base
 from examples.models import Backend, Model
+from test_base import ModelTest
+from typing import List
+
+def map_backend_name(name: str) -> str:
+    # Map the backend name to the string used by the Windows test jobs, which use
+    # a slightly different convention. This is an artifact of us being mid-update
+    # of the model test logic.
+    # TODO(gjcomer) Clean this up when we update the model test CI.
+
+    if name == "xnnpack-quantization-delegation":
+        return "xnnpack-q8"
+    else:
+        return name
+
+def run_tests(model_tests: List[ModelTest]) -> None:
+    for model_test in model_tests:
+        subprocess.run(
+            [
+                os.path.join(test_base._repository_root_dir(), ".ci/scripts/test_model.ps1"),
+                "-ModelName",
+                str(model_test.model),
+                "-Backend",
+                map_backend_name(str(model_test.backend)),
+            ],
+            check=True,
+            cwd=test_base._repository_root_dir(),
+        )
+
 
 if __name__ == "__main__":
     test_base.run_tests(
         model_tests=[
-            test_base.ModelTest(
+            ModelTest(
                 model=Model.Mv3,
                 backend=Backend.XnnpackQuantizationDelegation,
             ),
