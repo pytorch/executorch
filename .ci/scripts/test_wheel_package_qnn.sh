5745bc3212fdc2a6729cab78fe1293b4693fa7f2
Chen Lai <chenlai@fb.com>
2025-09-08 19:13:44 -0700

    force install torch cpu

--- .ci/scripts/test_wheel_package_qnn.sh
+++ .ci/scripts/test_wheel_package_qnn.sh
@@ -90,6 +90,8 @@ echo "=== Building Wheel Package ==="
 # pip install torch=="2.9.0.dev20250801" --index-url "https://download.pytorch.org/whl/nightly/cpu"
 source .ci/scripts/utils.sh
 install_executorch "--use-pt-pinned-commit"
+pip uninstall -y torch
+pip install torch=="2.9.0.dev20250801" --index-url "https://download.pytorch.org/whl/nightly/cpu"
 EXECUTORCH_BUILDING_WHEEL=1 python setup.py bdist_wheel
 unset EXECUTORCH_BUILDING_WHEEL
 
