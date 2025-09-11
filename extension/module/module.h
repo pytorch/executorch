03f43217a591637aa035acc8fd98a2a13e422f62
pytorchbot <soumith+bot@pytorch.org>
2025-09-11 01:50:49 -0400

    Deprecate debug buffer in Module (#14199)
    This PR was created by the merge bot to help merge the original PR into
    the main branch.
    ghstack PR number: https://github.com/pytorch/executorch/pull/14161 by
    @lucylq
    ^ Please use this as the source of truth for the PR details, comments,
    and reviews
    ghstack PR base:
    https://github.com/pytorch/executorch/tree/gh/lucylq/112/base
    ghstack PR head:
    https://github.com/pytorch/executorch/tree/gh/lucylq/112/head
    Merge bot PR base: https://github.com/pytorch/executorch/tree/main
    Merge bot PR head:
    https://github.com/pytorch/executorch/tree/gh/lucylq/112/orig
    @diff-train-skip-merge
    
    Co-authored-by: lucylq <lfq@meta.com>

--- extension/module/module.h
+++ extension/module/module.h
@@ -598,8 +598,9 @@ class Module {
     return event_tracer_.get();
   }
 
-  ET_NODISCARD
-  runtime::Span<uint8_t> debug_buffer() {
+  // Note: this debug_buffer will always be empty. The one being used is in
+  // the event_tracer attached to module. Please use that one.
+  ET_DEPRECATED ET_NODISCARD runtime::Span<uint8_t> debug_buffer() {
     return runtime::Span<uint8_t>(debug_buffer_.data(), debug_buffer_.size());
   }
 
@@ -622,7 +623,7 @@ class Module {
   std::unique_ptr<runtime::EventTracer> event_tracer_;
   std::unique_ptr<runtime::DataLoader> data_map_loader_;
   std::unique_ptr<NamedDataMap> data_map_;
-  std::vector<uint8_t> debug_buffer_;
+  ET_DEPRECATED std::vector<uint8_t> debug_buffer_;
 
  protected:
   std::unordered_map<std::string, MethodHolder> methods_;
