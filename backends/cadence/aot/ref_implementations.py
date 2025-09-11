27cc35397d137bd3e066d0263a47ffc0579bf198
agrebenisan <agrebenisan@meta.com>
2025-09-11 09:55:54 -0700

    All variants of quantized relu
    Summary: Create a generic quantized relu and decorators for all custom quantized relu ops.
    
    Differential Revision: D81948125

--- backends/cadence/aot/ref_implementations.py
+++ backends/cadence/aot/ref_implementations.py
@@ -748,13 +748,12 @@ def quantized_conv_nhwc_depthwise_asym8sxsym8s_asym8s_per_tensor() -> torch.Tens
 def quantized_conv_nhwc_depthwise_asym8uxsym8u_asym8u_per_tensor() -> torch.Tensor: ...
 
 
-@impl(m, "quantized_relu")
-def quantized_relu(
+def quantized_relu_common(
     X: torch.Tensor,
-    X_zero_point: torch.Tensor,
+    X_zero_point: torch.Tensor | int,
     out_zero_point: int,
-    out_multiplier: torch.Tensor,
-    out_shift: torch.Tensor,
+    out_multiplier: int,
+    out_shift: int,
 ) -> torch.Tensor:
     """
     Quantized ReLU operation followed by requantization.
@@ -770,7 +769,7 @@ def quantized_relu(
     if X.dtype not in supported_dtypes:
         raise ValueError(f"X dtype must be one of {supported_dtypes}. Got {X.dtype}")
 
-    out_scale = -out_multiplier * (1 / (1 << 31)) * (2 ** out_shift[0])
+    out_scale = -out_multiplier * (1 / (1 << 31)) * (2**out_shift)
     dequantized_X = torch.where(X > X_zero_point, X - X_zero_point, torch.zeros_like(X))
     return quantize_per_tensor(
         dequantized_X,
@@ -780,6 +779,79 @@ def quantized_relu(
         torch.iinfo(X.dtype).max,
         X.dtype,
     )
+
+
+def quantized_relu_variant(
+    per_tensor: bool,
+    dtype: torch.dtype | None = None,
+) -> Callable[[Callable[..., torch.Tensor]], Callable[..., torch.Tensor]]:
+    """Create a quantized relu variant with type checking."""
+
+    def decorator(_: Callable[..., torch.Tensor]) -> Callable[..., torch.Tensor]:
+        def variant(
+            X: torch.Tensor,
+            X_zero_point: torch.Tensor | int,
+            out_zero_point: int,
+            out_multiplier: torch.Tensor | int,
+            out_shift: torch.Tensor | int,
+        ) -> torch.Tensor:
+            if per_tensor:
+                if dtype and X.dtype != dtype:
+                    raise ValueError(f"X dtype must be {dtype}. Got {X.dtype}")
+
+                assert isinstance(out_shift, int)
+                assert isinstance(out_multiplier, int)
+                _out_shift = out_shift
+                _out_multiplier = out_multiplier
+            else:
+                assert isinstance(out_multiplier, torch.Tensor)
+                if out_multiplier.numel() > 1:
+                    raise ValueError("Only scalar out_multiplier is supported")
+
+                assert isinstance(out_shift, torch.Tensor)
+                if out_shift.numel() > 1:
+                    raise ValueError("Only scalar out_shift is supported")
+
+                assert isinstance(X_zero_point, torch.Tensor)
+                if X_zero_point.shape != X.shape:
+                    raise ValueError(
+                        f"X_zero_point shape must be {X.shape}. Got {X_zero_point.shape}"
+                    )
+
+                _out_multiplier = int(out_multiplier.item())
+                _out_shift = int(out_shift.item())
+
+            return quantized_relu_common(
+                X,
+                X_zero_point,
+                out_zero_point,
+                _out_multiplier,
+                _out_shift,
+            )
+
+        return variant
+
+    return decorator
+
+
+@impl(m, "quantized_relu")
+@quantized_relu_variant(False)
+def quantized_relu() -> torch.Tensor: ...
+
+
+@impl(m, "quantized_relu.per_tensor")
+@quantized_relu_variant(True)
+def quantized_relu_per_tensor() -> torch.Tensor: ...
+
+
+@impl(m, "quantized_relu_asym8s_asym8s.per_tensor")
+@quantized_relu_variant(True, torch.int8)
+def quantized_relu_asym8s_asym8s_per_tensor() -> torch.Tensor: ...
+
+
+@impl(m, "quantized_relu_asym8u_asym8u.per_tensor")
+@quantized_relu_variant(True, torch.uint8)
+def quantized_relu_asym8u_asym8u_per_tensor() -> torch.Tensor: ...
 
 
 @impl(m, "requantize")
