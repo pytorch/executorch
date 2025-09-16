92683a34e84e32d46fbda7ee5013285e1d27f1e1
Gregory James Comer <gjcomer@meta.com>
2025-09-16 16:49:09 -0700

    [Windows] Reduce Windows unit test CI usage
    ghstack-source-id: c3f80af781b243d7d30441949b98ef802b043426
    ghstack-comment-id: 3300262560
    Pull-Request: https://github.com/pytorch/executorch/pull/14350

--- .ci/scripts/unittest-windows.ps1
+++ .ci/scripts/unittest-windows.ps1
@@ -1,38 +1,44 @@
 param (
-    [string]$buildMode = "Release"
+    [string]$buildMode = "Release",
+    [bool]$runPythonTests = $true,
+    [bool]$runNativeTests = $true
 )
 
 Set-PSDebug -Trace 1
 $ErrorActionPreference = 'Stop'
 $PSNativeCommandUseErrorActionPreference = $true
 
+if ($runNativeTests) {
 # Run native unit tests (via ctest)
-New-Item -Path "test-build" -ItemType Directory
-cd "test-build"
+    New-Item -Path "test-build" -ItemType Directory
+    cd "test-build"
 
-cmake .. --preset windows -B . -DEXECUTORCH_BUILD_TESTS=ON -DCMAKE_BUILD_TYPE=$buildMode
-if ($LASTEXITCODE -ne 0) {
-    Write-Host "CMake configuration was unsuccessful. Exit code: $LASTEXITCODE."
-    exit $LASTEXITCODE
+    cmake .. --preset windows -B . -DEXECUTORCH_BUILD_TESTS=ON -DCMAKE_BUILD_TYPE=$buildMode
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "CMake configuration was unsuccessful. Exit code: $LASTEXITCODE."
+        exit $LASTEXITCODE
+    }
+
+    cmake --build . -j8 --config $buildMode --verbose
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "CMake build was unsuccessful. Exit code: $LASTEXITCODE."
+        exit $LASTEXITCODE
+    }
+
+    ctest -j8 . --build-config $buildMode --output-on-failure -E "method_test|tensor_parser_test"
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "CTest run was unsuccessful. Exit code: $LASTEXITCODE."
+        exit $LASTEXITCODE
+    }
+
+    cd ..
 }
 
-cmake --build . -j8 --config $buildMode --verbose
-if ($LASTEXITCODE -ne 0) {
-    Write-Host "CMake build was unsuccessful. Exit code: $LASTEXITCODE."
-    exit $LASTEXITCODE
-}
-
-ctest -j8 . --build-config $buildMode --output-on-failure -E "method_test|tensor_parser_test"
-if ($LASTEXITCODE -ne 0) {
-    Write-Host "CTest run was unsuccessful. Exit code: $LASTEXITCODE."
-    exit $LASTEXITCODE
-}
-
-cd ..
-
+if ($runPythonTests) {
 # Run pytest
-pytest -v -c pytest-windows.ini
-if ($LASTEXITCODE -ne 0) {
-    Write-Host "Pytest invocation was unsuccessful. Exit code: $LASTEXITCODE."
-    exit $LASTEXITCODE
+    pytest -v -c pytest-windows.ini
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "Pytest invocation was unsuccessful. Exit code: $LASTEXITCODE."
+        exit $LASTEXITCODE
+    }
 }
