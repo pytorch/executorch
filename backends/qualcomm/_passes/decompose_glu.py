501b6248e2d56a2a67dcbcc37242d7bc7ee8b5f5
haowhsu-quic <111341466+haowhsu-quic@users.noreply.github.com>
2025-09-18 05:52:54 +0800

    Qualcomm AI Engine Direct - issue fix #2 (#14378)
    ### Summary
    - #14048 > add quantized test case with GLU decomposition
    - #14049 > add e2e example where constant expansion is applied
    - #14050 > add e2e example and source transform for 6D operation
    - #14051 > add e2e example and complement missed annotation
    - #14052 > add e2e example and dedicated passe for 6D partition
    
    
    Fixes #14048
    Fixes #14049
    Fixes #14050
    Fixes #14051
    Fixes #14052
    
    ### Test plan
    MATRIX = {convnext_small, maxvit_t, swin_v2_t, vit_b_16}
    ```bash
    python backends/qualcomm/tests/test_qnn_delegate.py TestExampleOssScript.test_${MATRIX} -b build-android/ -m SM8750 -s $SN -a /path/to/test_artifacts/ -i /path/to/imagenet_1k/imagenet-mini/val -r .
    ```
    ```bash
    python backends/qualcomm/tests/test_qnn_delegate.py TestQuantizedModel.test_qnn_backend_conformer -b build-android/ -m SM8750 -s $SN -a /path/to/test_artifacts/
    ```

--- backends/qualcomm/_passes/decompose_glu.py
+++ backends/qualcomm/_passes/decompose_glu.py
@@ -5,47 +5,38 @@
 # LICENSE file in the root directory of this source tree.
 
 import torch
-from executorch.exir import to_edge
 from executorch.exir.pass_base import ExportPass, PassResult
 
 from .utils import merge_decomposed_graph
 
 
-class Any(torch.nn.Module):
-    def __init__(self, dim, keepdim):
+# this wrapper is required for IO name mapping with decomposed graph
+class Glu(torch.nn.Module):
+    def __init__(self, dim=-1):
         super().__init__()
-        self.dim = tuple(dim) if isinstance(dim, list) else dim
-        self.keepdim = keepdim
+        self.glu = torch.nn.GLU(dim=dim)
 
     def forward(self, x):
-        if self.dim is None:
-            x = torch.flatten(x)
-            self.dim = 0
-
-        x = x.to(torch.bool).to(torch.int32)
-        x = torch.sum(x, dim=self.dim, keepdim=self.keepdim, dtype=torch.int32)
-        return torch.not_equal(x, torch.zeros(1, dtype=torch.int32))
+        return self.glu(x)
 
 
-class DecomposeAny(ExportPass):
+class DecomposeGlu(ExportPass):
     """
-    Decompose for math equivalent op.
+    Decompose glu for quantization annotation to work properly.
     """
 
-    def __init__(self, quantization_capture=False) -> None:
+    def __init__(self) -> None:
         super().__init__()
 
     def call(self, graph_module: torch.fx.GraphModule) -> PassResult:
         graph = graph_module.graph
         for node in graph.nodes:
-            if "any.dim" in str(node.target):
-                dim = node.args[1] if len(node.args) > 1 else None
-                keepdim = node.args[2] if len(node.args) > 2 else False
-                model = Any(dim, keepdim)
-                edge_mgr = to_edge(
-                    torch.export.export(model, (node.args[0].meta["val"],), strict=True)
+            if node.target == torch.ops.aten.glu.default:
+                ep = torch.export.export(
+                    Glu(dim=-1 if len(node.args) < 2 else node.args[1]),
+                    (node.args[0].meta["val"],),
                 )
-                decomposed_module = edge_mgr.exported_program()
+                decomposed_module = ep.run_decompositions().graph_module
 
                 with graph.inserting_before(node):
                     # remap is used to map original node values to new node values,
