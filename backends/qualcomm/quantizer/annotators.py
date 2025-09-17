58c69ee21a4b7843094f0d00172d19849ceaa74d
haowhsu-quic <111341466+haowhsu-quic@users.noreply.github.com>
2025-09-18 05:52:54 +0800

    Qualcomm AI Engine Direct - issue fix #2 (#14378)
    ### Summary
    - #14048 > add quantized test case with GLU decomposition
    - #14049 > add e2e example where constant expansion is applied
    - #14050 > add e2e example and source transform for 6D operation
    - #14051 > add e2e example and complement missed annotation
    - #14052 > add e2e example and dedicated passe for 6D partition
    
    Fixes #14048
    Fixes #14049
    Fixes #14050
    Fixes #14051
    Fixes #14052
    
    ### Test plan
    MATRIX = {convnext_small, maxvit_t, swin_v2_t, vit_b_16}
    ```bash
    python backends/qualcomm/tests/test_qnn_delegate.py TestExampleOssScript.test_${MATRIX} -b build-android/ -m SM8750 -s $SN -a /path/to/test_artifacts/ -i /path/to/imagenet_1k/imagenet-mini/val -r .
    ```
    ```bash
    python backends/qualcomm/tests/test_qnn_delegate.py TestQuantizedModel.test_qnn_backend_conformer -b build-android/ -m SM8750 -s $SN -a /path/to/test_artifacts/
    ```
    
    (cherry picked from commit 2b54a19ec5d35e1981848fa86ad423c2b37d49f4)

--- backends/qualcomm/quantizer/annotators.py
+++ backends/qualcomm/quantizer/annotators.py
@@ -674,7 +674,7 @@ def annotate_pad(node: Node, quantization_config: QuantizationConfig) -> None:
     annotate_single_in_single_out(node, quantization_config)
 
 
-@register_annotator([torch.ops.aten.reshape.default])
+@register_annotator([torch.ops.aten.reshape.default, torch.ops.aten.unflatten.int])
 def annotate_reshape(node: Node, quantization_config: QuantizationConfig) -> None:
     annotate_single_in_single_out(node, quantization_config)
 
@@ -879,7 +879,7 @@ def annotate_unsqueeze_copy(
         annotate_single_in_share_out(node, quantization_config)
 
 
-@register_annotator([torch.ops.aten.transpose.int])
+@register_annotator([torch.ops.aten.transpose.int, torch.ops.aten.swapaxes.default])
 def annotate_transpose(node: Node, quantization_config: QuantizationConfig) -> None:
     annotate_in_out_obs_sharing_op(node, quantization_config)
     if not _is_annotated([node]):
