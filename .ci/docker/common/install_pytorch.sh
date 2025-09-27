c9c6759c673ad86222d4ae83a7512022a81aef3d
Huy Do <huydhn@gmail.com>
2025-09-27 00:45:24 -0700

    Typo
    Signed-off-by: Huy Do <huydhn@gmail.com>

--- .ci/docker/common/install_pytorch.sh
+++ .ci/docker/common/install_pytorch.sh
@@ -12,12 +12,12 @@ source "$(dirname "${BASH_SOURCE[0]}")/utils.sh"
 
 install_domains() {
   echo "Install torchvision and torchaudio"
-  pip install --no-use-pep517 --user "git+https://github.com/pytorch/audio.git@${TORCHAUDIO_VERSION}"
-  pip install --no-use-pep517 --user "git+https://github.com/pytorch/vision.git@${TORCHVISION_VERSION}"
+  pip_install --no-use-pep517 --user "git+https://github.com/pytorch/audio.git@${TORCHAUDIO_VERSION}"
+  pip_install --no-use-pep517 --user "git+https://github.com/pytorch/vision.git@${TORCHVISION_VERSION}"
 }
 
 install_pytorch_and_domains() {
-  pip install torch==2.9.0 torchvision torchaudio --index-url https://download.pytorch.org/whl/test/cpu
+  pip_install torch==2.9.0 torchvision torchaudio --index-url https://download.pytorch.org/whl/test/cpu
 }
 
 install_pytorch_and_domains
