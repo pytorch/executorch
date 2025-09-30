96dc88ee61a991649e0bdff042ad718abb7f99ac
Scott Roy <161522778+metascroy@users.noreply.github.com>
2025-09-29 17:07:27 -0700

    up

--- examples/models/llama/export_llama_lib.py
+++ examples/models/llama/export_llama_lib.py
@@ -757,7 +757,7 @@ def _prepare_for_llama_export(llm_config: LlmConfig) -> LLMEdgeManager:
             preq_embedding_quantize=llm_config.base.preq_embedding_quantize,
             local_global_attention=llm_config.model.local_global_attention,
             use_torchao_kernels_linear=llm_config.backend.torchao.use_torchao_kernels_linear,
-            use_torchao_kernels_tied_embedding=llm_config.backend.torchao.convert_tied_embedding,
+            use_torchao_kernels_tied_embedding=llm_config.backend.torchao.use_torchao_kernels_tied_embedding,
         )
     )
 
