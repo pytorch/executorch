8364dd8d7e164b1bdf73ab9a809690ca71d5da9d
billmguo <minguo@meta.com>
2025-10-06 21:47:12 -0700

    oss et update to support SAR2230P
    Differential Revision: D83934187
    
    Pull Request resolved: https://github.com/pytorch/executorch/pull/14808

--- backends/qualcomm/utils/utils.py
+++ backends/qualcomm/utils/utils.py
@@ -1099,6 +1099,7 @@ def get_soc_to_arch_map():
         "SXR1230P": HtpArch.V73,
         "SXR2230P": HtpArch.V69,
         "SXR2330P": HtpArch.V79,
+        "SAR2230P": HtpArch.V81,
     }
 
 
@@ -1115,6 +1116,7 @@ def get_soc_to_chipset_map():
         "SXR1230P": QcomChipset.SXR1230P,
         "SXR2230P": QcomChipset.SXR2230P,
         "SXR2330P": QcomChipset.SXR2330P,
+        "SAR2230P": QcomChipset.SAR2230P,
     }
 
 
