0b65f38c3ac7b0a9f1aa18dec4ec71f22ffa647c
Martin Lindström <Martin.Lindstroem@arm.com>
2025-10-07 10:38:30 +0200

    Revert "Arm backend: Move rescales from SUB visitor to pass"
    This reverts commit f21cf7ffd100e6a83bdc677c69d0dc8f7c5792d6.

--- backends/arm/test/passes/test_insert_rescale_i32_pass.py
+++ backends/arm/test/passes/test_insert_rescale_i32_pass.py
@@ -22,11 +22,10 @@ class NeedsRescaleOps(torch.nn.Module):
         super().__init__()
 
     def forward(self, x, y):
-        a = x - y
-        c = torch.maximum(a, y)
-        d = torch.abs(c)
-        e = d > c
-        return e
+        a = torch.maximum(x, y)
+        b = torch.abs(a)
+        c = a > b
+        return c
 
     def get_inputs(self, dtype) -> input_t:
         if dtype == torch.float32:
@@ -46,8 +45,8 @@ def test_insert_rescales():
     ops_not_before = {"executorch_exir_dialects_backend__ops_tosa_RESCALE_default"}
     ops_after = {
         # "number of op nodes with i8 output" + "number of i8 node inputs"
-        "executorch_exir_dialects_backend__ops_tosa_RESCALE_default": 3
-        + 7,
+        "executorch_exir_dialects_backend__ops_tosa_RESCALE_default": 2
+        + 5,
     }
     pipeline = PassPipeline[input_t](
         module,
