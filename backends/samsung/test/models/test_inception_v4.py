588a04d07ba8d06004ca7a59063b1f26c5ca8eea
Jiseong.oh <jiseong.oh@samsung.com>
2025-10-13 17:32:31 +0800

    Add float models e2e test
    The original float models are tested by executing aot_compiler script.
    Additionally, unify the outputs to tuple format in base tester.
    
    Co-authored-by: chong-chen <chong.chen@samsung.com>

--- backends/samsung/test/models/test_inception_v4.py
+++ backends/samsung/test/models/test_inception_v4.py
@@ -8,8 +8,6 @@
 
 import unittest
 
-import torch
-
 from executorch.backends.samsung.serialization.compile_options import (
     gen_samsung_backend_compile_spec,
 )
@@ -17,17 +15,16 @@ from executorch.backends.samsung.test.tester import SamsungTester
 from executorch.examples.models.inception_v4 import InceptionV4Model
 
 
-class Test_Milestone_InceptionV4(unittest.TestCase):
+class TestMilestoneInceptionV4(unittest.TestCase):
     def test_inception_v4_fp16(self):
         model = InceptionV4Model().get_eager_model()
         example_input = InceptionV4Model().get_example_inputs()
-
+        tester = SamsungTester(
+            model, example_input, [gen_samsung_backend_compile_spec("E9955")]
+        )
         (
-            SamsungTester(model,
-                          example_input,
-                          [gen_samsung_backend_compile_spec("E9955")],)
-            .export()
+            tester.export()
             .to_edge_transform_and_lower()
             .to_executorch()
-            .run_method_and_compare_outputs(atol=0.02, rtol=0.02)
+            .run_method_and_compare_outputs(inputs=example_input, atol=0.02, rtol=0.02)
         )
