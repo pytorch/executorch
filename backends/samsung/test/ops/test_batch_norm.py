65f21ef82ab7268d2fc034e4e9a464bc4d5ade94
Jiseong.oh <jiseong.oh@samsung.com>
2025-10-13 09:01:56 +0800

    Run on device and compare outputs
    For op test, add run_method_and_compare_outputs to make the test complete.
    The Exynos-backend-specific PTE file will be executed on device, and its outputs will be checked.
    
    Co-authored-by: chong-chen <chong.chen@samsung.com>

--- backends/samsung/test/ops/test_batch_norm.py
+++ backends/samsung/test/ops/test_batch_norm.py
@@ -43,6 +43,7 @@ class TestBatchNorm(unittest.TestCase):
             )
             .check_count({"torch.ops.higher_order.executorch_call_delegate": 1})
             .to_executorch()
+            .run_method_and_compare_outputs(inputs=inputs)
         )
 
     def test_fp32_batch_norm(self):
