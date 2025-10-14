a5f277b4e34cdba9c2ea07ddedd9f5ff1d185b02
tmsl <phaiting@gmail.com>
2025-10-14 07:47:21 -0700

    Introduce ktfmt for kotlin files under executorch_android (#15094)
    ### Summary
    The google-jave-format doesn't work for kotlin file, this diff
    introduced the standard kotlin format - ktfmt.
    
    run $./gradlew spotlessCheck to see the format issues
    run $./gradlew spotlessApply to apply the kotlin format.
    
    ### Test plan
    run via CI jobs
    
    Co-authored-by: Haiting Pu <haiting@meta.com>

--- extension/android/executorch_android/src/androidTest/java/org/pytorch/executorch/ModuleE2ETest.kt
+++ extension/android/executorch_android/src/androidTest/java/org/pytorch/executorch/ModuleE2ETest.kt
@@ -27,83 +27,83 @@ import org.pytorch.executorch.TestFileUtils.getTestFilePath
 /** Unit tests for [Module]. */
 @RunWith(AndroidJUnit4::class)
 class ModuleE2ETest {
-    @get:Rule
-    var runtimePermissionRule: GrantPermissionRule =
-        GrantPermissionRule.grant(Manifest.permission.READ_EXTERNAL_STORAGE)
+  @get:Rule
+  var runtimePermissionRule: GrantPermissionRule =
+      GrantPermissionRule.grant(Manifest.permission.READ_EXTERNAL_STORAGE)
 
-    @Throws(IOException::class, URISyntaxException::class)
-    fun testClassification(filePath: String) {
-        val pteFile = File(getTestFilePath(filePath))
-        val inputStream = javaClass.getResourceAsStream(filePath)
-        FileUtils.copyInputStreamToFile(inputStream, pteFile)
-        inputStream.close()
+  @Throws(IOException::class, URISyntaxException::class)
+  fun testClassification(filePath: String) {
+    val pteFile = File(getTestFilePath(filePath))
+    val inputStream = javaClass.getResourceAsStream(filePath)
+    FileUtils.copyInputStreamToFile(inputStream, pteFile)
+    inputStream.close()
 
-        val imgInputStream = javaClass.getResourceAsStream("/banana.jpeg")
-        var bitmap = BitmapFactory.decodeStream(imgInputStream)
-        bitmap = Bitmap.createScaledBitmap(bitmap!!, 224, 224, true)
-        imgInputStream.close()
+    val imgInputStream = javaClass.getResourceAsStream("/banana.jpeg")
+    var bitmap = BitmapFactory.decodeStream(imgInputStream)
+    bitmap = Bitmap.createScaledBitmap(bitmap!!, 224, 224, true)
+    imgInputStream.close()
 
-        val inputTensor =
-            bitmapToFloat32Tensor(
-                bitmap,
-                TensorImageUtils.TORCHVISION_NORM_MEAN_RGB,
-                TensorImageUtils.TORCHVISION_NORM_STD_RGB,
-            )
+    val inputTensor =
+        bitmapToFloat32Tensor(
+            bitmap,
+            TensorImageUtils.TORCHVISION_NORM_MEAN_RGB,
+            TensorImageUtils.TORCHVISION_NORM_STD_RGB,
+        )
 
-        val module = Module.load(getTestFilePath(filePath))
+    val module = Module.load(getTestFilePath(filePath))
 
-        val results = module.forward(EValue.from(inputTensor))
-        Assert.assertTrue(results[0].isTensor)
-        val scores = results[0].toTensor().dataAsFloatArray
+    val results = module.forward(EValue.from(inputTensor))
+    Assert.assertTrue(results[0].isTensor)
+    val scores = results[0].toTensor().dataAsFloatArray
 
-        val bananaClass = 954 // From ImageNet 1K
-        Assert.assertEquals(bananaClass.toLong(), argmax(scores).toLong())
-    }
+    val bananaClass = 954 // From ImageNet 1K
+    Assert.assertEquals(bananaClass.toLong(), argmax(scores).toLong())
+  }
 
-    @Test
-    @Throws(IOException::class, URISyntaxException::class)
-    fun testXnnpackBackendRequired() {
-        val pteFile = File(getTestFilePath("/mv3_xnnpack_fp32.pte"))
-        val inputStream = javaClass.getResourceAsStream("/mv3_xnnpack_fp32.pte")
-        FileUtils.copyInputStreamToFile(inputStream, pteFile)
-        inputStream.close()
+  @Test
+  @Throws(IOException::class, URISyntaxException::class)
+  fun testXnnpackBackendRequired() {
+    val pteFile = File(getTestFilePath("/mv3_xnnpack_fp32.pte"))
+    val inputStream = javaClass.getResourceAsStream("/mv3_xnnpack_fp32.pte")
+    FileUtils.copyInputStreamToFile(inputStream, pteFile)
+    inputStream.close()
 
-        val module = Module.load(getTestFilePath("/mv3_xnnpack_fp32.pte"))
-        val expectedBackends = arrayOf("XnnpackBackend")
-        assertArrayEquals(expectedBackends, module.getMethodMetadata("forward").backends)
-    }
+    val module = Module.load(getTestFilePath("/mv3_xnnpack_fp32.pte"))
+    val expectedBackends = arrayOf("XnnpackBackend")
+    assertArrayEquals(expectedBackends, module.getMethodMetadata("forward").backends)
+  }
 
-    @Test
-    @Throws(IOException::class, URISyntaxException::class)
-    fun testMv2Fp32() {
-        testClassification("/mv2_xnnpack_fp32.pte")
-    }
+  @Test
+  @Throws(IOException::class, URISyntaxException::class)
+  fun testMv2Fp32() {
+    testClassification("/mv2_xnnpack_fp32.pte")
+  }
 
-    @Test
-    @Throws(IOException::class, URISyntaxException::class)
-    fun testMv3Fp32() {
-        testClassification("/mv3_xnnpack_fp32.pte")
-    }
+  @Test
+  @Throws(IOException::class, URISyntaxException::class)
+  fun testMv3Fp32() {
+    testClassification("/mv3_xnnpack_fp32.pte")
+  }
 
-    @Test
-    @Throws(IOException::class, URISyntaxException::class)
-    fun testResnet50() {
-        testClassification("/resnet50_xnnpack_q8.pte")
-    }
+  @Test
+  @Throws(IOException::class, URISyntaxException::class)
+  fun testResnet50() {
+    testClassification("/resnet50_xnnpack_q8.pte")
+  }
 
-    companion object {
+  companion object {
 
-        fun argmax(array: FloatArray): Int {
-            require(array.isNotEmpty()) { "Array cannot be empty" }
-            var maxIndex = 0
-            var maxValue = array[0]
-            for (i in 1 until array.size) {
-                if (array[i] > maxValue) {
-                    maxValue = array[i]
-                    maxIndex = i
-                }
-            }
-            return maxIndex
+    fun argmax(array: FloatArray): Int {
+      require(array.isNotEmpty()) { "Array cannot be empty" }
+      var maxIndex = 0
+      var maxValue = array[0]
+      for (i in 1 until array.size) {
+        if (array[i] > maxValue) {
+          maxValue = array[i]
+          maxIndex = i
         }
+      }
+      return maxIndex
     }
+  }
 }
--- extension/android/executorch_android/src/androidTest/java/org/pytorch/executorch/ModuleInstrumentationTest.kt
+++ extension/android/executorch_android/src/androidTest/java/org/pytorch/executorch/ModuleInstrumentationTest.kt
@@ -8,7 +8,6 @@
 package org.pytorch.executorch
 
 import android.Manifest
-import androidx.test.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.GrantPermissionRule
 import java.io.File
@@ -28,151 +27,151 @@ import org.pytorch.executorch.TestFileUtils.getTestFilePath
 /** Unit tests for [Module]. */
 @RunWith(AndroidJUnit4::class)
 class ModuleInstrumentationTest {
-    @Before
-    @Throws(IOException::class)
-    fun setUp() {
-        // copy zipped test resources to local device
-        val addPteFile = File(getTestFilePath(TEST_FILE_NAME))
-        var inputStream = javaClass.getResourceAsStream(TEST_FILE_NAME)
-        FileUtils.copyInputStreamToFile(inputStream, addPteFile)
-        inputStream.close()
+  @Before
+  @Throws(IOException::class)
+  fun setUp() {
+    // copy zipped test resources to local device
+    val addPteFile = File(getTestFilePath(TEST_FILE_NAME))
+    var inputStream = javaClass.getResourceAsStream(TEST_FILE_NAME)
+    FileUtils.copyInputStreamToFile(inputStream, addPteFile)
+    inputStream.close()
 
-        val nonPteFile = File(getTestFilePath(NON_PTE_FILE_NAME))
-        inputStream = javaClass.getResourceAsStream(NON_PTE_FILE_NAME)
-        FileUtils.copyInputStreamToFile(inputStream, nonPteFile)
-        inputStream.close()
-    }
+    val nonPteFile = File(getTestFilePath(NON_PTE_FILE_NAME))
+    inputStream = javaClass.getResourceAsStream(NON_PTE_FILE_NAME)
+    FileUtils.copyInputStreamToFile(inputStream, nonPteFile)
+    inputStream.close()
+  }
 
-    @get:Rule
-    var runtimePermissionRule: GrantPermissionRule =
-        GrantPermissionRule.grant(Manifest.permission.READ_EXTERNAL_STORAGE)
+  @get:Rule
+  var runtimePermissionRule: GrantPermissionRule =
+      GrantPermissionRule.grant(Manifest.permission.READ_EXTERNAL_STORAGE)
 
-    @Test
-    @Throws(IOException::class, URISyntaxException::class)
-    fun testModuleLoadAndForward() {
-        val module = Module.load(getTestFilePath(TEST_FILE_NAME))
+  @Test
+  @Throws(IOException::class, URISyntaxException::class)
+  fun testModuleLoadAndForward() {
+    val module = Module.load(getTestFilePath(TEST_FILE_NAME))
 
+    val results = module.forward()
+    Assert.assertTrue(results[0].isTensor)
+  }
+
+  @Test
+  @Throws(IOException::class, URISyntaxException::class)
+  fun testMethodMetadata() {
+    val module = Module.load(getTestFilePath(TEST_FILE_NAME))
+  }
+
+  @Test
+  @Throws(IOException::class)
+  fun testModuleLoadMethodAndForward() {
+    val module = Module.load(getTestFilePath(TEST_FILE_NAME))
+
+    val loadMethod = module.loadMethod(FORWARD_METHOD)
+    Assert.assertEquals(loadMethod.toLong(), OK.toLong())
+
+    val results = module.forward()
+    Assert.assertTrue(results[0].isTensor)
+  }
+
+  @Test
+  @Throws(IOException::class)
+  fun testModuleLoadForwardExplicit() {
+    val module = Module.load(getTestFilePath(TEST_FILE_NAME))
+
+    val results = module.execute(FORWARD_METHOD)
+    Assert.assertTrue(results[0].isTensor)
+  }
+
+  @Test(expected = RuntimeException::class)
+  @Throws(IOException::class)
+  fun testModuleLoadNonExistantFile() {
+    val module = Module.load(getTestFilePath(MISSING_FILE_NAME))
+  }
+
+  @Test
+  @Throws(IOException::class)
+  fun testModuleLoadMethodNonExistantMethod() {
+    val module = Module.load(getTestFilePath(TEST_FILE_NAME))
+
+    val loadMethod = module.loadMethod(NONE_METHOD)
+    Assert.assertEquals(loadMethod.toLong(), INVALID_ARGUMENT.toLong())
+  }
+
+  @Test(expected = RuntimeException::class)
+  @Throws(IOException::class)
+  fun testNonPteFile() {
+    val module = Module.load(getTestFilePath(NON_PTE_FILE_NAME))
+
+    val loadMethod = module.loadMethod(FORWARD_METHOD)
+    Assert.assertEquals(loadMethod.toLong(), INVALID_ARGUMENT.toLong())
+  }
+
+  @Test
+  @Throws(IOException::class)
+  fun testLoadOnDestroyedModule() {
+    val module = Module.load(getTestFilePath(TEST_FILE_NAME))
+
+    module.destroy()
+
+    val loadMethod = module.loadMethod(FORWARD_METHOD)
+    Assert.assertEquals(loadMethod.toLong(), INVALID_STATE.toLong())
+  }
+
+  @Test
+  @Throws(IOException::class)
+  fun testForwardOnDestroyedModule() {
+    val module = Module.load(getTestFilePath(TEST_FILE_NAME))
+
+    val loadMethod = module.loadMethod(FORWARD_METHOD)
+    Assert.assertEquals(loadMethod.toLong(), OK.toLong())
+
+    module.destroy()
+
+    val results = module.forward()
+    Assert.assertEquals(0, results.size.toLong())
+  }
+
+  @Test
+  @Throws(InterruptedException::class, IOException::class)
+  fun testForwardFromMultipleThreads() {
+    val module = Module.load(getTestFilePath(TEST_FILE_NAME))
+
+    val numThreads = 100
+    val latch = CountDownLatch(numThreads)
+    val completed = AtomicInteger(0)
+
+    val runnable = Runnable {
+      try {
+        latch.countDown()
+        latch.await(5000, TimeUnit.MILLISECONDS)
         val results = module.forward()
         Assert.assertTrue(results[0].isTensor)
+        completed.incrementAndGet()
+      } catch (_: InterruptedException) {}
     }
 
-    @Test
-    @Throws(IOException::class, URISyntaxException::class)
-    fun testMethodMetadata() {
-        val module = Module.load(getTestFilePath(TEST_FILE_NAME))
+    val threads = arrayOfNulls<Thread>(numThreads)
+    for (i in 0 until numThreads) {
+      threads[i] = Thread(runnable)
+      threads[i]!!.start()
     }
 
-    @Test
-    @Throws(IOException::class)
-    fun testModuleLoadMethodAndForward() {
-        val module = Module.load(getTestFilePath(TEST_FILE_NAME))
-
-        val loadMethod = module.loadMethod(FORWARD_METHOD)
-        Assert.assertEquals(loadMethod.toLong(), OK.toLong())
-
-        val results = module.forward()
-        Assert.assertTrue(results[0].isTensor)
+    for (i in 0 until numThreads) {
+      threads[i]!!.join()
     }
 
-    @Test
-    @Throws(IOException::class)
-    fun testModuleLoadForwardExplicit() {
-        val module = Module.load(getTestFilePath(TEST_FILE_NAME))
+    Assert.assertEquals(numThreads.toLong(), completed.get().toLong())
+  }
 
-        val results = module.execute(FORWARD_METHOD)
-        Assert.assertTrue(results[0].isTensor)
-    }
-
-    @Test(expected = RuntimeException::class)
-    @Throws(IOException::class)
-    fun testModuleLoadNonExistantFile() {
-        val module = Module.load(getTestFilePath(MISSING_FILE_NAME))
-    }
-
-    @Test
-    @Throws(IOException::class)
-    fun testModuleLoadMethodNonExistantMethod() {
-        val module = Module.load(getTestFilePath(TEST_FILE_NAME))
-
-        val loadMethod = module.loadMethod(NONE_METHOD)
-        Assert.assertEquals(loadMethod.toLong(), INVALID_ARGUMENT.toLong())
-    }
-
-    @Test(expected = RuntimeException::class)
-    @Throws(IOException::class)
-    fun testNonPteFile() {
-        val module = Module.load(getTestFilePath(NON_PTE_FILE_NAME))
-
-        val loadMethod = module.loadMethod(FORWARD_METHOD)
-        Assert.assertEquals(loadMethod.toLong(), INVALID_ARGUMENT.toLong())
-    }
-
-    @Test
-    @Throws(IOException::class)
-    fun testLoadOnDestroyedModule() {
-        val module = Module.load(getTestFilePath(TEST_FILE_NAME))
-
-        module.destroy()
-
-        val loadMethod = module.loadMethod(FORWARD_METHOD)
-        Assert.assertEquals(loadMethod.toLong(), INVALID_STATE.toLong())
-    }
-
-    @Test
-    @Throws(IOException::class)
-    fun testForwardOnDestroyedModule() {
-        val module = Module.load(getTestFilePath(TEST_FILE_NAME))
-
-        val loadMethod = module.loadMethod(FORWARD_METHOD)
-        Assert.assertEquals(loadMethod.toLong(), OK.toLong())
-
-        module.destroy()
-
-        val results = module.forward()
-        Assert.assertEquals(0, results.size.toLong())
-    }
-
-    @Test
-    @Throws(InterruptedException::class, IOException::class)
-    fun testForwardFromMultipleThreads() {
-        val module = Module.load(getTestFilePath(TEST_FILE_NAME))
-
-        val numThreads = 100
-        val latch = CountDownLatch(numThreads)
-        val completed = AtomicInteger(0)
-
-        val runnable = Runnable {
-            try {
-                latch.countDown()
-                latch.await(5000, TimeUnit.MILLISECONDS)
-                val results = module.forward()
-                Assert.assertTrue(results[0].isTensor)
-                completed.incrementAndGet()
-            } catch (_: InterruptedException) {}
-        }
-
-        val threads = arrayOfNulls<Thread>(numThreads)
-        for (i in 0 until numThreads) {
-            threads[i] = Thread(runnable)
-            threads[i]!!.start()
-        }
-
-        for (i in 0 until numThreads) {
-            threads[i]!!.join()
-        }
-
-        Assert.assertEquals(numThreads.toLong(), completed.get().toLong())
-    }
-
-    companion object {
-        private const val TEST_FILE_NAME = "/ModuleAdd.pte"
-        private const val MISSING_FILE_NAME = "/missing.pte"
-        private const val NON_PTE_FILE_NAME = "/test.txt"
-        private const val FORWARD_METHOD = "forward"
-        private const val NONE_METHOD = "none"
-        private const val OK = 0x00
-        private const val INVALID_STATE = 0x2
-        private const val INVALID_ARGUMENT = 0x12
-        private const val ACCESS_FAILED = 0x22
-    }
+  companion object {
+    private const val TEST_FILE_NAME = "/ModuleAdd.pte"
+    private const val MISSING_FILE_NAME = "/missing.pte"
+    private const val NON_PTE_FILE_NAME = "/test.txt"
+    private const val FORWARD_METHOD = "forward"
+    private const val NONE_METHOD = "none"
+    private const val OK = 0x00
+    private const val INVALID_STATE = 0x2
+    private const val INVALID_ARGUMENT = 0x12
+    private const val ACCESS_FAILED = 0x22
+  }
 }
