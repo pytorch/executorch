77022d1836192bf722dbe5096d88b1459484a425
Jacob Szwejbka <jakeszwe@fb.com>
2025-10-16 22:13:16 -0700

    ET Restrict macro

--- kernels/optimized/cpu/op_log_softmax.cpp
+++ kernels/optimized/cpu/op_log_softmax.cpp
@@ -31,8 +31,8 @@ namespace {
 
 template <typename IN_T, typename OUT_T>
 void log_softmax_kernel(const Tensor& input, int64_t dim, Tensor& out) {
-  const IN_T* __restrict__ input_data_base = input.const_data_ptr<IN_T>();
-  OUT_T* __restrict__ output_data_base = out.mutable_data_ptr<OUT_T>();
+  const IN_T* ET_RESTRICT input_data_base = input.const_data_ptr<IN_T>();
+  OUT_T* ET_RESTRICT output_data_base = out.mutable_data_ptr<OUT_T>();
 
   if (input.dim() == 0) {
     output_data_base[0] = 0;
