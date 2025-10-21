12545a3fcc814263b23c19238c547cdef150ad2d
Hansong Zhang <hsz@meta.com>
2025-10-20 17:09:09 -0700

    Should use pos_ to text_token_generator

--- extension/llm/runner/text_llm_runner.cpp
+++ extension/llm/runner/text_llm_runner.cpp
@@ -190,7 +190,7 @@ Error TextLLMRunner::generate(
   // Generate max_new_tokens - 1 because prefill already generated 1 token.
   auto generate_result = text_token_generator_->generate(
       prompt_tokens,
-      num_prompt_tokens,
+      pos_,
       max_new_tokens - 1,
       temperature_ == -1.0f ? config.temperature : temperature_,
       wrapped_callback);
