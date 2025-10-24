7508aea825d4b06066cf7d8e09e0e459a2f73c78
Sebastian Larsson <38941629+Sebastian-Larsson@users.noreply.github.com>
2025-10-24 09:57:54 +0200

    Arm backend: Remove pyre-unsafe from _passes/ (#15351)
    Pyre is no longer used.
    
    Signed-off-by: Sebastian Larsson <sebastian.larsson@arm.com>

--- backends/arm/_passes/arm_pass_manager.py
+++ backends/arm/_passes/arm_pass_manager.py
@@ -5,8 +5,6 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-# pyre-unsafe
-
 
 from collections import defaultdict
 
