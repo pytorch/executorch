ca9237d3174318f9a39edcf1566367c8675ad6b3
Martin Lindström <33344797+martinlsm@users.noreply.github.com>
2025-10-24 11:26:41 +0200

    Arm backend: Merge passes that replace scalars (#15298)
    Besides having obsolete names, ReplaceScalarWithTensorPassTOSAMI and
    ReplaceScalarWithTensorPassTOSABI was causing difficulties for defining
    chronological pass dependencies with the `_passes_required_after`
    attribute. This is because, with the current design, there is no way to
    distinguish which profile is referred to when defining
    `_passes_required_after`; a pass simply declares its chronological
    dependencies globally.
    
    Solve this by merging the two pass classes together into one called
    `ReplaceScalarWithTensorByProfilePass`. This means that a pass should
    include this new pass in `_passes_required_after` no matter which TOSA
    profile its working towards.
    
    ### Test plan
    This is a refactoring patch. No behavior is modified. Thus the tests
    already in place are enough to test this change.
    
    
    cc @freddan80 @per @zingo @oscarandersson8218 @digantdesai
    
    Signed-off-by: Martin Lindström <Martin.Lindstroem@arm.com>
    Co-authored-by: Martin Lindström <Martin.Lindstroem@arm.com>

--- backends/arm/_passes/arm_pass_manager.py
+++ backends/arm/_passes/arm_pass_manager.py
@@ -87,8 +87,7 @@ from executorch.backends.arm._passes import (
     QuantizeOperatorArguments,
     RemoveNoopPass,
     ReplaceInfValues,
-    ReplaceScalarWithTensorArgPassTOSABI,
-    ReplaceScalarWithTensorArgPassTOSAMI,
+    ReplaceScalarWithTensorByProfilePass,
     RetraceFoldedDtypesPass,
     RewriteConv2dPass,
     RewriteMatmulPass,
@@ -172,7 +171,7 @@ class ArmPassManager(PassManager):
             self.add_pass(CastToInt32Pass())
 
         self.add_pass(CastBoolToInt8Pass())
-        self.add_pass(ReplaceScalarWithTensorArgPassTOSABI())
+        self.add_pass(ReplaceScalarWithTensorByProfilePass())
         self.add_pass(AnnotateDecomposedMatmulPass())
         self.add_pass(QuantizeOperatorArguments())
         self.add_pass(ConvertELUParamsPass())
@@ -242,7 +241,7 @@ class ArmPassManager(PassManager):
         self.add_pass(DecomposeSinhPass())
         self.add_pass(DecomposeSignPass())
         self.add_pass(DecomposeDivTensorModePass())
-        self.add_pass(ReplaceScalarWithTensorArgPassTOSAMI())
+        self.add_pass(ReplaceScalarWithTensorByProfilePass())
         self.add_pass(DecomposeEmbeddingPass())
         self.add_pass(FuseQuantizedActivationPass())
         self.add_pass(RemoveGetItemPass())
@@ -335,7 +334,7 @@ class ArmPassManager(PassManager):
         self.add_pass(DecomposeAddmmPass())
         self.add_pass(DecomposeDivTensorModePass())
         self.add_pass(DecomposeAddSubAlphaPass())
-        self.add_pass(ReplaceScalarWithTensorArgPassTOSABI())
+        self.add_pass(ReplaceScalarWithTensorByProfilePass())
         self.add_pass(ScalarsToAttributePass())
         self.add_pass(DecomposeGroupNormPass())
         self.add_pass(DecomposeLayerNormPass())
