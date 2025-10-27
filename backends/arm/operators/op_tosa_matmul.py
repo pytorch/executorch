5c59fa0f6e21cf003e8c7dcd36dcc1cee5b27e63
Gregory James Comer <gjcomer@meta.com>
2025-10-27 11:16:35 -0700

    Revert "Arm backend: Remove pyre-unsafe from operators/ (#15376)"
    This reverts commit cf407fc91912795a4eba0df0dfc6ac1ac5330624.

--- backends/arm/operators/op_tosa_matmul.py
+++ backends/arm/operators/op_tosa_matmul.py
@@ -4,6 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+# pyre-unsafe
 """Provide a visitor for lowering batched matmul (BMM) to TOSA."""
 
 from typing import Any, List
