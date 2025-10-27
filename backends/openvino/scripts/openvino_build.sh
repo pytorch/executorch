f0d4fbf80ca1376380d46567ef4eaf3338f93f27
Mustafa Cavus <mustafa.cavus@intel.com>
2025-10-27 16:13:04 -0700

    Remove llama runner build from openvino build script

--- backends/openvino/scripts/openvino_build.sh
+++ backends/openvino/scripts/openvino_build.sh
@@ -16,6 +16,8 @@ install_requirements() {
 build_cpp_runtime() {
     echo "Building C++ Runtime Libraries"
 
+    local llm_enabled=${1:-0}
+
     # Set build directory
     local build_dir="cmake-out"
 
@@ -23,45 +25,31 @@ build_cpp_runtime() {
     cd "$EXECUTORCH_ROOT"
     rm -rf "${build_dir}"
 
+    CMAKE_ARGS=(
+        "-DCMAKE_BUILD_TYPE=Release"
+        "-DEXECUTORCH_BUILD_OPENVINO=ON"
+        "-DEXECUTORCH_BUILD_EXTENSION_DATA_LOADER=ON"
+        "-DEXECUTORCH_BUILD_EXTENSION_MODULE=ON"
+        "-DEXECUTORCH_BUILD_EXTENSION_NAMED_DATA_MAP=ON"
+        "-DEXECUTORCH_BUILD_EXTENSION_RUNNER_UTIL=ON"
+        "-DEXECUTORCH_BUILD_EXTENSION_FLAT_TENSOR=ON"
+        "-DEXECUTORCH_BUILD_EXTENSION_TENSOR=ON"
+        "-DEXECUTORCH_BUILD_EXECUTOR_RUNNER=ON"
+        "-DEXECUTORCH_BUILD_KERNELS_QUANTIZED=ON"
+    )
+
+    if [[ "$llm_enabled" -eq 1 ]]; then
+	    CMAKE_ARGS+=("-DEXECUTORCH_BUILD_EXTENSION_LLM=ON -DEXECUTORCH_BUILD_EXTENSION_LLM_RUNNER=ON")
+    fi
+
     # Configure the project with CMake
     # Note: Add any additional configuration options you need here
     cmake -DCMAKE_INSTALL_PREFIX="${build_dir}" \
-          -DCMAKE_BUILD_TYPE=Release \
-          -DEXECUTORCH_BUILD_OPENVINO=ON \
-          -DEXECUTORCH_BUILD_EXTENSION_DATA_LOADER=ON \
-          -DEXECUTORCH_BUILD_EXTENSION_MODULE=ON \
-          -DEXECUTORCH_BUILD_EXTENSION_NAMED_DATA_MAP=ON \
-          -DEXECUTORCH_BUILD_EXTENSION_RUNNER_UTIL=ON \
-          -DEXECUTORCH_BUILD_EXTENSION_FLAT_TENSOR=ON \
-          -DEXECUTORCH_BUILD_EXTENSION_TENSOR=ON \
-          -DEXECUTORCH_BUILD_EXECUTOR_RUNNER=ON \
-          -DEXECUTORCH_BUILD_KERNELS_QUANTIZED=ON \
-          -DEXECUTORCH_BUILD_EXTENSION_LLM=ON \
-          -DEXECUTORCH_BUILD_EXTENSION_LLM_RUNNER=ON \
+          ${CMAKE_ARGS[@]} \
           -B"${build_dir}"
-
 
     # Build the project
     cmake --build ${build_dir} --target install --config Release -j$(nproc)
-}
-
-build_llama_runner() {
-    echo "Building Export Llama Runner"
-
-    # Set build directory
-    local build_dir="cmake-out"
-
-    # Enter the Executorch root directory
-    cd "$EXECUTORCH_ROOT"
-
-    # Configure the project with CMake
-    # Note: Add any additional configuration options you need here
-    cmake -DCMAKE_INSTALL_PREFIX="${build_dir}" \
-        -DCMAKE_BUILD_TYPE=Release \
-        -B"${build_dir}"/examples/models/llama \
-        examples/models/llama
-    # Build the export llama runner
-    cmake --build cmake-out/examples/models/llama -j$(nproc) --config Release
 }
 
 build_python_enabled() {
@@ -92,7 +80,6 @@ main() {
         install_requirements
         build_python_enabled
         build_cpp_runtime
-        build_llama_runner
 
     # If the first arguments is --cpp_runtime, build libraries for C++ runtime
     elif [[ "$build_type" == "--cpp_runtime" ]]; then
@@ -100,8 +87,8 @@ main() {
 
     # If the first arguments is --llama_runner, build export llama runner binary
     # Note: c++ runtime with openvino backend should be built before building export llama runner
-    elif [[ "$build_type" == "--llama_runner" ]]; then
-        build_llama_runner
+    elif [[ "$build_type" == "--cpp_runtime_llm" ]]; then
+        build_cpp_runtime 1
 
     # If the first arguments is --enable_python, build python package with python bindings
     elif [[ "$build_type" == "--enable_python" ]]; then
