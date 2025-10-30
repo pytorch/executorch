4fd6ef5a7b3848aa756930052e90fadf4cf78caf
lucylq <lfq@meta.com>
2025-10-30 10:43:02 -0700

    Deserialize to named data store output
    Should this be a public API? May be relatively easy for user to impl with the existing deserialize functionality.
    
    Differential Revision: [D83510300](https://our.internmc.facebook.com/intern/diff/D83510300/)
    
    [ghstack-poisoned]

--- extension/flat_tensor/serialize/serialize.py
+++ extension/flat_tensor/serialize/serialize.py
@@ -13,13 +13,17 @@ import math
 import os
 import tempfile
 from dataclasses import dataclass
-from typing import ClassVar, Dict, List, Literal, Optional
+from typing import ClassVar, Dict, List, Literal, Optional, Union
 
 import executorch.extension.flat_tensor.serialize as serialize_package
 
 from executorch.exir._serialize._cord import Cord
 from executorch.exir._serialize._dataclass import _DataclassEncoder, _json_to_dataclass
 from executorch.exir._serialize._flatbuffer import _flatc_compile, _flatc_decompile
+from executorch.exir._serialize._named_data_store import (
+    NamedDataStore,
+    NamedDataStoreOutput,
+)
 from executorch.exir._serialize._program import _insert_flatbuffer_header
 from executorch.exir._serialize.data_serializer import (
     DataEntry,
@@ -389,6 +393,8 @@ class FlatTensorSerializer(DataSerializer):
     def deserialize(self, blob: Cord) -> DataPayload:
         """
         Deserializes a flat_tensor blob into a list of tensor metadata and tensors.
+
+        Note: deserialization does not preserve alignment information.
         """
 
         data = bytes(blob)
@@ -436,3 +442,12 @@ class FlatTensorSerializer(DataSerializer):
             payload.named_data[named_data.key] = entry
 
         return payload
+
+    def deserialize_to_named_data_store_output(self, blob: bytes, name: str) -> NamedDataStoreOutput:
+        bytes = Cord(blob)
+        data_payload = self.deserialize(bytes)
+        return NamedDataStoreOutput(
+            buffers = data_payload.buffers,
+            pte_data = {},
+            external_data = {name:data_payload.named_data}
+        )
