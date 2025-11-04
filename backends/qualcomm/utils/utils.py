74a481d194f4d4cf884f5e2f83f257a6c0af0077
haowhsu-quic <111341466+haowhsu-quic@users.noreply.github.com>
2025-11-05 02:15:37 +0800

    Qualcomm AI Engine Direct - supprot SAR2230P (#15437)
    ### Summary
    - add soc SAR2230P
    
    ### Test plan
    ```bash
    cd $EXECUTORCH_ROOT/examples
    python qualcomm/scripts/export_example.py --model_name mul --output_folder . --generate_etrecord --soc SAR2230P -q ptq
    ```

--- backends/qualcomm/utils/utils.py
+++ backends/qualcomm/utils/utils.py
@@ -1100,6 +1100,7 @@ def get_soc_to_arch_map():
         "SXR2230P": HtpArch.V69,
         "SXR2330P": HtpArch.V79,
         "QCS9100": HtpArch.V73,
+        "SAR2230P": HtpArch.V81,
     }
 
 
@@ -1117,6 +1118,7 @@ def get_soc_to_chipset_map():
         "SXR2230P": QcomChipset.SXR2230P,
         "SXR2330P": QcomChipset.SXR2330P,
         "QCS9100": QcomChipset.QCS9100,
+        "SAR2230P": QcomChipset.SAR2230P,
     }
 
 
