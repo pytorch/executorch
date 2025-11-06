98432224460cf2162ae5b6fdfba398970e6caa1d
Adrian Lundell <36153706+AdrianLundell@users.noreply.github.com>
2025-11-06 13:44:57 +0100

    Cortex_m backend: Simplify add + linear fusion passes (#15526)
    Reuses the FoldAndAnnotateQParamsPass from the Arm backend to greatly
    simplify the logic for fusing the ops.
    
    Additionally updates the linear kernel to be numerically correct and
    computes the kernel_sum aot in the quantized_linear_fusion pass. Note
    that since this replaces the bias node it typically causes no extra
    memory usage.
    
    Updates the Linear tests to mirror this, including removing the various
    matmul tests. Since the linear is handled as a separate op rather than a
    particular type of matmul these tests are not related anymore.
    
    Removes unnecessary stub definitions in operators.py, operators.yaml and
    op_quantized_linear.cpp
    
    Leaving a few TODO:s since the patch is large already.
    
    
    Signed-off-by: Adrian Lundell <adrian.lundell@arm.com>

--- backends/cortex_m/test/ops/test_linear.py
+++ backends/cortex_m/test/ops/test_linear.py
@@ -4,8 +4,8 @@
 # LICENSE file in the root directory of this source tree.
 
 
-import pytest
 import torch
+from executorch.backends.arm.test.common import parametrize
 from executorch.backends.cortex_m.test.tester import (
     CortexMTester,
     McuTestCase,
@@ -13,12 +13,9 @@ from executorch.backends.cortex_m.test.tester import (
 )
 
 
-class CortexMMm(torch.nn.Module):
-    def forward(self, x, y):
-        return torch.mm(x, y)
-
+class CortexMLinear(torch.nn.Module):
     ops_before_transforms = {
-        "executorch_exir_dialects_edge__ops_aten_mm_default": 1,
+        "executorch_exir_dialects_edge__ops_aten_linear_default": 1,
         "executorch_exir_dialects_edge__ops_quantized_decomposed_quantize_per_tensor_default": 2,
         "executorch_exir_dialects_edge__ops_quantized_decomposed_dequantize_per_tensor_default": 3,
     }
@@ -29,124 +26,70 @@ class CortexMMm(torch.nn.Module):
         "executorch_exir_dialects_edge__ops_cortex_m_dequantize_per_tensor_default": 1,
     }
 
-
-class CortexMBmm(torch.nn.Module):
-    def forward(self, x, y):
-        return torch.bmm(x, y)
-
-    ops_before_transforms = {
-        "executorch_exir_dialects_edge__ops_aten_bmm_default": 1,
-        "executorch_exir_dialects_edge__ops_quantized_decomposed_quantize_per_tensor_default": 2,
-        "executorch_exir_dialects_edge__ops_quantized_decomposed_dequantize_per_tensor_default": 3,
-    }
-
-    ops_after_transforms = {
-        "executorch_exir_dialects_edge__ops_cortex_m_quantized_linear_default": 1,
-        "executorch_exir_dialects_edge__ops_cortex_m_quantize_per_tensor_default": 1,
-        "executorch_exir_dialects_edge__ops_cortex_m_dequantize_per_tensor_default": 1,
-    }
-
-
-class CortexMAddmm(torch.nn.Module):
-    def forward(self, x, y, z, alpha=None, beta=None):
-        return torch.addmm(beta, x, alpha, y, z)
-
-    ops_before_transforms = {
-        "executorch_exir_dialects_edge__ops_aten_addmm_default": 1,
-        "executorch_exir_dialects_edge__ops_quantized_decomposed_quantize_per_tensor_default": 2,
-        "executorch_exir_dialects_edge__ops_quantized_decomposed_dequantize_per_tensor_default": 3,
-    }
-
-    ops_after_transforms = {
-        "executorch_exir_dialects_edge__ops_cortex_m_quantized_linear_default": 1,
-        "executorch_exir_dialects_edge__ops_cortex_m_quantize_per_tensor_default": 1,
-        "executorch_exir_dialects_edge__ops_cortex_m_dequantize_per_tensor_default": 1,
-    }
-
-
-class CortexMAt(CortexMMm):
-    def forward(self, x, y):
-        return x @ y
-
-
-class CortexMMatmul(CortexMMm):
-    def forward(self, x, y):
-        return torch.matmul(x, y)
-
-
-class CortexMLinear(CortexMMatmul):
     def __init__(self, *args, **kwargs):
         super().__init__()
         self.linear = torch.nn.Linear(*args, bias=False)
+        self.linear.weight.data.fill_(1.0)
 
     def forward(self, x):
         return self.linear(x)
 
 
-class CortexMLinearBias(CortexMAddmm):
+class CortexMLinearX3(torch.nn.Module):
+    ops_before_transforms = {
+        "executorch_exir_dialects_edge__ops_aten_linear_default": 3,
+        "executorch_exir_dialects_edge__ops_quantized_decomposed_quantize_per_tensor_default": 4,
+        "executorch_exir_dialects_edge__ops_quantized_decomposed_dequantize_per_tensor_default": 7,
+    }
+
+    ops_after_transforms = {
+        "executorch_exir_dialects_edge__ops_cortex_m_quantized_linear_default": 3,
+        "executorch_exir_dialects_edge__ops_cortex_m_quantize_per_tensor_default": 1,
+        "executorch_exir_dialects_edge__ops_cortex_m_dequantize_per_tensor_default": 1,
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.linear = torch.nn.Linear(*args, bias=False)
+        self.linear.weight.data.fill_(1.0)
+
+    def forward(self, x):
+        x = self.linear(x)
+        x = self.linear(x)
+        x = self.linear(x)
+        return x
+
+
+class CortexMLinearBias(torch.nn.Module):
+    ops_before_transforms = {
+        "executorch_exir_dialects_edge__ops_aten_linear_default": 1,
+        "executorch_exir_dialects_edge__ops_quantized_decomposed_quantize_per_tensor_default": 2,
+        "executorch_exir_dialects_edge__ops_quantized_decomposed_dequantize_per_tensor_default": 4,
+    }
+
+    ops_after_transforms = {
+        "executorch_exir_dialects_edge__ops_cortex_m_quantized_linear_default": 1,
+        "executorch_exir_dialects_edge__ops_cortex_m_quantize_per_tensor_default": 1,
+        "executorch_exir_dialects_edge__ops_cortex_m_dequantize_per_tensor_default": 1,
+    }
+
     def __init__(self, *args, **kwargs):
         super().__init__()
         self.linear = torch.nn.Linear(*args, bias=True)
         self.relu = torch.nn.ReLU()
 
     def forward(self, x):
-        return self.relu(self.linear(x))
+        return self.linear(x)
 
 
 test_cases = {
-    "mm": McuTestCase(
-        model=CortexMMm(),
-        example_inputs=(
-            ramp_tensor(0, 10, (1, 16)),
-            ramp_tensor(0, 10, (16, 16)),
-        ),
-    ),
-    "bmm": McuTestCase(
-        model=CortexMBmm(),
-        example_inputs=(
-            ramp_tensor(0, 10, (1, 16, 16)),
-            ramp_tensor(0, 10, (1, 16, 16)),
-        ),
-    ),
-    "addmm": McuTestCase(
-        model=CortexMAddmm(),
-        example_inputs=(
-            ramp_tensor(0, 10, (1, 16)),
-            ramp_tensor(0, 10, (16, 16)),
-            ramp_tensor(0, 10, (16, 16)),
-            2,
-            4,
-        ),
-    ),
-    "addmm_scalars": McuTestCase(
-        model=CortexMAddmm(),
-        example_inputs=(
-            ramp_tensor(0, 10, (1, 16)),
-            ramp_tensor(0, 10, (16, 16)),
-            ramp_tensor(0, 10, (16, 16)),
-        ),
-    ),
-    "@-operator": McuTestCase(
-        model=CortexMAt(),
-        example_inputs=(
-            ramp_tensor(0, 10, (1, 16)),
-            ramp_tensor(0, 10, (16, 16)),
-        ),
-    ),
-    "matmul": McuTestCase(
-        model=CortexMMatmul(),
-        example_inputs=(
-            ramp_tensor(0, 10, (1, 16)),
-            ramp_tensor(0, 10, (16, 16)),
-        ),
-    ),
     "linear_rank1": McuTestCase(
-        model=CortexMLinear(2, 3),
-        example_inputs=(ramp_tensor(-1, 1, (2,)),),
+        model=CortexMLinear(1, 2),
+        example_inputs=(torch.Tensor([1]),),
     ),
     "linear_rank2_pos": McuTestCase(
-        model=CortexMLinear(8, 3),
-        example_inputs=(ramp_tensor(0, 10, (2, 8)),),
+        model=CortexMLinear(1, 2),
+        example_inputs=(ramp_tensor(-1, 1, (1, 1)),),
     ),
     "linear_rank3_neg": McuTestCase(
         model=CortexMLinear(5, 3),
@@ -164,22 +107,24 @@ test_cases = {
         model=CortexMLinearBias(61, 37),
         example_inputs=(ramp_tensor(0, 10, (8, 61)),),
     ),
+    "linear_x3": McuTestCase(
+        model=CortexMLinearX3(4, 4),
+        example_inputs=(ramp_tensor(0, 10, (2, 4)),),
+    ),
 }
 
 
-@pytest.mark.skip(
-    reason="Skipping until the quantized_linear_fusion_pass is updated to work with non decomposed linear ops."
-)
+@parametrize("test_case", test_cases)
 def test_dialect_linear(test_case):
     tester = CortexMTester(test_case.model, test_case.example_inputs)
     tester.test_dialect(
-        test_case.model.ops_before_transforms, test_case.model.ops_after_transforms
+        test_case.model.ops_before_transforms,
+        test_case.model.ops_after_transforms,
+        qtol=1,
     )
 
 
-@pytest.mark.skip(
-    reason="Skipping until the quantized_linear_fusion_pass is updated to work with non decomposed linear ops."
-)
+@parametrize("test_case", test_cases)
 def test_implementation_linear(test_case):
     tester = CortexMTester(test_case.model, test_case.example_inputs)
-    tester.test_implementation()
+    tester.test_implementation(qtol=1)
