8ab98085cfa3cd71e65289d0b50f26fab4001973
ssjia <ssjia@devvm1479.ncg0.facebook.com>
2025-11-10 13:32:35 -0800

    [ET-VK] Move operator specific quantize/dequantize ops to QuantizeDequantize.cpp
    Pull Request resolved: https://github.com/pytorch/executorch/pull/15707
    
    Title says it all! This diff is mostly just moving code around for better organization. Specifically, functions that handle quantizing / dequantizing tensors are placed in a new file `QuantizeDequantize.cpp`.
    ghstack-source-id: 322214452
    @exported-using-ghexport
    
    Differential Revision: [D86674168](https://our.internmc.facebook.com/intern/diff/D86674168/)

---
+++ backends/vulkan/runtime/graph/ops/impl/QuantizeDequantize.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <executorch/backends/vulkan/runtime/graph/ComputeGraph.h>
+#include <executorch/backends/vulkan/runtime/graph/ops/impl/utils/QuantizationConfig.h>
+
+namespace vkcompute {
+
+//
+// General utils
+//
+
+bool is_gemv(ComputeGraph* graph, const ValueRef& fp_input);
+
+//
+// Quantize, Dequantize for Linear/Matmul
+//
+
+void add_quantize_and_pack_linear_input_node(
+    ComputeGraph& graph,
+    const QuantizationConfig& input_quant_config,
+    const ValueRef fp_input,
+    const ValueRef packed_input_scale,
+    const ValueRef packed_input_zp,
+    const ValueRef input_scale_data,
+    const ValueRef input_zp_data,
+    const ValueRef packed_int_input,
+    const ValueRef group_size);
+
+void add_quantize_and_pack_linear_input_with_sums_node(
+    ComputeGraph& graph,
+    const QuantizationConfig& input_quant_config,
+    const ValueRef fp_input,
+    const ValueRef int_input_sums,
+    const ValueRef packed_input_scales,
+    const ValueRef packed_input_zps,
+    const ValueRef packed_int_input,
+    const ValueRef group_size);
+
+//
+// Quantize, Dequantize for Convolution
+//
+
+void add_quantize_and_pack_q8ta_conv2d_input_node(
+    ComputeGraph& graph,
+    const ValueRef fp_input,
+    const ValueRef input_scale,
+    const ValueRef input_zp,
+    const ValueRef packed_int8_input);
+
+void add_unpack_and_dequantize_q8ta_conv2d_output_node(
+    ComputeGraph& graph,
+    const ValueRef packed_int8_output,
+    const ValueRef output_scale,
+    const ValueRef output_zp,
+    const ValueRef fp_output);
+
+} // namespace vkcompute
