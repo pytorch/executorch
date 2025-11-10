c560e1c9adb72ed15206c5fae64f22316c228b76
Kimish Patel <kimishpatel@fb.com>
2025-11-10 09:18:08 -0800

    [Executorch] Introduce caching cpu memory allocator
    Pull Request resolved: https://github.com/pytorch/executorch/pull/15611
    
    Meant to use this for temp allocator for kernels. Specifically for sdpa, it seems that on iOS there is a significant overhead coming from allocations
    ghstack-source-id: 322129662
    @exported-using-ghexport
    
    Differential Revision: [D85532079](https://our.internmc.facebook.com/intern/diff/D85532079/)

---
+++ extension/memory_allocator/cpu_caching_malloc_allocator.cpp
@@ -0,0 +1,89 @@
+#include <cstdlib>
+
+#include <executorch/extension/memory_allocator/cpu_caching_malloc_allocator.h>
+
+namespace executorch::extension {
+
+namespace {
+size_t get_alignment_adjusted_size(size_t size, size_t alignment) {
+  alignment = std::max(alignment, kDefaultAlignment);
+  if (size % alignment != 0) {
+    // Adjust size to the next multiple of alignment
+    // This is needed for aligned_alloc to work
+    return (size + alignment) & ~(alignment - 1);
+  } else {
+    return size;
+  }
+}
+} // namespace
+
+CPUCachingAllocator::CPUCachingAllocator(uint32_t max_size)
+    : MemoryAllocator(0, nullptr) {
+  max_size_ = max_size;
+  current_size_ = 0;
+}
+
+void* CPUCachingAllocator::allocate(size_t size, size_t alignment) {
+  EXECUTORCH_TRACK_ALLOCATION(prof_id(), size);
+
+  if (!isPowerOf2(alignment)) {
+    ET_LOG(Error, "Alignment %zu is not a power of 2", alignment);
+    return nullptr;
+  }
+  size = get_alignment_adjusted_size(size, alignment);
+
+  std::lock_guard<std::mutex> guard(mutex_);
+  const auto& it = available_map_.find(size);
+  if (it == available_map_.end() || it->second.empty()) {
+    if (current_size_ + size > max_size_) {
+      // Freeing while holding the lock will cause performance issues
+      // we probably should log how often this happens so as to allow
+      // for calling site to adjust the max_size_ parameter
+      free_cached();
+    }
+    void* ptr = std::aligned_alloc(alignment, size);
+    current_size_ += size;
+    if (ptr == nullptr) {
+      ET_LOG(Error, "Failed to allocate memory");
+      return nullptr;
+    }
+    allocation_map_[ptr] = size;
+    return ptr;
+  }
+  void* ptr = it->second.back();
+  it->second.pop_back();
+  allocation_map_[ptr] = size;
+  return ptr;
+}
+
+void CPUCachingAllocator::free_cached() {
+  // We dont lock mutex_ here because it will cause deadlock otherwise
+  // we could use recursive_mutex but we just design this differently since
+  // free_cache is not a public API anyways
+  for (const auto& it : available_map_) {
+    for (const auto ptr : it.second) {
+      std::free(ptr);
+    }
+  }
+  available_map_.clear();
+}
+
+void CPUCachingAllocator::reset() {
+  std::lock_guard<std::mutex> guard(mutex_);
+  for (auto& it : allocation_map_) {
+    void* ptr = it.first;
+    size_t alloc_size = it.second;
+    // Cache the memory
+    available_map_[alloc_size].push_back(ptr);
+    current_size_ -= alloc_size;
+  }
+  allocation_map_.clear();
+}
+
+CPUCachingAllocator::~CPUCachingAllocator() {
+  // destructor must be called in thread safe manner
+  reset();
+  free_cached();
+}
+
+} // namespace executorch::extension
