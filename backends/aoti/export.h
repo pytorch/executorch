69d5fce1bd24287f3411fbd746de2a44195226b3
Mengwei Liu <mengweiliu600267@gmail.com>
2025-11-11 09:25:25 -0800

    More fixes

--- backends/aoti/export.h
+++ backends/aoti/export.h
@@ -9,7 +9,7 @@
 #pragma once
 
 // Define export macro for Windows DLL
-// When building the aoti_cuda library, EXPORT_AOTI_FUNCTIONS is defined by
+// When building the aoti_cuda_backend library, EXPORT_AOTI_FUNCTIONS is defined by
 // CMake, which causes this macro to export symbols using __declspec(dllexport).
 // When consuming the library, the macro imports symbols using
 // __declspec(dllimport). On non-Windows platforms, the macro is empty and has
