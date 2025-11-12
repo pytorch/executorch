e4a3480aaa4bf743e337d4b04fd08a4c1adfc404
Mengwei Liu <larryliu0820@users.noreply.github.com>
2025-11-12 08:49:35 -0800

    Change aoti_cuda in executorch-config.cmake to aoti_cuda_backend (#15726)
    As titled.
    
    ### Summary
    [PLEASE REMOVE] See [CONTRIBUTING.md's Pull
    Requests](https://github.com/pytorch/executorch/blob/main/CONTRIBUTING.md#pull-requests)
    for ExecuTorch PR guidelines.
    
    [PLEASE REMOVE] If this PR closes an issue, please add a `Fixes
    #<issue-id>` line.
    
    [PLEASE REMOVE] If this PR introduces a fix or feature that should be
    the upcoming release notes, please add a "Release notes: <area>" label.
    For a list of available release notes labels, check out
    [CONTRIBUTING.md's Pull
    Requests](https://github.com/pytorch/executorch/blob/main/CONTRIBUTING.md#pull-requests).
    
    ### Test plan
    [PLEASE REMOVE] How did you test this PR? Please write down any manual
    commands you used and note down tests that you have written if
    applicable.

--- backends/aoti/export.h
+++ backends/aoti/export.h
@@ -9,9 +9,10 @@
 #pragma once
 
 // Define export macro for Windows DLL
-// When building the aoti_cuda library, EXPORT_AOTI_FUNCTIONS is defined by
-// CMake, which causes this macro to export symbols using __declspec(dllexport).
-// When consuming the library, the macro imports symbols using
+// When building the aoti_cuda_backend library, EXPORT_AOTI_FUNCTIONS is defined
+// by CMake, which causes this macro to export symbols using
+// __declspec(dllexport). When consuming the library, the macro imports symbols
+// using
 // __declspec(dllimport). On non-Windows platforms, the macro is empty and has
 // no effect.
 #ifdef _WIN32
