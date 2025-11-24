613849a74dcaf0f95c72ef32d149cdf3031af656
Ryan O'Shea <ryan.oshea3@arm.com>
2025-11-25 00:58:40 +0100

    Arm backend: Add support for int32 clamp
     Tosa does not support int32 dtype for clamp operator
     so instead we need to use Min/Max to implement clamp.
    
    Signed-off-by: Ryan O'Shea <ryan.oshea3@arm.com>
    Change-Id: Iea442901b2227610ebb5e5a0f1bca8d236e70d9d

--- backends/arm/operators/op_clamp.py
+++ backends/arm/operators/op_clamp.py
@@ -22,6 +22,7 @@ from executorch.backends.arm.operators.operator_validation_utils import (
 from executorch.backends.arm.tosa import TosaSpecification
 
 from executorch.backends.arm.tosa.mapping import TosaArg
+from executorch.backends.arm.tosa.utils import tosa_shape
 from torch.fx import Node
 
 
@@ -40,7 +41,6 @@ class ClampVisitor(NodeVisitor):
     def _get_min_max_arguments(
         self, node: Node, dtype: torch.dtype
     ) -> Tuple[int | float, int | float]:
-
         def cast_type(value: Any) -> int | float:
             if isinstance(value, int):
                 return value
@@ -91,13 +91,25 @@ class ClampVisitor(NodeVisitor):
         validate_valid_dtype(
             self.target,
             [inputs[0], output],
-            [ts.DType.INT8, ts.DType.INT16, ts.DType.FP16, ts.DType.FP32],
+            [
+                ts.DType.INT8,
+                ts.DType.INT16,
+                ts.DType.INT32,
+                ts.DType.FP16,
+                ts.DType.FP32,
+            ],
             output.tosa_spec,
         )
 
         node_input_dtype = node.meta["val"].dtype
         # NOTE: Quantization of the min/max arguments is handled by QuantizeClampArgumentsPass
         min_val, max_val = self._get_min_max_arguments(node, node_input_dtype)
+
+        if inputs[0].dtype == ts.DType.INT32:
+            self._define_int32_clamp(
+                node, tosa_graph, inputs, output, int(min_val), int(max_val)
+            )
+            return
 
         attr = ts.TosaSerializerAttribute()
         attr.ClampAttribute(
@@ -113,4 +125,61 @@ class ClampVisitor(NodeVisitor):
             [inputs[0].name],
             [output.name],
             attr,
+        )
+
+    def _define_int32_clamp(
+        self,
+        node: Node,
+        tosa_graph: Any,
+        inputs: List[TosaArg],
+        output: TosaArg,
+        min_val: int,
+        max_val: int,
+    ) -> None:
+        """Lower int32 clamp via MIN/MAX because TOSA lacks int32 CLAMP."""
+
+        broadcast_shape = list(tosa_shape(inputs[0].shape, inputs[0].dim_order))
+        const_shape = tuple(broadcast_shape)
+        numel = int(np.prod(broadcast_shape)) if broadcast_shape else 1
+
+        max_tensor = tosa_graph.addConst(
+            const_shape,
+            inputs[0].dtype,
+            [max_val] * numel,
+            name=f"{output.name}_int32_max",
+        )
+        min_tensor = tosa_graph.addConst(
+            const_shape,
+            inputs[0].dtype,
+            [min_val] * numel,
+            name=f"{output.name}_int32_min",
+        )
+
+        intermediate_name = f"{output.name}_int32_tmp"
+        tosa_graph.currRegion.currBasicBlock.addTensor(
+            intermediate_name,
+            list(tosa_shape(output.shape, output.dim_order)),
+            output.dtype,
+        )
+
+        min_attr = ts.TosaSerializerAttribute()
+        min_attr.MinimumAttribute(nan_mode=ts.NanPropagationMode.PROPAGATE)
+        self._serialize_operator(
+            node,
+            tosa_graph,
+            ts.Op.MINIMUM,
+            [inputs[0].name, max_tensor.name],
+            [intermediate_name],
+            min_attr,
+        )
+
+        max_attr = ts.TosaSerializerAttribute()
+        max_attr.MaximumAttribute(nan_mode=ts.NanPropagationMode.PROPAGATE)
+        self._serialize_operator(
+            node,
+            tosa_graph,
+            ts.Op.MAXIMUM,
+            [intermediate_name, min_tensor.name],
+            [output.name],
+            max_attr,
         )
