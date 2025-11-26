033c82470de901919b79585c7346ba50ffe1e29d
Adrian Lundell <adrian.lundell@arm.com>
2025-11-26 15:28:21 +0100

    Cortex-M backend: Set calibration set for MV3
    - Set seed=0 for testcase
    
    Signed-off-by: Saoirse Stewart <saoirse.stewart@arm.com>

--- backends/cortex_m/test/models/test_mobilenet_v3.py
+++ backends/cortex_m/test/models/test_mobilenet_v3.py
@@ -3,6 +3,9 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+import random
+
+import pytest
 import torch
 from executorch.backends.arm.test.common import parametrize
 
@@ -36,7 +39,20 @@ ops_after_transforms: dict[str, int] = {
     "executorch_exir_dialects_edge__ops_cortex_m_quantize_per_tensor_default": 2,
 }
 
-example_input = torch.randn(1, 3, 224, 224).to(memory_format=torch.channels_last)
+
+@pytest.fixture
+def static_seed():
+    random.seed(0)
+    torch.manual_seed(0)
+
+
+example_input = torch.randn(1, 3, 232, 232).to(memory_format=torch.channels_last)
+
+# Use bigger sample set for calibration.
+calibration_samples = [
+    (torch.randn(1, 3, 232, 232).to(memory_format=torch.channels_last),)
+    for i in (range(100))
+]
 
 test_cases = {
     "mobilenet_v3_small": McuTestCase(
@@ -49,16 +65,20 @@ test_cases = {
 
 
 @parametrize("test_case", test_cases)
-def test_dialect_mv3(test_case):
+def test_dialect_mv3(test_case, static_seed):
     tester = CortexMTester(test_case.model, test_case.example_inputs)
     tester.test_dialect(
         ops_before_transforms,
         ops_after_transforms,
         qtol=20,
+        calibration_samples=calibration_samples,
     )
 
 
 @parametrize("test_case", test_cases)
-def test_implementation_mv3(test_case):
+def test_implementation_mv3(test_case, static_seed):
     tester = CortexMTester(test_case.model, test_case.example_inputs)
-    tester.test_implementation(qtol=20)
+    tester.test_implementation(
+        qtol=20,
+        calibration_samples=calibration_samples,
+    )
