fe1bc8a31d28d10d359f8e03468001aa35a128b3
Adrian Lundell <36153706+AdrianLundell@users.noreply.github.com>
2025-12-02 14:15:09 +0100

    Cortex_M backend: Fuse clamp + hardswish decompostion (#16016)
    Adds quantization and fusion of clamp.
    
    This is in turn used to decompose the hardswish operator in two passes,
    one clamping the dynamic range before quantization, and one decomposing
    the reminder of the operation into a maximum and mul op.
    
    The tests in this patch exposes an issue in the runtime dim_order check
    as it cannot differ between channels_last/channels_first for tensors
    with C=1 or H=W=1. Therefore the check is removed and added as a TBD
    instead.
    
    Additionally fixes per_tensor quantization for conv2d.
    
    ---------
    
    Signed-off-by: Adrian Lundell <adrian.lundell@arm.com>

--- backends/cortex_m/passes/__init__.py
+++ backends/cortex_m/passes/__init__.py
@@ -4,7 +4,9 @@
 # LICENSE file in the root directory of this source tree.
 
 from .activation_fusion_pass import ActivationFusionPass  # noqa
+from .clamp_hardswish_pass import ClampHardswishPass  # noqa
 from .convert_to_cortex_m_pass import ConvertToCortexMPass  # noqa
+from .decompose_hardswish_pass import DecomposeHardswishPass  # noqa
 from .quantized_op_fusion_pass import QuantizedOpFusionPass  # noqa
 from .replace_quant_nodes_pass import ReplaceQuantNodesPass  # noqa
 from .cortex_m_pass_manager import CortexMPassManager  # noqa  # usort: skip
