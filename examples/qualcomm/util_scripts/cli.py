f243590851d9322b20c4c2c06478dd4d241f4944
chenweng-quic <168707118+chenweng-quic@users.noreply.github.com>
2025-12-02 10:00:11 +0800

    Update cli.py

--- examples/qualcomm/util_scripts/cli.py
+++ examples/qualcomm/util_scripts/cli.py
@@ -4,6 +4,9 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+# This tool supports the QC internal QA pipeline by quantizing, compiling,
+# and executing models under various configuration flags.
+
 import argparse
 import importlib
 import logging
