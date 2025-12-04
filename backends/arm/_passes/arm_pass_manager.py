ac0717b68e74a0a214599f247beca55f5ed86f75
Oscar Andersson <87121123+oscarandersson8218@users.noreply.github.com>
2025-12-04 20:06:06 +0100

    Arm backend: Move FuseConstantArgsPass (#16076)
    Move FuseConstantArgsPass to run after ComputeConstantOpsAOTPass to make
    sure that ops without input nodes are pre-computed first.
    
    
    Signed-off-by: Oscar Andersson <oscar.andersson@arm.com>

--- backends/arm/_passes/arm_pass_manager.py
+++ backends/arm/_passes/arm_pass_manager.py
@@ -244,7 +244,6 @@ class ArmPassManager(PassManager):
                 # passes. Ticket: MLETORCH-1540
                 DecomposeNotEqualPass(),
                 MatchArgRanksPass(exported_program),
-                FuseConstantArgsPass(exported_program),
             ]
         )
 
@@ -266,6 +265,7 @@ class ArmPassManager(PassManager):
                 DecomposeAvgPool2dPass(),
                 DecorateFp32toInt32CastingPass(),
                 ComputeConstantOpsAOTPass(exported_program),
+                FuseConstantArgsPass(exported_program),
                 ConvertExpandCopyToRepeatPass(),
                 UnsqueezeBeforeRepeatPass(),
                 DecomposeCumsumPass(exported_program),
