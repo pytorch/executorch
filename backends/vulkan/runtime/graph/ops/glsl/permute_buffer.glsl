fa5152a93a5354680ac71b700b9fb2a093ed41d2
ssjia <ssjia@devvm1479.ncg0.facebook.com>
2025-12-17 16:34:26 -0800

    [ET-VK][2/n] Remove dynamic array indexing in permute
    ## Context
    
    See D89226115
    
    ## Changes
    
    Removes dynamic array access from the permute shader by doing the following:
    
    1. Passing in permute order via specialization constant
    2. Using the hashed layout specialization constant to convert between linear index and tensor index
    
    Differential Revision: [D89421393](https://our.internmc.facebook.com/intern/diff/D89421393/)
    
    [ghstack-poisoned]

--- backends/vulkan/runtime/graph/ops/glsl/permute_buffer.glsl
+++ backends/vulkan/runtime/graph/ops/glsl/permute_buffer.glsl
@@ -26,9 +26,27 @@ ${layout_declare_tensor(B, "r", "t_inp", DTYPE, "buffer")}
 ${layout_declare_ubo(B, "BufferMetadata", "outp")}
 ${layout_declare_ubo(B, "BufferMetadata", "inp")}
 
-${layout_declare_ubo(B, "ivec4[DIMLIMIT_DIV4]", "permute_order")}
+${layout_declare_spec_const(C, "int", "outp_layout", "CONTIG_LAYOUT_INT")}
+${layout_declare_spec_const(C, "int", "inp_layout", "CONTIG_LAYOUT_INT")}
+${layout_declare_spec_const(C, "int", "permute_order", "0")}
 
 layout(local_size_x_id = 0, local_size_y_id = 1, local_size_z_id = 2) in;
+
+TensorIndex permute(TensorIndex tidx) {
+  TensorIndex new_tidx = tidx;
+
+  new_tidx.data[0][0] = idx_at(tidx, extract_4b(permute_order, 0));
+  new_tidx.data[0][1] = idx_at(tidx, extract_4b(permute_order, 1));
+  new_tidx.data[0][2] = idx_at(tidx, extract_4b(permute_order, 2));
+  new_tidx.data[0][3] = idx_at(tidx, extract_4b(permute_order, 3));
+
+  new_tidx.data[1][0] = idx_at(tidx, extract_4b(permute_order, 4));
+  new_tidx.data[1][1] = idx_at(tidx, extract_4b(permute_order, 5));
+  new_tidx.data[1][2] = idx_at(tidx, extract_4b(permute_order, 6));
+  new_tidx.data[1][3] = idx_at(tidx, extract_4b(permute_order, 7));
+
+  return new_tidx;
+}
 
 void main() {
   const uint inp_bufi = gl_GlobalInvocationID.x;
@@ -36,12 +54,10 @@ void main() {
     return;
   }
 
-  TensorIndex inp_tidx = linear_idx_to_tensor_idx(inp, inp_bufi);
-
-  TensorIndex outp_tidx = inp_tidx;
-  permute(outp_tidx, permute_order);
-
+  TensorIndex inp_tidx = linear_idx_to_tensor_idx(inp, inp_bufi, inp_layout);
+  TensorIndex outp_tidx = permute(inp_tidx);
   const uint outp_bufi = tensor_idx_to_linear_idx(outp, outp_tidx);
+
   // Copy data from input to output
   t_outp[outp_bufi] = t_inp[inp_bufi];
 }
