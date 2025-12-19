436ca0a27315dee8f1bd6405b867f0529365be57
gasoonjia <gasoonjia@icloud.com>
2025-12-18 16:09:37 -0800

    [slimtensor migration 5/n] reuse et c10 headers
    This stack aims to migrate slim tensor into ExecuTorch stack to make it as internal tensor representation of cudabackend.
    
    This diff makes slimtensor depends on pytorch/pytorch c10 headers living in ExecuTorch space by creating a lean wrapper on it to avoid large amout of migration work.
    
    Differential Revision: [D89505483](https://our.internmc.facebook.com/intern/diff/D89505483/)
    
    [ghstack-poisoned]

--- backends/aoti/slim/c10/util/safe_numerics.h
+++ backends/aoti/slim/c10/util/safe_numerics.h
@@ -1,95 +1,27 @@
 #pragma once
-#include <executorch/backends/aoti/slim/c10/macros/Macros.h>
 
-#include <cstdint>
+// Thin wrapper to reuse ExecuTorch's c10 safe_numerics implementation.
+// This provides backward compatibility for SlimTensor code that uses
+// executorch::backends::aoti::slim::c10::{safe_multiplies_u64, add_overflows,
+// mul_overflows}.
+//
+// NOTE: multiply_integers is defined in accumulate.h (SlimTensor-specific).
+// NOTE: sub_overflows is not available in ET c10 safe_numerics.
 
-// GCC has __builtin_mul_overflow from before it supported __has_builtin
-#ifdef _MSC_VER
-#define STANDALONE_HAS_BUILTIN_OVERFLOW() (0)
-#include <executorch/backends/aoti/slim/c10/util/llvmMathExtras.h>
-#include <intrin.h>
-#else
-#define STANDALONE_HAS_BUILTIN_OVERFLOW() (1)
-#endif
+#include <c10/util/safe_numerics.h>
 
-namespace executorch::backends::aoti::slim::c10 {
+namespace executorch {
+namespace backends {
+namespace aoti {
+namespace slim {
+namespace c10 {
 
-STANDALONE_ALWAYS_INLINE bool
-add_overflows(uint64_t a, uint64_t b, uint64_t* out) {
-#if STANDALONE_HAS_BUILTIN_OVERFLOW()
-  return __builtin_add_overflow(a, b, out);
-#else
-  unsigned long long tmp;
-#if defined(_M_IX86) || defined(_M_X64)
-  auto carry = _addcarry_u64(0, a, b, &tmp);
-#else
-  tmp = a + b;
-  unsigned long long vector = (a & b) ^ ((a ^ b) & ~tmp);
-  auto carry = vector >> 63;
-#endif
-  *out = tmp;
-  return carry;
-#endif
-}
+using ::c10::add_overflows;
+using ::c10::mul_overflows;
+using ::c10::safe_multiplies_u64;
 
-STANDALONE_ALWAYS_INLINE bool
-mul_overflows(uint64_t a, uint64_t b, uint64_t* out) {
-#if STANDALONE_HAS_BUILTIN_OVERFLOW()
-  return __builtin_mul_overflow(a, b, out);
-#else
-  *out = a * b;
-  // This test isnt exact, but avoids doing integer division
-  return (
-      (executorch::backends::aoti::slim::c10::llvm::countLeadingZeros(a) +
-       executorch::backends::aoti::slim::c10::llvm::countLeadingZeros(b)) < 64);
-#endif
-}
-
-STANDALONE_ALWAYS_INLINE bool
-mul_overflows(int64_t a, int64_t b, int64_t* out) {
-#if STANDALONE_HAS_BUILTIN_OVERFLOW()
-  return __builtin_mul_overflow(a, b, out);
-#else
-  volatile int64_t tmp = a * b;
-  *out = tmp;
-  if (a == 0 || b == 0) {
-    return false;
-  }
-  return !(a == tmp / b);
-#endif
-}
-
-template <typename It>
-bool safe_multiplies_u64(It first, It last, uint64_t* out) {
-#if STANDALONE_HAS_BUILTIN_OVERFLOW()
-  uint64_t prod = 1;
-  bool overflow = false;
-  for (; first != last; ++first) {
-    overflow |= executorch::backends::aoti::slim::c10::mul_overflows(
-        prod, *first, &prod);
-  }
-  *out = prod;
-  return overflow;
-#else
-  uint64_t prod = 1;
-  uint64_t prod_log2 = 0;
-  bool is_zero = false;
-  for (; first != last; ++first) {
-    auto x = static_cast<uint64_t>(*first);
-    prod *= x;
-    // log2(0) isn't valid, so need to track it specially
-    is_zero |= (x == 0);
-    prod_log2 += executorch::backends::aoti::slim::c10::llvm::Log2_64_Ceil(x);
-  }
-  *out = prod;
-  // This test isnt exact, but avoids doing integer division
-  return !is_zero && (prod_log2 >= 64);
-#endif
-}
-
-template <typename Container>
-bool safe_multiplies_u64(const Container& c, uint64_t* out) {
-  return safe_multiplies_u64(c.begin(), c.end(), out);
-}
-
-} // namespace executorch::backends::aoti::slim::c10
+} // namespace c10
+} // namespace slim
+} // namespace aoti
+} // namespace backends
+} // namespace executorch
