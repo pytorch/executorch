8c30e9326a843e9b0051620cecac7db63225a263
Martin Lindström <Martin.Lindstroem@arm.com>
2026-02-05 11:32:38 +0100

    Arm backend: Add FP16 support to operators pt.9
    Add fp16 support for ops:
     - le
     - lt
     - repeat
     - transpose conv2d
     - resize
    
    Signed-off-by: Martin Lindström <Martin.Lindstroem@arm.com>
    Change-Id: Ic0040601338c50f923131ca1ebe29f6a1480825c

--- backends/arm/test/ops/test_le.py
+++ backends/arm/test/ops/test_le.py
@@ -1,4 +1,4 @@
-# Copyright 2025 Arm Limited and/or its affiliates.
+# Copyright 2025-2026 Arm Limited and/or its affiliates.
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
@@ -69,6 +69,16 @@ test_data_tensor = {
     "le_tensor_rank3_randn": lambda: op_le_tensor_rank3_randn,
     "le_tensor_rank4_randn": lambda: op_le_tensor_rank4_randn,
 }
+test_data_tensor_fp16 = {
+    "le_tensor_rank2_rand_fp16": lambda: LessEqual(
+        torch.rand(4, 5, dtype=torch.float16),
+        torch.rand(1, 5, dtype=torch.float16),
+    ),
+    "le_tensor_rank3_randn_fp16": lambda: LessEqual(
+        torch.randn(2, 3, 4, dtype=torch.float16),
+        torch.randn(2, 3, 4, dtype=torch.float16),
+    ),
+}
 
 test_data_scalar = {
     "le_scalar_rank1_ones": lambda: op_le_scalar_rank1_ones,
@@ -76,9 +86,17 @@ test_data_scalar = {
     "le_scalar_rank3_randn": lambda: op_le_scalar_rank3_randn,
     "le_scalar_rank4_randn": lambda: op_le_scalar_rank4_randn,
 }
+test_data_scalar_fp16 = {
+    "le_scalar_rank2_rand_fp16": lambda: LessEqual(
+        torch.rand(4, 5, dtype=torch.float16), 0.2
+    ),
+    "le_scalar_rank3_randn_fp16": lambda: LessEqual(
+        torch.randn(2, 3, 4, dtype=torch.float16), -0.1
+    ),
+}
 
 
-@common.parametrize("test_module", test_data_tensor)
+@common.parametrize("test_module", test_data_tensor | test_data_tensor_fp16)
 def test_le_tensor_tosa_FP(test_module):
     pipeline = TosaPipelineFP[input_t](
         test_module(),
@@ -89,7 +107,7 @@ def test_le_tensor_tosa_FP(test_module):
     pipeline.run()
 
 
-@common.parametrize("test_module", test_data_scalar)
+@common.parametrize("test_module", test_data_scalar | test_data_scalar_fp16)
 def test_le_scalar_tosa_FP(test_module):
     pipeline = TosaPipelineFP[input_t](
         test_module(),
@@ -244,7 +262,7 @@ def test_le_scalar_16a8w_u85_INT(test_module):
     pipeline.run()
 
 
-@common.parametrize("test_module", test_data_tensor)
+@common.parametrize("test_module", test_data_tensor | test_data_tensor_fp16)
 @common.SkipIfNoModelConverter
 def test_le_tensor_vgf_no_quant(test_module):
     pipeline = VgfPipeline[input_t](
@@ -270,7 +288,7 @@ def test_le_tensor_vgf_quant(test_module):
     pipeline.run()
 
 
-@common.parametrize("test_module", test_data_scalar)
+@common.parametrize("test_module", test_data_scalar | test_data_scalar_fp16)
 @common.SkipIfNoModelConverter
 def test_le_scalar_vgf_no_quant(test_module):
     pipeline = VgfPipeline[input_t](
