20787cbb812bb91a25f032d25245030e581f2bd8
Sebastian Larsson <sebastian.larsson@arm.com>
2026-02-10 15:25:20 +0100

    Arm backend: Consolidate simple operator visitors
    Signed-off-by: Sebastian Larsson <sebastian.larsson@arm.com>
    Change-Id: I23339f808f1074adea1fafddf90110c04fc5695f

--- backends/arm/operators/op_add.py
+++ backends/arm/operators/op_add.py
@@ -3,52 +3,24 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-
-from typing import Any, List
-
 import tosa_serializer as ts
 
-from executorch.backends.arm.operators.node_visitor import (
-    NodeVisitor,
-    register_node_visitor,
+from executorch.backends.arm.operators.node_visitor import register_node_visitor
+from executorch.backends.arm.operators.simple_node_visitor import (
+    SimpleNodeVisitor,
+    SimpleNodeVisitorConfig,
 )
-from executorch.backends.arm.operators.operator_validation_utils import (
-    validate_num_inputs,
-    validate_same_dtype,
-    validate_valid_dtype,
-)
-from executorch.backends.arm.tosa.mapping import TosaArg
-from torch.fx import Node
 
 
 @register_node_visitor
-class AddVisitor(NodeVisitor):
+class AddVisitor(SimpleNodeVisitor):
     target = "aten.add.Tensor"
 
-    def define_node(
-        self,
-        node: Node,
-        tosa_graph: Any,
-        inputs: List[TosaArg],
-        output: TosaArg,
-    ) -> None:
-        validate_num_inputs(self.target, inputs, 2)
-        validate_same_dtype(self.target, [*inputs, output], ts)
-        validate_valid_dtype(
-            self.target,
-            [*inputs, output],
-            [ts.DType.INT32, ts.DType.FP16, ts.DType.FP32, ts.DType.BF16],
-            self.tosa_spec,
-        )
-
-        attr = ts.TosaSerializerAttribute()
-        attr.AddAttribute()
-
-        self._serialize_operator(
-            node,
-            tosa_graph,
-            ts.Op.ADD,
-            [inputs[0].name, inputs[1].name],
-            [output.name],
-            attr,
+    @classmethod
+    def get_config(cls) -> SimpleNodeVisitorConfig:
+        return SimpleNodeVisitorConfig(
+            tosa_op=ts.Op.ADD,
+            attr_method="AddAttribute",
+            num_inputs=2,
+            input_dtypes=[ts.DType.INT32, ts.DType.FP16, ts.DType.FP32, ts.DType.BF16],
         )
