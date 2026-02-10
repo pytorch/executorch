ed41c7a6107332a209fd76961cfec97c7f9dd819
Abdelaziz Mahdy <zezohassam@gmail.com>
2026-02-10 18:57:13 -0400

    Revert "Fix missing vmaFlushAllocation in StagingBuffer"
    This reverts commit 950906427dcc03255b2af992103aa6509fa7f590.

--- backends/vulkan/runtime/api/containers/StagingBuffer.h
+++ backends/vulkan/runtime/api/containers/StagingBuffer.h
@@ -88,11 +88,6 @@ class StagingBuffer final {
     for (size_t i = 0; i < numel; ++i) {
       dst[i] = static_cast<DST_T>(src[i]);
     }
-    vmaFlushAllocation(
-        vulkan_buffer_.vma_allocator(),
-        vulkan_buffer_.allocation(),
-        0u,
-        VK_WHOLE_SIZE);
   }
 
   void cast_half_to_float_and_copy_from(
@@ -122,11 +117,6 @@ class StagingBuffer final {
 
   inline void set_staging_zeros() {
     memset(data(), 0, nbytes());
-    vmaFlushAllocation(
-        vulkan_buffer_.vma_allocator(),
-        vulkan_buffer_.allocation(),
-        0u,
-        VK_WHOLE_SIZE);
   }
 
   template <typename T>
