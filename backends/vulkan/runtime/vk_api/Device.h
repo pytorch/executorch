8d6f4561d753be6d253d0411c93c49abb916e4b2
Hongbing Hu <harrishhu@gmail.com>
2026-02-19 14:17:36 -0800

    [ET-VK] Add NV cooperative matrix extension
    Differential Revision: D92570456
    
    Pull Request resolved: https://github.com/pytorch/executorch/pull/17544

--- backends/vulkan/runtime/vk_api/Device.h
+++ backends/vulkan/runtime/vk_api/Device.h
@@ -52,6 +52,14 @@ struct PhysicalDevice final {
       shader_int_dot_product_properties;
 #endif /* VK_KHR_shader_integer_dot_product */
 
+#ifdef VK_KHR_cooperative_matrix
+  VkPhysicalDeviceCooperativeMatrixFeaturesKHR cooperative_matrix_features;
+#endif /* VK_KHR_cooperative_matrix */
+
+#ifdef VK_NV_cooperative_matrix2
+  VkPhysicalDeviceCooperativeMatrix2FeaturesNV cooperative_matrix2_features;
+#endif /* VK_NV_cooperative_matrix2 */
+
   // Available GPU queues
   std::vector<VkQueueFamilyProperties> queue_families;
 
