a3ee2405a109c857945d94bf9298d29762ef3551
Siddartha Pothapragada <sidart@meta.com>
2026-02-19 13:04:39 -0800

    Fix multi_modal_runner initialization with mode: MmapUseMlockIgnoreErrors

--- extension/android/jni/jni_layer_llama.cpp
+++ extension/android/jni/jni_layer_llama.cpp
@@ -183,9 +183,11 @@ class ExecuTorchLlmJni : public facebook::jni::HybridClass<ExecuTorchLlmJni> {
     model_type_category_ = model_type_category;
     std::vector<std::string> data_files_vector;
     if (model_type_category == MODEL_TYPE_CATEGORY_MULTIMODAL) {
-      multi_modal_runner_ = llm::create_multimodal_runner(
+       multi_modal_runner_ = llm::create_multimodal_runner(
           model_path->toStdString().c_str(),
-          llm::load_tokenizer(tokenizer_path->toStdString()));
+          llm::load_tokenizer(tokenizer_path->toStdString()),
+          std::nullopt,
+          executorch::extension::Module::LoadMode::MmapUseMlockIgnoreErrors);
     } else if (model_type_category == MODEL_TYPE_CATEGORY_LLM) {
       if (data_files != nullptr) {
         // Convert Java List<String> to C++ std::vector<string>
