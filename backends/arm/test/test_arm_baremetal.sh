64b461b0c356906638ef88ac9e25adc94e6cb4ef
Måns Nilsson <mans.nilsson@arm.com>
2026-02-19 09:56:04 +0100

    Arm backend: Reduce log level in test_smaller_stories_llama
    Change-Id: Ife03e7cb43d3a51b6784dafc75dcbe9499be1ca4
    
    Signed-off-by: Måns Nilsson <mans.nilsson@arm.com>
    Change-Id: I763a35a027c84071d2428ee9b04a146b1baf6fd9

--- backends/arm/test/test_arm_baremetal.sh
+++ backends/arm/test/test_arm_baremetal.sh
@@ -298,7 +298,6 @@ test_smaller_stories_llama() {
     --numprocesses=auto \
     --log-level=DEBUG \
     --junit-xml=stories110M/test-reports/unittest.xml \
-    -s \
     backends/arm/test/models/test_llama.py \
     --llama_inputs stories110M/stories110M.pt stories110M/params.json stories110m
 
