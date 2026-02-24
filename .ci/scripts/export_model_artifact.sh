d091abc09a4e5278e4c4d6738d63072a2faa51b5
Manuel Candales <mcandales@meta.com>
2026-02-24 13:56:25 -0500

    fix identation

--- .ci/scripts/export_model_artifact.sh
+++ .ci/scripts/export_model_artifact.sh
@@ -262,7 +262,7 @@ if [ "$MODEL_NAME" = "voxtral_realtime" ]; then
   elif [ "$MODE" = "vr-offline" ]; then
     USE_STREAMING="false"
   elif [ -z "$MODE" ]; then
-  if [ "$DEVICE" = "xnnpack" ] || [ "$DEVICE" = "metal" ]; then
+    if [ "$DEVICE" = "xnnpack" ] || [ "$DEVICE" = "metal" ]; then
       USE_STREAMING="true"
     fi
   fi
