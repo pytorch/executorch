795218e58c2311bb6f70fb71dffeb2ed24806ebb
Anthony Shoumikhin <shoumikhin@meta.com>
2026-02-28 12:46:32 -0800

    Add directory structure and initialization
    Summary:
    Establish the foundation for integrating TensorRT as an ExecuTorch delegate backend.
    
    TensorRT enables GPU-accelerated inference on NVIDIA hardware. This initial
    commit sets up the project structure and build configuration to support both
    OSS (CMake) and internal (Buck) builds, without functional code.
    
    Differential Revision: D93275046

---
+++ backends/nvidia/tensorrt/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""TensorRT backend for ExecuTorch.
+
+This module provides TensorRT delegate support for accelerating
+PyTorch models on NVIDIA GPUs.
+"""
