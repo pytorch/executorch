cb90f6b9a70a077e9002e45831ff9a6d33069559
Sebastian Larsson <38941629+Sebastian-Larsson@users.noreply.github.com>
2026-03-03 15:53:23 +0100

    Arm backend: Format docs for remaining files in backends/arm/test/ (#17806)
    Signed-off-by: Sebastian Larsson <sebastian.larsson@arm.com>

--- backends/arm/test/models/test_nn_functional.py
+++ backends/arm/test/models/test_nn_functional.py
@@ -2,7 +2,6 @@
 
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
-
 """Tests 10 popular torch.nn.functional not tested in other ways or training
 related.
 
