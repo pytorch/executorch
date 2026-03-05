0455bf0f474c0d78fb2d6490810c4a8210aaff96
shoumikhin <shoumikhin@devvm28124.vll0.facebook.com>
2026-03-05 02:34:24 -0800

    [executorch][nvidia][tensorrt][2/n] Add TensorRT backend stub
    Add initial backend stub with empty preprocess implementation and TensorRT backend registration.
    
    Differential Revision: [D93275053](https://our.internmc.facebook.com/intern/diff/D93275053/)
    
    ghstack-source-id: 347879273
    Pull Request resolved: https://github.com/pytorch/executorch/pull/17876

--- backends/nvidia/tensorrt/__init__.py
+++ backends/nvidia/tensorrt/__init__.py
@@ -9,3 +9,25 @@
 This module provides TensorRT delegate support for accelerating
 PyTorch models on NVIDIA GPUs.
 """
+
+import os
+import platform
+import sys
+
+# On Jetson (aarch64 with JetPack), make system and user-installed packages
+# available inside venvs. This is needed for TensorRT (JetPack system package)
+# and optionally for packages like onnx installed via pip at system level.
+if platform.machine() == "aarch64" and os.path.exists("/etc/nv_tegra_release"):
+    _py_ver = f"python{sys.version_info.major}.{sys.version_info.minor}"
+    for _pkg_dir in [
+        f"/usr/lib/{_py_ver}/dist-packages",
+        os.path.expanduser(f"~/.local/lib/{_py_ver}/site-packages"),
+    ]:
+        if os.path.isdir(_pkg_dir) and _pkg_dir not in sys.path:
+            sys.path.append(_pkg_dir)
+
+from executorch.backends.nvidia.tensorrt.backend import TensorRTBackend
+
+__all__ = [
+    "TensorRTBackend",
+]
