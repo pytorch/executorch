bbe9956a3f0e996096ccdad076e905a9c529c2d3
shoumikhin <shoumikhin@devvm28124.vll0.facebook.com>
2026-03-06 11:52:50 -0800

    [executorch][nvidia][tensorrt][7/n] Add converter utils and add converter
    Add utility functions for TensorRT converter implementations and the add operation converter.
    
    Differential Revision: [D93275044](https://our.internmc.facebook.com/intern/diff/D93275044/)
    
    [ghstack-poisoned]

--- backends/nvidia/tensorrt/test/__init__.py
+++ backends/nvidia/tensorrt/test/__init__.py
@@ -4,7 +4,4 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-"""TensorRT converters for ExecuTorch operations."""
-
-# Import converters to trigger registration via @converter decorator
-from executorch.backends.nvidia.tensorrt.converters import add  # noqa: F401
+"""Tests for TensorRT backend."""
