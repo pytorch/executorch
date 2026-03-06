f7127609ef38460ab64f89571dd4b7b23ec6143e
shoumikhin <shoumikhin@devvm28124.vll0.facebook.com>
2026-03-06 11:53:29 -0800

    [executorch][nvidia][tensorrt][16/n] Enable MobileNetV3 model support
    Add converters and optimizations to enable MobileNetV3 model support with the TensorRT backend.
    
    Differential Revision: [D93275043](https://our.internmc.facebook.com/intern/diff/D93275043/)
    
    ghstack-source-id: 348631671
    Pull Request resolved: https://github.com/pytorch/executorch/pull/17969

--- backends/nvidia/tensorrt/converters/__init__.py
+++ backends/nvidia/tensorrt/converters/__init__.py
@@ -7,11 +7,29 @@
 """TensorRT converters for ExecuTorch operations."""
 
 # Import converters to trigger registration via @converter decorator
+from executorch.backends.nvidia.tensorrt.converters import activations  # noqa: F401
 from executorch.backends.nvidia.tensorrt.converters import add  # noqa: F401
 from executorch.backends.nvidia.tensorrt.converters import addmm  # noqa: F401
+from executorch.backends.nvidia.tensorrt.converters import batch_norm  # noqa: F401
+from executorch.backends.nvidia.tensorrt.converters import clamp  # noqa: F401
+from executorch.backends.nvidia.tensorrt.converters import concat  # noqa: F401
+from executorch.backends.nvidia.tensorrt.converters import conv2d  # noqa: F401
+from executorch.backends.nvidia.tensorrt.converters import dim_order_ops  # noqa: F401
 from executorch.backends.nvidia.tensorrt.converters import div  # noqa: F401
+from executorch.backends.nvidia.tensorrt.converters import getitem  # noqa: F401
+from executorch.backends.nvidia.tensorrt.converters import linear  # noqa: F401
 from executorch.backends.nvidia.tensorrt.converters import mm  # noqa: F401
 from executorch.backends.nvidia.tensorrt.converters import mul  # noqa: F401
 from executorch.backends.nvidia.tensorrt.converters import permute_copy  # noqa: F401
+from executorch.backends.nvidia.tensorrt.converters import pooling  # noqa: F401
+from executorch.backends.nvidia.tensorrt.converters import reduction  # noqa: F401
 from executorch.backends.nvidia.tensorrt.converters import relu  # noqa: F401
+from executorch.backends.nvidia.tensorrt.converters import reshape  # noqa: F401
 from executorch.backends.nvidia.tensorrt.converters import sub  # noqa: F401
+
+
+def clear_converter_weight_storage() -> None:
+    """Clear weight storage to free memory after engine build."""
+    conv2d.clear_weight_storage()
+    batch_norm.clear_weight_storage()
+    linear.clear_weight_storage()
