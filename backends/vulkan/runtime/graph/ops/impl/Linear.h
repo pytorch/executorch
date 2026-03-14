7a63aff49f6c9c269a9bb67bddfffd93232e3aca
Sicheng Stephen Jia <ssjia@meta.com>
2026-03-14 04:48:47 -0400

    [ET-VK][matmul] Re-implement fp32/fp16 matmul and linear with tiled compute and blocked weight packing
    Differential Revision: D96488384
    
    Pull Request resolved: https://github.com/pytorch/executorch/pull/18171

---
+++ backends/vulkan/runtime/graph/ops/impl/Linear.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <executorch/backends/vulkan/runtime/graph/ComputeGraph.h>
+
+namespace vkcompute {
+
+ValueRef prepack_fp_linear_weight(
+    ComputeGraph& graph,
+    const ValueRef weight_data,
+    bool is_transposed,
+    int64_t B);
+
+void add_linear_tiled_node(
+    ComputeGraph& graph,
+    const ValueRef input,
+    const ValueRef packed_weight,
+    const ValueRef packed_bias,
+    bool has_bias,
+    const ValueRef out,
+    int32_t weight_B = 1,
+    float alpha = 1.0f,
+    float beta = 1.0f);
+
+} // namespace vkcompute
