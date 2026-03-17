3a3c88880102bf8cbe7070c43831fef77c7d307c
Jonathan Piat <piat.jonathan@gmail.com>
2026-03-17 11:27:19 +0100

    Fixing issue with min function not taking different type on its input on xtensa.

--- extension/threadpool/threadpool.cpp
+++ extension/threadpool/threadpool.cpp
@@ -145,7 +145,7 @@ ThreadPool* get_threadpool() {
      * tricky to detect if we are running under tsan, for now capping the
      * default threadcount to the tsan limit unconditionally.
      */
-    constexpr unsigned int tsan_thread_limit = 63;
+    constexpr long unsigned int tsan_thread_limit = 63;
     return std::min(result, tsan_thread_limit);
   })();
 
