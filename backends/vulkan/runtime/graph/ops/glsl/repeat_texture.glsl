bca9be7d82317de6fb5f80a0bd658c49f288f7e2
ssjia <ssjia@devgpu053.atn3.facebook.com>
2026-03-17 14:19:23 -0700

    [ET-VK] Modernize repeat
    Modernize repeat to support ANY_STORAGE. Rewrite texture shader to use
    TextureMetadata with indexing.glslh helpers for coordinate conversion.
    Add buffer shader variant using BufferMetadata. Unify dispatch to use
    graph.meta_ubo() for both paths. Add symint support for dynamic repeat
    counts.
    
    Pull Request resolved: https://github.com/pytorch/executorch/pull/18056
    ghstack-source-id: 353546685
    @exported-using-ghexport
    
    Differential Revision: [D95970170](https://our.internmc.facebook.com/intern/diff/D95970170/)

---
+++ backends/vulkan/runtime/graph/ops/glsl/repeat_texture.glsl
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#version 450 core
+
+${define_required_extensions("texture3d", DTYPE)}
+
+#define PRECISION ${PRECISION}
+
+#define VEC4_T ${texel_load_type(DTYPE, "texture3d")}
+
+${define_active_storage_type("texture3d")}
+
+#extension GL_EXT_control_flow_attributes : require
+
+layout(std430) buffer;
+
+#include "common.glslh"
+#include "indexing.glslh"
+
+${layout_declare_tensor(B, "w", "t_out", DTYPE, "texture3d")}
+${layout_declare_tensor(B, "r", "t_in", DTYPE, "texture3d")}
+
+${layout_declare_ubo(B, "TextureMetadata", "out_meta")}
+${layout_declare_ubo(B, "TextureMetadata", "in_meta")}
+
+layout(local_size_x_id = 0, local_size_y_id = 1, local_size_z_id = 2) in;
+
+${layout_declare_spec_const(C, "int", "out_layout", "CONTIG_LAYOUT_INT")}
+const int packed_dim = get_packed_dim(out_layout);
+
+void main() {
+  const ivec3 out_pos = ivec3(gl_GlobalInvocationID);
+
+  if (out_of_bounds(out_pos, out_meta)) {
+    return;
+  }
+
+  TensorIndex4D out_tidx = texture_pos_to_tensor4d_idx_simple(out_meta, out_pos);
+
+  VEC4_T out_texel = VEC4_T(0);
+
+  const int limit = min(
+      4, out_meta.sizes[packed_dim] - out_tidx.data[packed_dim]);
+  for (int comp = 0; comp < limit; comp++) {
+    TensorIndex4D in_tidx = out_tidx;
+    in_tidx.data = ivec4(
+        out_tidx.data.x % in_meta.sizes.x,
+        out_tidx.data.y % in_meta.sizes.y,
+        out_tidx.data.z % in_meta.sizes.z,
+        out_tidx.data.w % in_meta.sizes.w);
+
+    TextureElementIndex in_elem =
+        tensor4d_idx_to_texture_element_idx_simple(in_meta, in_tidx);
+
+    VEC4_T in_texel = texelFetch(t_in, in_elem.pos, 0);
+    out_texel[comp] = in_texel[in_elem.comp];
+
+    out_tidx.data[packed_dim]++;
+  }
+
+  imageStore(t_out, out_pos, out_texel);
+}
