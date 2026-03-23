31a15a47cc8e9fa9864c26f86a86cd3852a1a24a
Adrian Lundell <36153706+AdrianLundell@users.noreply.github.com>
2026-03-23 10:10:24 +0100

    Arm backend: Add amin support for Ethos-U55 (#18367)
    Amin support was enabled in Vela 5.0.0
    
    
    Signed-off-by: Adrian Lundell <adrian.lundell@arm.com>

--- backends/arm/operator_support/ethos_u55_support.py
+++ backends/arm/operator_support/ethos_u55_support.py
@@ -191,7 +191,6 @@ class EthosU55NotSupported(OperatorSupportBase):
         exir_ops.edge.aten.logical_or.default,
         exir_ops.edge.aten.logical_xor.default,
         exir_ops.edge.aten.logical_not.default,
-        exir_ops.edge.aten.amin.default,  # REDUCE_MIN
         exir_ops.edge.aten.conv3d.default,  # CONV3D
         exir_ops.edge.aten.conv3d.padding,  # CONV3D (deprecated alias)
         exir_ops.edge.aten.eq.Tensor,
