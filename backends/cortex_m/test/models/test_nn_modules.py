d8e939411144f4e52951f5a5bce4db7df6755f04
Adrian Lundell <36153706+AdrianLundell@users.noreply.github.com>
2026-03-27 14:54:46 +0100

    Cortex-M backend: Enable test linting (#18489)
    Signed-off-by: Adrian Lundell <adrian.lundell@arm.com>

--- backends/cortex_m/test/models/test_nn_modules.py
+++ backends/cortex_m/test/models/test_nn_modules.py
@@ -24,7 +24,7 @@ Modules tested:
 """
 
 import torch
-from executorch.backends.arm.test.common import parametrize
+from executorch.backends.arm.test.common import parametrize, xfail_type
 from executorch.backends.cortex_m.test.tester import (
     CortexMTester,
     McuTestCase,
@@ -188,7 +188,7 @@ test_cases = {
     ),
 }
 
-xfails = {
+xfails: dict[str, xfail_type] = {
     "conv_add_relu": "Activation fusion does not support relu after add",
 }
 
