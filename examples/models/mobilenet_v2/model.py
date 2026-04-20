fba84e7780a408c2b45697676c41eea318d5ad05
Jacob Szwejbka <jakeszwe@fb.com>
2026-04-20 08:47:54 -0700

    delete real image

--- examples/models/mobilenet_v2/model.py
+++ examples/models/mobilenet_v2/model.py
@@ -15,8 +15,7 @@ from ..model_base import EagerModelBase
 
 
 class MV2Model(EagerModelBase):
-    def __init__(self, use_real_input=True):
-        self.use_real_input = use_real_input
+    def __init__(self):
         pass
 
     def get_eager_model(self) -> torch.nn.Module:
@@ -26,37 +25,7 @@ class MV2Model(EagerModelBase):
         return mv2
 
     def get_example_inputs(self):
-        tensor_size = (1, 3, 224, 224)
-        if self.use_real_input:
-            try:
-                import urllib.request
-
-                url, filename = (
-                    "https://github.com/pytorch/hub/raw/master/images/dog.jpg",
-                    "dog.jpg",
-                )
-                urllib.request.urlretrieve(url, filename)
-                from PIL import Image
-                from torchvision import transforms
-
-                input_image = Image.open(filename)
-                preprocess = transforms.Compose(
-                    [
-                        transforms.Resize(256),
-                        transforms.CenterCrop(224),
-                        transforms.ToTensor(),
-                        transforms.Normalize(
-                            mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-                        ),
-                    ]
-                )
-                input_tensor = preprocess(input_image)
-                return (input_tensor.unsqueeze(0),)
-            except Exception:
-                logging.warning(
-                    "Failed to download real input image, using random input"
-                )
-        return (torch.randn(tensor_size),)
+        return (torch.randn(1, 3, 224, 224),)
 
 
 class MV2UntrainedModel(EagerModelBase):
