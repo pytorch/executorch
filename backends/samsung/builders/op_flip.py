d05ceaee6927e2cada5ab48d87a637cdf24d2174
Jiseong Oh <jiseong.oh@samsung.com>
2026-05-11 13:44:23 +0800

    Support quant ops in test suite
    - If all users of placeholder are not in OP_ANNOTATOR, don't annotate
    it.
    - Unsqueeze weight shape when conv1d to conv2d and weight is buffer.
    - It doesn't need to decompose pad before partition, and add pad builder.
      and split leaky_relu and prelu.
    - It need to remove useless annotator.
    - Add const and getitem annotator and concat and flip annoatator in annotate_1in1out_with_SharedQuant
    - Add flip op builder
    
    Signed-off-by: Jiseong Oh <jiseong.oh@samsung.com>
    Co-authored-by: Jingya Zhang <jingya.zhang@samsung.com>

--- backends/samsung/builders/op_flip.py
+++ backends/samsung/builders/op_flip.py
@@ -1,10 +1,10 @@
-# Copyright (c) 2025 Samsung Electronics Co. LTD
+# Copyright (c) 2026 Samsung Electronics Co. LTD
 # All rights reserved
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import cast, Dict
+from typing import Dict
 
 import torch
 from executorch.backends.samsung.builders.node_visitor import (
@@ -15,8 +15,8 @@ from executorch.backends.samsung.serialization.enn_graph_schema import EnnGraph
 
 
 @register_node_visitor
-class LeakyReluVisitor(NodeVisitor):
-    target = ["aten.leaky_relu.default"]
+class FlipVisitor(NodeVisitor):
+    target = "aten.flip.default"
 
     def __init__(self, *args) -> None:
         super().__init__(*args)
@@ -27,19 +27,25 @@ class LeakyReluVisitor(NodeVisitor):
         enn_graph: EnnGraph,
         vals_to_ids: Dict[torch.Tensor, int],
     ) -> bool:
-        all_input_tensors = []
-        input_id = self.define_tensor(node.args[0], enn_graph, vals_to_ids)
-        all_input_tensors.append(input_id)
+        input = node.args[0]
+        input_id = self.define_tensor(input, enn_graph, vals_to_ids)
+        axes = node.args[1]
+        # Convert list to tensor for axes parameter
+        axes_tensor = torch.tensor(axes, dtype=torch.int32)
+        axes_id = enn_graph.define_tensor(
+            f"{node.name}_axes",
+            list(axes_tensor.shape),
+            "INT32",
+            "CONSTANT",
+            data=axes_tensor,
+        )
 
-        negative_slope = cast(float, node.args[1]) if len(node.args) > 1 else 0.01
-
-        params = {"alpha": negative_slope}
+        params = {}
         self._update_params_qdtype(node, params)
-
         output_id = self.define_tensor(node, enn_graph, vals_to_ids)
 
         enn_graph.define_op(
-            node.name, "LeakyRelu", all_input_tensors, [output_id], params
+            node.name, "ReverseV2", [input_id, axes_id], [output_id], params
         )
 
         return True
---
+++ backends/samsung/builders/op_pad.py
@@ -0,0 +1,59 @@
+# Copyright (c) 2026 Samsung Electronics Co. LTD
+# All rights reserved
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from typing import cast, Dict, List
+
+import torch
+from executorch.backends.samsung.builders.node_visitor import (
+    NodeVisitor,
+    register_node_visitor,
+)
+from executorch.backends.samsung.serialization.enn_graph_schema import EnnGraph
+from executorch.backends.transforms import get_shape
+
+
+@register_node_visitor
+class PadVisitor(NodeVisitor):
+    target = "aten.pad.default"
+
+    def __init__(self, *args) -> None:
+        super().__init__(*args)
+
+    def define_node(
+        self,
+        node: torch.fx.Node,
+        enn_graph: EnnGraph,
+        vals_to_ids: Dict[torch.Tensor, int],
+    ) -> bool:
+
+        input1 = node.args[0]
+        input_id_1 = self.define_tensor(input1, enn_graph, vals_to_ids)
+
+        padding = cast(List[int], node.args[1])
+
+        # padding from last dim, here stores padding from first dim and fill 0 at the beginning.
+        # Reverse padding in pairs of two elements
+        padding = [
+            x for i in range(len(padding) - 2, -2, -2) for x in padding[i : i + 2]
+        ]
+        input_shape = get_shape(input1)
+        padding = [0] * (len(input_shape) * 2 - len(padding)) + padding
+        # Rearrange padding: even indices first, then odd indices
+        padding = [padding[i] for i in range(0, len(padding), 2)] + [
+            padding[i] for i in range(1, len(padding), 2)
+        ]
+
+        mode = "constant"
+        if len(node.args) > 2:
+            mode = node.args[2]
+        params = {"pads": padding, "mode": mode}
+        self._update_params_qdtype(node, params)
+
+        output_id = self.define_tensor(node, enn_graph, vals_to_ids)
+
+        enn_graph.define_op(node.name, "Pad", [input_id_1], [output_id], params)
+
+        return True
