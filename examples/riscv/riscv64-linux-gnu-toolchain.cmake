d0cbb51aa1d25e6ebf29adaf032656906b52a852
Ludovic Henry <git@ludovic.dev>
2026-05-11 22:56:48 +0200

    Cleanup examples/riscv/riscv64-linux-gnu-toolchain.cmake
    It didn't need to be that complicated

--- examples/riscv/riscv64-linux-gnu-toolchain.cmake
+++ examples/riscv/riscv64-linux-gnu-toolchain.cmake
@@ -8,39 +8,17 @@
 # Resulting binaries can be executed under qemu-user-static (qemu-riscv64) or
 # directly on a riscv64 Linux host.
 
-if(CMAKE_VERSION VERSION_LESS 3.20)
-  message(FATAL_ERROR "This toolchain file requires at least CMake 3.20")
-endif()
-
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR riscv64)
 
-set(_RISCV_TRIPLE "riscv64-linux-gnu")
-
-set(CMAKE_C_COMPILER
-    "${_RISCV_TRIPLE}-gcc"
-    CACHE FILEPATH "RISC-V cross C compiler"
-)
-set(CMAKE_CXX_COMPILER
-    "${_RISCV_TRIPLE}-g++"
-    CACHE FILEPATH "RISC-V cross C++ compiler"
-)
-set(CMAKE_AR
-    "${_RISCV_TRIPLE}-ar"
-    CACHE FILEPATH "RISC-V archiver"
-)
-set(CMAKE_RANLIB
-    "${_RISCV_TRIPLE}-ranlib"
-    CACHE FILEPATH "RISC-V ranlib"
-)
-set(CMAKE_STRIP
-    "${_RISCV_TRIPLE}-strip"
-    CACHE FILEPATH "RISC-V strip"
-)
+set(CMAKE_C_COMPILER "riscv64-linux-gnu-gcc" CACHE FILEPATH "RISC-V cross C compiler")
+set(CMAKE_CXX_COMPILER "riscv64-linux-gnu-g++" CACHE FILEPATH "RISC-V cross C++ compiler")
+set(CMAKE_AR "riscv64-linux-gnu-ar" CACHE FILEPATH "RISC-V archiver")
+set(CMAKE_RANLIB "riscv64-linux-gnu-ranlib" CACHE FILEPATH "RISC-V ranlib")
+set(CMAKE_STRIP "riscv64-linux-gnu-strip" CACHE FILEPATH "RISC-V strip")
 
 # Sysroot installed by the apt package gcc-riscv64-linux-gnu.
-set(CMAKE_SYSROOT "")
-set(CMAKE_FIND_ROOT_PATH "${CMAKE_SYSROOT}")
+set(CMAKE_FIND_ROOT_PATH "/usr/riscv64-linux-gnu")
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
