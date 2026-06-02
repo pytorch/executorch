9371b6d41c93ebc57579cc2339f5af7ef6e37fc5
Saoirse Stewart <saoirse.stewart@arm.com>
2026-06-02 11:50:01 +0100

    Arm backend: Add TOSA dialect unary elementwise node visitors
    Signed-off-by: Saoirse Stewart <saoirse.stewart@arm.com>

--- backends/arm/operators/op_tosa_log.py
+++ backends/arm/operators/op_tosa_log.py
@@ -1,4 +1,4 @@
-# Copyright 2025-2026 Arm Limited and/or its affiliates.
+# Copyright 2024-2026 Arm Limited and/or its affiliates.
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
@@ -12,19 +12,19 @@ from executorch.backends.arm.operators.simple_node_visitor import (
 )
 from executorch.backends.arm.tosa import TosaSpecification
 
-INT_SPECS = TosaSpecification.all_versions_for_profile("INT")
+FP_SPECS = TosaSpecification.all_versions_for_profile("FP")
 
 
 @register_node_visitor
-class BitwiseNotVisitor(SimpleNodeVisitor):
-    target = "aten.bitwise_not.default"
-    tosa_specs = INT_SPECS
+class LogVisitor(SimpleNodeVisitor):
+    target = "tosa.LOG.default"
+    tosa_specs = FP_SPECS
 
     @classmethod
     def get_config(cls) -> SimpleNodeVisitorConfig:
         return SimpleNodeVisitorConfig(
-            tosa_op=ts.Op.BITWISE_NOT,
-            attr_method="BitwiseNotAttribute",
+            tosa_op=ts.Op.LOG,
+            attr_method="LogAttribute",
             num_inputs=1,
-            input_dtypes=[ts.DType.INT8, ts.DType.INT16, ts.DType.INT32],
+            input_dtypes=[ts.DType.FP16, ts.DType.FP32, ts.DType.BF16],
         )
