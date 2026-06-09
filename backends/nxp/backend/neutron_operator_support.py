7e29253ad93072acb874a5bc3169f162a2a09809
Martin Pavella <martin.pavella@nxp.com>
2026-06-09 23:08:12 +0200

    NXP backend: Enable `permute_copy` with new Neutron MLIR flow. (#19974)
    ### Summary
    This PR updates the support for the `permute_copy` operator in the NXP
    backend to reflect the requirements of the new Neutron MLIR flow. In
    short, Neutron now supports all possible permutations without any
    restrictions.
    
    ### Test plan
    Unit tests provided.
    
    
    cc @robert-kalmar @JakeStevens @digantdesai @rascani

--- backends/nxp/backend/neutron_operator_support.py
+++ backends/nxp/backend/neutron_operator_support.py
@@ -35,54 +35,10 @@ def transposition_is_supported_on_neutron(
     :param permutation: The permutation the `Transpose` operator is computing.
     :param neutron_target_spec: Object for querying the target platform to retrieve its properties.
     """
-    num_macs = neutron_target_spec.get_num_macs()
-
-    if is_tensor_invariant_permutation(input_shape, permutation):
-        # The `Transpose` will be turned into a `Reshape` by Neutron. The check includes the identity permutation.
-        return True
-
-    if permutation == [0, 3, 1, 2]:
-        # NHWC -> NCHW
-        n, h, w, c = input_shape
-
-        if h * w * c % num_macs != 0:  # Official Neutron requirement.
-            return False
-
-        if not (
-            c % num_macs == 0 and h * w % num_macs == 0
-        ):  # Neutron would produce incorrect outputs.
-            return False
-
-        if n != 1:
-            # Neutron only supports `Transpose` operators where the dimensions can be combined into 2 consecutive
-            #  groups. These 2 groups are then transposed like a matrix, and the result is reshaped. Therefore, for the
-            #  [0, 3, 1, 2] permutation, when h * w != 1 and c != 1, batch size must be 1.
-            return False
-
-        return True
-
-    elif permutation == [0, 2, 3, 1]:
-        # NCHW -> NHWC
-
-        n, c, h, w = input_shape
-
-        if w % num_macs != 0:  # Official Neutron requirement.
-            return False
-
-        if not (
-            c % num_macs == 0 and h * w % num_macs == 0
-        ):  # Neutron would produce incorrect outputs.
-            return False
-
-        if n != 1:
-            # Neutron only supports `Transpose` operators where the dimensions can be combined into 2 consecutive
-            #  groups. These 2 groups are then transposed like a matrix, and the result is reshaped. Therefore, for the
-            #  [0, 2, 3, 1] permutation, when h * w != 1 and c != 1, batch size must be 1.
-            return False
-
-        return True
-
-    return False
+    # Neutron C currently supports all transpositions.
+    # The function is not removed in case the support conditions ever change (for example with the introduction of
+    #  Neutron S into ExecuTorch).
+    return True
 
 
 def activation_supported_on_target(
