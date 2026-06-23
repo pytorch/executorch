6aa1fce98e58db51f93a8436e38512854c42430d
Martin Lindström <33344797+martinlsm@users.noreply.github.com>
2026-06-23 13:49:05 +0200

    Arm backend: Add MXFP support for Conv2d (#20421)
    Add the possibility to convert `torch.nn.Conv2d` submodules to the
    custom implemented MXFP counterpart `MXFPConv2dOp`.
    
    Rewrite the MXFP Conv2d custom op into block-scaled TOSA ops and
    fix the serializer, typing.
    
    Signed-off-by: Sebastian Larsson <sebastian.larsson@arm.com>
    Signed-off-by: Martin Lindström <Martin.Lindstroem@arm.com>

--- backends/arm/operator_support/tosa_supported_operators.py
+++ backends/arm/operator_support/tosa_supported_operators.py
@@ -14,6 +14,10 @@ import operator
 import typing
 from typing import final, Optional, Sequence, Type
 
+# Register Arm-specific torch.library ops and MXFP transforms at package
+# import time.
+import executorch.backends.arm.ao_ext  # noqa: F401
+
 import torch
 import torch.fx as fx
 from executorch.backends.arm._passes.arm_pass_utils import (
@@ -27,7 +31,6 @@ from executorch.backends.arm._passes.fuse_quantized_activation_pass import (
     FuseQuantizedActivationPass,
 )
 from executorch.backends.arm._passes.insert_table_ops import TableOps
-from executorch.backends.arm.ao_ext.ops import mxfp_linear_op  # noqa: F401
 from executorch.backends.arm.common.annotation_meta import ArmAnnotationInfo
 from executorch.backends.arm.constants import DQ_OPS, MAX_RANK, Q_OPS
 from executorch.backends.arm.operator_support.control_flow_support import (
@@ -240,7 +243,10 @@ def get_registered_tosa_support_checks(
 class MXOpsSupportList(OperatorSupportBase):
     """Accept Arm MX custom ops when the active spec enables MX support."""
 
-    targets = (exir_ops.edge.tosa_mxfp.linear.default,)
+    targets = (
+        exir_ops.edge.tosa_mxfp.conv2d.default,
+        exir_ops.edge.tosa_mxfp.linear.default,
+    )
 
     def is_node_supported(
         self, submodules: typing.Mapping[str, torch.nn.Module], node: fx.Node
