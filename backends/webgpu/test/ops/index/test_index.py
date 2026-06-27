ac4dc89fd4f12b4875aba5429bd6a2a8c1ac9e00
Julian Ng-Thow-Hing <juliannth@meta.com>
2026-06-27 13:49:04 -0700

    Update
    [ghstack-poisoned]

--- backends/webgpu/test/ops/index/test_index.py
+++ backends/webgpu/test/ops/index/test_index.py
@@ -20,9 +20,7 @@ import unittest
 
 import torch
 
-from executorch.backends.vulkan.partitioner.vulkan_partitioner import (
-    VulkanPartitioner,
-)
+from executorch.backends.vulkan.partitioner.vulkan_partitioner import VulkanPartitioner
 from executorch.exir import to_edge_transform_and_lower
 
 # name -> (self_len, index_values)
