85391ad7e5c5e132195efa60a49669d4e26bebe8
Julian Ng-Thow-Hing <juliannth@meta.com>
2026-07-14 15:30:06 -0700

    Update (base update)
    [ghstack-poisoned]

--- backends/webgpu/test/ops/test_gather.py
+++ backends/webgpu/test/ops/test_gather.py
@@ -20,9 +20,7 @@ import unittest
 
 import torch
 
-from executorch.backends.vulkan.partitioner.vulkan_partitioner import (
-    VulkanPartitioner,
-)
+from executorch.backends.vulkan.partitioner.vulkan_partitioner import VulkanPartitioner
 from executorch.exir import to_edge_transform_and_lower
 
 # name -> (self_shape, dim, index_shape). Ranks stay <= 4 (TensorMeta MAX_NDIM).
