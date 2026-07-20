f16809eabf23b991e277dab5b95b1cb99770d282
Julian Ng-Thow-Hing <juliannth@meta.com>
2026-07-20 13:38:53 -0700

    Update (base update)
    [ghstack-poisoned]

--- backends/vulkan/runtime/graph/ops/impl/QuantizedLinearDw.cpp
+++ backends/vulkan/runtime/graph/ops/impl/QuantizedLinearDw.cpp
@@ -14,7 +14,7 @@
 
 namespace vkcompute {
 
-void resize_linear_q4gsw_dw_node(
+void resize_linear_q4gsw_dW_node(
     ComputeGraph* graph,
     const std::vector<ArgGroup>& args,
     const std::vector<ValueRef>& resize_args) {
@@ -27,7 +27,7 @@ void resize_linear_q4gsw_dw_node(
   graph->virtual_resize(dW, {N, K});
 }
 
-utils::uvec3 linear_q4gsw_dw_global_wg_size(
+utils::uvec3 linear_q4gsw_dW_global_wg_size(
     ComputeGraph* graph,
     const vkapi::ShaderInfo& shader,
     const std::vector<ArgGroup>& args,
@@ -41,7 +41,7 @@ utils::uvec3 linear_q4gsw_dw_global_wg_size(
   return {tiles, 1u, 1u};
 }
 
-utils::uvec3 linear_q4gsw_dw_local_wg_size(
+utils::uvec3 linear_q4gsw_dW_local_wg_size(
     ComputeGraph* graph,
     const vkapi::ShaderInfo& shader,
     const utils::uvec3& global_workgroup_size,
@@ -55,7 +55,7 @@ utils::uvec3 linear_q4gsw_dw_local_wg_size(
   return {64u, 1u, 1u};
 }
 
-void linear_q4gsw_dw(ComputeGraph& graph, const std::vector<ValueRef>& args) {
+void linear_q4gsw_dW(ComputeGraph& graph, const std::vector<ValueRef>& args) {
   int32_t i = 0;
   const ValueRef d_out = args.at(i++);
   const ValueRef x = args.at(i++);
@@ -79,9 +79,9 @@ void linear_q4gsw_dw(ComputeGraph& graph, const std::vector<ValueRef>& args) {
       utils::div_up_4(static_cast<uint32_t>(K));
   VK_CHECK_COND(
       (tiles + 63u) / 64u <= 65535u,
-      "linear_q4gsw_dw: tile count exceeds max workgroup count");
+      "linear_q4gsw_dW: tile count exceeds max workgroup count");
 
-  std::string kernel_name = "linear_q4gsw_dw";
+  std::string kernel_name = "linear_q4gsw_dW";
   kernel_name.reserve(kShaderNameReserve);
   add_storage_type_suffix(kernel_name, graph.storage_type_of(dW));
   add_dtype_suffix(kernel_name, graph.dtype_of(dW));
@@ -89,8 +89,8 @@ void linear_q4gsw_dw(ComputeGraph& graph, const std::vector<ValueRef>& args) {
   graph.execute_nodes().emplace_back(new DynamicDispatchNode(
       graph,
       VK_KERNEL_FROM_STR(kernel_name),
-      linear_q4gsw_dw_global_wg_size,
-      linear_q4gsw_dw_local_wg_size,
+      linear_q4gsw_dW_global_wg_size,
+      linear_q4gsw_dW_local_wg_size,
       // Inputs and Outputs
       {{dW, vkapi::kWrite}, {{d_out, x}, vkapi::kRead}},
       // Shader params buffers
@@ -102,11 +102,11 @@ void linear_q4gsw_dw(ComputeGraph& graph, const std::vector<ValueRef>& args) {
       // Resize Args
       {},
       // Resizing Logic
-      resize_linear_q4gsw_dw_node));
+      resize_linear_q4gsw_dW_node));
 }
 
 REGISTER_OPERATORS {
-  VK_REGISTER_OP(et_vk.linear_q4gsw_dw.default, linear_q4gsw_dw);
+  VK_REGISTER_OP(et_vk.linear_q4gsw_dW.default, linear_q4gsw_dW);
 }
 
 } // namespace vkcompute
