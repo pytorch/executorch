41d29de025228f4e76b8b4ff98574a14bbbe18ff
George Gekov <george.gekov@arm.com>
2026-08-17 17:24:47 +0100

    Arm backend: Remove RESIZE that don't change shape
    Sometimes, we get a TOSA RESIZE op stemming from
    upsample_bilinear2d or upsample_nearest2d that doesn't
    change the input & output resolution, it just passes from
    int8 to int32 and back to int8. Removing these
    resizes provides good uplift at runtime, especially
    if they operate on large tensors.
    
    Signed-off-by: George Gekov <george.gekov@arm.com>
    Change-Id: If0215efe423228f05c092795cbb28d13b61bcfc6

--- backends/arm/test/ops/test_upsample_nearest2d.py
+++ backends/arm/test/ops/test_upsample_nearest2d.py
@@ -41,7 +41,6 @@ test_data_suite = {
     "rand_double_size": lambda: (torch.rand(2, 4, 8, 3), (16, 6), None, True),
     "rand_one_double_scale": lambda: (torch.rand(2, 4, 1, 1), None, 2.0, True),
     "rand_one_double_size": lambda: (torch.rand(2, 4, 1, 1), (2, 2), None, True),
-    "rand_one_same_scale": lambda: (torch.rand(2, 4, 1, 1), None, 1.0, True),
     "rand_one_same_size": lambda: (torch.rand(2, 4, 1, 1), (1, 1), None, True),
     # Can't compare outputs as the rounding when selecting the nearest pixel is
     # different between PyTorch and TOSA. Just check the legalization went well.
