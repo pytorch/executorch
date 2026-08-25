536e8333ca0232910a9631dbf733ad13112f5233
RJ Ascani <rja@meta.com>
2026-08-25 08:17:11 -0700

    Update
    [ghstack-poisoned]

--- backends/arm/_passes/propagate_view_copy_permute_pass.py
+++ backends/arm/_passes/propagate_view_copy_permute_pass.py
@@ -12,9 +12,9 @@ from dataclasses import dataclass
 from typing import Any, cast, Set, Type
 
 import torch
-from executorch.backends.transforms.dim_maps import PermuteMap, ViewMap
 from executorch.backends.arm.tosa.mapping import TosaSpecialDtype
 from executorch.backends.arm.tosa.specification import get_context_spec
+from executorch.backends.transforms.dim_maps import PermuteMap, ViewMap
 from executorch.exir import ExportedProgram
 from executorch.exir.dialects._ops import ops as exir_ops
 from executorch.exir.pass_base import ExportPass, PassResult
