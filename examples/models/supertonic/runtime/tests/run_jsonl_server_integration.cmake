ccea26623880dbd259e9437538a951ded7a2fbb0
Young Han <younghan@meta.com>
2026-08-27 02:29:47 -0700

    Set JSONL integration CMake policies

--- examples/models/supertonic/runtime/tests/run_jsonl_server_integration.cmake
+++ examples/models/supertonic/runtime/tests/run_jsonl_server_integration.cmake
@@ -1,3 +1,5 @@
+cmake_minimum_required(VERSION 3.24)
+
 foreach(required RUNNER PTE ASSET_DIR STYLE WORK_DIR)
   if(NOT DEFINED ${required})
     message(FATAL_ERROR "Missing JSONL runner variable: ${required}")
