7ff3ca6372c970327fdf7e8680103b8d8abc5439
Jiri Ocenasek <jiri.ocenasek@nxp.com>
2026-09-01 09:19:16 +0200

    NXP backend: switch to neutron compiler

--- backends/nxp/nxp_backend.py
+++ backends/nxp/nxp_backend.py
@@ -25,8 +25,8 @@ from executorch.backends.nxp.backend.edge_program_converter import (
     EdgeProgramToIRConverter,
 )
 from executorch.backends.nxp.backend.ir.conversion_config import ConversionConfig
-from executorch.backends.nxp.backend.neutron_converter_manager import (
-    NeutronConverterManager,
+from executorch.backends.nxp.backend.neutron_compiler_manager import (
+    NeutronCompilerManager,
 )
 
 from executorch.backends.nxp.backend.neutron_map import NeutronMap
@@ -86,10 +86,10 @@ class NeutronCompileSpecBuilder:
         :param use_neutron_for_format_conversion: If True, the EdgeProgramToIRConverter will insert `Transpose` ops to
                                                 ensure that the IO matches the executorch partition, which will be
                                                 delegated to Neutron.
-        :param fetch_constants_to_sram: If True, the Neutron Converter will insert microinstructions to prefetch weights
+        :param fetch_constants_to_sram: If True, the Neutron Compiler will insert microinstructions to prefetch weights
                                      from FLASH to SRAM. This should be used when the whole model does not fit into SRAM.
-        :param dump_kernel_selection_code: Whether Neutron converter dumps kernel selection code.
-        :param use_profiling: If true Neutron Converter will enable profiling for neutron delegated model
+        :param dump_kernel_selection_code: Whether Neutron Compiler dumps kernel selection code.
+        :param use_profiling: If true Neutron Compiler will enable profiling for neutron delegated model
         :return: self for method chaining
         """
 
@@ -282,9 +282,9 @@ class NeutronBackend(BackendDetails):
             )
 
             with capture_fd_output() as tmp:
-                neutron_model = NeutronConverterManager(
+                neutron_model = NeutronCompilerManager(
                     dump_kernel_selection_code
-                ).convert(
+                ).compile(
                     tflite_model,
                     target,
                     delegation_tag,
