22858d7d144d590e7fe6d19dba3a498169850470
Irina Korchakova <irina.trukhina@nxp.com>
2026-09-01 13:38:24 +0200

    Fix lintrunner

--- backends/nxp/runtime/NeutronDriver.h
+++ backends/nxp/runtime/NeutronDriver.h
@@ -124,14 +124,14 @@ typedef struct {
   void (*wait)(uint32_t channel);
 } NeutronConfig;
 
-/// This structure contains semantic version of the Neutron SDK (major.minor.patch)
-/// and the SHA version (string and uint32_t).
+/// This structure contains semantic version of the Neutron SDK
+/// (major.minor.patch) and the SHA version (string and uint32_t).
 typedef struct {
-    uint32_t major;
-    uint32_t minor;
-    uint32_t patch;
-    const char *hashString;
-    uint32_t hashUint32;
+  uint32_t major;
+  uint32_t minor;
+  uint32_t patch;
+  const char* hashString;
+  uint32_t hashUint32;
 } NeutronSdkVersion;
 
 /* Invalid handle, returned by neutronModelPrepare() if an error occurred. */
