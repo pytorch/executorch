0f4cb3e67c0d43d41ac4d5503e41c9919718a4dd
Vaclav Novak <vaclav.novak@nxp.com>
2026-09-10 13:55:15 +0200

    feat: added test structure for mlperf tiny keyword spotting

--- backends/nxp/tests/models/test_mlperf_tiny_keyword_spotting.py
+++ backends/nxp/tests/models/test_mlperf_tiny_keyword_spotting.py
@@ -6,6 +6,7 @@
 from functools import partial
 
 import numpy as np
+import pytest
 import torch
 from executorch.backends.nxp.tests.dataset_creator import (
     FromCalibrationDataDatasetCreator,
@@ -16,21 +17,25 @@ from executorch.backends.nxp.tests.model_output_comparator import (
     ClassificationAccuracyOutputComparator,
     NumericalStatsOutputComparator,
 )
-
 from executorch.backends.nxp.tests.nsys_testing import (
     lower_run_compare,
     lower_run_compare_ptq_qat,
     ReferenceModel,
 )
 from executorch.backends.nxp.tests.use_qat import *  # noqa F403
-import pytest
-from executorch.examples.nxp.models.mlperf_tiny.image_classification.mlperf_tiny_image_classification import (
-    MLPerfTinyImageClassification,
+from executorch.examples.nxp.models.mlperf_tiny.keyword_spotting.mlperf_tiny_keyword_spotting import (
+    MLPerfTinyKeywordSpotting,
 )
 
 BOUNDS_MSE = {
-    "PTQ": {"channels-last": 1.859e-05, "channels-first": 7.432e-09},
-    "QAT": {"channels-last": 2.751e-07, "channels-first": 5.205e-06},
+    "PTQ": {
+        "channels-last": np.inf,
+        "channels-first": 5.5e-7,
+    },
+    "QAT": {
+        "channels-last": np.inf,
+        "channels-first": 3.3e-5,
+    },
 }
 
 
@@ -40,50 +45,55 @@ def reseed_model_per_test_run():
     np.random.seed(23)
 
 
-@pytest.mark.parametrize("channels_last", [False, True])
-def test_mlperf_tiny_classification_mse_cpu_vs_npu(
-    mocker, request, channels_last, use_qat
-):
-    # 10 samples per class
+@pytest.mark.parametrize(
+    "channels_last",
+    [
+        False,
+        pytest.param(
+            True,
+            marks=pytest.mark.xfail(
+                reason="EIEX-1082, don't forget to readjust bounds when it start working",
+                strict=True,
+            ),
+        ),
+    ],
+)
+def test_mlperf_tiny_kws_mse_cpu_vs_npu(mocker, request, channels_last, use_qat):
+    # approx. 5 samples per class
     num_samples = 60
 
-    img_classification = MLPerfTinyImageClassification(
-        num_samples=num_samples, use_random_dataset=True
-    )
-    model = img_classification.get_eager_model()
-    dataset = img_classification.dataset
-    labels = img_classification.labels
+    kws = MLPerfTinyKeywordSpotting(num_samples=num_samples, use_random_dataset=True)
+    model = kws.get_eager_model()
+    dataset = kws.dataset
+    labels = kws.labels
 
     dataset_creator = FromCalibrationDataDatasetCreator(
         dataset, num_examples=num_samples, idx_to_label=labels
     )
 
-    input_spec = ModelInputSpec(img_classification.input_shape)
+    input_spec = ModelInputSpec(kws.input_shape)
     if channels_last:
         model.to(memory_format=torch.channels_last)
         input_spec.dim_order = torch.channels_last
 
     quant_type_key = "QAT" if use_qat else "PTQ"
     format_key = "channels-last" if channels_last else "channels-first"
-
     mse = BOUNDS_MSE[quant_type_key][format_key]
     comparator = NumericalStatsOutputComparator(
-        max_mse_error=mse, use_softmax=True, is_classification_task=True
+        max_mse_error=mse, is_classification_task=True
     )
     model_verifier = BaseGraphVerifier(1, [])
     train_fn = (
-        partial(img_classification.train_model_fn, channels_last=channels_last)
-        if use_qat
-        else None
+        partial(kws.train_model_fn, channels_last=channels_last) if use_qat else None
     )
 
-    # This model does not work in channels-last format and QAT when running using portable kernels.
+    # This model does not work in channels-last format when running with portable kernels.
     # See more information below.
-    # Github issue: https://github.com/pytorch/executorch/issues/22179
-    # NXP internal issue ID: EIEX-1065
+    # Github issue: https://github.com/pytorch/executorch/issues/22520
+    # NXP internal issue ID: EIEX-1074
     ref_model = (
         ReferenceModel.QUANTIZED_EDGE_PYTHON
-        if channels_last and use_qat
+        if channels_last
         else ReferenceModel.QUANTIZED_EXECUTORCH_CPP
     )
 
@@ -94,31 +104,29 @@ def test_mlperf_tiny_classification_mse_cpu_vs_npu(
         request,
         dataset_creator=dataset_creator,
         output_comparator=comparator,
-        reference_model=ref_model,
         mocker=mocker,
+        reference_model=ref_model,
         use_qat=use_qat,
         train_fn=train_fn,
     )
 
 
-def test_mlperf_tiny_image_classification_ptq_qat_equivalence(request):
-    # 10 samples per class
+def test_mlperf_tiny_kws_ptq_qat_equivalence(request):
+    # approx. 5 samples per class
     num_samples = 60
 
-    img_classification = MLPerfTinyImageClassification(
-        num_samples=num_samples, use_random_dataset=True
-    )
+    kws = MLPerfTinyKeywordSpotting(num_samples=num_samples, use_random_dataset=True)
 
-    model = img_classification.get_eager_model()
-    dataset = img_classification.dataset
-    labels = img_classification.labels
+    model = kws.get_eager_model()
+    dataset = kws.dataset
+    labels = kws.labels
 
     dataset_creator = FromCalibrationDataDatasetCreator(
         dataset, num_examples=num_samples, idx_to_label=labels
     )
     comparator = ClassificationAccuracyOutputComparator(class_dict=labels)
 
-    input_spec = ModelInputSpec(img_classification.input_shape)
+    input_spec = ModelInputSpec(kws.input_shape)
     model_verifier = BaseGraphVerifier(1, [])
 
     lower_run_compare_ptq_qat(
@@ -126,7 +134,7 @@ def test_mlperf_tiny_image_classification_ptq_qat_equivalence(request):
         [input_spec],
         model_verifier,
         request,
-        train_fn=img_classification.train_model_fn,
+        train_fn=kws.train_model_fn,
         dataset_creator=dataset_creator,
         output_comparator=comparator,
     )
