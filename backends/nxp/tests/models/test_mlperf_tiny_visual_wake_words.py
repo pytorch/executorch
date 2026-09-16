4b4dac84bfc21917c29c64404160736e6ee73192
Vaclav Novak <vaclav.novak@nxp.com>
2026-09-16 17:39:40 +0200

    feat: refactor MLPerf Tiny Visual Wake Words tests

--- backends/nxp/tests/models/test_mlperf_tiny_visual_wake_words.py
+++ backends/nxp/tests/models/test_mlperf_tiny_visual_wake_words.py
@@ -20,20 +20,18 @@ from executorch.backends.nxp.tests.model_output_comparator import (
     ClassificationAccuracyOutputComparator,
     NumericalStatsOutputComparator,
 )
-
 from executorch.backends.nxp.tests.nsys_testing import (
     lower_run_compare,
     lower_run_compare_ptq_qat,
-    ReferenceModel,
 )
 from executorch.backends.nxp.tests.use_qat import *  # noqa F403
-from executorch.examples.nxp.models.mlperf_tiny.image_classification.mlperf_tiny_image_classification import (
-    MLPerfTinyImageClassification,
+from executorch.examples.nxp.models.mlperf_tiny.visual_wake_words.mlperf_tiny_visual_wake_words import (
+    MLPerfTinyVisualWakeWords,
 )
 
 BOUNDS_MSE = {
-    "PTQ": {"channels-last": 1.859e-05, "channels-first": 7.432e-09},
-    "QAT": {"channels-last": 2.751e-07, "channels-first": 5.205e-06},
+    "PTQ": {"channels-last": 4.6e-8, "channels-first": 5.0e-8},
+    "QAT": {"channels-last": 3.0e-6, "channels-first": 3.7e-6},
 }
 
 
@@ -44,50 +42,37 @@ def reseed_model_per_test_run():
 
 
 @pytest.mark.parametrize("channels_last", [False, True])
-def test_mlperf_tiny_classification_mse_cpu_vs_npu(
-    mocker, request, channels_last, use_qat
-):
-    # 10 samples per class
-    num_samples = 60
+def test_mlperf_tiny_vww_mse_cpu_vs_npu(mocker, request, channels_last, use_qat):
+    # 20 samples per class
+    num_samples = 40
 
-    img_classification = MLPerfTinyImageClassification(
+    visual_wake_words = MLPerfTinyVisualWakeWords(
         num_samples=num_samples, use_random_dataset=True
     )
-    model = img_classification.get_eager_model()
-    dataset = img_classification.dataset
-    labels = img_classification.labels
+    model = visual_wake_words.get_eager_model()
+    dataset = visual_wake_words.dataset
+    labels = visual_wake_words.labels
 
     dataset_creator = FromCalibrationDataDatasetCreator(
         dataset, num_examples=num_samples, idx_to_label=labels
     )
 
-    input_spec = ModelInputSpec(img_classification.input_shape)
+    input_spec = ModelInputSpec(visual_wake_words.input_shape)
     if channels_last:
         model.to(memory_format=torch.channels_last)
         input_spec.dim_order = torch.channels_last
 
     quant_type_key = "QAT" if use_qat else "PTQ"
     format_key = "channels-last" if channels_last else "channels-first"
-
     mse = BOUNDS_MSE[quant_type_key][format_key]
     comparator = NumericalStatsOutputComparator(
         max_mse_error=mse, use_softmax=True, is_classification_task=True
     )
     model_verifier = BaseGraphVerifier(1, [])
     train_fn = (
-        partial(img_classification.train_model_fn, channels_last=channels_last)
+        partial(visual_wake_words.train_model_fn, channels_last=channels_last)
         if use_qat
         else None
-    )
-
-    # This model does not work in channels-last format and QAT when running using portable kernels.
-    # See more information below.
-    # Github issue: https://github.com/pytorch/executorch/issues/22179
-    # NXP internal issue ID: EIEX-1065
-    ref_model = (
-        ReferenceModel.QUANTIZED_EDGE_PYTHON
-        if channels_last and use_qat
-        else ReferenceModel.QUANTIZED_EXECUTORCH_CPP
     )
 
     lower_run_compare(
@@ -98,30 +83,29 @@ def test_mlperf_tiny_classification_mse_cpu_vs_npu(
         dataset_creator=dataset_creator,
         output_comparator=comparator,
         mocker=mocker,
-        reference_model=ref_model,
         use_qat=use_qat,
         train_fn=train_fn,
     )
 
 
-def test_mlperf_tiny_image_classification_ptq_qat_equivalence(request):
-    # 10 samples per class
-    num_samples = 60
+def test_mlperf_tiny_vww_ptq_qat_equivalence(request):
+    # 20 samples per class
+    num_samples = 40
 
-    img_classification = MLPerfTinyImageClassification(
+    visual_wake_words = MLPerfTinyVisualWakeWords(
         num_samples=num_samples, use_random_dataset=True
     )
 
-    model = img_classification.get_eager_model()
-    dataset = img_classification.dataset
-    labels = img_classification.labels
+    model = visual_wake_words.get_eager_model()
+    dataset = visual_wake_words.dataset
+    labels = visual_wake_words.labels
 
     dataset_creator = FromCalibrationDataDatasetCreator(
         dataset, num_examples=num_samples, idx_to_label=labels
     )
     comparator = ClassificationAccuracyOutputComparator(class_dict=labels)
 
-    input_spec = ModelInputSpec(img_classification.input_shape)
+    input_spec = ModelInputSpec(visual_wake_words.input_shape)
     model_verifier = BaseGraphVerifier(1, [])
 
     lower_run_compare_ptq_qat(
@@ -129,7 +113,7 @@ def test_mlperf_tiny_image_classification_ptq_qat_equivalence(request):
         [input_spec],
         model_verifier,
         request,
-        train_fn=img_classification.train_model_fn,
+        train_fn=visual_wake_words.train_model_fn,
         dataset_creator=dataset_creator,
         output_comparator=comparator,
     )
--- examples/nxp/aot_neutron_compile.py
+++ examples/nxp/aot_neutron_compile.py
@@ -49,6 +49,9 @@ from executorch.examples.nxp.models.mlperf_tiny.image_classification.mlperf_tiny
 from executorch.examples.nxp.models.mlperf_tiny.keyword_spotting.mlperf_tiny_keyword_spotting import (
     MLPerfTinyKeywordSpotting,
 )
+from executorch.examples.nxp.models.mlperf_tiny.visual_wake_words.mlperf_tiny_visual_wake_words import (
+    MLPerfTinyVisualWakeWords,
+)
 from executorch.examples.nxp.models.mobilenet_v2 import MobilenetV2
 from executorch.exir import (
     EdgeCompileConfig,
@@ -69,6 +72,7 @@ MODELS = {
     "mobilenetv2": MobilenetV2,
     "mlperf_tiny_image_classification": MLPerfTinyImageClassification,
     "mlperf_tiny_keyword_spotting": MLPerfTinyKeywordSpotting,
+    "mlperf_tiny_visual_wake_words": MLPerfTinyVisualWakeWords,
 }
 
 FORMAT = "[%(levelname)s %(asctime)s %(filename)s:%(lineno)s] %(message)s"
@@ -126,7 +130,11 @@ def _get_model_info_from_name(
                 )
             model_cls_inst = model_cls()
 
-        elif model_cls in (MLPerfTinyImageClassification, MLPerfTinyKeywordSpotting):
+        elif model_cls in (
+            MLPerfTinyImageClassification,
+            MLPerfTinyKeywordSpotting,
+            MLPerfTinyVisualWakeWords,
+        ):
             model_cls_inst = model_cls(
                 dataset_path=dataset_path,
                 use_random_dataset=use_random_dataset,
@@ -337,7 +345,12 @@ if __name__ == "__main__":  # noqa C901
         if args.use_qat:
             if not isinstance(
                 model_cls_inst,
-                (CifarNet, MLPerfTinyImageClassification, MLPerfTinyKeywordSpotting),
+                (
+                    CifarNet,
+                    MLPerfTinyImageClassification,
+                    MLPerfTinyKeywordSpotting,
+                    MLPerfTinyVisualWakeWords,
+                ),
             ):
                 raise ValueError(
                     f"QAT training is not supported for model '{args.model_name}'"
